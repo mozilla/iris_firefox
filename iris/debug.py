1a38cfb19d34f1f862754d86f85803bc803d131f
Matt Wobensmith <mwobensmith@mozilla.com>
2018-02-14 19:39:31 -0800

    More debugging

--- iris/debug.py
+++ iris/debug.py
@@ -2,23 +2,16 @@
 import subprocess
 import os
 
-module_dir = os.path.split(__file__)[0]
-print module_dir
+temp = os.path.expanduser(os.getcwd()).split("/")
+jar_path = os.path.join(temp[1], temp[2], "Sikuli/sikulix.jar")
 
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print dir_path
-
-path = os.path.join(dir_path, "../")
-print path
-
-print os.path.expanduser(os.getcwd())
-
-jar_path = "~/Sikuli/sikulix.jar"
 
 package = "org.python.util.jython"
-init_path = "~/Sikuli/test.py"
+
+
+
+init_path = os.path.join(temp[1], temp[2], "Sikuli/test.py")
+
 
 cmd = ['java', '-cp', jar_path, package, init_path]
 print cmd
