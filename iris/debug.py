649fff057866f059709664ca436c9deb5f418ebe
Matt Wobensmith <mwobensmith@mozilla.com>
2018-02-14 19:15:09 -0800

    More debugging

--- iris/debug.py
+++ iris/debug.py
@@ -2,13 +2,18 @@
 import subprocess
 import os
 
-
-
 module_dir = os.path.split(__file__)[0]
+print module_dir
 
-path = os.path.join(module_dir, "..")
 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print dir_path
+
+path = os.path.join(dir_path, "../")
 print path
+
+print os.getcwd()
 
 jar_path = "~/Sikuli/sikulix.jar"
 
