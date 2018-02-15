254d0b92076e36423f42238f514a9097225961f8
Matt Wobensmith <mwobensmith@mozilla.com>
2018-02-14 19:47:06 -0800

    Cleaning up

--- iris/main.py
+++ iris/main.py
@@ -34,13 +34,10 @@ def main(argv=None):
         temp = os.path.expanduser(os.getcwd()).split("/")
         jar_path = os.path.join("/", temp[1], temp[2], "Sikuli/sikulix.jar")
 
-
     package = "org.python.util.jython"
     init_path = os.path.join(module_dir, "app.py")
 
-
     cmd = ['java', '-cp', jar_path, package, init_path]
-    print cmd
     p = subprocess.Popen(cmd).communicate()
 
 
