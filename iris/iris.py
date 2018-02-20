8407d854f706ac468139d0e6360a6eca1242c1b1
Matt Wobensmith <mwobensmith@mozilla.com>
2018-02-20 11:52:33 -0800

    Code cleanup, plus better Linux messages

--- iris/iris.py
+++ iris/iris.py
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
 import test_runner
+
 
 class Iris(object):
 
     def __init__(self):
-        print "app.py: This is our main app"
+        print "iris.py: This is our main app"
 
         """
         Things to do here:
@@ -16,13 +16,6 @@ class Iris(object):
             * download and install Firefox
             * set up logging
         """
-        try:
-            self.platform = os.environ["PLATFORM_NAME"]
-            self.os = os.environ["OS_NAME"]
-        except:
-            # temp fix for Linux issues
-            self.platform = "linux"
-            self.os = "linux"
 
         test_runner.run(self)
 
