24a7c874bfb8c288db69b3a6f4d89d9cf51fa8c0
Tracy Walker <tracy@Tracys-Mac-mini.local>
2018-02-20 16:31:08 -0600

    add keyboard shortcuts for Current Page control and Editing

--- iris/api/helpers/keyboard_shortcuts.py
+++ iris/api/helpers/keyboard_shortcuts.py
@@ -7,7 +7,14 @@ from api.core import *
 
 # This helper defines keyboard shortcuts for many common actions in Firefox usage.
 # We should be using these keyboard shortcuts whenever possible.
+#
+# Try to keep them organized by area of influence
+# - Navigation
+# - Current Page
+# - Editing
 
+
+########## Navigation ##########
 
 # Navigates back in browsing history one page visit
 def navigate_back():
@@ -30,6 +37,7 @@ def navigate_home():
     type(text=Key.HOME, modifier=KeyModifier.ALT)
 
 
+# Set focus to the locationbar
 def select_location_bar():
     if get_os() == "osx":
         type(text="l", modifier=KeyModifier.CMD)
@@ -57,12 +65,166 @@ def force_reload_page():
 
 # Stop the current in progress web page from loading
 def stop_page_load():
-    if get_os() == "osx":
-        type(text=Key.ESC)
-    else:
-        type(text=Key.ESC)
-    # is this right? both actions are the same
+    type(text=Key.ESC)
 
+########## end Navigation ##########
+
+########## Current Page ##########
+
+# Scroll down one increment (equivilant to 3 mousewheel steps)
+def scroll_down():
+    type(text=Key.DOWN)
+
+
+# Scroll up one increment (equivilant to 3 mousewheel steps)
+def scroll_up():
+    type(text=Key.UP)
+
+
+# Jump down one screen
+def page_down():
+    type(text=Key.SPACE)
+
+
+# Jump up one screen
+def page_up():
+    type(text=key.SPACE, KeyModifier.SHIFT)
+
+
+# Jump to the bottom of the page
+def page_end():
+    type(text=Key.END)
+
+
+# Jump to the top of the page
+def page_home():
+    type(text=Key.HOME)
+
+
+# Focus next actionable item
+def focus_next_item():
+    type(text=Key.TAB)
+
+
+# Focus previous actionable item
+def focus_previous_item():
+    type(text=key.TAB, KeyModifier.SHIFT)
+
+
+# Move to the next frame (can be in content or in chrome)
+def next_frame():
+    type(text=Key.F6)
+
+
+# Move to the previous frame (can be in content or in chrome)
+def previous_frame():
+    type(text=Key.F6, KeyModifier.SHIFT)
+
+
+# Open the print dialog
+def open_print_page():
+    if get_os() == "osx":
+        type(text="p", modifier=KeyModifier.CMD)
+    else:
+        type(text="p", modifier=KeyModifier.CTRL)
+
+
+# Open the save dialog
+def open_save_page():
+    if get_os() == "osx":
+        type(text="s", modifier=KeyModifier.CMD)
+    else:
+        type(text="s", modifier=KeyModifier.CTRL)
+
+
+# Zoom in one increment
+def zoom_in():
+    if get_os() == "osx":
+        type(text="+", modifier=KeyModifier.CMD)
+    else:
+        type(text="+", modifier=KeyModifier.CTRL)
+
+
+# Zoom out one increment
+def zoom_out():
+    if get_os() == "osx":
+        type(text="-", modifier=KeyModifier.CMD)
+    else:
+        type(text="-", modifier=KeyModifier.CTRL)
+
+
+# Restores zoom level to page default
+def restore_zoom():
+    if get_os() == "osx":
+        type(text="0", modifier=KeyModifier.CMD)
+    else:
+        type(text="0", modifier=KeyModifier.CTRL)
+
+########## end Current Page ##########
+
+########## Editing ##########
+
+# Copy selection to clipboard
+def edit_copy():
+    if get_os() == "osx":
+        type(text="c", modifier=KeyModifier.CMD)
+    else:
+        type(text="c", modifier=KeyModifier.CTRL)
+
+
+# Cut selection to clipboard
+def edit_cut():
+    if get_os() == "osx":
+        type(text="x", modifier=KeyModifier.CMD)
+    else:
+        type(text="x", modifier=KeyModifier.CTRL)
+
+
+# Delete selection or if nothing is selected, delete previous character
+def edit_delete():
+    type(text=Key.DELETE)
+
+
+# Paste contents of the clipboard to the focused text field
+def edit_paste():
+    if get_os() == "osx":
+        type(text="v", modifier=KeyModifier.CMD)
+    else:
+        type(text="v", modifier=KeyModifier.CTRL)
+
+
+# Paste contents of the clipboard, as plain text, to the focus text field
+def edit_paste_plain():
+    if get_os() == "osx":
+        type(text="v", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="v", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+# Redo the last operation of Undo
+def edit_redo():
+    if get_os() == "osx":
+        type(text="z", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="z", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+# Selects the entire contents of focused field or page
+def edit_select_all():
+    if get_os() == "osx":
+        type(text="a", modifier=KeyModifier.CMD)
+    else:
+        type(text="a", modifier=KeyModifier.CTRL)
+
+
+# Undoes the previous operation
+def edit_undo():
+    if get_os() == "osx":
+        type(text="z", modifier=KeyModifier.CMD)
+    else:
+        type(text="z", modifier=KeyModifier.CTRL)
+
+########## end Editing ##########
 
 def open_file_picker():
     if get_os() == "osx":
