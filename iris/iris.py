4cd898ed32895c8aa4185c895dd496759b7c14ff
Matt Wobensmith <mwobensmith@mozilla.com>
2018-02-26 09:35:41 +0100

    Testing argument passing between iris.py and main.py

--- iris/iris.py
+++ iris/iris.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
 import test_runner
 
 
@@ -16,7 +17,7 @@ class Iris(object):
             * download and install Firefox
             * set up logging
         """
-
+        print ' '.join(sys.argv[1:])
         test_runner.run(self)
 
 
