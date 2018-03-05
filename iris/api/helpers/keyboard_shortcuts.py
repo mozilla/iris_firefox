94d287630f4f69a78a0d32085c59025d552f8ab8
Tracy Walker <tracy@Tracys-Mac-mini.local>
2018-03-05 15:20:50 -0600

    more shortcuts

--- iris/api/helpers/keyboard_shortcuts.py
+++ iris/api/helpers/keyboard_shortcuts.py
@@ -11,376 +11,431 @@ from api.core import *
 # Try to keep them organized by area of influence
 # Roughly follow the organization from
 # https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly
+
 # - Navigation
 # - Current Page
 # - Editing
 # - Search
+# - Windows & Tabs
+# - History & Bookmarks
+# - Tools
 
+# Keyboard shortcuts for Navigation.
 
-########## Navigation ##########
-
-# Navigates back in browsing history one page visit
 def navigate_back():
+    """Navigate back in browsing history one page visit.
+    """
     if get_os() == "osx":
         type(text="[", modifier=KeyModifier.CMD)
     else:
         type(text="{", modifier=KeyModifier.ALT)
 
 
-# Navigates forward in browsing history one page visit
 def navigate_forward():
+    """Navigate forward in browsing history one page visit.
+    """
     if get_os() == "osx":
         type(text="]", modifier=KeyModifier.CMD)
     else:
         type(text="]", modifier=KeyModifier.ALT)
 
 
-# Navigates the browser to whatever is set as the Home page
 def navigate_home():
+    """Navigate the browser to whatever is set as the Home page.
+    """
     type(text=Key.HOME, modifier=KeyModifier.ALT)
 
 
-# Opens the system file picker
 def open_file_picker():
+    """Open the system file picker.
+    """
     if get_os() == "osx":
         type(text="o", modifier=KeyModifier.CMD)
     else:
         type(text="o", modifier=KeyModifier.CTRL)
 
 
-# Set focus to the locationbar
 def select_location_bar():
+    """Set focus to the locationbar.
+    """
     if get_os() == "osx":
         type(text="l", modifier=KeyModifier.CMD)
     else:
         type(text="l", modifier=KeyModifier.CTRL)
-    # wait a little bit to allow location bar to become responsive
-    sleep(1)
+    sleep(1) # wait to allow the location bar to become responsive.
 
 
-# Reload the current web page
 def reload_page():
+    """Reload the current web page.
+    """
     if get_os() == "osx":
         type(text="r", modifier=KeyModifier.CMD)
     else:
         type(text="r", modifier=KeyModifier.CTRL)
 
 
-# Reload the current web page with cache override
 def force_reload_page():
+    """Reload the current web page with cache override.
+    """
     if get_os() == "osx":
         type(text="r", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="r", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Stop the current in progress web page from loading
 def stop_page_load():
+    """Stop the current in progress web page from loading.
+    """
     type(text=Key.ESC)
 
 
-########## end Navigation ##########
+# End of Navigation keyboard shortcuts.
 
-########## Current Page ##########
+# Keyboard shortcuts for Current Page.
 
-# Scroll down one increment (equivilant to 3 mousewheel steps)
 def scroll_down():
+    """Scroll down one increment (equivalant to 3 mousewheel steps).
+    """
     type(text=Key.DOWN)
 
 
-# Scroll up one increment (equivilant to 3 mousewheel steps)
 def scroll_up():
+    """Scroll up one increment (equivalant to 3 mousewheel steps).
+    """
     type(text=Key.UP)
 
 
-# Jump down one screen
 def page_down():
+    """Jump down one screen.
+    """
     type(text=Key.SPACE)
 
 
-# Jump up one screen
 def page_up():
+    """Jump up one screen.
+    """
     type(text=Key.SPACE, modifier=KeyModifier.SHIFT)
 
 
-# Jump to the bottom of the page
 def page_end():
+    """Jump to the bottom of the page.
+    """
     type(text=Key.END)
 
 
-# Jump to the top of the page
 def page_home():
+    """Jump to the top of the page.
+    """
     type(text=Key.HOME)
 
 
-# Focus next actionable item
 def focus_next_item():
+    """Focus next actionable item.
+    """
     type(text=Key.TAB)
 
 
-# Focus previous actionable item
 def focus_previous_item():
+    """Focus previous actionable item.
+    """
     type(text=Key.TAB, modifier=KeyModifier.SHIFT)
 
 
-# Move to the next frame (can be in content or in chrome)
 def next_frame():
+    """Move to the next frame (can be in content or in chrome).
+    """
     type(text=Key.F6)
 
 
-# Move to the previous frame (can be in content or in chrome)
 def previous_frame():
+    """Move to the previous frame (can be in content or in chrome).
+    """
     type(text=Key.F6, modifier=KeyModifier.SHIFT)
 
 
-# Open the print dialog
 def open_print_page():
+    """Open the Print dialog.
+    """
     if get_os() == "osx":
         type(text="p", modifier=KeyModifier.CMD)
     else:
         type(text="p", modifier=KeyModifier.CTRL)
 
 
-# Open the save dialog
 def open_save_page():
+    """Open the Save dialog.
+    """
     if get_os() == "osx":
         type(text="s", modifier=KeyModifier.CMD)
     else:
         type(text="s", modifier=KeyModifier.CTRL)
 
 
-# Zoom in one increment
 def zoom_in():
+    """Zoom in one increment.
+    """
     if get_os() == "osx":
         type(text="+", modifier=KeyModifier.CMD)
     else:
         type(text="+", modifier=KeyModifier.CTRL)
 
 
-# Zoom out one increment
 def zoom_out():
+    """Zoom out one increment.
+    """
     if get_os() == "osx":
         type(text="-", modifier=KeyModifier.CMD)
     else:
         type(text="-", modifier=KeyModifier.CTRL)
 
 
-# Restores zoom level to page default
 def restore_zoom():
+    """Restores zoom level to page default.
+    """
     if get_os() == "osx":
         type(text="0", modifier=KeyModifier.CMD)
     else:
         type(text="0", modifier=KeyModifier.CTRL)
 
 
-########## end Current Page ##########
+# End of Current Page keyboard shortcuts.
 
-########## Editing ##########
+# Keyboard shortcuts for Editing.
 
-# Copy selection to clipboard
 def edit_copy():
+    """Copy selection to clipboard.
+    """
     if get_os() == "osx":
         type(text="c", modifier=KeyModifier.CMD)
     else:
         type(text="c", modifier=KeyModifier.CTRL)
 
 
-# Cut selection to clipboard
 def edit_cut():
+    """Cut selection to clipboard.
+    """
     if get_os() == "osx":
         type(text="x", modifier=KeyModifier.CMD)
     else:
         type(text="x", modifier=KeyModifier.CTRL)
 
 
-# Delete selection or if nothing is selected, delete previous character
 def edit_delete():
+    """Delete selected text.
+    If nothing is selected, delete previous character.
+    """
     type(text=Key.DELETE)
 
 
-# Paste contents of the clipboard to the focused text field
 def edit_paste():
+    """Paste contents of the clipboard to the focused text field.
+    """
     if get_os() == "osx":
         type(text="v", modifier=KeyModifier.CMD)
     else:
         type(text="v", modifier=KeyModifier.CTRL)
 
 
-# Paste contents of the clipboard, as plain text, to the focus text field
 def edit_paste_plain():
+    """Paste contents of the clipboard, as plain text, to the focused text field.
+    """
     if get_os() == "osx":
         type(text="v", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="v", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Redo the last operation of Undo
 def edit_redo():
+    """Redo the last operation of Undo.
+    """
     if get_os() == "osx":
         type(text="z", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="z", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Selects the entire contents of focused field or page
 def edit_select_all():
+    """Selects the entire contents of focused field or page.
+    """
     if get_os() == "osx":
         type(text="a", modifier=KeyModifier.CMD)
     else:
         type(text="a", modifier=KeyModifier.CTRL)
 
 
-# Undoes the previous operation
 def edit_undo():
+    """Undoes the previous operation.
+    """
     if get_os() == "osx":
         type(text="z", modifier=KeyModifier.CMD)
     else:
         type(text="z", modifier=KeyModifier.CTRL)
 
 
-########## end Editing ##########
+# End of Editing keyboard shortcuts.
 
-########## Search ##########
+# Keyboard shortcuts for Search.
 
-# Open the find toolbar
 def open_find():
+    """Open the find toolbar.
+    """
     if get_os() == "osx":
         type(text="f", modifier=KeyModifier.CMD)
     else:
         type(text="f", modifier=KeyModifier.CTRL)
 
 
-# Find next occurance of term if find is already active on a search term
-# Find next (again) can also find the next occurance of a term without opening the find toolbar
 def find_next():
+    """Find next occurance of term if find is already active on a search term.
+    Find next (again) can also find the next occurance of a term without opening the find toolbar."""
     if get_os() == "osx":
         type(text="g", modifier=KeyModifier.CMD)
     else:
         type(text="g", modifier=KeyModifier.CTRL)
 
 
-# Find the previous occurance of term if find is already active on a search term
-# Find previous can also find the next occurance of a term without opening the find toolbar
 def find_previous():
+    """Find the previous occurance of term, if find is already active on a search term.
+    Find previous can also find the previous occurance of a term without opening the find toolbar.
+    """
     if get_os() == "osx":
         type(text="g", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="g", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Quick find opens simple find toolbar that remains active for only six seconds
 def quick_find():
+    """Quick find opens simple find toolbar that remains active for only six seconds.
+    """
     if get_os() == "osx":
         type(text="/", modifier=KeyModifier.CMD)
     else:
         type(text="/", modifier=KeyModifier.CTRL)
 
 
-# Quick find opens simple find link toolbar that remains active for only six seconds
 def quick_find_link():
+    """Quick find opens simple find link toolbar that remains active for only six seconds.
+    """
     if get_os() == "osx":
         type(text="'", modifier=KeyModifier.CMD)
     else:
         type(text="'", modifier=KeyModifier.CTRL)
 
 
-# Close the regular find toolbar or quick find toolbar, if  it has focus
 def close_find():
+    """Close the regular find toolbar or quick find toolbar, if it has focus.
+    """
     type(text=Key.ESC)
 
 
-# If the search bar is present, select the search bar, otherwise this selects the location bar
 def select_search_bar():
+    """If the search bar is present, select the search bar, otherwise this selects the location bar.
+    """
     if get_os() == "osx":
         type(text="k", modifier=KeyModifier.CMD)
     else:
         type(text="k", modifier=KeyModifier.CTRL)
 
 
-# If the search bar has focus change the search engine to the next in the list
-# (side effect: this also opens the search engine manager, if it wasn't alredy open)
 def change_search_next():
+    """If the search bar has focus, change the search engine to the next in the list.
+    (side effect: this also opens the search engine manager, if it wasn't alredy open).
+    """
     if get_os() == "osx":
         type(text=Key.DOWN, modifier=KeyModifier.CMD)
     else:
         type(text=Key.DOWN, modifier=KeyModifier.CTRL)
 
 
-# If the search bar has focus change the search engine to the prvious in the list
-# (side effect: this also opens the search engine manager, if it wasn't alredy open)
 def change_search_previous():
+    """If the search bar has focus, change the search engine to the previous in the list.
+    (side effect: this also opens the search engine manager, if it wasn't already open).
+    """
     if get_os() == "osx":
         type(text=Key.UP, modifier=KeyModifier.CMD)
     else:
         type(text=Key.UP, modifier=KeyModifier.CTRL)
 
 
-# If the search bar has focus open the search engine manager
 def open_search_manager():
+    """If the search bar has focus open the search engine manager.
+    """
     type(text=Key.DOWN, modifier=KeyModifier.ALT)
 
 
-########## end Search ##########
+# End of Search keyboard shortcuts.
 
-########## Windows & Tabs ##########
+# Keyboard shortcuts for Windows & Tabs.
 
-# Close the currently focused tab (Except for app tabs)
 def close_tab():
+    """Close the currently focused tab (Except for app tabs).
+    """
     if get_os() == "osx":
         type(text="w", modifier=KeyModifier.CMD)
     else:
         type(text="w", modifier=KeyModifier.CTRL)
 
 
-# Close the currently focused window
 def close_window():
+    """Close the currently focused window.
+    """
     if get_os() == "osx":
         type(text="w", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="w", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Mute/Unmute audio
-def toggle_audio():
-    type(text="m", modifier=KeyModifier.CTRL)
+def full_screen():
+    """Toggle full screen mode.
+    """
+    if get_os() == "osx":
+        type(text="f", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text=Key.F11)
 
 
-# Open a new browser tab
 def new_tab():
+    """Open a new browser tab.
+    """
     if get_os() == "osx":
         type(text="t", modifier=KeyModifier.CMD)
     else:
         type(text="t", modifier=KeyModifier.CTRL)
 
 
-# Open a new browser window
 def new_window():
+    """Open a new browser window.
+    """
     if get_os() == "osx":
         type(text="n", modifier=KeyModifier.CMD)
     else:
         type(text="n", modifier=KeyModifier.CTRL)
 
 
-# Open a new private browser window
 def new_private_window():
+    """Open a new private browser window.
+    """
     if get_os() == "osx":
         type(text="p", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="p", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Focus the next tab (one over to the right)
 def next_tab():
+    """Focus the next tab (one over to the right).
+    """
     type(text=Key.TAB, modifier=KeyModifier.CTRL)
 
 
-# Focus the previous tab (one over to the left)
 def previous_tab():
+    """Focus the previous tab (one over to the left).
+    """
     type(text=Key.TAB, modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Quit the browser
 def quit_firefox():
+    """Quit the browser.
+    """
     if get_os() == "osx":
         type(text="q", modifier=KeyModifier.CMD)
     elif get_os() == "win":
@@ -389,9 +444,10 @@ def quit_firefox():
         type(text="q", modifier=KeyModifier.CTRL)
 
 
-# Select a given tab (only 1-8)
-# param:  num  is a string 1-8. example: "4"
 def select_tab(num):
+    """Select a given tab (only 1-8).
+    param:  num  is a string 1-8. example: '4'.
+    """
     if get_os() == "osx":
         type(text=num, modifier=KeyModifier.CMD)
     elif get_os() == "win":
@@ -400,8 +456,9 @@ def select_tab(num):
         type(text=num, modifier=KeyModifier.CTRL)
 
 
-# Select the last tab
 def select_last_tab():
+    """Select the last tab.
+    """
     if get_os() == "osx":
         type(text="9", modifier=KeyModifier.CMD)
     elif get_os() == "win":
@@ -410,19 +467,120 @@ def select_last_tab():
         type(text="9", modifier=KeyModifier.CTRL)
 
 
-# Re-opens the previously closed tab
+def toggle_audio():
+    """Mute/Unmute audio.
+    """
+    type(text="m", modifier=KeyModifier.CTRL)
+
+
+
 def undo_close_tab():
+    """Re-opens the previously closed tab.
+    """
     if get_os() == "osx":
         type(text="t", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="t", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
 
-# Re-opens the previously closed browser window
 def undo_close_window():
+    """Re-opens the previously closed browser window.
+    """
     if get_os() == "osx":
         type(text="n", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
     else:
         type(text="n", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
-########## end Windows & Tabs ##########
+
+# End of Windows & Tabs keyboard shortcuts.
+
+# Keyboard shortcuts for History & Bookmarks.
+
+def history_sidebar():
+    """Toggle open/close the history sidebar.
+    """
+    if get_os() == "osx":
+        type(text="h", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="h", modifier=KeyModifier.CTRL)
+
+
+def clear_recent_history():
+    """Open the Clear Recent History dialog.
+    """
+    if get_os() == "osx":
+        type(text=Key.TAB, modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text=Key.TAB, modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+def bookmark_all_tabs():
+    """Open the Bookmark All Tabs dialog.
+    """
+    if get_os() == "osx":
+        type(text="d", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="d", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+def bookmark_page():
+    """Bookmark the current page.
+    """
+    if get_os() == "osx":
+        type(text="d", modifier=KeyModifier.CMD)
+    else:
+        type(text="d", modifier=KeyModifier.CTRL)
+
+
+def bookmarks_sidebar():
+    """Toggle open/close the bookmarks sidebar.
+    """
+    if get_os() == "osx":
+        type(text="b", modifier=KeyModifier.CMD)
+    else:
+        type(text="b", modifier=KeyModifier.CTRL)
+
+
+def open_library():
+    """Open the Library window.
+    """
+    if get_os() == "osx":
+        type(text="b", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="b", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+# End History & Bookmarks keyboard shortcuts.
+
+# Keyboard shortcuts for Tools.
+
+def open_addons():
+    """Open the Add-ons Manager page.
+    """
+    if get_os() == "osx":
+        type(text="a", modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+    else:
+        type(text="a", modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+
+
+def open_downloads():
+    """Open the Downloads dialog.
+    """
+    if get_os() == "osx":
+        type(text="j", modifier=KeyModifier.CMD)
+    elif get_os() == "win":
+        type(text="j", modifier=KeyModifier.CTRL)
+    else:
+        type(text="y", modifier=KeyModifier.CTRL)
+
+
+def open_page_source():
+    """Open the current page's page source
+    """
+    if get_os() == "osx":
+        type(text="u", modifier=KeyModifier.CMD)
+    else:
+        type(text="u", modifier=KeyModifier.CTRL)
+
+
+# End of Tools keyboard shortcuts.
