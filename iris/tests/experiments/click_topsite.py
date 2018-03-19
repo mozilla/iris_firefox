b7e4c95e05b92749cdb8761837aef3e9dc484673
Ioana Crisan <icrisan@mozilla.com>
2018-03-19 14:27:40 +0200

    add a test for opening the first default topsite from TOP SITES list by clicking on it

--- iris/tests/experiments/click_topsite.py
+++ iris/tests/experiments/click_topsite.py
@@ -5,30 +5,21 @@
 
 from test_case import *
 
-
-
 class test(base_test):
 
     def __init__(self, app):
         base_test.__init__(self, app)
         base_test.set_image_path(self, os.path.split(__file__)[0])
         self.assets = os.path.join(os.path.split(__file__)[0], "assets")
-        self.meta = "This is a test for opening the first default topsite from TOP SITES list by pressing the ENTER key"
+        self.meta = "This is a test for opening the first default topsite from TOP SITES list by clicking on it"
 
 
     def run(self):
         url = "about:home"
         navigate(url)
 
-        number_of_iterations = 1
         if exists("top_sites.png", 10):
-            #  The TOP SITES section is populated by default with the sites listed in the following document https://docs.google.com/spreadsheets/d/15yLsFBkic5DFUSdbvSFlR3Rzjxqwmizq9EmksK28l6I/edit#gid=170333837
-            #  Focus the first default TOP SITE and press the ENTER key
-            while number_of_iterations <= 8:
-                focus_next_item()
-                number_of_iterations += 1
-            type(Key.ENTER)
-
+            click("youtube_top_site.png")
             # Check that the first default TOP SITE is opened
             if exists("youtube.png", 10):
                 print "PASS"
