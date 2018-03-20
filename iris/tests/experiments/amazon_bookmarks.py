bf6d6d464d1c2eb05af02e2eae4a063e51d77426
Matt Wobensmith <mwobensmith@mozilla.com>
2018-03-19 20:38:36 -0700

    Fixing bookmark test for Linux, timing issues

--- iris/tests/experiments/amazon_bookmarks.py
+++ iris/tests/experiments/amazon_bookmarks.py
@@ -18,20 +18,33 @@ class test(base_test):
             # Add bookmark with keyboard shortcut
             bookmark_page()
 
+            # Sometimes we need to wait a bit for favicon to be loaded
+            time.sleep(3)
+            type(Key.ENTER)
+
+            # Sometimes we need to wait because UI is animating after
+            # bookmark was created and library icon image isn't found
+            time.sleep(5)
+
             # Look for new bookmark via library menu button
             click("library.png")
+            time.sleep(1)
             type(Key.DOWN)
             type(Key.ENTER)
 
-            if exists("amazon_bookmark.png", 10):
+            if exists("amazon_bookmark_1.png", 5):
                 print "PASS"
             else:
                 print "FAIL"
 
+            # We need to close the library menu before next test
+            type(Key.ESC)
+
             # Look for bookmark in bookmark menu
             bookmarks_sidebar()
+            time.sleep(1)
             type("amazon")
-            if exists("amazon_bookmark.png", 10):
+            if exists("amazon_bookmark_2.png", 5):
                 print "PASS"
             else:
                 print "FAIL"
