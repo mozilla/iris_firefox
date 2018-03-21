79ef5ca47e895cfd951ded6b00c997f609102374
andrei.filip <afilip@mozilla.com>
2018-03-21 17:03:11 +0200

    fixed tests for osx

--- iris/tests/web_compat/wikipedia.py
+++ iris/tests/web_compat/wikipedia.py
@@ -33,44 +33,47 @@ class test(base_test):
         if exists(page_title,3):
             logger.debug("Page is succesfully loaded!!")
             #access wikipedia on english version
-            click(page_title)
-            type(Key.TAB)
+
+            logger.debug("Search in wikipedia with default language")
+            type(keyword)
             type(Key.ENTER)
-            if exists("welcome_wikipedia.png",10):
-                #search with default 'en' language
+                #  wait("iris.png",5)
+            time.sleep(3)
+                #Scrool down
+            logger.debug("Scroll down")
+            for x in range(10):
+                scroll_down()
+            logger.debug("Scroll up")
+                #Scrool up
+            time.sleep(3)
+            for x in range(10):
+                scroll_up()
+            if exists("iris.png",5):
                 logger.debug("Navigate back")
                 navigate_back()
-                time.sleep(4)
-                #sleep is needed since the actions are happening to fasttime.sleep(5)
                 wait(page_title,5)
-                logger.debug("Search in wikipedia with default language")
+                logger.debug("Change language")
                 type(keyword)
-                type(Key.ENTER)
-                #  wait("iris.png",5)
-                time.sleep(3)
-                #Scrool down
-                logger.debug("Scroll down")
-                for x in range(10):
-                    scroll_down()
-                logger.debug("Scroll up")
-                #Scrool up
-                time.sleep(3)
-                for x in range(10):
-                    scroll_up()
-                if exists("iris.png",5):
-                    logger.debug("Navigate back")
-                    navigate_back()
-                    wait(page_title,5)
-                    logger.debug("Change language")
-                    type(keyword)
+                if get_os() == "osx":
+                    print "sistem is osx"
+                    type(Key.TAB)
+                    time.sleep(2)
+                    type(Key.DOWN)
+                    time.sleep(2)
+                    type(Key.DOWN)
+                    time.sleep(2)
+                    type(Key.ENTER)
+                    type(Key.TAB)
+                    type(Key.ENTER)
+                else:
                     type(Key.TAB)
                     type(Key.DOWN)
                     type(Key.TAB)
                     type(Key.ENTER)
-                    if exists("iris.png",5):
-                        logger.debug( "Test passed")
-                    else:
-                        logger.debug ("Test failed")
+                if exists("iris.png",5):
+                    logger.debug( "Test passed")
+                else:
+                    logger.debug ("Test failed")
             else:
 
                 logger.debug( "Test failed")
