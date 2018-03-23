59e8f32c49e7a03fed55b0cf9650e72fe0884885
SilviuFlorin <silviu19sr@yahoo.com>
2018-03-23 17:22:30 +0200

    small experiment with test report and assert
    run iris -t empty for a quick demo
    this implementation is on top of Geroge's prototype branch
    currently is reported the outcome and duration of a test + the duration of the entire run (all tests) and number of success, failed and skipped

--- iris/api/helpers/results.py
+++ iris/api/helpers/results.py
@@ -3,27 +3,12 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-import datetime
-
-today = datetime.date.today()
-
-
-def begin_results_file():
-    f = open('testresults.txt', 'w')
-    f.write('Test run results for %s\n' % today)
-    f.close()
-    return
+def format_outcome(outcome):
+    if outcome:
+        return 'PASS'
+    else:
+        return 'FAIL'
 
 
-def append_results_file(result_msg):
-    f = open('testresults.txt', 'a')
-    f.write(result_msg + '\n')
-    f.close()
-    return
-
-
-def conclude_results_file():
-    f = open('testresults.txt', 'a')
-    f.write('This concludes our testing for %s\n' % today)
-    f.close()
-    return
+def get_duration(start_time, end_time):
+    return round(end_time - start_time, 2)
