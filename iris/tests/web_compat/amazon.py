9f63ac3be751869b2790e6a91b8cbff22f2ed94c
George Echim <george.echim@gmail.com>
2018-03-23 13:06:13 +0200

    Replace jython with python. Replace Sikuli with opencv. Replace Robots with autogui.

--- iris/tests/web_compat/amazon.py
+++ iris/tests/web_compat/amazon.py
@@ -6,43 +6,38 @@
 from test_case import *
 
 
-
 class test(base_test):
 
     def __init__(self, app):
         base_test.__init__(self, app)
-        base_test.set_image_path(self, os.path.split(__file__)[0])
-        self.assets = os.path.join(os.path.split(__file__)[0], "assets")
         self.meta = "Web compability test for amazon.com"
         self.enable = False
 
-
     def login_amazon(self):
         try:
-            wait("amazon_sign_in_button.png",3)
+            wait("amazon_sign_in_button.png", 3)
         except:
-            logger.debug( "Exit login")
+            logger.debug("Exit login")
         else:
             click("amazon_sign_in_button.png")
             time.sleep(3)
-            paste(get_credential("Amazon","username"))
+            paste(get_credential("Amazon", "username"))
             type(Key.ENTER)
             time.sleep(3)
-            paste(get_credential("Amazon","password"))
+            paste(get_credential("Amazon", "password"))
             type(Key.ENTER)
             time.sleep(3)
-            if exists("amazon_logo.png",5):
-                logger.debug( "Login was succesfully performed")
+            if exists("amazon_logo.png", 5):
+                logger.debug("Login was succesfully performed")
             else:
-                logger.debug( "Something went wrong and the user was not logged in")
-
+                logger.debug("Something went wrong and the user was not logged in")
 
     def run(self):
-        url="www.amazon.com"
+        url = "www.amazon.com"
         navigate(url)
-        keyword="Lord of the rings"
+        keyword = "Lord of the rings"
         try:
-            wait("amazon_logo.png",10)
+            wait("amazon_logo.png", 10)
         except:
             logger.debug("Page was not loaded")
 
@@ -50,21 +45,21 @@ class test(base_test):
             self.login_amazon()
             time.sleep(4)
             type(Key.ESC)
-            search=Pattern("amazon_search_button.png").targetOffset(-100,0)
+            search = Pattern("amazon_search_button.png").targetOffset(-100, 0)
             click(search)
             logger.debug("Amazon search")
             paste(keyword)
             type(Key.ENTER)
 
-            amazon_search_result=Pattern('amazon_search_results.png')
+            amazon_search_result = Pattern('amazon_search_results.png')
 
-            found=False
-            while found==False:
-                if exists(amazon_search_result,0.25):
+            found = False
+            while found == False:
+                if exists(amazon_search_result, 0.25):
                     logger.debug("Book result is found")
                     found = True
                     break
-                elif exists('amazon_next_page.png',0.25):
+                elif exists('amazon_next_page.png', 0.25):
                     logger.error("Searching result not found in page")
                     break
 
@@ -73,26 +68,26 @@ class test(base_test):
                     scroll_down()
 
             if found:
-                amazon_cart=Pattern('amazon_cart.png')
-                amazon_add_to_cart=Pattern('amazon_add_to_cart.png')
-                amazon_delete_cart=Pattern('amazon_delete_cart.png')
+                amazon_cart = Pattern('amazon_cart.png')
+                amazon_add_to_cart = Pattern('amazon_add_to_cart.png')
+                amazon_delete_cart = Pattern('amazon_delete_cart.png')
                 click(amazon_search_result)
-                if exists(amazon_add_to_cart,5):
+                if exists(amazon_add_to_cart, 5):
                     logger.debug("Add product to cart")
                     click(amazon_add_to_cart)
-                    if exists(amazon_cart,10):
+                    if exists(amazon_cart, 10):
                         logger.debug("Product was added to cart successfully")
                         click(amazon_cart)
                         time.sleep(4)
-                        if exists(amazon_delete_cart,10):
+                        if exists(amazon_delete_cart, 10):
                             click(amazon_delete_cart)
                             logger.debug("Product was successfully deleted from cart")
                             time.sleep(4)
                             print "PASS"
 
                 else:
-                    logger.debug( 'Product was not added to cart')
+                    logger.debug('Product was not added to cart')
                     print "FAIL"
             else:
-                logger.debug( "Image not found")
+                logger.debug("Image not found")
                 print "FAIL"
