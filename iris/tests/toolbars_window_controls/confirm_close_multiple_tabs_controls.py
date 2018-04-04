271425b5ee00ce33dd8a9fe2d0765895cb962beb
Paul Silaghi <psilaghi@mozilla.com>
2018-04-04 17:55:43 +0300

    Confirm close multiple tabs test Win

---
+++ iris/tests/toolbars_window_controls/confirm_close_multiple_tabs_controls.py
@@ -0,0 +1,49 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from test_case import *
+
+
+
+class test(base_test):
+
+    def __init__(self, app):
+        base_test.__init__(self, app)
+        base_test.set_image_path(self, os.path.split(__file__)[0])
+        self.assets = os.path.join(os.path.split(__file__)[0], "assets")
+        self.meta = "This is a test of the 'About Firefox' window"
+
+
+    def run(self):
+        new_tab()
+        new_tab()
+        if exists("x_button_main_window.png", 10):
+            click("x_button_main_window.png")
+            if exists("close_multiple_tabs_warning.png", 10):
+                print "Close multiple tabs warning was displayed successfully"
+                click("x_button_close_multiple_tabs_warning.png")
+                if (waitVanish("close_multiple_tabs_warning.png", 10) and exists("x_button_main_window.png", 10)):
+                    print "Close multiple tabs warning was canceled successfully"
+                else:
+                    print "Close multiple tabs warning was not canceled successfully"
+                    result = "FAIL"
+            else:
+                print "Close multiple tabs warning was not displayed"
+                result = "FAIL"
+            click("x_button_main_window.png")
+            if exists("close_multiple_tabs_warning.png", 10):
+                print "Close multiple tabs warning was displayed successfully"
+                click("close_multiple_tabs_warning.png")
+                if (waitVanish("close_multiple_tabs_warning.png", 10) and waitVanish("x_button_main_window.png", 10)):
+                    print "The browser was closed successfully"
+                    result = "PASS"
+                else:
+                    print "The browser was not closed successfully"
+                    result = "FAIL"
+        else:
+            print "X button for the main browser window is not found"
+            result = "FAIL"
+
+        print result
