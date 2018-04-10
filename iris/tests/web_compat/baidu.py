5e133cce629c4eb8d552ff338a874a7ba80bfd3d
George Echim <george.echim@gmail.com>
2018-04-10 14:37:46 +0300

    Test baidu with the new asserts

--- iris/tests/web_compat/baidu.py
+++ iris/tests/web_compat/baidu.py
@@ -14,24 +14,21 @@ class test(base_test):
 
     def run(self):
         url = "www.baidu.com"
-        home = "baidu_home.png"
+        baidu_logo = "baidu_home.png"
         search_item = "Barack Obama"
         search_page = "baidu_search_page.png"
 
         logger.debug("Accessing " + url + "...")
         navigate(url)
 
-        if exists(home, 10):
-            logger.debug(url + " successfully loaded")
-            type(search_item)
-            type(Key.ENTER)
-            logger.debug("Searching " + search_item)
+        expected_1 = exists(baidu_logo, 0.5)
+        assert_true(self, expected_1, 'Baidu logo should be visible')
 
-            if exists(search_page, 10):
-                result = "PASS"
-            else:
-                result = "FAIL"
+        logger.debug(url + " successfully loaded")
 
-            print result
-        else:
-            logger.error(url + " can not be accessed...")
+        type(search_item)
+        type(Key.ENTER)
+        logger.debug("Searching " + search_item)
+
+        search_confirmation = exists(search_page, 0.5)
+        assert_true(self, search_confirmation, 'Search action should be finished')
