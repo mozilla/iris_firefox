9cd44f379d270b3afa7b117d826279d34dfa8900
tracywalker <twalker@mozilla.com>
2018-04-12 14:08:42 -0500

    Sidebar controls test
    * Sidebar controls test and associated images

---
+++ iris/tests/toolbars_window_controls/sidebar_controls.py
@@ -0,0 +1,39 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from test_case import *
+
+
+
+class test(base_test):
+
+    def __init__(self, app):
+        base_test.__init__(self, app)
+        base_test.set_image_path(self, os.path.split(__file__)[0])
+        self.assets = os.path.join(os.path.split(__file__)[0], "assets")
+        self.meta = "This is a test of the sidebar controls"
+
+
+    def run(self):
+        bookmarks_sidebar()
+        if exists("x_button_sidebar.png", 10):
+            print "Sidebar was opened successfully"
+            hover("x_button_sidebar.png")
+            if exists("x_button_sidebar_hovered.png", 10):
+                print "Hover state displayed properly"
+                click("x_button_sidebar_hovered.png")
+                if waitVanish("sidebar_title.png", 10):
+                    print "Sidebar was closed successfully"
+                    result = "PASS"
+                else:
+                    print "Sidebar is still open"
+                    result = "FAIL"
+            else:
+                result = "FAIL"
+        else:
+            print "Sidebar is not open"
+            result = "FAIL"
+
+        print result
