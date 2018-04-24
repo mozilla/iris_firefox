81efe19d4d8959cec627c88715c2cdf18938316b
mwobensmith <mwobensmith@mozilla.com>
2018-04-24 09:03:03 -0700

    Merging prototype into master repo

--- iris/tests/toolbars_window_controls/web_console_controls.py
+++ iris/tests/toolbars_window_controls/web_console_controls.py
@@ -3,110 +3,73 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-from test_case import *
+from iris.test_case import *
 
 
-class test(base_test):
+class Test(BaseTest):
 
     def __init__(self, app):
-        base_test.__init__(self, app)
-        base_test.set_image_path(self, os.path.split(__file__)[0])
-        self.assets = os.path.join(os.path.split(__file__)[0], "assets")
-        self.meta = "This is a test case that checks if the Web Console controls work as expected"
+        BaseTest.__init__(self, app)
+        self.meta = 'This is a test case that checks if the Web Console controls work as expected'
 
     def run(self):
 
-        url = "about:home"
-        navigate(url)
+        url = 'about:home'
+        dock_button = 'dock_to_side.png'
+        dock_button_activated = 'dock_to_side_activated.png'
+        separate_window_button = 'separate_window.png'
+        menu = 'hamburger_menu.png'
+        close_dev_tools_button = 'close_dev_tools.png'
+        dev_tools_window = 'dev_tools_window.png'
+        dock_message = 'Dock'
+        separate_window_message = 'Show'
+        close_message = 'Close'
 
+        navigate(url)
         open_web_console()
 
-        dock_button = "dock_to_side.png"
-        dock_button_activated = "dock_to_side_activated.png"
-        separate_window_button = "separate_window.png"
-        close_dev_tools_button = "close_dev_tools.png"
-        menu = "hamburger_menu.png"
-        dev_tools_window = "dev_tools_window.png"
-
         buttons = [dock_button, separate_window_button, close_dev_tools_button]
+        button_messages = [dock_message, separate_window_message, close_message]
 
-        a = 0
-        found = False
-        for i in buttons:
-            if exists(i, 3):
-                a = a + 1
-            if a == len(buttons):
-                logger.debug("Buttons has been found")
-                found = True
+        for button in buttons:
+            if not exists(button, 5):
+                logger.error('Button%s has not been found' % button)
 
-        if found:
-            # Check if the labels are displayed when the cursor hovers over the buttons.
-            screen = get_screen()
-            buttons_region = Sikuli.Region(screen.getW()/2, screen.getH()/2, screen.getW()/2, screen.getH()/2)
+        # Check if the labels are displayed when the cursor hovers over the buttons.
 
-            hover(dock_button)
+        for m_index, button in enumerate(buttons):
+            hover(button)
+            time.sleep(5)
+            coord = find(button)
+            button_region = Region(coord.x - 300, coord.y, 400, 100)
+            expected_messages = button_region.exists(button_messages[m_index], 10)
+            assert_true(self, expected_messages, 'Message %s found' % button_messages[m_index])
 
-            dock_message = 'Dock to side'
+        # Checking the buttons functionality.
+        coord = find(menu)
+        right_upper_corner = Region(coord.x - 300, 0, 300, 300)
 
-            if buttons_region.exists(dock_message, 10):
-                logger.debug("'Dock to side of browser window' label is displayed")
-                print "PASS"
-            else:
-                logger.error("'Dock to side of browser window' is not displayed")
-                print "FAIL"
+        click(dock_button)
 
-            hover(separate_window_button)
+        expected_1 = right_upper_corner.exists(dock_button_activated, 10)
+        assert_true(self, expected_1, 'Dock to side button works !!!')
 
-            separate_window_message = 'Show in'
+        click(separate_window_button)
 
-            if buttons_region.exists(separate_window_message, 10):
-                logger.debug("'Show in separate window' label is displayed")
-                print "PASS"
-            else:
-                logger.error("Show in separate window' is not displayed")
-                print "FAIL"
+        expected_2 = exists(dev_tools_window, 10)
+        assert_true(self, expected_2, 'Show in separate window button works !!!')
 
-            hover(close_dev_tools_button)
+        time.sleep(2)
 
-            close_message = 'Close'
+        # Here is necessary to return back at the initial state in order to verify the close button functionality.
+        click(dock_button_activated)
 
-            if buttons_region.exists(close_message, 10):
-                logger.debug("'Close Developer Tools' label is displayed")
-                print "PASS"
-            else:
-                logger.error("Close Developer Tools' is not displayed")
-                print "FAIL"
+        time.sleep(1)
 
-            # Checking the buttons functionality.
-            coord = find(menu)
-            right_uper_corner = Sikuli.Region(coord.x - 300, 0, 300, 300)
-
-            click(dock_button)
-            if right_uper_corner.exists(dock_button_activated, 5):
-                logger.debug("Dock to side button works !!!")
-                print "PASS"
-            else:
-                logger.error("Dock to side doesn't work")
-                print "FAIL"
-
-            click(separate_window_button)
-            if exists(dev_tools_window, 10):
-                logger.debug("Show in separate window button works !!!")
-                print "PASS"
-            else:
-                logger.error("Show in separate window button doesn't work")
-                print "FAIL"
-
-            # Here is necessary to return back at the initial state in order to verify the close button functionality.
-            click(dock_button_activated)
-
-            click(close_dev_tools_button)
-            if waitVanish(dock_button_activated, 10):
-                logger.debug("Web Console has been closed !")
-                print "PASS"
-            else:
-                logger.error("The Web Console can not be closed!! Aborting..")
-                print "FAIL"
-        else:
-            logger.error("Buttons has not been found")
-            print "FAIL"
+        click(close_dev_tools_button)
+        try:
+            expected_3 = waitVanish(dock_button_activated, 10)
+            assert_true(self, expected_3, 'Close button works !!!')
+        except Exception as error:
+            logger.error('The Web Console can not be closed!! Aborting..')
+            raise error
