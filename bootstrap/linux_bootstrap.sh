af3def1adb0b2a0fae6a6acab2834632af9fded7
Matt Wobensmith <mwobensmith@mozilla.com>
2018-04-25 01:02:55 -0700

    Changing linux bootstrap for tesseract install dependencies

--- bootstrap/linux_bootstrap.sh
+++ bootstrap/linux_bootstrap.sh
@@ -5,12 +5,25 @@ sudo apt-get -y install \
     python-dev \
     python-pip \
     scrot \
+    xsel \
     p7zip-full \
     libopencv-dev \
-    tesseract-ocr \
+    autoconf automake libtool \
+    autoconf-archive \
+    pkg-config \
+    libpng12-dev \
+    libjpeg8-dev \
+    libtiff5-dev \
+    zlib1g-dev \
+    libicu-dev \
+    libpango1.0-dev \
+    libcairo2-dev \
     firefox \
     wmctrl \
     xdotool
+
+# Not installing tesseract-ocr here, see Iris instructions on how to get this
+
 
 # The virtualenv package is not consistently named across distros
 sudo apt-get -y install virtualenv \
