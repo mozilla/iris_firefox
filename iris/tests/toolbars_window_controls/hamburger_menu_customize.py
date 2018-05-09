86bdebed7cc872cad2d7e38acabbd101d4b8fcae
mwobensmith <mwobensmith@mozilla.com>
2018-05-09 15:18:29 -0700

    Minor typos

--- iris/tests/toolbars_window_controls/hamburger_menu_customize.py
+++ iris/tests/toolbars_window_controls/hamburger_menu_customize.py
@@ -10,19 +10,19 @@ class Test(BaseTest):
 
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'This is a test case that checks the hamburger menu > Customize opens the customize page'
+        self.meta = 'This is a test case that checks the Hamburger menu > Customize opens the customize page'
 
     def run(self):
         url = 'about:home'
         navigate(url)
 
-        # Open Customize from te Hamburger Menu
+        # Open Customize from the Hamburger Menu
         click_hamburger_menu_option('Customize...')
         time.sleep(1)
 
         try:
             expected_1 = wait('Drag', 10)
-            assert_true(self, expected_1, 'customize page present.')
+            assert_true(self, expected_1, 'Customize page present.')
         except Exception as error:
             raise error
         else:
