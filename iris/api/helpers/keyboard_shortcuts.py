74b027ebc8f53911374ce17d77027bd6f5eba27a
mwobensmith <mwobensmith@mozilla.com>
2018-05-11 09:11:33 -0700

    Fixing for issue #372

--- iris/api/helpers/keyboard_shortcuts.py
+++ iris/api/helpers/keyboard_shortcuts.py
@@ -393,7 +393,7 @@ def maximize_window():
 def minimize_window():
     """Minimize the browser window to the application launch bar"""
     if Settings.getOS() == Platform.MAC:
-        type(text='m', modifier=Key.CMD)
+        type(text='m', modifier=KeyModifier.CMD)
     else:
         type(text=Key.DOWN, modifier=KeyModifier.WIN)
 
