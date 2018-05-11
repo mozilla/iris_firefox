34d5088569376f474667842abed9f6cf46e09c61
Paul <psilaghi@mozilla.com>
2018-05-11 16:51:50 +0300

    fix issues 403 & 404

--- iris/tests/toolbars_window_controls/library_controls.py
+++ iris/tests/toolbars_window_controls/library_controls.py
@@ -12,66 +12,50 @@ class Test(BaseTest):
         BaseTest.__init__(self, app)
         self.meta = 'This is a test of the library window controls'
 
-
     def run(self):
         library_title = 'library_title.png'
         maximize_button = 'maximize_button.png'
         restore_button = 'restore_button.png'
-        minimize_button = 'minimize_button.png'
         library_restore_button = 'library_restore_button.png'
         library_maximize_button = 'library_maximize_button.png'
 
+        time.sleep(1)
         open_library()
         time.sleep(1)
 
         expected_1 = exists(library_title, 10)
         assert_true(self, expected_1, 'The library was opened successfully')
 
-        if get_os() == 'linux':
-            click(maximize_button)
+        if get_os() == 'osx':
+            maximize_auxiliary_window()
             time.sleep(1)
 
-            expected_2 = exists(restore_button, 10)
-            assert_true(self, expected_2, 'The library was maximized successfully')
-
-            click(restore_button)
-            time.sleep(1)
-
-            expected_3 = exists(maximize_button, 10)
-            assert_true(self, expected_3, 'The library was restored successfully')
-
-            click(minimize_button)
-            time.sleep(1)
-
-        elif get_os() == 'win':
-            maximize_window()
-            time.sleep(1)
-
-            expected_4 = exists(library_restore_button, 10)
-            assert_true(self, expected_4, 'The library was maximized successfully')
-
-            minimize_window()
-            time.sleep(1)
-
-            expected_5 = exists(library_maximize_button, 10)
-            assert_true(self, expected_5, 'The library was restored successfully')
-
-            minimize_window()
+            maximize_auxiliary_window()
             time.sleep(1)
 
         else:
-            maximize_auxiliary_window()
+            maximize_window()
             time.sleep(1)
-
-            maximize_auxiliary_window()
-            time.sleep(1)
+            if get_os() == 'win':
+                expected_2 = exists(library_restore_button, 10)
+            else:
+                expected_2 = exists(restore_button, 10)
+            assert_true(self, expected_2, 'The library was maximized successfully')
 
             minimize_window()
             time.sleep(1)
+            if get_os() == 'win':
+                expected_3 = exists(library_maximize_button, 10)
+            else:
+                expected_3 = exists(maximize_button, 10)
+            assert_true(self, expected_3, 'The library was restored successfully')
+
+        minimize_window()
+        time.sleep(1)
 
         try:
-            expected_6 = waitVanish(library_title, 10)
-            assert_true(self, expected_6, 'Window successfully minimized')
+            expected_4 = waitVanish(library_title, 10)
+            assert_true(self, expected_4, 'Window successfully minimized')
         except:
             raise FindError('Window not minimized, aborting test')
 
@@ -86,12 +70,12 @@ class Test(BaseTest):
         else:
             restore_window_from_taskbar()
 
-        expected_7 = exists(library_title, 10)
-        assert_true(self, expected_7, 'The library was restored successfully')
+        expected_5 = exists(library_title, 10)
+        assert_true(self, expected_5, 'The library was restored successfully')
 
         close_auxiliary_window()
         try:
-            expected_8 = waitVanish(library_title, 10)
-            assert_true(self, expected_8, 'The library was closed successfully')
+            expected_6 = waitVanish(library_title, 10)
+            assert_true(self, expected_6, 'The library was closed successfully')
         except:
             raise FindError('The library didn\'t close successfully')
