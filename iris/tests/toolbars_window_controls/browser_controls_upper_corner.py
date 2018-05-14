c48f13d3532702b8841a94c745b7159822ae72bd
Ioana Crisan <icrisan@mozilla.com>
2018-05-14 15:44:29 +0300

    updated the browser_controls_upper_corner test case

--- iris/tests/toolbars_window_controls/browser_controls_upper_corner.py
+++ iris/tests/toolbars_window_controls/browser_controls_upper_corner.py
@@ -10,140 +10,100 @@ class Test(BaseTest):
 
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = "This is a test case that checks that the upper corner browser controls work as expected"
+        self.meta = 'This is a test case that checks that the upper corner browser controls work as expected'
 
     def run(self):
-        url = "about:home"
+        url = 'about:home'
+        window_controls_minimize = 'window_controls_minimize.png'
+        hover_minimize_control = 'hover_minimize_control.png'
+        window_controls_restore = 'window_controls_restore.png'
+        hover_restore_control = 'hover_restore_control.png'
+        window_controls_maximize = 'window_controls_maximize.png'
+        hover_maximize_control = 'hover_maximize_control.png'
+        window_controls_close = 'window_controls_close.png'
+        hover_close_control = 'hover_close_control.png'
+        hamburger_menu = 'hamburger_menu.png'
+
         navigate(url)
 
-        if exists("browser_controls_upper_corner.png", 10):
-            hover("window_controls_minimize.png")
+        hover(window_controls_minimize)
+        time.sleep(0.5)
+
+        expected = exists(hover_minimize_control, 10)
+        assert_true(self, expected, 'Hover over the \'minimize\' button works correctly.')
+
+        if Settings.getOS() == Platform.WINDOWS or Settings.getOS() == Platform.LINUX:
+            hover(window_controls_restore)
             time.sleep(0.5)
-            if exists("hover_minimize_control.png", 10):
-                print "PASS"
-                logger.debug("hover over the 'minimize' controler works correctly")
-            else:
-                print "FAIL"
-                logger.debug("hover over the 'minimize' controler doesn't work correctly")
+            expected = exists(hover_restore_control, 10)
+            assert_true(self, expected, 'Hover over the \'restore\' button works correctly.')
 
-            if Settings.getOS() == Platform.WINDOWS:
-                hover("window_controls_restore.png")
-                time.sleep(0.5)
-                if exists("hover_restore_control.png", 10):
-                    print "PASS"
-                    logger.debug("hover over the 'restore' controler works correctly")
-                else:
-                    print "FAIL"
-                    logger.debug("hover over the 'restore' controler doesn't work correctly")
-            else:
-                hover("window_controls_maximize.png")
-                time.sleep(0.5)
-                if exists("hover_maximize_control.png", 10):
-                    print "PASS"
-                    logger.debug("hover over the 'maximize' controler works correctly")
-                else:
-                    print "FAIL"
-                    logger.debug("hover over the 'maximize' controler doesn't work correctly")
-
-            hover("window_controls_close.png")
+        if Settings.getOS() == Platform.MAC:
+            middle = find(hover_maximize_control)
+            hover(Location(middle.x + 10, middle.y + 5))
             time.sleep(1)
-            if exists("hover_close_control.png", 10):
-                print "PASS"
-                logger.debug("hover over the 'close' controler works correctly")
-            else:
-                print "FAIL"
-                logger.debug("hover over the 'close' controler doesn't work correctly")
-            time.sleep(0.5)
 
-            if Settings.getOS() == Platform.WINDOWS:
-                # Restore window
-                minimize_window()
-                time.sleep(1)
-                hover("window_controls_maximize.png")
-                time.sleep(0.5)
-                if exists("hover_maximize_control.png", 10):
-                    print "PASS"
-                    logger.debug("hover over the 'maximize' controler works correctly")
-                    logger.debug("window successfully restored")
-                else:
-                    print "FAIL"
-                    logger.debug("hover over the 'maximize' controler doesn't work correctly")
-            elif Settings.getOS() == Platform.LINUX:
-                # Maximize window
-                hover("window_controls_maximize.png")
-                time.sleep(0.5)
-                click("window_controls_maximize.png")
-                time.sleep(0.5)
-                keyDown(Key.ALT)
-                hover("window_controls_restore.png")
-                keyUp(Key.ALT)
-                time.sleep(0.5)
-                if exists("hover_restore_control.png", 10):
-                    print "PASS"
-                    logger.debug("hover over the 'restore' controler works correctly")
-                    logger.debug("window successfully maximized")
-                else:
-                    print "FAIL"
-                    logger.debug("hover over the 'restore' controler doesn't work correctly")
-            else:
-                # Maximize window
-                maximize_window()
-                time.sleep(0.5)
-                hover("window_controls_restore.png")
-                time.sleep(0.5)
-                if exists("hover_restore_control.png", 10):
-                    print "PASS"
-                    logger.debug("hover over the 'restore' controler works correctly")
-                    logger.debug("window successfully maximized")
-                else:
-                    print "FAIL"
-                    logger.debug("hover over the 'restore' controler doesn't work correctly")
+            expected = exists(hover_maximize_control, 10)
+            assert_true(self, expected, 'Hover over the \'maximize\' button works correctly.')
 
-            if Settings.getOS() == Platform.LINUX:
-                # Minimize window
-                click("window_controls_minimize.png")
-                time.sleep(0.5)
-            else:
-                if exists("hamburger_menu.png", 10):
-                    # Minimize window
-                    minimize_window()
-                    time.sleep(0.5)
-                    if waitVanish("hamburger_menu.png", 10):
-                        print "PASS"
-                        logger.debug("window successfully minimized")
-                    else:
-                        print "FAIL"
-                        logger.error("window not minimized, aborting test")
-                else:
-                    print "FAIL"
-                    logger.error("Can't find the 'hamburger menu' in the page, aborting test.")
+            hover(Location(middle.x - 10, middle.y + 5))
+            time.sleep(1)
 
-            # Focus on Firefox and open the browser again
-            restore_window_from_taskbar()
-            if Settings.getOS() == Platform.LINUX:
-                time.sleep(0.5)
-            elif Settings.getOS() == Platform.WINDOWS:
-                maximize_window()
-                time.sleep(0.5)
-
-            if exists("hamburger_menu.png", 10):
-                print "PASS"
-                logger.debug("window successfully opened again")
-            else:
-                print "FAIL"
-                logger.error("window not in view, aborting test.")
-
-            # Close the window
-            close_window()
-            time.sleep(0.5)
-            type(Key.ENTER)
-            time.sleep(0.5)
-            if waitVanish("hamburger_menu.png", 10):
-                print "PASS"
-                logger.debug("window successfully closed")
-            else:
-                print "FAIL"
-                logger.error("window not closed, aborting test")
+            expected = exists(hover_close_control, 10)
+            assert_true(self, expected, 'Hover over the \'close\' button works correctly.')
         else:
-            print "FAIL"
-            logger.error("Can't find the upper corner controls in the page, aborting test.")
+            hover(window_controls_close)
+            expected = exists(hover_close_control, 10)
+            assert_true(self, expected, 'Hover over the \'close\' button works correctly.')
+            time.sleep(0.5)
+
+        if Settings.getOS() == Platform.WINDOWS or Settings.getOS() == Platform.LINUX:
+            # Restore window
+            minimize_window()
+            time.sleep(1)
+            hover(window_controls_maximize)
+            time.sleep(0.5)
+            expected = exists(hover_maximize_control, 10)
+            assert_true(self, expected,
+                        'Hover over the \'maximize\' button works correctly; Window successfully restored.')
+
+        if get_os() == 'linux':
+            # Minimize window
+            click(window_controls_minimize)
+            time.sleep(0.5)
+        else:
+            if exists(hamburger_menu, 10):
+                # Minimize window
+                minimize_window()
+                time.sleep(0.5)
+                try:
+                    expected = waitVanish(hamburger_menu, 10)
+                    assert_true(self, expected, 'Window successfully minimized.')
+                except Exception as error:
+                    logger.error('Window not minimized.')
+                    raise error
+            else:
+                logger.error('Can\'t find the \'hamburger menu\' in the page.')
+
+        # Focus on Firefox and open the browser again
+        restore_window_from_taskbar()
+        if get_os() == 'linux':
+            time.sleep(0.5)
+        elif get_os() == 'win':
+            maximize_window()
+            time.sleep(0.5)
+
+        expected = exists(hamburger_menu, 10)
+        assert_true(self, expected, 'Window successfully opened again.')
+
+        # Close the window
+        close_window()
+        time.sleep(0.5)
+        type(Key.ENTER)
+        time.sleep(0.5)
+        try:
+            expected = waitVanish(hamburger_menu, 10)
+            assert_true(self, expected, 'Window successfully closed.')
+        except Exception as error:
+            logger.error('Window not closed.')
+            raise error
