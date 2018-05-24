c193eb24c03802633663df0284019b1ea0148708
Ioana Crisan <icrisan@mozilla.com>
2018-05-24 15:49:07 +0300

    added coverage for issue 241, 242 and 243

--- iris/api/core.py
+++ iris/api/core.py
@@ -1790,6 +1790,39 @@ def scroll(clicks):
     pyautogui.scroll(clicks)
 
 
+class ZoomType:
+    if Settings.getOS() == Platform.WINDOWS:
+        up = 300
+        down = -300
+    else:
+        up = 1
+        down = -1
+
+
+def zoom_with_mouse_wheel(nr_of_times=1, zoom_type=None):
+    """Zoom in/Zoom out using the mouse wheel
+
+    :param nr_of_times: Number of times the 'zoom in'/'zoom out' action should take place
+    :param zoom_type: Type of the zoom action('zoom in'/'zoom out') intended to perform
+    :return: None
+    """
+
+    # move focus in the middle of the page to be able to use the scroll
+    pyautogui.moveTo(screen_width / 4, screen_height / 2)
+    for i in range(nr_of_times):
+        if Settings.getOS() == Platform.MAC:
+            pyautogui.keyDown('command')
+        else:
+            pyautogui.keyDown('ctrl')
+        pyautogui.scroll(zoom_type)
+        if Settings.getOS() == Platform.MAC:
+            pyautogui.keyUp('command')
+        else:
+            pyautogui.keyUp('ctrl')
+        time.sleep(0.5)
+    pyautogui.moveTo(0, 0)
+
+
 def paste(text):
     # load to clipboard
     pyperclip.copy(text)
