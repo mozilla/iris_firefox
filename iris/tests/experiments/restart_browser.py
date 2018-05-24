42c2df8d68509940d202c38c90f54368c8c3c40c
mwobensmith <mwobensmith@mozilla.com>
2018-05-24 13:42:21 -0700

    Fix indent issue

--- iris/tests/experiments/restart_browser.py
+++ iris/tests/experiments/restart_browser.py
@@ -7,27 +7,25 @@ from iris.test_case import *
 
 
 class Test(BaseTest):
-
     def __init__(self, app):
         BaseTest.__init__(self, app)
         self.app = app
         self.meta = 'This is a test case that restarts the browser'
-        
-   def setup(self):
+
+    def setup(self):
         """ Test case setup
         This overrides the setup method in the BaseTest class,
         so that it can use a profile that already has been launched.
         """
         self.profile = Profile.TEN_BOOKMARKS
         launch_firefox(path=self.app.fx_path, profile=self.profile, url='about:blank')
-        return        
+        return
 
     def run(self):
-
         google_search_image = 'google_search.png'
         amazon_image = 'amazon.png'
 
-        navigate('https://google.com')
+        navigate('https://www.google.com/?hl=EN')
         expected_1 = exists(google_search_image, 10)
         assert_true(self, expected_1, 'Find Google search image')
 
