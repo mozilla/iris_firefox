51630a36c28ef1b3683590c8ec51d23fe2f45c85
mwobensmith <mwobensmith@mozilla.com>
2018-05-24 13:22:30 -0700

    Prevent modal dialog from hindering test

--- iris/tests/experiments/restart_browser.py
+++ iris/tests/experiments/restart_browser.py
@@ -12,6 +12,15 @@ class Test(BaseTest):
         BaseTest.__init__(self, app)
         self.app = app
         self.meta = 'This is a test case that restarts the browser'
+        
+   def setup(self):
+        """ Test case setup
+        This overrides the setup method in the BaseTest class,
+        so that it can use a profile that already has been launched.
+        """
+        self.profile = Profile.TEN_BOOKMARKS
+        launch_firefox(path=self.app.fx_path, profile=self.profile, url='about:blank')
+        return        
 
     def run(self):
 
