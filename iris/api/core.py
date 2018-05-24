5e8fa091392195f0c7b83243371deedfe2d10f82
mwobensmith <mwobensmith@mozilla.com>
2018-05-23 20:23:07 -0700

    Issue 487: Increase wait between copy/paste operation

--- iris/api/core.py
+++ iris/api/core.py
@@ -1793,8 +1793,8 @@ def scroll(clicks):
 def paste(text):
     # load to clipboard
     pyperclip.copy(text)
-    # We must introduce a slight delay, as the copy operation is not instant.
-    time.sleep(.5)
+    # We must introduce a delay, as the copy operation is not instant.
+    time.sleep(2)
     if Settings.getOS() == Platform.MAC:
         pyautogui.hotkey('command', 'v')
     else:
