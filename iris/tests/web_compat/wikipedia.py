4aaae32faacdb1befbb31bd81dcc91e2c8247584
SilviuFlorin <schecherita@mozilla.com>
2018-06-12 11:39:42 +0300

    Fix some typos in Wikipedia test, replace type with paste

--- iris/tests/web_compat/wikipedia.py
+++ iris/tests/web_compat/wikipedia.py
@@ -9,7 +9,7 @@ from iris.test_case import *
 class Test(BaseTest):
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'Web compability test for wikipedia.org'
+        self.meta = 'Web compatibility test for wikipedia.org'
 
     def run(self):
         url = 'www.wikipedia.org'
@@ -20,49 +20,50 @@ class Test(BaseTest):
 
         try:
             wait(page_title, 10)
-            logger.debug('Page is succesfully loaded')
-        except:
-            # If we can't find the Wikipedia logo, there is no sense going further
+            logger.debug('Page is successfully loaded')
+        except (FindError, ValueError):
             logger.error('Can\'t find Wikipedia image in page, aborting test.')
             return
 
-        # Access Wikipedia on English version
         logger.debug('Search in Wikipedia with default English language')
-        type(keyword)
+        paste(keyword)
         type(Key.ENTER)
 
-        # Test if we get a search result
         try:
             wait(iris_text, 10)
-            logger.debug('Search is succesfully loaded')
-        except:
+            logger.debug('Search is successfully loaded')
+        except (FindError, ValueError):
             logger.error('Can\'t find search image in page')
-        # Scroll down
+
         logger.debug('Scroll down')
         for x in range(10):
             scroll_down()
             time.sleep(.25)
+
         scroll_down_assert = exists(iris_text, 1)
         assert_false(self, scroll_down_assert, 'Iris text was found and scroll down was performed')
+
         logger.debug('Scroll up')
         for x in range(10):
             scroll_up()
             time.sleep(.25)
+
         scroll_up_assert = exists(iris_text, 1)
-        assert_true(self, scroll_up_assert, 'Scroll was succesfully performed')
+        assert_true(self, scroll_up_assert, 'Scroll was successfully performed')
+
         logger.debug('Page was scrolled back up')
         navigate_back()
         logger.debug('Navigate back')
 
         try:
             wait(page_title, 10)
-            logger.debug('Page is succesfully loaded')
-        except:
+            logger.debug('Page is successfully loaded')
+        except (FindError, ValueError):
             logger.error('Can\'t find Wikipedia image in page, aborting test.')
             return
         else:
             logger.debug('Change language to Spanish')
-            type(keyword)
+            paste(keyword)
             if Settings.getOS() == Platform.MAC:
                 type(Key.TAB)
                 time.sleep(1)
@@ -80,7 +81,7 @@ class Test(BaseTest):
                 type(Key.ENTER)
 
             spanish_text_assert = exists('wikipedia_spanish_page.png', 10)
-            assert_true(self, spanish_text_assert, 'Wikipedia spanish page is loaded')
+            assert_true(self, spanish_text_assert, 'Wikipedia Spanish page is loaded')
 
             iris_spanish_search_bar_assert = exists('wikipedia_spanish_search.png', 10)
             assert_true(self, iris_spanish_search_bar_assert, 'Spanish search bar is displayed in page')
