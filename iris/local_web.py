095ad7b0786bca2c275ee33feac3045bebfba13e
Matt Wobensmith <mwobensmith@mozilla.com>
2018-06-22 19:06:38 -0700

    WIP: fix server issue, take 6

--- iris/local_web.py
+++ iris/local_web.py
@@ -5,10 +5,8 @@
 
 import logging
 import os
-import socket
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-from SocketServer import BaseServer
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +14,12 @@ logger = logging.getLogger(__name__)
 class CustomHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        SimpleHTTPRequestHandler.do_GET(self)
+        try:
+            SimpleHTTPRequestHandler.do_GET(self)
+        except Exception, e:
+            logger.info('Exception in do_GET')
+            if '10053' in e:
+                logger.info('Browser closed connection before response completed.')
 
     def do_HEAD(self):
         SimpleHTTPRequestHandler.do_HEAD(self)
@@ -27,125 +30,22 @@ class CustomHandler(SimpleHTTPRequestHandler):
             output += str(arg) + '\t'
         logger.debug(output)
 
-    def send_header(self, keyword, value):
-        """Send a MIME header."""
-        try:
-            if self.request_version != 'HTTP/0.9':
-                self.wfile.write("%s: %s\r\n" % (keyword, value))
-
-            if keyword.lower() == 'connection':
-                if value.lower() == 'close':
-                    self.close_connection = 1
-                elif value.lower() == 'keep-alive':
-                    self.close_connection = 0
-        except Exception, e:
-            # Errno 10053 is to be ignored, as the browser connection closes before
-            # the server's response is sent, causing an error
-            logger.info(e)
-            if '10053' in e:
-                logger.info('Browser closed connection before response completed.')
-
     def finish(self):
-        """
-        This comes from StreamRequestHandler, except the part where we call close().
-        We wish to capture a specific exception that happens primarily on
-        Windows, and essentially ignore it.
-        """
-        if not self.wfile.closed:
-            try:
-                self.wfile.flush()
-            except socket.error:
-                # An final socket error may have occurred here, such as
-                # the local error ECONNABORTED.
-                pass
         try:
-            self.wfile.close()
-            self.rfile.close()
+            SimpleHTTPRequestHandler.finish(self)
         except Exception, e:
-            # Errno 10053 is to be ignored, as the browser connection closes before
-            # the server's response is sent, causing an error
-            logger.info(e)
+            logger.info('Exception in finish')
             if '10053' in e:
                 logger.info('Browser closed connection before response completed.')
 
     def handle_one_request(self):
-        """
-        This comes from BaseHTTPRequest, except the part where we call flush().
-        We wish to capture a specific exception that happens primarily on
-        Windows, and essentially ignore it.
-        """
         try:
-            self.raw_requestline = self.rfile.readline(65537)
-            if len(self.raw_requestline) > 65536:
-                self.requestline = ''
-                self.request_version = ''
-                self.command = ''
-                self.send_error(414)
-                return
-            if not self.raw_requestline:
-                self.close_connection = 1
-                return
-            if not self.parse_request():
-                # An error code has been sent, just exit
-                return
-            mname = 'do_' + self.command
-            if not hasattr(self, mname):
-                self.send_error(501, "Unsupported method (%r)" % self.command)
-                return
-            method = getattr(self, mname)
-            method()
-            try:
-                self.wfile.flush() #actually send the response if not already done.
-            except Exception, e:
-                # Errno 10053 is to be ignored, as the browser connection closes before
-                # the server's response is sent, causing an error
-                if '10053' in e:
-                    logger.info('Browser closed connection before response completed.')
-        except socket.timeout, e:
-            #a read or a write timed out.  Discard this connection
-            self.log_error("Request timed out: %r", e)
-            self.close_connection = 1
-            return
-
-class ModifiedHTTPServer(HTTPServer):
-
-    def __init__(self, address, handler):
-        HTTPServer.__init__(self, address, handler)
-    """
-    def _handle_request_noblock(self):
-        try:
-            HTTPServer._handle_request_noblock(self)
+            SimpleHTTPRequestHandler.handle_one_request(self)
         except Exception, e:
-            # Errno 10053 is to be ignored, as the browser connection closes before
-            # the server's response is sent, causing an error
+            logger.info('Exception in handle_one_request')
             if '10053' in e:
                 logger.info('Browser closed connection before response completed.')
-    """
 
-    def handle_request(self):
-        """Handle one request, possibly blocking.
-
-        Respects self.timeout.
-        """
-        # Support people who used socket.settimeout() to escape
-        # handle_request before self.timeout was available.
-        try:
-            BaseServer.handle_request(self)
-        except:
-            logger.info('Uh oh')
-
-        """
-        timeout = self.socket.gettimeout()
-        if timeout is None:
-            timeout = self.timeout
-        elif self.timeout is not None:
-            timeout = min(timeout, self.timeout)
-        fd_sets = _eintr_retry(select.select, [self], [], [], timeout)
-        if not fd_sets[0]:
-            self.handle_timeout()
-            return
-        self._handle_request_noblock()
-        """
 
 
 class LocalWebServer(object):
@@ -159,7 +59,7 @@ class LocalWebServer(object):
     def start(self):
         os.chdir(self.web_root)
         handler = SimpleHTTPRequestHandler
-        server = ModifiedHTTPServer
+        server = HTTPServer
 
         try:
             server_address = (self.host, self.port)
