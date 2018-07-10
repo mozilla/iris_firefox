83db7b852dcef60ad6b49d4fb11798e10d8f9ae2
Paul Silaghi <psilaghi@mozilla.com>
2018-07-10 14:54:47 +0300

    fix issue 672 - test 1

--- iris/tests/history/history_sidebar.py
+++ iris/tests/history/history_sidebar.py
@@ -10,7 +10,7 @@ class Test(BaseTest):
 
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'This is a test of the History sidebar.'
+        self.meta = 'Open the History sidebar.'
 
     def setup(self):
         """Test case setup
@@ -22,43 +22,18 @@ class Test(BaseTest):
         return
 
     def run(self):
-        youtube_logo = 'youtube_banner.png'
-        history_sidebar_youtube = 'history_sidebar_youtube.png'
         search_history_box = 'search_history_box.png'
-        history_empty = 'history_empty.png'
-        x_button_search_history_box = 'x_button_search_history_box.png'
         expand_button_history_sidebar = 'expand_button_history_sidebar.png'
-        history_sidebar_items = 'history_sidebar_items.png'
-
-        # Open a page to create some history.
-        navigate('https://www.youtube.com/')
-        expected_1 = exists(youtube_logo, 10)
-        assert_true(self, expected_1, 'Page loaded successfully.')
+        history_sidebar_default = 'history_sidebar_default.png'
 
         # Open the History sidebar.
         history_sidebar()
-        expected_2 = exists(search_history_box, 10)
-        assert_true(self, expected_2, 'Sidebar was opened successfully.')
-        expected_3 = exists(expand_button_history_sidebar, 10)
-        assert_true(self, expected_3, 'Expand history button displayed properly')
+        expected_1 = exists(search_history_box, 10)
+        assert_true(self, expected_1, 'Sidebar was opened successfully.')
+        expected_2 = exists(expand_button_history_sidebar, 10)
+        assert_true(self, expected_2, 'Expand history button displayed properly.')
         click(expand_button_history_sidebar)
-        click(search_history_box)
 
-        # Check that Youtube is displayed in the History list.
-        paste('youtube')
-        expected_4 = exists(history_sidebar_youtube, 10)
-        assert_true(self, expected_4, 'Youtube displayed in the History list successfully.')
-
-        # Clear the History search box.
-        expected_5 = exists(x_button_search_history_box, 10)
-        assert_true(self, expected_5, 'Clear field button was displayed properly.')
-        click(x_button_search_history_box)
-        time.sleep(0.5)
-        expected_6 = exists(history_sidebar_items, 10)
-        expected_7 = exists(search_history_box, 10)
-        assert_true(self, expected_6 and expected_7, 'The expected items are displayed in the History list.')
-
-        # Check that an unavailable page is not found in the History list.
-        paste('test')
-        expected_8 = exists(history_empty, 10)
-        assert_true(self, expected_8, 'The page wasn\'t found in the History list.')
+        # Check the default items are displayed.
+        expected_3 = exists(history_sidebar_default, 10)
+        assert_true(self, expected_3, 'The expected items are displayed in the History list.')
