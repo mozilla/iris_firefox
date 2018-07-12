74d70047fef5872d78534b6c1f058822aea7a5d0
Matt Wobensmith <mwobensmith@mozilla.com>
2018-07-12 12:19:08 -0700

    Adding examples of tests that use inheritance

---
+++ iris/tests/experiments/subclassed_one.py
@@ -0,0 +1,40 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Rename the imported Test class to avoid a conflict with this class.
+from basic_url import Test as BasicUrl
+
+# Make sure to import everything that the BaseTest class has.
+from iris.test_case import *
+
+
+class Test(BasicUrl):
+
+    def __init__(self, app):
+        # As always, call the superclass constructor first.
+        BasicUrl.__init__(self, app)
+        self.meta = "This is a subclassed test from basic_url"
+
+    def setup(self):
+        # As above, call the superclass constructor.
+        BasicUrl.setup(self)
+
+        # This test wants to use a private window on startup.
+        self.private_window = True
+
+    def run(self):
+        # If you wiash to repeat the superclass test steps, call its run
+        # method here.
+        BasicUrl.run(self)
+
+        # Subclassed test logic goes here.
+        self.test_vars()
+        navigate(self.new_test_url)
+        expected = exists(self.amazon_image, 10)
+        assert_true(self, expected, 'Wait for Amazon image to appear')
+
+    def test_vars(self):
+        self.new_test_url = 'https://www.amazon.com'
+        self.amazon_image = 'amazon.png'
+        self.amazon_history_image = 'amazon_history.png'
---
+++ iris/tests/experiments/subclassed_two.py
@@ -0,0 +1,47 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Rename the imported Test classes to avoid a conflict with this class.
+from basic_url import Test as BasicUrl
+from subclassed_one import Test as SubclassedOne
+
+# Make sure to import everything that the BaseTest class has.
+from iris.test_case import *
+
+
+class Test(SubclassedOne):
+
+    def __init__(self, app):
+        # As always, call the superclass constructor first.
+        SubclassedOne.__init__(self, app)
+        self.meta = "This is a subclassed test from subclassed_one"
+
+    def setup(self):
+        # As above, call the superclass constructor.
+        SubclassedOne.setup(self)
+
+        # This test wants to use a normal browsing window on startup,
+        # unlike its superclass, which uses a private window.
+        self.private_window = False
+
+    def run(self):
+        # This test does not wish to repeat its superclass test logic, so
+        # it will not call its immediate superclass run method.
+        # However, it still wants to run the logic from the topmost superclass,
+        # which navigates to the Google search page. So, it calls that instead.
+        BasicUrl.run(self)
+
+        # New test logic goes here. The variables for the steps below come from calling a
+        # method in the superclass called "test_vars". This is one way to share
+        # data across tests via inheritance.
+        self.test_vars()
+
+        # We will confirm that we do not see the Amazon logo.
+        expected = exists(self.amazon_image, 5)
+        assert_false(self, expected, 'Amazon image is not present')
+
+        # We can continue to write test logic thereafter.
+        navigate(LocalWeb.FIREFOX_TEST_SITE)
+        expected = exists(LocalWeb.FIREFOX_IMAGE, 5)
+        assert_true(self, expected, 'Firefox image is present')
