ece346522bbead9a0cf40d68a307a4f222847d91
SilviuFlorin <schecherita@mozilla.com>
2018-07-13 08:56:18 +0300

    changed some variables names and raised correct error

--- iris/tests/bookmark_tests/bookmark_via_url_sidebar.py
+++ iris/tests/bookmark_tests/bookmark_via_url_sidebar.py
@@ -10,22 +10,22 @@ class Test(BaseTest):
 
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'This is a bunch of test cases that checks if the bookmarks can be bookmarked via URL drag & drop'
+        self.meta = 'Websites can be bookmarked via URL drag & drop onto the Bookmarks Sidebar.'
 
     def run(self):
 
-        url3 = 'bing.com'
-        draggable_url_2 = 'draggable_url_bing.png'
+        url = 'bing.com'
+        draggable_url = 'draggable_url_bing.png'
         sidebar_bookmarks = 'library_bookmarks.png'
-        drag_area_2 = 'drag_area_bing.png'
+        drag_area = 'drag_area_bing.png'
         bing_home = 'bing_home.png'
         bing_bookmark = 'bing_bookmark.png'
         bookmarked_star = 'blue_star.png'
 
-        navigate(url3)
+        navigate(url)
 
-        expected_5 = exists(bing_home, 10)
-        assert_true(self, expected_5, 'Bing has been successfully accessed.')
+        expected = exists(bing_home, 10)
+        assert_true(self, expected, 'Bing has been successfully accessed.')
 
         bookmarks_sidebar('open')
 
@@ -34,11 +34,11 @@ class Test(BaseTest):
             logger.debug('Bookmarks section can be accessed.')
             click(sidebar_bookmarks)
         except FindError:
-            raise APIHelperError('Bookmarks section is not present on the page, aborting.')
+            raise FindError('Bookmarks section is not present on the page, aborting.')
 
         select_location_bar()
 
-        dragDrop(draggable_url_2, drag_area_2, 0.5)
+        dragDrop(draggable_url, drag_area, 0.5)
 
         star_shaped_button_assert = exists(bookmarked_star, 10)
         assert_true(self, star_shaped_button_assert, 'Star-shaped button has changed its color to blue.')
