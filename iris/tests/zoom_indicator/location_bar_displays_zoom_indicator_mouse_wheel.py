59ef4582c81ac236ddd1083b8152b756e6d6e277
icrisan <icrisan@mozilla.com>
2018-07-18 15:07:08 +0300

    updated tests to use local sites

--- iris/tests/zoom_indicator/location_bar_displays_zoom_indicator_mouse_wheel.py
+++ iris/tests/zoom_indicator/location_bar_displays_zoom_indicator_mouse_wheel.py
@@ -13,23 +13,24 @@ class Test(BaseTest):
         self.meta = 'This is a test case that checks the zoom level using the mousewheel.'
 
     def run(self):
-        url = 'en.wikipedia.org'
-        hamburger_menu = 'hamburger_menu.png'
-        search_bar_wikipedia_110_zoom_level = 'search_bar_wikipedia_110_zoom_level.png'
-        search_bar_wikipedia_default_zoom_level = 'search_bar_wikipedia_default_zoom_level.png'
+        url = LocalWeb.FIREFOX_TEST_SITE
+        url_bar_default_zoom_level = 'url_bar_default_zoom_level.png'
+        url_bar_110_zoom_level = 'url_bar_110_zoom_level.png'
 
         navigate(url)
 
-        expected = exists(hamburger_menu, 10)
-        assert_true(self, expected, 'Page successfully loaded, hamburger menu found.')
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
 
         region = create_region_for_url_bar()
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10)
         assert_true(self, expected, 'Zoom level not displayed by default in the url bar.')
 
         # zoom in ONE time.
         zoom_with_mouse_wheel(1, ZoomType.IN)
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        new_region = create_region_for_url_bar()
+
+        expected = new_region.exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully increased, zoom controls found in the url bar.')
