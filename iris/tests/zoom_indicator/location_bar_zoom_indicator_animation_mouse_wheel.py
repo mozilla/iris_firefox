b85d7b92d109504fdfc107cda8e83d957fa6a69a
icrisan <icrisan@mozilla.com>
2018-07-19 15:40:21 +0300

    updated test case to use local site

--- iris/tests/zoom_indicator/location_bar_zoom_indicator_animation_mouse_wheel.py
+++ iris/tests/zoom_indicator/location_bar_zoom_indicator_animation_mouse_wheel.py
@@ -13,83 +13,72 @@ class Test(BaseTest):
         self.meta = 'This test case checks the zoom indicator animation using the mousewheel.'
 
     def run(self):
-        url = 'en.wikipedia.org'
-        hamburger_menu = 'hamburger_menu.png'
-        search_bar_wikipedia_110_zoom_level = 'search_bar_wikipedia_110_zoom_level.png'
-        search_bar_wikipedia_default_zoom_level = 'search_bar_wikipedia_default_zoom_level.png'
-        search_bar_wikipedia_300_zoom_level = 'search_bar_wikipedia_300_zoom_level.png'
-        search_bar_wikipedia_30_zoom_level = 'search_bar_wikipedia_30_zoom_level.png'
+        url = LocalWeb.FIREFOX_TEST_SITE
+        url_bar_default_zoom_level = 'url_bar_default_zoom_level.png'
+        url_bar_30_zoom_level = 'url_bar_30_zoom_level.png'
+        url_bar_110_zoom_level = 'url_bar_110_zoom_level.png'
+        url_bar_300_zoom_level = 'url_bar_300_zoom_level.png'
 
         navigate(url)
 
-        expected = exists(hamburger_menu, 10)
-        assert_true(self, expected, 'Page successfully loaded, hamburger menu found.')
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
 
         region = create_region_for_url_bar()
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10, 0.92)
         assert_true(self, expected, 'Zoom level not displayed by default in the url bar.')
 
-        restart_firefox(self.app.fx_path, self.profile_path, url='en.wikipedia.org')
+        restart_firefox(self.app.fx_path, self.profile_path, url=LocalWeb.FIREFOX_TEST_SITE)
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10, 0.92)
         assert_true(self, expected, 'Zoom level not displayed by default in the url bar.')
 
         # zoom in ONE time.
         zoom_with_mouse_wheel(1, ZoomType.IN)
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        new_region = create_region_for_url_bar()
+
+        expected = new_region.exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully increased, zoom controls found in the url bar.')
 
-        restart_firefox(self.app.fx_path, self.profile_path, url='en.wikipedia.org')
+        restart_firefox(self.app.fx_path, self.profile_path, url=LocalWeb.FIREFOX_TEST_SITE)
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        expected = new_region.exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully increased, zoom controls found in the url bar.')
 
         # zoom out ONE time.
         zoom_with_mouse_wheel(1, ZoomType.OUT)
 
-        expected = exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = new_region.exists(url_bar_default_zoom_level, 10, 0.92)
         assert_true(self, expected, 'Zoom controls not found in the url bar after browser restore its zoom level.')
 
         # zoom in until the maximum zoom level(300%) is reached.
         zoom_with_mouse_wheel(20, ZoomType.IN)
 
-        if Settings.get_os() == Platform.WINDOWS:
-            expected = exists(search_bar_wikipedia_300_zoom_level, 10, 0.99)
-            assert_true(self, expected, 'Zoom level successfully increased, maximum zoom level(300%) reached.')
-        else:
-            expected = exists(search_bar_wikipedia_300_zoom_level, 10)
-            assert_true(self, expected, 'Zoom level successfully increased, maximum zoom level(300%) reached.')
+        expected = new_region.exists(url_bar_300_zoom_level, 10)
+        assert_true(self, expected, 'Zoom level successfully increased, maximum zoom level(300%) reached.')
 
         # zoom in ONE more time.
         zoom_with_mouse_wheel(1, ZoomType.IN)
 
-        if Settings.get_os() == Platform.WINDOWS:
-            expected = exists(search_bar_wikipedia_300_zoom_level, 10, 0.99)
-            assert_true(self, expected, 'Zoom level still displays 300%.')
-        else:
-            expected = exists(search_bar_wikipedia_300_zoom_level, 10)
-            assert_true(self, expected, 'Zoom level still displays 300%.')
+        expected = new_region.exists(url_bar_300_zoom_level, 10)
+        assert_true(self, expected, 'Zoom level still displays 300%.')
 
         # zoom out until the default zoom level(100%) is reached.
         restore_zoom()
 
-        expected = exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = new_region.exists(url_bar_default_zoom_level, 10, 0.92)
         assert_true(self, expected, 'Zoom controls not found in the url bar after browser restore its zoom level.')
 
         # zoom out until de minimum zoom level(30%) is reached.
         zoom_with_mouse_wheel(7, ZoomType.OUT)
 
-        expected = exists(search_bar_wikipedia_30_zoom_level, 10)
+        expected = new_region.exists(url_bar_30_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully decreased, minimum zoom level(30%) reached.')
 
         # zoom out ONE more time.
         zoom_with_mouse_wheel(1, ZoomType.OUT)
 
-        if Settings.get_os() == Platform.WINDOWS:
-            expected = exists(search_bar_wikipedia_30_zoom_level, 10, 0.99)
-            assert_true(self, expected, 'Zoom level still displays 30%.')
-        else:
-            expected = exists(search_bar_wikipedia_30_zoom_level, 10)
-            assert_true(self, expected, 'Zoom level still displays 30%.')
+        expected = new_region.exists(url_bar_30_zoom_level, 10)
+        assert_true(self, expected, 'Zoom level still displays 30%.')
