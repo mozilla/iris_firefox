71bb14be2931e75ec72d3b237ad066e86b12f9e2
icrisan <icrisan@mozilla.com>
2018-07-19 10:21:08 +0300

    updates test cases to use local sites

--- iris/tests/zoom_indicator/location_bar_zoom_indicator_hides.py
+++ iris/tests/zoom_indicator/location_bar_zoom_indicator_hides.py
@@ -13,39 +13,40 @@ class Test(BaseTest):
         self.meta = 'This test case checks that zoom indicator hides.'
 
     def run(self):
-        url = 'en.wikipedia.org'
-        hamburger_menu = 'hamburger_menu.png'
-        search_bar_wikipedia_default_zoom_level = 'search_bar_wikipedia_default_zoom_level.png'
-        search_bar_wikipedia_110_zoom_level = 'search_bar_wikipedia_110_zoom_level.png'
-        search_bar_wikipedia_90_zoom_level = 'search_bar_wikipedia_90_zoom_level.png'
+        url = LocalWeb.FIREFOX_TEST_SITE
+        url_bar_default_zoom_level = 'url_bar_default_zoom_level.png'
+        url_bar_110_zoom_level = 'url_bar_110_zoom_level.png'
+        url_bar_90_zoom_level = 'url_bar_90_zoom_level.png'
 
         navigate(url)
 
-        expected = exists(hamburger_menu, 10)
-        assert_true(self, expected, 'Page successfully loaded, hamburger menu found.')
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
 
         region = create_region_for_url_bar()
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10, 0.94)
         assert_true(self, expected, 'Zoom level not displayed by default in the url bar.')
 
         zoom_in()
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        new_region = create_region_for_url_bar()
+
+        expected = new_region.exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully increased, zoom controls found in the url bar.')
 
         zoom_out()
 
-        expected = exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = new_region.exists(url_bar_default_zoom_level, 10, 0.94)
         assert_true(self, expected, 'Zoom level successfully decreased, zoom controls not found in the url bar for 100%'
                                     ' zoom level.')
 
         zoom_out()
 
-        expected = exists(search_bar_wikipedia_90_zoom_level, 10)
+        expected = new_region.exists(url_bar_90_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully decreased, zoom controls found in the url bar.')
 
         zoom_in()
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = new_region.exists(url_bar_default_zoom_level, 10, 0.94)
         assert_true(self, expected, 'Zoom level successfully increased, zoom controls not found in the url bar.')
