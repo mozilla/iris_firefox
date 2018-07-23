f98375759b3b56c1112170b7a13373b74e63e32d
icrisan <icrisan@mozilla.com>
2018-07-23 12:17:08 +0300

    added coverage for issue 251

---
+++ iris/tests/zoom_indicator/reset_zoom_level_menu_bar.py
@@ -0,0 +1,39 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'This is a test case that checks the reset zoom level from menu bar functionality.'
+
+    def run(self):
+        url = LocalWeb.FIREFOX_TEST_SITE
+        url_bar_default_zoom_level = 'url_bar_default_zoom_level.png'
+        url_bar_110_zoom_level = 'url_bar_110_zoom_level.png'
+
+        navigate(url)
+
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
+
+        expected = exists(url_bar_default_zoom_level, 10)
+        assert_true(self, expected, 'Zoom level not displayed by default in the url bar.')
+
+        open_zoom_menu(Option.ZOOM_IN)
+
+        region = create_region_for_url_bar()
+
+        expected = region.exists(url_bar_110_zoom_level, 10)
+        assert_true(self, expected, 'Zoom level successfully increased, zoom controls found in the url bar.')
+
+        # Reset the zoom level from the menu bar.
+        open_zoom_menu(Option.RESET)
+
+        expected = region.exists(url_bar_default_zoom_level, 10, 0.92)
+        assert_true(self, expected, 'Zoom level not displayed in the url bar after zoom level reset.')
