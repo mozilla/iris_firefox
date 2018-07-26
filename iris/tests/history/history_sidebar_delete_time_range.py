c29eafec9404869f515fde848eaaaf0b3c73ccff
Paul Silaghi <psilaghi@mozilla.com>
2018-07-26 15:52:39 +0300

    fix issue 193 - Delete a time range from the History sidebar

---
+++ iris/tests/history/history_sidebar_delete_time_range.py
@@ -0,0 +1,44 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'Delete a time range from the History sidebar.'
+
+    def setup(self):
+        """Test case setup
+
+        This overrides the setup method in the BaseTest class, so that it can use a brand new profile.
+        """
+        BaseTest.setup(self)
+        self.profile = Profile.BRAND_NEW
+        return
+
+    def run(self):
+        search_history_box = 'search_history_box.png'
+        expand_button_history_sidebar = 'expand_button_history_sidebar.png'
+
+        # Open a page to create some history.
+        navigate(LocalWeb.MOZILLA_TEST_SITE)
+        expected_1 = exists(LocalWeb.MOZILLA_LOGO, 10)
+        assert_true(self, expected_1, 'Mozilla page loaded successfully.')
+
+        # Open the History sidebar.
+        history_sidebar()
+        expected_2 = exists(search_history_box, 10)
+        assert_true(self, expected_2, 'Sidebar was opened successfully.')
+        expected_3 = exists(expand_button_history_sidebar, 10)
+        assert_true(self, expected_3, 'Expand history button displayed properly.')
+
+        # Delete a time range from the History sidebar.
+        right_click(expand_button_history_sidebar)
+        type(text='d')
+        expected_4 = exists(expand_button_history_sidebar, 5)
+        assert_false(self, expected_4, 'Time range was deleted successfully from the history sidebar.')
