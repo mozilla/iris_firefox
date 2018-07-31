0a1a37a55e1b333f0df86250e9ebd8ad61a80406
icrisan <icrisan@mozilla.com>
2018-07-31 10:32:06 +0300

    added coverage for issue 194, 196 and 197

---
+++ iris/tests/awesomebar/search_in_new_tab_using_one_off_context_menu.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'This test case verifies that \'Search in New Tab\' option works correctly using an one-off.'
+
+    def run(self):
+        moz = 'moz.png'
+        url = LocalWeb.FIREFOX_TEST_SITE
+        one_off_searches = 'one_off_searches.png'
+        google_one_off_button = 'google_one_off_button.png'
+        twitter_one_off_button_highlight = 'twitter_one_off_button_highlight.png'
+        set_as_default_search_engine = 'set_as_default_search_engine.png'
+        search_in_new_tab = 'search_in_new_tab.png'
+        new_tab_twitter_search_results = 'new_tab_twitter_search_results.png'
+
+        region = Region(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2)
+
+        navigate(url)
+
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
+
+        select_location_bar()
+        paste('moz')
+
+        expected = region.exists(moz, 10)
+        assert_true(self, expected, 'Searched string found at the bottom of the drop-down list.')
+
+        expected = region.exists(one_off_searches, 10)
+        assert_true(self, expected, 'The one-off searches are displayed in the awesome bar.')
+
+        hover(google_one_off_button)
+
+        try:
+            expected = region.wait_vanish(moz, 10)
+            assert_true(self, expected, 'The \'Google\' one-off button is highlighted.')
+        except FindError:
+            raise FindError('The \'Google\' one-off button is not highlighted.')
+
+        for i in range(15):
+            scroll_down()
+
+        expected = region.exists(twitter_one_off_button_highlight, 10)
+        assert_true(self, expected, 'The \'Twitter\' one-off button is highlighted.')
+
+        right_click(twitter_one_off_button_highlight)
+
+        expected = exists(search_in_new_tab, 10)
+        assert_true(self, expected, 'The \'Search in New Tab\' option found.')
+
+        expected = exists(set_as_default_search_engine, 10)
+        assert_true(self, expected, 'The \'Set As Default Search Engine\' option found.')
+
+        click(search_in_new_tab)
+        time.sleep(DEFAULT_UI_DELAY_LONG)
+
+        # move focus to the new tab opened.
+        next_tab()
+
+        expected = exists(new_tab_twitter_search_results, 10)
+        assert_true(self, expected, 'A new tab with the Twitter search results is opened.')
