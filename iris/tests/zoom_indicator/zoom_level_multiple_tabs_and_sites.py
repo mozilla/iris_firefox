2fedf2505932fff273a3773d55576859d8f55ce5
icrisan <icrisan@mozilla.com>
2018-08-01 16:04:45 +0300

    updated tests to use 2 different origins

--- iris/tests/zoom_indicator/zoom_level_multiple_tabs_and_sites.py
+++ iris/tests/zoom_indicator/zoom_level_multiple_tabs_and_sites.py
@@ -13,32 +13,34 @@ class Test(BaseTest):
         self.meta = 'This test case verifies the zoom level on multiple tabs using multiple sites.'
 
     def run(self):
-        url_1 = 'en.wikipedia.org'
-        url_2 = 'www.amazon.com'
-        search_bar_wikipedia_default_zoom_level = 'search_bar_wikipedia_default_zoom_level.png'
-        search_bar_wikipedia_110_zoom_level = 'search_bar_wikipedia_110_zoom_level.png'
-        hamburger_menu = NavBar.HAMBURGER_MENU
+        url_1 = LocalWeb.FIREFOX_TEST_SITE
+        url_2 = LocalWeb.FIREFOX_TEST_SITE_2
+        url_bar_default_zoom_level = 'url_bar_default_zoom_level.png'
+        url_bar_110_zoom_level = 'url_bar_110_zoom_level.png'
 
         navigate(url_1)
 
-        expected = exists(hamburger_menu, 10)
-        assert_true(self, expected, 'Page successfully loaded, hamburger menu found.')
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
 
         region = create_region_for_url_bar()
 
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10)
         assert_true(self, expected, 'Zoom indicator not displayed by default in the url bar.')
 
         zoom_in()
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        expected = exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected, 'Zoom level successfully increased, zoom indicator found in the url bar.')
 
         new_tab()
 
         navigate(url_1)
 
-        expected = exists(search_bar_wikipedia_110_zoom_level, 10)
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
+
+        expected = exists(url_bar_110_zoom_level, 10)
         assert_true(self, expected,
                     'Zoom indicator still displays 110% in the new tab opened for the site for which the '
                     'zoom level was set.')
@@ -47,7 +49,9 @@ class Test(BaseTest):
 
         navigate(url_2)
 
-        # Location bar looks the same for both wikipedia and amazon sites.
+        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
+
         # Zoom level set for one site does not propagate to other sites.
-        expected = region.exists(search_bar_wikipedia_default_zoom_level, 10)
+        expected = region.exists(url_bar_default_zoom_level, 10)
         assert_true(self, expected, 'Zoom indicator not displayed in the url bar.')
