58a68b1975fa7ce1e22f137fce1d1f32711a07e6
icrisan <icrisan@mozilla.com>
2018-08-07 08:07:49 +0300

    splitted the image with the one offs list into individual one offs images

--- iris/tests/awesomebar/one_off_searches_displayed_in_awesome_bar.py
+++ iris/tests/awesomebar/one_off_searches_displayed_in_awesome_bar.py
@@ -14,8 +14,14 @@ class Test(BaseTest):
 
     def run(self):
         url = LocalWeb.FIREFOX_TEST_SITE
-        one_off_searches = 'one_off_searches.png'
         search_settings = 'search_settings.png'
+        amazon_one_off_button = 'amazon_one_off_button.png'
+        bing_one_off_button = 'bing_one_off_button.png'
+        duck_duck_go_one_off_button = 'duck_duck_go_one_off_button.png'
+        ebay_one_off_button = 'ebay_one_off_button.png'
+        google_one_off_button = 'google_one_off_button.png'
+        twitter_one_off_button = 'twitter_one_off_button.png'
+        wikipedia_one_off_button = 'wikipedia_one_off_button.png'
         moz = 'moz.png'
 
         region = Region(0, 0, SCREEN_WIDTH, 2 * SCREEN_HEIGHT / 3)
@@ -34,5 +40,23 @@ class Test(BaseTest):
         expected = region.exists(search_settings, 10)
         assert_true(self, expected, 'The \'Search settings\' button is displayed in the awesome bar.')
 
-        expected = region.exists(one_off_searches, 10)
-        assert_true(self, expected, 'The one-off searches are displayed in the awesome bar.')
+        expected = region.exists(amazon_one_off_button, 10)
+        assert_true(self, expected, 'The \'Amazon\' one-off button found.')
+
+        expected = region.exists(bing_one_off_button, 10)
+        assert_true(self, expected, 'The \'Bing\' one-off button found.')
+
+        expected = region.exists(duck_duck_go_one_off_button, 10)
+        assert_true(self, expected, 'The \'DuckDuckGo\' one-off button found.')
+
+        expected = region.exists(ebay_one_off_button, 10)
+        assert_true(self, expected, 'The \'Ebay\' one-off button found.')
+
+        expected = region.exists(google_one_off_button, 10)
+        assert_true(self, expected, 'The \'Google\' one-off button found.')
+
+        expected = region.exists(twitter_one_off_button, 10)
+        assert_true(self, expected, 'The \'Twitter\' one-off button found.')
+
+        expected = region.exists(wikipedia_one_off_button, 10)
+        assert_true(self, expected, 'The \'Wikipedia\' one-off button found.')
