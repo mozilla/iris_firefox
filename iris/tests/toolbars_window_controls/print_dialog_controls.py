af6b20489c41720e89660e6d2dd147b741271652
tracywalker <twalker@mozilla.com>
2018-08-14 14:06:00 -0500

    Add test case and images for print dialog controls
    closes #435
    
    	new file:   iris/local_web/tests/toolbars_window_controls/print_dialog_controls/moz.pdf
    	new file:   iris/tests/toolbars_window_controls/images/linux/pdf_print_button.png
    	new file:   iris/tests/toolbars_window_controls/images/linux/print_dialog.png
    	new file:   iris/tests/toolbars_window_controls/images/osx/pdf_print_button.png
    	new file:   iris/tests/toolbars_window_controls/images/osx/print_dialog.png
    	new file:   iris/tests/toolbars_window_controls/images/win/pdf_print_button.png
    	new file:   iris/tests/toolbars_window_controls/images/win/print_dialog.png
    	new file:   iris/tests/toolbars_window_controls/print_dialog_controls.py

---
+++ iris/tests/toolbars_window_controls/print_dialog_controls.py
@@ -0,0 +1,41 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'This is a test of the Download dialog controls'
+
+    def run(self):
+        url = self.get_web_asset_path('moz.pdf')
+        test_pdf_pattern = Pattern('moz_fast.png')
+        print_button_pattern = Pattern('pdf_print_button.png')
+        dialog_pattern = Pattern('print_dialog.png')
+
+        navigate(url)
+
+        # Check to make sure the test PDF is loaded, Then click the download button.
+        expected = exists(test_pdf_pattern, 10)
+        assert_true(self, expected, 'The test PDF is present')
+        click(print_button_pattern)
+
+        # Ensure the dialog appears
+        expected = exists(dialog_pattern, 10)
+        assert_true(self, expected, 'Print dialog is present')
+
+        # Close the dialog
+        if Settings.get_os() == Platform.MAC:
+            click_cancel_button()
+        else:
+            click_auxiliary_window_control('close')
+
+        try:
+            expected = wait_vanish(dialog_pattern, 5)
+            assert_true(self, expected, 'Download dialog was closed')
+        except FindError:
+            raise FindError('Download dialog is still present')
