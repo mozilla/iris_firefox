8e07c022cbec1ea9cb01c33ee9dd1e9223f45579
icrisan <icrisan@mozilla.com>
2018-08-17 12:06:51 +0300

    added coverage for issue 205

--- iris/tests/awesomebar/__init__.py
+++ iris/tests/awesomebar/__init__.py
@@ -1,2 +1,2 @@
-# TODO: enable test "disable_web_search_awesomebar.py", "two_selections_at_the_same_time.py"  and
-# "disable_one_off_searches_from_awesomebar.py" once issue #950 is fixed.
+# TODO: enable test "disable_web_search_awesomebar.py", "two_selections_at_the_same_time.py",
+# "disable_one_off_searches_from_awesomebar.py" and  "remove_checks_from_search_engines.py" once issue #950 is fixed.
