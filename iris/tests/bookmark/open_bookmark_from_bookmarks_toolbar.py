b06f144f2368c5988544b5a795f9859308f22f85
Ionut Budeanu <ibudeanu@mozilla.com>
2018-08-21 16:50:36 +0300

    Updated 3rd set of bookmark tests

--- iris/tests/bookmark/open_bookmark_from_bookmarks_toolbar.py
+++ iris/tests/bookmark/open_bookmark_from_bookmarks_toolbar.py
@@ -9,26 +9,24 @@ from iris.test_case import *
 class Test(BaseTest):
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'This is a test case that checks if a bookmark can be opened from Bookmarks Toolbar.'
-        self.test_case_id='4093'
-        self.test_suite_id='75'
+        self.meta = 'Bookmarks can be opened from Bookmarks Toolbar.'
+        self.test_case_id = '4093'
+        self.test_suite_id = '75'
+
+    def setup(self):
+        """Test case setup
+
+        This overrides the setup method in the BaseTest class, so that it can use a brand new profile.
+        """
+        BaseTest.setup(self)
+        self.profile = Profile.TEN_BOOKMARKS
+        return
 
     def run(self):
-        amazon_home_pattern = Pattern('amazon.png')
-        amazon_draggable_pattern = Pattern('amazon_draggable.png')
+        moz_draggable_pattern = Pattern('moz_sidebar_bookmark.png')
         drag_area_pattern = Pattern('drag_area.png')
-        dragged_bookmark_pattern = Pattern('toolbar_dragged_bookmark.png')
+        dragged_bookmark_pattern = Pattern('moz_toolbar_dragged_bookmark.png')
         view_bookmarks_toolbar_pattern = LibraryMenu.BookmarksOption.BookmarkingTools.VIEW_BOOKMARKS_TOOLBAR
-
-        navigate('www.amazon.com')
-
-        amazon_banner_assert = exists(amazon_home_pattern, 10)
-        assert_true(self, amazon_banner_assert, 'Amazon page has been successfully loaded.')
-
-        nav_bar_favicon_assert = exists(Pattern('amazon_favicon.png'), 15)
-        assert_true(self, nav_bar_favicon_assert, 'Page is fully loaded and favicon displayed.')
-
-        bookmark_page()
 
         navigate('about:blank')
 
@@ -36,24 +34,17 @@ class Test(BaseTest):
 
         bookmarks_sidebar('open')
 
-        paste('amazon')
+        paste('Mozilla')
 
-        drag_drop(amazon_draggable_pattern, drag_area_pattern, 0.5)
+        drag_drop(moz_draggable_pattern, drag_area_pattern, 0.5)
 
         bookmark_drag_assert = exists(dragged_bookmark_pattern, 10)
-        assert_true(self, bookmark_drag_assert, 'Bookmark was dragged successfully.')
+        assert_true(self, bookmark_drag_assert, 'Moz Bookmark was dragged successfully.')
 
         bookmarks_sidebar('close')
 
-        amazon_bookmark_sidebar_left_corner_assert = exists(dragged_bookmark_pattern, 10)
-        assert_true(self, amazon_bookmark_sidebar_left_corner_assert,
-                    'Amazon bookmark can be accessed from the Bookmarks Toolbar.')
-
         click(dragged_bookmark_pattern)
 
-        nav_bar_favicon_assert = exists(Pattern('amazon_favicon.png'), 15)
-        assert_true(self, nav_bar_favicon_assert, 'Page is fully loaded and favicon displayed.')
-
-        amazon_bookmark_toolbar = exists(amazon_home_pattern, 10)
-        assert_true(self, amazon_bookmark_toolbar,
-                    'Amazon bookmark has been successfully accessed from the Bookmarks Toolbar.')
+        mozilla_page_assert = exists(LocalWeb.MOZILLA_LOGO, 10)
+        assert_true(self, mozilla_page_assert,
+                    'Moz bookmark has been successfully accessed from the Bookmarks Toolbar.')
