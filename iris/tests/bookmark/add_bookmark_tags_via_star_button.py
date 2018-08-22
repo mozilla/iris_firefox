d302ad536b3fcce0248d07806e8dec27eae964ea
Ionut Budeanu <ibudeanu@mozilla.com>
2018-08-22 16:52:51 +0300

    Added coverage for issue 120

---
+++ iris/tests/bookmark/add_bookmark_tags_via_star_button.py
@@ -0,0 +1,52 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'Tags can be added to bookmarks using the star-shaped button.'
+        self.test_case_id = '4145'
+        self.test_suite_id = '75'
+
+    def run(self):
+
+        bookmark_button_pattern = LocationBar.BOOKMARK_BUTTON
+        tags_field = Pattern('tags_field.png')
+        done = Pattern('done_button.png')
+        moz_tagged_bookmark = Pattern('moz_sidebar_bookmark.png')
+
+        navigate(LocalWeb.MOZILLA_TEST_SITE)
+
+        try:
+            wait(bookmark_button_pattern, 10)
+            logger.debug('Bookmark star is present on the page.')
+            click(bookmark_button_pattern)
+        except FindError:
+            raise FindError('Bookmark star is not present on the page, aborting.')
+
+        page_bookmarked_assert = exists(Pattern('page_bookmarked.png'), 10)
+        assert_true(self, page_bookmarked_assert, 'The page was successfully bookmarked via star button.')
+
+        try:
+            wait(tags_field, 10)
+            logger.debug('Tags field can be edit.')
+            click(tags_field)
+        except FindError:
+            raise FindError('Tags field is NOT present on the page, aborting.')
+
+        paste('Iris')
+
+        click(done)
+
+        bookmarks_sidebar('open')
+
+        paste('Iris')
+
+        tagged_bookmark_assert = exists(moz_tagged_bookmark, 10)
+        assert_true(self, tagged_bookmark_assert, 'Moz bookmark has been successfully tagged.')
