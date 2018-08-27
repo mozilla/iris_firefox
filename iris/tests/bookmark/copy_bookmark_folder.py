a9269caf5fc223ae55eb49a02bbb8b83f4aa7521
Ionut Budeanu <ibudeanu@mozilla.com>
2018-08-27 16:45:10 +0300

    Added coverage for issue 126

---
+++ iris/tests/bookmark/copy_bookmark_folder.py
@@ -0,0 +1,88 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'Bookmark folders can be copied via context menu.'
+        self.test_case_id = '4151'
+        self.test_suite_id = '75'
+
+    def setup(self):
+        """Test case setup
+
+        This overrides the setup method in the BaseTest class, so that it can use a brand new profile.
+        """
+        BaseTest.setup(self)
+        self.profile = Profile.TEN_BOOKMARKS
+        return
+
+    def run(self):
+        moz_draggable_pattern = Pattern('moz_sidebar_bookmark.png')
+        drag_area_pattern = Pattern('drag_area.png')
+        dragged_bookmark_pattern = Pattern('dragged_to_folder.png')
+        new_folder = Pattern('new_folder_option.png')
+        add_button = Pattern('add_button.png')
+        moz_bookmark = Pattern('moz_bookmark_folder.png')
+        pasted_bookmark_folder = Pattern('pasted_bookmark_folder.png')
+        bookmarks_sidebar_menu_pattern = SidebarBookmarks.BOOKMARKS_MENU
+        bookmarks_sidebar_menu_selected_pattern = SidebarBookmarks.BOOKMARKS_MENU_SELECTED
+        view_bookmarks_toolbar_pattern = LibraryMenu.BookmarksOption.BookmarkingTools.VIEW_BOOKMARKS_TOOLBAR
+
+        navigate('about:blank')
+
+        access_bookmarking_tools(view_bookmarks_toolbar_pattern)
+
+        right_click(drag_area_pattern)
+
+        bookmark_options(new_folder)
+
+        new_folder_window_assert = exists(add_button, 10)
+        assert_true(self, new_folder_window_assert, 'New Folder window is present on the page.')
+
+        paste('moz_bookmark')
+
+        click(add_button)
+
+        moz_bookmark_folder_assert = exists(moz_bookmark, 10)
+        assert_true(self, moz_bookmark_folder_assert, 'Moz Bookmark folder is present on the page.')
+
+        bookmarks_sidebar('open')
+
+        paste('mozilla')
+
+        drag_drop(moz_draggable_pattern, moz_bookmark, 0.5)
+
+        bookmark_drag_assert = exists(dragged_bookmark_pattern, 10)
+        assert_true(self, bookmark_drag_assert, 'Moz Bookmark was dragged successfully inside the bookmark folder.')
+
+        click(Pattern('close_sidebar_search.png'))
+
+        bookmarks_sidebar('close')
+
+        right_click(moz_bookmark)
+
+        bookmark_options(Pattern('copy_option.png'))
+
+        bookmarks_sidebar('open')
+
+        try:
+            wait(bookmarks_sidebar_menu_pattern, 10)
+            logger.debug('Bookmarks sidebar menu is present.')
+            click(bookmarks_sidebar_menu_pattern)
+        except FindError:
+            logger.error('Can\'t find the Bookmarks sidebar menu.')
+            raise FindError
+
+        right_click(bookmarks_sidebar_menu_selected_pattern)
+
+        bookmark_options(Pattern('paste_option.png'))
+
+        pasted_bookmark_folder_assertion = exists(pasted_bookmark_folder, 10)
+        assert_true(self, pasted_bookmark_folder_assertion, 'Moz Bookmark Folder is present into a different directory,'
+                                                            ' copy option works as expected.')
