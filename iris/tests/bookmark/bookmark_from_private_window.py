934e33458ae38ed590eb2b0c5f39c1732f3c85f2
Ionut Budeanu <ibudeanu@mozilla.com>
2018-08-29 14:46:24 +0300

    Added coverage for issue 130

---
+++ iris/tests/bookmark/bookmark_from_private_window.py
@@ -0,0 +1,37 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'Websites can be bookmarked from a private window.'
+        self.test_case_id = '4176'
+        self.test_suite_id = '75'
+
+    def run(self):
+
+        bookmark_button_pattern = LocationBar.BOOKMARK_BUTTON
+        private_browsing_pattern = Pattern('private_browsing.png')
+
+        new_private_window()
+
+        expected_1 = exists(private_browsing_pattern, 10)
+        assert_true(self, expected_1, 'Find private browsing image')
+
+        navigate(LocalWeb.MOZILLA_TEST_SITE)
+
+        try:
+            wait(bookmark_button_pattern, 10)
+            logger.debug('Bookmark star is present on the page.')
+            click(bookmark_button_pattern)
+        except FindError:
+            raise FindError('Bookmark star is not present on the page, aborting.')
+
+        page_bookmarked_assert = exists(Pattern('page_bookmarked.png'), 10)
+        assert_true(self, page_bookmarked_assert, 'The page was successfully bookmarked from a private window.')
