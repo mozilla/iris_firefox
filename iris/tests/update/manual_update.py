d24e0879899c95d43fcb74be63998af76f56a597
AndreiH <ahutusoru@mozilla.com>
2018-09-19 14:19:11 +0300

    Updating tests with Pep8

--- iris/tests/update/manual_update.py
+++ iris/tests/update/manual_update.py
@@ -15,22 +15,21 @@ class Test(BaseTest):
 
     def setup(self):
         BaseTest.setup(self)
-        self.set_profile_pref("app.update.log;true")
+        self.set_profile_pref('app.update.log;true')
         self.maximize_window = False
 
     def run(self):
         update_restart_pattern = Pattern('manual_restart_to_update_button.png')
         firefox_up_to_date_pattern = Pattern('firefox_up_to_date.png')
         current_version = self.app.args.firefox
-        channel = get_firefox_channel(self.app.fx_path)
+        channel = self.app.fx_channel
         rule_dict = get_rule_for_current_channel(channel)
         if rule_dict is None:
-            raise ValueError('No rules found for %s channel' % channel)
+            raise ValueError('No rules found for %s channel. Please update config.ini file.' % channel)
 
         starting_condition = rule_dict['starting_condition']
         watershed_version = rule_dict['watershed_version']
         latest_version = rule_dict['latest_version']
-        print('CHANNEL', get_firefox_channel(self.app.fx_path))
         assert_contains(self, current_version, get_firefox_version(self.app.fx_path),
                         'Firefox version is correct (%s).' % current_version)
 
