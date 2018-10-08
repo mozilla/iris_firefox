fb4f6b71fdc0bec39aca5763b058722ce207e6cf
Matt Wobensmith <mwobensmith@mozilla.com>
2018-10-08 15:18:33 -0700

    Force regeneration of last_fail

--- iris/__main__.py
+++ iris/__main__.py
@@ -363,6 +363,8 @@ class Iris(object):
         path = os.path.join(master_run_directory, 'last_fail.txt')
 
         if len(failures):
+            if os.path.exists(path):
+                os.remove(path)
             last_fail = open(path, 'w')
             for item in failures:
                 for package in self.master_test_list:
