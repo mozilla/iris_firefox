1c71b644f5c7942764daf802d7283ae4ee28b148
tracywalker <twalker@mozilla.com>
2018-10-25 00:46:51 -0500

    Revert developer comment style to use hash

--- iris/api/core/profile.py
+++ iris/api/core/profile.py
@@ -44,7 +44,7 @@ class _IrisProfile(object):
         :param path:
         :return:
         """
-        """Disk location for staged profiles."""
+        # Disk location for staged profiles.
         _staged_profiles = os.path.join(IrisCore.get_module_dir(), 'iris', 'profiles')
 
         sz_bin = find_executable('7z')
@@ -61,24 +61,24 @@ class _IrisProfile(object):
             raise Exception('Unable to unzip profile.')
         logger.debug('7zip succeeded: %s' % repr(output))
 
-        """Find the desired profile."""
+        # Find the desired profile.
         from_directory = os.path.join(_staged_profiles, profile_name)
 
-        """Create a folder to hold that profile's contents."""
+        # Create a folder to hold that profile's contents.
         to_directory = path
         logger.debug('Creating new profile: %s' % to_directory)
 
-        """Duplicate profile."""
+        # Duplicate profile.
         dir_util.copy_tree(from_directory, to_directory)
 
-        """Remove old unzipped directory."""
+        # Remove old unzipped directory.
         try:
             shutil.rmtree(from_directory)
         except WindowsError:
             # This error can happen, but does not affect Iris.
             logger.debug('Error, can\'t remove orphaned directory, leaving in place.')
 
-        """Remove Mac resource fork folders left over from ZIP, if present."""
+        #Remove Mac resource fork folders left over from ZIP, if present.
         resource_fork_folder = os.path.join(_staged_profiles, '__MACOSX')
         if os.path.exists(resource_fork_folder):
             try:
@@ -105,14 +105,14 @@ class _IrisProfile(object):
             os.mkdir(profile_path)
 
         if template is _IrisProfile.BRAND_NEW:
-            """Make new, unique profile."""
+            # Make new, unique profile.
             logger.debug('Creating brand new profile: %s' % profile_path)
         elif template is _IrisProfile.LIKE_NEW:
-            """Open a staged profile that is nearly new, but with some first-run preferences altered."""
+            # Open a staged profile that is nearly new, but with some first-run preferences altered.
             logger.debug('Creating new profile from LIKE_NEW staged profile.')
             self._get_staged_profile(template, profile_path)
         elif template is _IrisProfile.TEN_BOOKMARKS:
-            """Open a staged profile that already has ten bookmarks."""
+            # Open a staged profile that already has ten bookmarks.
             logger.debug('Creating new profile from TEN_BOOKMARKS staged profile.')
             self._get_staged_profile(template, profile_path)
         else:
