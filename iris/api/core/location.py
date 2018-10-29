a2613b332ed1fd2c2f2c068e371e53a88de12abd
SilviuFlorin <silviu19sr@yahoo.com>
2018-10-29 11:15:04 +0200

    code cleanup for location module

--- iris/api/core/location.py
+++ iris/api/core/location.py
@@ -2,48 +2,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from util.core_helper import INVALID_NUMERIC_INPUT
-
 
 class Location(object):
     """Class handle single points on the screen directly by its position (x, y). It is mainly used in the actions on a
     region, to directly denote the click point. It contains methods, to move a point around on the screen."""
 
     def __init__(self, x=0, y=0):
-        """Function assign values to the location parameters x and y."""
-        self._x = x
-        self._y = y
+        self.x = x
+        self.y = y
 
-    @property
-    def x(self):
-        """Getter for the location x property."""
-        return self._x
+    def __str__(self):
+        return '(%s, %s)' % (self.x, self.y)
 
-    @x.setter
-    def x(self, x):
-        """Setter for the location x property."""
-        if isinstance(x, int):
-            self._x = x
-        else:
-            raise ValueError(INVALID_NUMERIC_INPUT)
-
-    @property
-    def y(self):
-        """Getter for the location y property."""
-        return self._y
-
-    @y.setter
-    def y(self, y):
-        """Setter for the location y property."""
-        if isinstance(y, int):
-            self._y = y
-        else:
-            raise ValueError(INVALID_NUMERIC_INPUT)
-
-    def set_location(self, x=0, y=0):
-        """Set the location to the specified coordinates."""
-        self._x = x
-        self._y = y
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__, self.x, self.y)
 
     def offset(self, away_x, away_y):
         """Return a location object which is away_x and away_y pixels away horizontally and vertically from the current
@@ -53,9 +25,9 @@ class Location(object):
         :param away_y: Offset added to location y parameter.
         :return: Location object.
         """
-        new_x = int(self._x + away_x)
-        new_y = int(self._y + away_y)
-        return Location(new_x, new_y)
+        self.x += away_x
+        self.y += away_y
+        return self
 
     def above(self, away_y):
         """Return a location object which is away_y pixels vertically above the current location.
@@ -63,8 +35,8 @@ class Location(object):
         :param away_y: Offset decreased from the location y parameter.
         :return: Location object.
         """
-        new_y = int(self._y - away_y)
-        return Location(self._x, new_y)
+        self.y -= away_y
+        return self
 
     def below(self, away_y):
         """Return a location object which is away_y pixels vertically below the current location.
@@ -72,8 +44,8 @@ class Location(object):
         :param away_y: Offset added to location y parameter.
         :return: Location object.
         """
-        new_y = int(self._y + away_y)
-        return Location(new_y)
+        self.y += away_y
+        return self
 
     def left(self, away_x):
         """Return a location object which is away_x pixels horizontally to the left of the current location.
@@ -81,8 +53,8 @@ class Location(object):
         :param away_x: Offset decreased from the location x parameter.
         :return: Location object.
         """
-        new_x = int(self._x - away_x)
-        return Location(new_x, self.y)
+        self.x -= away_x
+        return self
 
     def right(self, away_x):
         """Return a location object which is away_x pixels horizontally to the right of the current location.
@@ -90,5 +62,5 @@ class Location(object):
         :param away_x: Offset added to location x parameter.
         :return: Location object.
         """
-        new_x = int(self._x + away_x)
-        return Location(new_x, self.y)
+        self.x += away_x
+        return self
