82befec2da6f639ae56913fa992bcab72d0abdfa
tracywalker <twalker@mozilla.com>
2018-11-01 16:16:27 -0500

    Correct punctuation

--- iris/tests/history/history_menu_recent_history.py
+++ iris/tests/history/history_menu_recent_history.py
@@ -10,7 +10,7 @@ class Test(BaseTest):
 
     def __init__(self, app):
         BaseTest.__init__(self, app)
-        self.meta = 'Page visits show in Recent history'
+        self.meta = 'Page visits show in Recent history.'
         self.test_case_id = '1385083'
         self.test_suite_id = '908'
 
