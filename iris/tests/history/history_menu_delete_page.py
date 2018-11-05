b1ef4150af2d1d38b0c779de5ddc4d0b604d3835
alinrus <arus@mozilla.com>
2018-11-05 17:34:04 +0200

    Add coverage for issue_903

---
+++ iris/tests/history/history_menu_delete_page.py
@@ -0,0 +1,62 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self, app):
+        BaseTest.__init__(self, app)
+        self.meta = 'Delete a page from the Library - History menu.'
+        self.test_case_id = '174048'
+        self.test_suite_id = '903'
+        self.blocked_by = 'issue 1410'
+        self.locales = ['en-US']
+        self.exclude = Platform.ALL
+
+    def setup(self):
+        """Test case setup
+
+        Override the setup method to use a pre-canned bookmarks profile.
+        """
+        BaseTest.setup(self)
+        self.profile = Profile.TEN_BOOKMARKS
+        return
+
+    def run(self):
+        show_all_history = LibraryMenu.HistoryOption.SHOW_ALL_HISTORY
+        iris_bookmark = Pattern('iris_bookmark.png')
+        iris_bookmark_focus = Pattern('iris_bookmark_focus.png')
+
+        # Open History and check if is populated with Iris page.
+        open_library_menu('History')
+        right_upper_corner = Region(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        expected_1 = right_upper_corner.exists(iris_bookmark, 10)
+        assert_true(self, expected_1, 'Iris page is displayed in the History menu list successfully.')
+
+        click(show_all_history)
+
+        expected_2 = exists(iris_bookmark_focus, 10)
+        assert_true(self, expected_2, 'Iris page is displayed Recent History list successfully.')
+
+        # Delete Iris page.
+        right_click(iris_bookmark_focus)
+        type(text='d')
+
+        try:
+            expected_3 = wait_vanish(iris_bookmark_focus, 10)
+            assert_true(self, expected_3, 'Iris page was deleted successfully from the history.')
+        except FindError:
+            raise FindError('Iris page is still displayed in the history.')
+
+        click_auxiliary_window_control('close')
+
+        # Check that Mozilla page is not displayed in the Recent History list.
+        open_library_menu('History')
+        expected_4 = exists(iris_bookmark, 5)
+        assert_false(self, expected_4, 'Iris page is not displayed in the History menu list.')
+
+        type(Key.ESC)
