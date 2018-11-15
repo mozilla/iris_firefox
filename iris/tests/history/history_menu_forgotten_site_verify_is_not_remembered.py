fc397e86255eda4b74266f9d74ce65a656538c9a
icrisan <icrisan@mozilla.com>
2018-11-15 17:38:46 +0200

    Update test to avoid false pass results for the last but one assert

--- iris/tests/history/history_menu_forgotten_site_verify_is_not_remembered.py
+++ iris/tests/history/history_menu_forgotten_site_verify_is_not_remembered.py
@@ -10,7 +10,7 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = 'Verify if the deleted page from History is not retained in auto-complete.'
+        self.meta = 'Verify if the deleted page from History is not retained by auto-complete.'
         self.test_case_id = '174051'
         self.test_suite_id = '2000'
         self.locales = ['en-US']
@@ -19,7 +19,7 @@ class Test(BaseTest):
         show_all_history = LibraryMenu.HistoryOption.SHOW_ALL_HISTORY
         mozilla_bookmark_focus = Pattern('mozilla_bookmark_focus.png')
         mozilla_autocomplete = Pattern('mozilla_autocomplete.png')
-        iris_bookmark_pattern = Pattern('iris_bookmark.png')
+        recent_history_mozilla_pattern = Pattern('recent_history_mozilla.png')
 
         # Visit a page at least two times to make sure that auto-fill is working in the URL bar.
         new_tab()
@@ -35,23 +35,23 @@ class Test(BaseTest):
         close_tab()
         new_tab()
 
-        # Navigate to history and Forget Mozilla page.
+        # Navigate to history and forget the Mozilla page.
         open_library_menu('History')
 
         right_upper_corner = Region(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        expected = right_upper_corner.exists(iris_bookmark_pattern, 10)
-        assert_true(self, expected, 'Iris page is displayed in the History menu list.')
+        expected_2 = right_upper_corner.exists(recent_history_mozilla_pattern, 10)
+        assert_true(self, expected_2, 'Mozilla page is displayed in the Recent History list.')
 
         click(show_all_history)
-        expected_2 = exists(mozilla_bookmark_focus, 10)
-        assert_true(self, expected_2, 'Mozilla page is displayed in the History list successfully.')
+        expected_3 = exists(mozilla_bookmark_focus, 10)
+        assert_true(self, expected_3, 'Mozilla page is displayed in the History list successfully.')
 
         right_click(mozilla_bookmark_focus)
         type(text='f')
 
         try:
-            expected_3 = wait_vanish(mozilla_bookmark_focus, 10)
-            assert_true(self, expected_3, 'Mozilla page was deleted successfully from the Library.')
+            expected_4 = wait_vanish(mozilla_bookmark_focus, 10)
+            assert_true(self, expected_4, 'Mozilla page was deleted successfully from the Library.')
         except FindError:
             raise FindError('Mozilla page is still displayed in the Library.')
 
@@ -59,12 +59,12 @@ class Test(BaseTest):
 
         # Check that Mozilla page is not displayed in the Recent History list.
         open_library_menu('History')
-        expected_4 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL.similar(0.9), 5)
-        assert_false(self, expected_4, 'Mozilla page is not displayed in the Recent History list.')
+        expected_5 = exists(recent_history_mozilla_pattern, 5)
+        assert_false(self, expected_5, 'Mozilla page is not displayed in the Recent History list.')
 
         # Check that the forgotten page is not auto-completed in the URL bar.
         select_location_bar()
         paste('http://127.0.0.1:2000/m')
 
-        expected_5 = exists(mozilla_autocomplete.similar(0.9), 10)
-        assert_false(self, expected_5, 'Mozilla page is not auto-completed in URL bar.')
+        expected_6 = exists(mozilla_autocomplete.similar(0.9), 10)
+        assert_false(self, expected_6, 'Mozilla page is not auto-completed in the URL bar.')
