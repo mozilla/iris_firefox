9b6ab6badf126c156caa54086d155a86bdbdd34c
alinrus <arus@mozilla.com>
2018-11-15 09:56:29 +0200

    Add coverage for issue_906

---
+++ iris/tests/history/history_menu_forgotten_site_verify_is_not_remembered.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = 'Verify if the deleted page from History is not retained in auto-complete.'
+        self.test_case_id = '174051'
+        self.test_suite_id = '2000'
+        self.locales = ['en-US']
+
+    def run(self):
+        show_all_history = LibraryMenu.HistoryOption.SHOW_ALL_HISTORY
+        mozilla_bookmark_focus = Pattern('mozilla_bookmark_focus.png')
+        mozilla_autocomplete = Pattern('mozilla_autocomplete.png')
+        iris_bookmark_pattern = Pattern('iris_bookmark.png')
+
+        # Visit a page at least two times to make sure that auto-fill is working in the URL bar.
+        new_tab()
+        navigate(LocalWeb.MOZILLA_TEST_SITE)
+        navigate(LocalWeb.MOZILLA_TEST_SITE)
+
+        select_location_bar()
+        paste('http://127.0.0.1:2000/m')
+
+        expected_1 = exists(mozilla_autocomplete, 10)
+        assert_true(self, expected_1, 'Mozilla page is auto-completed successfully.')
+
+        close_tab()
+        new_tab()
+
+        # Navigate to history and Forget Mozilla page.
+        open_library_menu('History')
+
+        right_upper_corner = Region(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        expected = right_upper_corner.exists(iris_bookmark_pattern, 10)
+        assert_true(self, expected, 'Iris page is displayed in the History menu list.')
+
+        click(show_all_history)
+        expected_2 = exists(mozilla_bookmark_focus, 10)
+        assert_true(self, expected_2, 'Mozilla page is displayed in the History list successfully.')
+
+        right_click(mozilla_bookmark_focus)
+        type(text='f')
+
+        try:
+            expected_3 = wait_vanish(mozilla_bookmark_focus, 10)
+            assert_true(self, expected_3, 'Mozilla page was deleted successfully from the Library.')
+        except FindError:
+            raise FindError('Mozilla page is still displayed in the Library.')
+
+        click_auxiliary_window_control('close')
+
+        # Check that Mozilla page is not displayed in the Recent History list.
+        open_library_menu('History')
+        expected_4 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL.similar(0.9), 5)
+        assert_false(self, expected_4, 'Mozilla page is not displayed in the Recent History list.')
+
+        # Check that the forgotten page is not auto-completed in the URL bar.
+        select_location_bar()
+        paste('http://127.0.0.1:2000/m')
+
+        expected_5 = exists(mozilla_autocomplete.similar(0.9), 10)
+        assert_false(self, expected_5, 'Mozilla page is not auto-completed in URL bar.')
