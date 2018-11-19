6b41a59a04e606e40ac6b43ef7ec327cb8ef2360
Dmitry Bakaev <bakaev.dmitriy@gmail.com>
2018-11-19 19:33:24 +0200

    Fixed code due to Tracy feedback

--- iris/tests/find_toolbar/navigate_items.py
+++ iris/tests/find_toolbar/navigate_items.py
@@ -16,72 +16,22 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        """
-        Navigate through found items
-
-        STEP 1:
-            DESCRIPTION:
-                Open Firefox and navigate to a popular website (Ebay, Reddit, etc).
-
-            EXPECTED:
-                The page is successfully loaded.
-
-        STEP 2:
-            DESCRIPTION:
-                Open the Find toolbar.
-
-            EXPECTED:
-                Find Toolbar is opened.
-
-        STEP 3:
-            DESCRIPTION:
-                Search for a term that appears more than once in the page.
-
-            EXPECTED:
-                All the matching words/characters are found. The first one has a green background highlighted, and the others are not highlighted.
-
-        STEP 4:
-            DESCRIPTION:
-                Navigate forward and back throught found items using the toolbar arrows.
-
-            EXPECTED:
-                Navigation works fine. The current item is highlighted with green. No functional or visible issue appears.
-
-        STEP 5:
-            DESCRIPTION:
-                Inspect if the number of the current highlighted item from the Find Toolbar changes when navigating. (e.g 1 of 12 matches > 2 of 12 matches)
-
-            EXPECTED:
-                The number of the highlighted item changes when navigating.
-
-
-        NOTES:
-            Initial version - Dmitry Bakaev  - 19-Nov-2018
-            Code review complete - -
-        """
-
         soap_label_pattern = Pattern('soap_label.png')
         see_label_pattern = Pattern('see_label.png')
-        see_label_unhighlited_pattern = Pattern('see_label_unhighlited.png')
+        see_label_not_highlighted_pattern = Pattern('see_label_unhighlited.png')
         find_in_page_icon_pattern = Pattern('find_in_page_icon.png')
-
         arrow_up_button_pattern = Pattern('arrow_up_button.png')
         arrow_down_button_pattern = Pattern('arrow_down_button.png')
         cleaning_see_label_pattern = Pattern('cleaning_see_label.png')
         one_of_five_label_pattern = Pattern('1_of_5_matches_label.png')
-        two_of_five_lable_pattern = Pattern('2_of_5_matches_label.png')
+        two_of_five_label_pattern = Pattern('2_of_5_matches_label.png')
 
-        """ STEP 1 """
-
-        wikipedia_page_local = self.get_asset_path('soap.html')
-        navigate(wikipedia_page_local)
+        wiki_page_local = self.get_asset_path('soap.html')
+        navigate(wiki_page_local)
         soap_label_exists = exists(soap_label_pattern, 20)
         assert_true(self, soap_label_exists, 'The page is successfully loaded.')
 
-        """ END STEP 1 """
-
-        """ STEP 2 """
-
+        # open and clear find toolbar
         open_find()
         edit_select_all()
         edit_delete()
@@ -90,51 +40,32 @@ class Test(BaseTest):
 
         assert_true(self, find_toolbar_opened, 'Find Toolbar is opened.')
 
-        """ END STEP 2 """
-
-        """ STEP 3 """
-
         type('see', interval=1)
         type(Key.ENTER)
 
-        selected_label_exists = exists(see_label_pattern, 5)
-        unhighlighted_label_exists = exists(see_label_unhighlited_pattern, 5)
+        selected_label_exists = exists(see_label_pattern, 1)
+        not_highlighted_label_exists = exists(see_label_not_highlighted_pattern, 1)
 
         assert_true(self, selected_label_exists, 'The first one has a green background highlighted.')
-        assert_true(self, unhighlighted_label_exists,
+        assert_true(self, not_highlighted_label_exists,
                     'The others are not highlighted.')
 
-        """ END STEP 3 """
-
-        """ STEP 4 """
-
         click(arrow_up_button_pattern)
-        cleaning_see_label_exists = exists(cleaning_see_label_pattern, 5)
+        cleaning_see_label_exists = exists(cleaning_see_label_pattern, 1)
 
         assert_true(self, cleaning_see_label_exists, 'Navigation up works fine.')
 
         click(arrow_down_button_pattern)
-        selected_label_exists = exists(see_label_pattern, 5)
+        selected_label_exists = exists(see_label_pattern, 1)
 
         assert_true(self, selected_label_exists, 'Navigation down works fine.')
 
-
-
-        """ END STEP 4 """
-
-        """ STEP 5 """
-
         click(arrow_up_button_pattern)
-        one_of_five_label_exists = exists(one_of_five_label_pattern, 5)
+        one_of_five_label_exists = exists(one_of_five_label_pattern, 1)
 
         assert_true(self, one_of_five_label_exists, 'The number of the highlighted item changes when navigating')
 
         click(arrow_down_button_pattern)
-        two_of_five_label_exists = exists(two_of_five_lable_pattern, 5)
+        two_of_five_label_exists = exists(two_of_five_label_pattern, 1)
 
         assert_true(self, two_of_five_label_exists, 'The number of the highlighted item changes when navigating.')
-
-
-
-        """ END STEP 5 """
-
