8fa51a9e057252ef3e6b0cf9cbec42c600eeb390
Dmitry Bakaev <bakaev.dmitriy@gmail.com>
2018-11-20 13:03:26 +0200

    Initial commit of C127250 for OSX

---
+++ iris/tests/find_toolbar/search_button.py
@@ -0,0 +1,68 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = 'Search for text that appears on buttons'
+        self.test_case_id = '127250'
+        self.test_suite_id = '2085'
+        self.locales = ['en-US']
+
+    def run(self):
+
+        find_in_page_icon_pattern = Pattern('find_in_page_icon.png')
+        google_search_button_pattern = Pattern('google_search_button.png')
+        season_label_not_selected_pattern = Pattern('season_label_not_selected.png')
+        season_label_selected_pattern = Pattern('season_label_selected.png')
+        settings_label_not_selected_pattern = Pattern('settings_label_not_selected.png')
+        settings_label_selected = Pattern('settings_label_selected.png')
+
+        test_page_local = self.get_asset_path('google.htm')
+        navigate(test_page_local)
+
+        soap_label_exists = exists(google_search_button_pattern, 20)
+
+        assert_true(self, soap_label_exists, 'The page is successfully loaded.')
+
+        open_find()
+        edit_select_all()
+        edit_delete()
+
+        find_toolbar_opened = exists(find_in_page_icon_pattern, 10)
+
+        assert_true(self, find_toolbar_opened, 'Find Toolbar is opened.')
+
+        type('se', interval=1)
+
+        selected_label_exists = exists(season_label_selected_pattern, 1)
+        label_not_found = exists(settings_label_not_selected_pattern, 1)
+        button_not_selected = exists(google_search_button_pattern, 1)
+
+        assert_true(self, selected_label_exists, 'The first one has a green background highlighted')
+        assert_true(self, label_not_found, 'the others are not highlighted.')
+        assert_true(self, button_not_selected, 'Submit button is not highlighted.')
+
+        find_next()
+        settings_selected = exists(settings_label_selected, 1)
+        season_not_selected = exists(season_label_not_selected_pattern, 1)
+        button_not_selected = exists(google_search_button_pattern, 1)
+
+        assert_true(self, settings_selected, 'The first one has a green background highlighted')
+        assert_true(self, season_not_selected, 'the others are not highlighted.')
+        assert_true(self, button_not_selected, 'Submit button is not highlighted.')
+
+        find_previous()
+
+        season_selected = exists(season_label_selected_pattern, 1)
+        settings_not_selected = exists(settings_label_not_selected_pattern, 1)
+        button_not_selected = exists(google_search_button_pattern, 1)
+        assert_true(self, season_selected, 'The first one has a green background highlighted')
+        assert_true(self, settings_not_selected, 'the others are not highlighted.')
+        assert_true(self, button_not_selected, 'Submit button is not highlighted.')
