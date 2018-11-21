e1c230a398db5b17a355eab6bf741645bb64f62f
Dmitry Bakaev <bakaev.dmitriy@gmail.com>
2018-11-21 18:28:29 +0200

    Changed assets

--- iris/tests/find_toolbar/whole_words.py
+++ iris/tests/find_toolbar/whole_words.py
@@ -18,16 +18,16 @@ class Test(BaseTest):
     def run(self):
 
         find_in_page_icon_pattern = Pattern('find_in_page_icon.png')
-        soap_label_pattern = Pattern('soap_label.png')
+        tester_label_pattern = Pattern('tester_label.png')
         whole_words_button_pattern = Pattern('whole_words_button.png')
-        services_in_label_pattern = Pattern('services_in_label.png')
-        information_in_label_pattern = Pattern('information_in_label.png')
-        information_label_pattern = Pattern('information_label.png')
+        test_selected_label_pattern = Pattern('test_selected_label.png')
+        other_label_is_not_highlighted = Pattern('testex_label.png')
+        second_selected_label_pattern = Pattern('text_label_selected_second.png')
 
-        test_page_local = self.get_asset_path('wiki_soap.html')
+        test_page_local = self.get_asset_path('words.html')
         navigate(test_page_local)
 
-        soap_label_exists = exists(soap_label_pattern, 20)
+        soap_label_exists = exists(tester_label_pattern, 20)
 
         assert_true(self, soap_label_exists, 'The page is successfully loaded.')
 
@@ -39,10 +39,10 @@ class Test(BaseTest):
 
         assert_true(self, find_toolbar_opened, 'Find Toolbar is opened.')
 
-        type('in', interval=1)
+        type('test', interval=1)
         click(whole_words_button_pattern)
 
-        selected_label_exists = exists(information_in_label_pattern, 5)
+        selected_label_exists = exists(test_selected_label_pattern, 5)
 
         assert_true(self, selected_label_exists,
                     'All the matching words/characters are found. The first one has a green background highlighted, '
@@ -50,8 +50,8 @@ class Test(BaseTest):
 
         type(Key.F3, interval=1)
 
-        first_label_is_green = exists(services_in_label_pattern, 5)
-        other_label_is_unhighlighted = exists(information_label_pattern, 5)
+        first_label_is_green = exists(second_selected_label_pattern, 5)
+        other_label_is_not_highlighted = exists(other_label_is_not_highlighted, 5)
 
         assert_true(self, first_label_is_green, 'The next matching words/characters have a green background highlighted')
-        assert_true(self, other_label_is_unhighlighted, 'The other is not highlighted')
+        assert_true(self, other_label_is_not_highlighted, 'The other is not highlighted')
