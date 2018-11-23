d94c30ee68750c3751feb18539a673cd79736bf2
Ciapa Pavel <zpn333@gmail.com>
2018-11-23 01:45:52 -0800

    Added win 7 support fixed duplicate files

--- iris/tests/find_toolbar/search_high_contrast.py
+++ iris/tests/find_toolbar/search_high_contrast.py
@@ -16,58 +16,52 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        '''
-        "1. Windows: Go to Personalise> Themes> Theme Settings and activate a High Contrast Theme.
-         Ubuntu: Install ""gnome-control-center""- System settings-Appearance
-
-         Expected Result:
-
-         - The theme is activated.
-
-         2. Open Firefox and navigate to a popular website (Google.com, Amazon.com, NYTimes etc).
-
-         Expected Result:
-
-         - The page is successfully loaded.
-
-         3. Open the Find Toolbar.
-
-         Expected Result:
-
-         - Find Toolbar is opened.
-
-         4. Search for a term that appears more than once in the page.
-
-         Expected Result:
-
-         - All the matching words/characters are found. The first one has a green background highlighted, and the others are not highlighted.
-
-         5. Zoom the page in/out and check the highlighted items.
-
-         Expected Result:
-
-         - The highlight of the found items doesn't affect the visibility of other words/letters. No misplacement of the highlight is visible."
-        '''
 
         find_in_page_bar_contrast_pattern = Pattern('find_in_page_bar_contrast.png')
+        find_in_page_bar_contrast_pattern.similarity = 0.6
 
-        soap_page_url_contrast_pattern = Pattern('soap_page_url_contrast.png')
+        soap_page_loaded_contrast_pattern = Pattern('soap_page_loaded_contrast.png')
         see_label_contrast_pattern = Pattern('see_label_contrast.png')
         see_label_unhighlited_contrast_pattern = Pattern('see_label_unhighlited_contrast.png')
         see_label_zoom_in_contrast_pattern = Pattern('see_label_zoom_in_contrast.png')
         see_label_zoom_out_contrast_pattern = Pattern('see_label_zoom_out_contrast.png')
 
-        # windows theme settings
-        win_type_here_to_search_pattern = Pattern('win_type_here_to_search.png')
-        win_high_contrast_settings_pattern = Pattern('win_high_contrast_settings.png')
-        win_off_high_contrast_button_theme_pattern = Pattern('win_off_high_contrast_theme.png')
-        win_on_high_contrast_theme_pattern = Pattern('high_contrast_is_on.png') # high contrast is on
-        win_close_active_window_pattern = Pattern('win_close_active_window.png')
+        # win theme settings
+        close_active_contrast_window_pattern = Pattern('close_active_contrast_window.png')
+
+        win_off_high_contrast_button_theme_pattern = Pattern('win_off_high_contrast_theme.png') # 10
+
 
         if Settings.is_windows():
             if get_os_version() == 'win7':
-                pass
-            else:
+
+                # time.sleep(5)
+                type(Key.WIN)
+                type('cmd')
+                type(Key.ENTER)
+                type(Key.ENTER)
+                type(r'start "" "c:\windows\resources\ease of access themes\hcblack.theme"')
+                type(Key.ENTER)
+
+                time.sleep(5) # wait until contrast screen appears
+                close_active_contrast_window_exists = exists(close_active_contrast_window_pattern, 5)
+                assert_true(self, close_active_contrast_window_exists,
+                            'Windows theme changed to hcblack.theme')
+
+                # close theme window and console window
+                click(close_active_contrast_window_pattern, 1)
+                time.sleep(5)
+                type('exit')
+                type(Key.ENTER)
+                # Active window now Firefox
+
+            else:  # win 10
+                # windows 10 theme settings
+                win_type_here_to_search_pattern = Pattern('win_type_here_to_search.png')
+                win_high_contrast_settings_pattern = Pattern('win_high_contrast_settings.png')
+                win_on_high_contrast_theme_pattern = Pattern('high_contrast_is_on.png')  # high contrast is on
+
+
                 win_type_here_to_search_exists = exists(win_type_here_to_search_pattern, 10)
                 type_here_theme_options_location = find(win_type_here_to_search_pattern)
                 click(Location(type_here_theme_options_location.x+5,
@@ -91,70 +85,112 @@ class Test(BaseTest):
                 high_contrast_enabled = exists(win_on_high_contrast_theme_pattern, 5)
                 assert_true(self, high_contrast_enabled, 'High contrast option enabled on Windows 10')
 
-                win_close_active_window_location = find(win_close_active_window_pattern)
-                click(win_close_active_window_pattern, 1)
+                win_close_active_window_location = find(close_active_contrast_window_pattern)
+                click(close_active_contrast_window_pattern, 1)
 
-        test_page_local = self.get_asset_path('wiki_soap.html')
-        navigate(test_page_local)
+        # test body
 
-        soap_page_loaded_exists = exists(soap_page_url_contrast_pattern, 20)
+            test_page_local = self.get_asset_path('wiki_soap.html')
+            navigate(test_page_local)
 
-        assert_true(self, soap_page_loaded_exists, 'The page is successfully loaded.')
+            soap_page_loaded_exists = exists(soap_page_loaded_contrast_pattern, 20)
 
-        # delay for win 10 to prevent select all content
-        time.sleep(Settings.SYSTEM_DELAY)
+            assert_true(self, soap_page_loaded_exists, 'The page is successfully loaded.')
 
-        open_find()
-        edit_select_all()
-        edit_delete()
+            # delay for WIN 10 to prevent select all content
+            time.sleep(Settings.SYSTEM_DELAY)
 
-        find_toolbar_opened = exists(find_in_page_bar_contrast_pattern, 10)
+            open_find()
+            edit_select_all()
+            edit_delete()
 
-        assert_true(self, find_toolbar_opened, 'Find Toolbar is opened.')
+            if get_os_version() == 'win7':
+                # somehow win7 doesnt recognize Find bar input box pattern, using match case button instead
+                find_toolbar_match_case_contrast_opened = exists(find_in_page_bar_contrast_pattern, 5)
+                assert_true(self, find_toolbar_match_case_contrast_opened, 'Find Toolbar is opened.')
+            else:
+                # WIN 10 finds another pattern
+                find_toolbar_opened = exists(find_in_page_bar_contrast_pattern, 10)
+                assert_true(self, find_toolbar_opened, 'Find Toolbar opened')
 
-        type('see', interval=1)
-        type(Key.ENTER)
+            type('see', interval=1)
+            type(Key.ENTER)
 
-        selected_label_exists = exists(see_label_contrast_pattern, 5)
-        unhighlighted_label_exists = exists(see_label_unhighlited_contrast_pattern, 5)
+            selected_label_exists = exists(see_label_contrast_pattern, 5)
+            unhighlighted_label_exists = exists(see_label_unhighlited_contrast_pattern, 5)
 
-        assert_true(self, selected_label_exists, 'The first one has a green background highlighted.')
-        assert_true(self, unhighlighted_label_exists,
-                    'The others are not highlighted.')
+            assert_true(self, selected_label_exists, 'The first one has a green background highlighted.')
+            assert_true(self, unhighlighted_label_exists,
+                        'The others are not highlighted.')
 
-        zoom_in()
+            zoom_in()
 
-        selected_label_exists = exists(see_label_zoom_in_contrast_pattern, 5)
+            selected_label_exists = exists(see_label_zoom_in_contrast_pattern, 5)
 
-        assert_true(self, selected_label_exists,
-                    'Zoom in: The highlight of the found items does not affect the visibility of other words/letters')
+            assert_true(self, selected_label_exists,
+                        'Zoom in: The highlight of the found items does not affect the visibility of other words/letters')
 
-        zoom_out()
-        zoom_out()
+            zoom_out()
+            zoom_out()
 
-        selected_label_exists = exists(see_label_zoom_out_contrast_pattern, 5)
+            selected_label_exists = exists(see_label_zoom_out_contrast_pattern, 5)
 
-        assert_true(self, selected_label_exists,
-                    'Zoom out: The highlight of the found items does not affect the visibility of other words/letters')
+            assert_true(self, selected_label_exists,
+                        'Zoom out: The highlight of the found items does not affect the visibility of other words/letters')
 
-        # quit of high contrast mode
+            # Return back to normal mode,
+            # and close themes settings, and console window
 
-        click(Location(type_here_theme_options_location.x + 5,
-                       type_here_theme_options_location.y + 5), 1)
+            if Settings.is_windows():
+                if get_os_version() == 'win7':
+                    # windows 7 theme settings
 
-        paste('themes and related settings')
-        type(Key.ENTER)
+                    close_window_contrast_deactivated_pattern = Pattern('close_window_contrast_deactivated.png')
 
-        click(Location(win_high_contrast_settings_location.x + 10,
-                       win_high_contrast_settings_location.y + 10), 1)
 
-        click(Location(win_off_high_contrast_theme_location.x + 7,
-                       win_off_high_contrast_theme_location.y + 7), 1)
+                    type(Key.WIN)
+                    type('cmd')
+                    type(Key.ENTER)
+                    type(Key.ENTER)
+                    type(r'start "" "c:\windows\resources\ease of access themes\basic.theme"')
+                    type(Key.ENTER)
+                    time.sleep(5)
 
-        contrast_mode_off = exists(win_off_high_contrast_button_theme_pattern, 10)
+                    # wait until contrast screen appears
+                    close_active_contrast_deactivated_exists = exists(close_window_contrast_deactivated_pattern, 5)
+                    contrast_theme_deactivated = exists(win_off_high_contrast_button_theme_pattern, 5)
 
-        # close theme settings window
-        click(Location(win_close_active_window_location.x+2, win_close_active_window_location.y+2), 1)
+                    assert_true(self, close_active_contrast_deactivated_exists,
+                                'Windows theme changed to basic.theme')
 
-        assert_true(self, contrast_mode_off,
-                    'Exited contrast mode')
+                    # close theme window and console window
+                    click(close_window_contrast_deactivated_pattern)
+                    time.sleep(1)
+                    type('exit')
+                    type(Key.ENTER)
+                    # Active window now Firefox
+
+                    assert_true(self, contrast_theme_deactivated, 'High contrast mode deactivated')
+
+                else: # deactivate high contrast mode on WIN 10
+
+                    click(Location(type_here_theme_options_location.x + 5,
+                                   type_here_theme_options_location.y + 5), 1)
+
+                    paste('themes and related settings')
+                    type(Key.ENTER)
+
+                    click(Location(win_high_contrast_settings_location.x + 10,
+                                   win_high_contrast_settings_location.y + 10), 1)
+
+                    click(Location(win_off_high_contrast_theme_location.x + 7,
+                                   win_off_high_contrast_theme_location.y + 7), 1)
+
+                    contrast_mode_off = exists(win_off_high_contrast_button_theme_pattern, 10)
+
+                    # close theme settings window
+                    click(Location(win_close_active_window_location.x+2,
+                                   win_close_active_window_location.y+2), 1)
+
+                    assert_true(self, contrast_mode_off,
+                                'High contrast mode deactivated')
