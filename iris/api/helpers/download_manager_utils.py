2c9f7b005c1bce1f7e510bda3b99bec3ef8b7601
icrisan <icrisan@mozilla.com>
2018-12-03 18:26:35 +0200

    Add coverage for issue 1540.

---
+++ iris/api/helpers/download_manager_utils.py
@@ -0,0 +1,58 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from iris.api.core.errors import FindError, APIHelperError
+from iris.api.core.mouse import click
+from iris.api.core.region import wait, exists, Pattern, logger
+from iris.api.helpers.keyboard_shortcuts import scroll_down
+from iris.asserts import assert_true
+
+
+class DownloadFiles(object):
+    EXTRA_SMALL_FILE_5MB = Pattern('5MB.png')
+    SMALL_FILE_10MB = Pattern('10MB.png').similar(0.95)
+    SMALL_FILE_20MB = Pattern('20MB.png').similar(0.95)
+    MEDIUM_FILE_50MB = Pattern('50MB.png')
+    MEDIUM_FILE_100MB = Pattern('100MB.png')
+    LIBRARY_DOWNLOADS_5MB = Pattern('5MB_library_downloads.png')
+    LIBRARY_DOWNLOADS_10MB = Pattern('10MB_library_downloads.png')
+    LIBRARY_DOWNLOADS_20MB = Pattern('20MB_library_downloads.png')
+    LIBRARY_DOWNLOADS_50MB = Pattern('50MB_library_downloads.png')
+    LIBRARY_DOWNLOADS_100MB = Pattern('100MB_library_downloads.png')
+    ABOUT = Pattern('about.png')
+    SAVE_FILE = Pattern('save_file.png')
+    OK = Pattern('ok.png')
+
+
+def download_file(self, file_to_download, accept_download, msg):
+    """
+    :param self: Test object used to add test step resolution.
+    :param file_to_download: File to be downloaded.
+    :param accept_download: Accept download pattern.
+    :param msg: Text message.
+    :return: None.
+    """
+    expected = exists(file_to_download, 5)
+    while not expected:
+        scroll_down()
+        expected = exists(file_to_download, 5)
+        if exists(DownloadFiles.ABOUT, 1):
+            logger.error('Reaching the bottom of the file')
+            break
+    assert_true(self, expected, 'The %s button has been found.' % msg)
+    click(file_to_download)
+
+    try:
+        wait(DownloadFiles.SAVE_FILE, 5)
+        logger.debug('The \'Save file\' option is present in the page.')
+        click(DownloadFiles.SAVE_FILE)
+    except FindError:
+        raise APIHelperError('The \'Save file\' option is not present in the page, aborting.')
+
+    try:
+        expected = wait(accept_download, 5)
+        assert_true(self, expected, 'The OK button found in the page.')
+    except FindError:
+        raise APIHelperError('The OK button is not found in the page.')
+    click(accept_download)
