2b33e7167973ab428bbe45bdf481f9bbb345052f
Paul Prokhorov <pauprokhorov@mozilla.com>
2018-12-12 15:03:49 +0200

    Tests refactoring

--- iris/tests/find_toolbar/check_highlight.py
+++ iris/tests/find_toolbar/check_highlight.py
@@ -16,12 +16,12 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        soap_label_pattern = Pattern('soap_label.png')
-        see_label_pattern = Pattern('see_label.png')
-        see_label_unhighlighted_pattern = Pattern('see_label_unhighlited.png')
+        soap_label_pattern = LocalWeb.SOAP_WIKI_SOAP_LABEL
+        see_label_pattern = LocalWeb.SOAP_WIKI_SEE_LABEL
+        see_label_unhighlighted_pattern = LocalWeb.SOAP_WIKI_SEE_LABEL_UNHIGHLITED
 
         # Open Firefox and navigate to a popular website
-        navigate(LocalWeb.WIKI_TEST_SITE)
+        navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
         soap_label_exists = exists(soap_label_pattern, 20)
         assert_true(self, soap_label_exists, 'The page is successfully loaded.')
 
@@ -29,13 +29,16 @@ class Test(BaseTest):
         open_find()
         edit_select_all()
         edit_delete()
+
         find_toolbar_opened = exists(FindToolbar.FINDBAR_TEXTBOX, 10)
         assert_true(self, find_toolbar_opened, 'Find Toolbar is opened.')
 
         # Enter a search term and press ENTER. Check the position of the highlighted term
         type('see', interval=1)
         type(Key.ENTER)
+
         selected_label_exists = exists(see_label_pattern, 5)
         assert_true(self, selected_label_exists, 'The first one has a green background highlighted.')
+
         unhighlighted_label_exists = exists(see_label_unhighlighted_pattern, 5)
         assert_true(self, unhighlighted_label_exists, 'The others are not highlighted.')
