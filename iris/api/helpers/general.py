a8f9316efebaef0cf4f1c0667a781629ee649f83
tracywalker <twalker@mozilla.com>
2018-12-12 12:41:53 -0600

    Correctly alphabetize import list

--- iris/api/helpers/general.py
+++ iris/api/helpers/general.py
@@ -8,17 +8,17 @@ import mozversion
 from mozrunner import FirefoxRunner, errors
 
 from iris.api.core.environment import Env
-from iris.api.core.firefox_ui.menus import LibraryMenu
-from iris.api.core.firefox_ui.location_bar import LocationBar
+from iris.api.core.firefox_ui.bookmarks import Bookmarks
+from iris.api.core.firefox_ui.download_manager import DownloadManager
 from iris.api.core.firefox_ui.find_toolbar import FindToolbar
 from iris.api.core.firefox_ui.hamburger import HamburgerMenu
-from iris.api.core.firefox_ui.menu_bar import MenuBar
-from iris.api.core.firefox_ui.tabs import Tabs
-from iris.api.core.firefox_ui.nav_bar import NavBar
 from iris.api.core.firefox_ui.library import Library
-from iris.api.core.firefox_ui.download_manager import DownloadManager
-from iris.api.core.firefox_ui.window_controls import MainWindow, AuxiliaryWindow
-from iris.api.core.firefox_ui.bookmarks import Bookmarks
+from iris.api.core.firefox_ui.location_bar import LocationBar
+from iris.api.core.firefox_ui.menus import LibraryMenu
+from iris.api.core.firefox_ui.menu_bar import MenuBar
+from iris.api.core.firefox_ui.nav_bar import NavBar
+from iris.api.core.firefox_ui.tabs import Tabs
+from iris.api.core.firefox_ui.window_controls import AuxiliaryWindow, MainWindow
 from iris.api.core.key import *
 from iris.api.core.region import *
 from iris.api.core.screen import Screen
@@ -241,7 +241,6 @@ def confirm_close_multiple_tabs():
 def confirm_firefox_launch(image=None):
     """Waits for firefox to exist by waiting for the iris logo to be present.
 
-    :param app: Instance of FirefoxApp class.
     :param image: Pattern to confirm Firefox launch
     :return: None.
     """
