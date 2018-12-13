d0de74721c958e07577cd067ac35a51c0cd65df4
Dmitry Bakaev <bakaev.dmitriy@gmail.com>
2018-12-13 17:48:42 +0200

    Replaced local patterns with Firefox UI classes.

--- iris/tests/private_browsing/close_active_downloads.py
+++ iris/tests/private_browsing/close_active_downloads.py
@@ -16,28 +16,25 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        private_browsing_icon_pattern = Pattern('private_browsing_icon.png')
-        save_file_radiobutton_pattern = Pattern('save_file_radiobutton.png')
-        ok_save_file_button_pattern = Pattern('ok_save_file_button.png')
         stay_in_private_browsing_button_pattern = Pattern('stay_in_private_browsing_button.png')
         cancel_one_download_button_pattern = Pattern('cancel_one_download_button.png')
 
         new_private_window()
-        private_browsing_window_opened = exists(private_browsing_icon_pattern, 5)
+        private_browsing_window_opened = exists(PrivateWindow.private_window_pattern, 5)
         assert_true(self, private_browsing_window_opened, 'Private Browsing Window opened')
 
         navigate('http://releases.ubuntu.com/14.04.4/ubuntu-14.04.4-desktop-amd64.iso?_ga=2.161399869.1280481406'
                  '.1513173033-114787657.1513173033')
 
-        save_file_dialog_exists = exists(save_file_radiobutton_pattern, 10)
+        save_file_dialog_exists = exists(DownloadDialog.SAVE_FILE_RADIOBUTTON, 10)
         assert_true(self, save_file_dialog_exists, 'Save file dialog opened')
 
-        click(save_file_radiobutton_pattern)
+        click(DownloadDialog.SAVE_FILE_RADIOBUTTON)
 
-        ok_button_exists = exists(ok_save_file_button_pattern, 5)
+        ok_button_exists = exists(DownloadDialog.OK_BUTTON, 5)
         assert_true(self, ok_button_exists, 'Button OK exists')
 
-        click(ok_save_file_button_pattern)
+        click(DownloadDialog.OK_BUTTON)
 
         restore_firefox_focus()
 
@@ -62,7 +59,7 @@ class Test(BaseTest):
         click(cancel_one_download_button_pattern)
 
         try:
-            private_window_closed = wait_vanish(private_browsing_icon_pattern, 5)
+            private_window_closed = wait_vanish(PrivateWindow.private_window_pattern, 5)
             assert_true(self, private_window_closed, 'Private window is dismissed')
         except FindError:
             raise FindError('Private window is not dismissed')
