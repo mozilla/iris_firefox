7182b32ccade2c8bc2192a120f1b9a49091a9612
rrobotin <rrobotin@mozilla.com>
2018-12-14 11:36:11 +0200

    Fixed imports

--- iris/test_dependencies.py
+++ iris/test_dependencies.py
@@ -7,9 +7,17 @@ from iris.api.core.firefox_ui.utils import *
 from iris.api.core.firefox_ui.bookmarks import *
 from iris.api.core.firefox_ui.hamburger import *
 from iris.api.core.firefox_ui.library import *
+from iris.api.core.firefox_ui.library_menu import *
 from iris.api.core.firefox_ui.sidebar import *
 from iris.api.core.firefox_ui.download_manager import *
 from iris.api.core.firefox_ui.private_window import *
+from iris.api.core.firefox_ui.tabs import *
+from iris.api.core.firefox_ui.find_toolbar import *
+from iris.api.core.firefox_ui.nav_bar import *
+from iris.api.core.firefox_ui.location_bar import *
+from iris.api.core.firefox_ui.menu_bar import *
+
+
 from iris.api.helpers.history_utils import open_clear_recent_history_window
 from iris.api.helpers.download_manager_utils import *
 from iris.api.helpers.customize_utils import *
