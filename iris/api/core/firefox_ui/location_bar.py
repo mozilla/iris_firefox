93e131e03d14ffd665e284bddf27f5f17badbc1a
Paul Prokhorov <pauprokhorov@mozilla.com>
2018-12-14 18:54:24 +0200

    Renamed and moved pattern images for issue 1574 according to refactoring process

--- iris/api/core/firefox_ui/location_bar.py
+++ iris/api/core/firefox_ui/location_bar.py
@@ -24,4 +24,8 @@ class LocationBar(object):
 
     TRACKING_PROTECTION_SHIELD_ACTIVATED = Pattern('tracking-protection-icon_enabled.png')
     TRACKING_PROTECTION_SHIELD_DEACTIVATED = Pattern('tracking-protection-icon_disabled.png')
+    NEW_FIREFOX_CONTENT_BLOCKING_LABEL = Pattern('UITourTooltipTitle_new_in_firefox_content_blocking.png')
+    NEXT_BUTTON_TOUR_FIRST_STEP = Pattern('UITourTooltip_next_button.png')
+    TRACKING_ATTEMPTS_BLOCKED_MESSAGE = Pattern('tracking_attempts_blocked_message.png')
+    TRACKING_CONTENT_DETECTED_MESSAGE = Pattern('tracking_content_detected_message.png')
 
