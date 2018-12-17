78229d1564f995cae4d7e9079b0fe97d93bc90fa
AndreiH <ahutusoru@mozilla.com>
2018-12-17 15:10:35 +0200

    [FirefoxApp] Add first commit for Firefox app

--- firefox_ui/window_controls.py
+++ firefox_ui/window_controls.py
@@ -1,6 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from core.enums import OSPlatform
 from core.finder.pattern import Pattern
 from core.helpers.os_helpers import OSHelper
