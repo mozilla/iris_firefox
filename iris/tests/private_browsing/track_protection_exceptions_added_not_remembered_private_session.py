aabbde7551afb29b2517369d4f87b219af347b28
pavelciapa <pciapa@mozilla.com>
2019-01-10 19:32:27 +0200

    fixed script for issue_1577

--- iris/tests/private_browsing/track_protection_exceptions_added_not_remembered_private_session.py
+++ iris/tests/private_browsing/track_protection_exceptions_added_not_remembered_private_session.py
@@ -19,10 +19,10 @@ class Test(BaseTest):
     def run(self):
         tracking_protection_shield_pattern = LocationBar.TRACKING_PROTECTION_SHIELD_ACTIVATED
         tracking_protection_shield_deactivated_pattern = LocationBar.TRACKING_PROTECTION_SHIELD_DEACTIVATED
-
+        tracking_content_detected_pattern = LocationBar.TRACKING_CONTENT_DETECTED_MESSAGE
+        cnn_logo_pattern = LocalWeb.CNN_LOGO
         # Access the website
         blocking_turn_off_pattern = Pattern("blocking_turn_off.png")
-        cnn_logo_pattern = LocalWeb.CNN_LOGO
         empty_exc_list_pattern = Pattern("empty_exc_list.png")
         manage_exceptions_button_pattern = Pattern("manage_exceptions_button.png")
         private_browsing_tab_logo_pattern = Pattern("private_browsing_tab_logo.png")
@@ -33,37 +33,60 @@ class Test(BaseTest):
                     "Private window opened")
 
         navigate("https://edition.cnn.com/?refresh=1")
+
+        page_loaded = exists(cnn_logo_pattern, timeout=30)
+        assert_true(self, page_loaded,
+                    "The website is successfully displayed. ")
+
         tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern,
                                                       timeout=5)
         assert_true(self, tracking_protection_shield_displayed,
                     "Tracking protection shield displayed")
-        page_loaded = exists(cnn_logo_pattern, timeout=30)
-        assert_true(self, page_loaded,
-                    "Page loaded")
+
+        cnn_blocked_content_not_displayed = not exists(LocalWeb.CNN_BLOCKED_CONTENT_ADV, 5)
+        assert_true(self, cnn_blocked_content_not_displayed,
+                    'Websites content that contain tracking elements are not displayed on the page')
 
         shield_button = find(tracking_protection_shield_pattern)
         click(shield_button)
+
         protection_popup_opened = exists(blocking_turn_off_pattern, 20)
         assert_true(self, protection_popup_opened,
-                    "Protection popup opened")
+                    "The site information panel is displayed.")
 
-        turn_off_protection = find(blocking_turn_off_pattern)
-        click(turn_off_protection)
-        tracking_protection_deactivated = exists(tracking_protection_shield_deactivated_pattern,
-                                                 timeout=10)
-        assert_true(self, tracking_protection_deactivated,
-                    "Tracking protection deactivated")
+        click(blocking_turn_off_pattern)
+
+        tracking_protection_shield_deactivated_exists = exists(tracking_protection_shield_deactivated_pattern, 40)
+        assert_true(self, tracking_protection_shield_deactivated_exists,
+                    'The tracking protection shield is displayed as deactivated (red strikethrough).')
+
+        page_loaded = exists(cnn_logo_pattern, 30)
+        assert_true(self, page_loaded,
+                    "The website is loaded.")
+
+        click_location = Location(x=0, y=300)
+        click(click_location, 0.2)
+        type(Key.PAGE_DOWN)
+
+        cnn_blocked_content_displayed = exists(LocalWeb.CNN_BLOCKED_CONTENT_ADV, 30)
+        assert_true(self, cnn_blocked_content_displayed,
+                    'Websites content that contain tracking elements are displayed on the page')
+
+        hover(tracking_protection_shield_deactivated_pattern, 40)
+        tracking_content_detected_exists = exists(tracking_content_detected_pattern, 40)
+        assert_true(self, tracking_content_detected_exists,
+                    'On hover, the tracking protection shield displays a "Tracking content detected" tooltip message.')
+
         close_window()
 
         new_tab()
         navigate("about:preferences#privacy")
         privacy_prefs_opened = exists(manage_exceptions_button_pattern)
         assert_true(self, privacy_prefs_opened,
-                    "Privacy preferences page opened")
+                    "The about:preferences#privacy page is successfully displayed.")
 
         manage_exceptions_button = find(manage_exceptions_button_pattern)
         click(manage_exceptions_button)
         exceptions_window_opened = exists(empty_exc_list_pattern)
         assert_true(self, exceptions_window_opened,
-                    "Excepted sites list displayed, it's empty")
-
+                    "The previously accessed website is not displayed inside the Tracking Protection exceptions panel.")
