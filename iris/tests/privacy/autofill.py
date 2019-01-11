251ce07e16ecc12f623d77f8cc734c95fb411520
Pavel Ciapa <pavel@exaltcg.com>
2019-01-11 11:40:10 +0200

    added osx images for issue_1776

--- iris/tests/privacy/autofill.py
+++ iris/tests/privacy/autofill.py
@@ -16,11 +16,12 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
+        private_browsing_image_pattern = PrivateWindow.private_window_pattern
         twitter_tab_favicon_pattern = Pattern('twitter_favicon.png')
         login_field_pattern = Pattern('login_field.png')
         password_field_pattern = Pattern('password_field.png')
         save_credentials_button_pattern = Pattern('save_button.png')
-        private_browsing_tab_favicon_pattern = Pattern('private_browsing_tab_favicon.png')
+        # private_browsing_tab_favicon_pattern = Pattern('private_browsing_tab_favicon.png')
         autofill_asterisks_pattern = Pattern('autofill_asterisks.png')
 
         twitter_login = 'o7641655@nwytg.net'
@@ -32,22 +33,21 @@ class Test(BaseTest):
 
         login_field_exists = exists(login_field_pattern, 5)
         assert_true(self, login_field_exists, 'Login field is displayed on the page')
+        click(login_field_pattern)
+        type(twitter_login)
 
         password_field_exists = exists(password_field_pattern, 5)
         assert_true(self, password_field_exists, 'Password field is displayed on the page')
-
-        click(login_field_pattern)
-        type(twitter_login)
         click(password_field_pattern)
         type(twitter_password)
-        key_down(Key.ENTER)
-        key_up(Key.ENTER)
-        save_button_exists = exists(save_credentials_button_pattern)
+        type(Key.ENTER)
+
+        save_button_exists = exists(save_credentials_button_pattern, 10)
         assert_true(self, save_button_exists, 'Save button displays after entering twitter login and password')
         click(save_credentials_button_pattern)
 
         new_private_window()
-        new_private_window_exists = exists(private_browsing_tab_favicon_pattern)
+        new_private_window_exists = exists(private_browsing_image_pattern)
         assert_true(self, new_private_window_exists, 'The private browsing tab is displayed.')
 
         navigate('twitter.com')
@@ -55,6 +55,6 @@ class Test(BaseTest):
         assert_true(self, twitter_tab_favicon_exists, 'Twitter page successfully opens')
 
         save_credentials_exist = exists(autofill_asterisks_pattern)
-        assert_false(self, save_credentials_exist, 'The log in information is not autofilled')
+        assert_false(self, save_credentials_exist, 'The log in information is not autofilled.')
 
         close_window()
