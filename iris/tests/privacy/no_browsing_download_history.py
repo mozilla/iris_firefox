de4f2bb623c5796a6538af0bb9a034a964f1e711
dbakaev <dbakaev@mozilla.com>
2019-01-14 17:03:44 +0200

    minor changes

--- iris/tests/privacy/no_browsing_download_history.py
+++ iris/tests/privacy/no_browsing_download_history.py
@@ -10,10 +10,10 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = "Firefox can be set to no longer remember browsing and download history."
-        self.test_case_id = "105208"
-        self.test_suite_id = "1956"
-        self.locale = ["en-US"]
+        self.meta = 'Firefox can be set to no longer remember browsing and download history.'
+        self.test_case_id = '105208'
+        self.test_suite_id = '1956'
+        self.locale = ['en-US']
 
     def setup(self):
         BaseTest.setup(self)
@@ -26,52 +26,52 @@ class Test(BaseTest):
         downloads_cleanup()
 
     def run(self):
-        remember_history_pattern = Pattern("remember_history.png")
-        remember_browsing_download_pattern = Pattern("remember_browsing_history.png")
-        unticked_browsing_download_pattern = Pattern("remember_browsing_unticked.png")
-        custom_history_settings_pattern = Pattern("custom_history_settings.png")
-        download_pdf_pattern = Pattern("download_pdf_button.png")
-        pdf_downloaded = Pattern("downloaded_pdf.png")
+        remember_history_pattern = Pattern('remember_history.png')
+        remember_browsing_download_pattern = Pattern('remember_browsing_history.png')
+        unticked_browsing_download_pattern = Pattern('remember_browsing_unticked.png')
+        custom_history_settings_pattern = Pattern('custom_history_settings.png')
+        download_pdf_pattern = Pattern('download_pdf_button.png')
+        pdf_downloaded = Pattern('downloaded_pdf.png')
 
         new_tab()
-        navigate("about:preferences#privacy")
+        navigate('about:preferences#privacy')
         preferences_opened = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED)
-        assert_true(self, preferences_opened, "The \"about:preferences#privacy\" page is successfully displayed")
+        assert_true(self, preferences_opened, 'The "about:preferences#privacy" page is successfully displayed')
 
-        paste("remember")
+        paste('remember')
         remember_history_menu_found = exists(remember_history_pattern)
-        assert_true(self, remember_history_menu_found, "History menu found")
+        assert_true(self, remember_history_menu_found, 'History menu found')
 
         click(remember_history_pattern)
         history_dropdown_opened = exists(custom_history_settings_pattern)
-        assert_true(self, history_dropdown_opened, "The option is successfully selected and remembered.")
+        assert_true(self, history_dropdown_opened, 'The option is successfully selected and remembered.')
 
         click(custom_history_settings_pattern)
         click(remember_browsing_download_pattern)
         point_unpinned = exists(unticked_browsing_download_pattern)
-        assert_true(self, point_unpinned, " The checkbox is successfully unticked.")
+        assert_true(self, point_unpinned, 'The checkbox is successfully unticked.')
 
         close_tab()
         new_tab()
         navigate(LocalWeb.FIREFOX_TEST_SITE)
         firefox_test_site_opened = exists(LocalWeb.FIREFOX_LOGO, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, firefox_test_site_opened, "Firefox test site opened")
+        assert_true(self, firefox_test_site_opened, 'Firefox test site opened.')
 
         close_tab()
         new_tab()
         navigate(LocalWeb.FOCUS_TEST_SITE)
         focus_test_site_opened = exists(LocalWeb.FOCUS_LOGO, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, focus_test_site_opened, "Focus test site opened")
+        assert_true(self, focus_test_site_opened, 'Focus test site opened.')
 
         close_tab()
         new_tab()
         navigate(LocalWeb.POCKET_TEST_SITE)
         pocket_site_opened = exists(LocalWeb.POCKET_LOGO, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, pocket_site_opened, "Pocket site opened")
+        assert_true(self, pocket_site_opened, 'Pocket site opened')
         close_tab()
         click(NavBar.LIBRARY_MENU)
         library_dropdown_opened = exists(LibraryMenu.HISTORY_BUTTON)
-        assert_true(self, library_dropdown_opened, "Library dropdown opened")
+        assert_true(self, library_dropdown_opened, 'Library dropdown opened.')
 
         click(LibraryMenu.HISTORY_BUTTON)
 
@@ -79,16 +79,16 @@ class Test(BaseTest):
         focus_test_site_not_in_history = not exists(LocalWeb.FOCUS_BOOKMARK_SMALL)
         pocket_site_not_in_history = not exists(LocalWeb.POCKET_BOOKMARK_SMALL)
 
-        assert_true(self, firefox_test_site_not_in_history, "First site not in history")
-        assert_true(self, focus_test_site_not_in_history, "Second site not in history")
+        assert_true(self, firefox_test_site_not_in_history, 'First site not in history.')
+        assert_true(self, focus_test_site_not_in_history, 'Second site not in history.')
         assert_true(self, pocket_site_not_in_history,
-                    "The accessed websites are not remembered in the browser history.")
+                    'The accessed websites are not remembered in the browser history.')
 
         restore_firefox_focus()
         new_tab()
-        navigate("https://www.stmarys-ca.edu/sites/default/files/attachments/files/Faust.pdf")
+        navigate('https://www.stmarys-ca.edu/sites/default/files/attachments/files/Faust.pdf')
         pdf_bar_located = exists(download_pdf_pattern, 30)
-        assert_true(self, pdf_bar_located, "PDF buffered")
+        assert_true(self, pdf_bar_located, 'PDF buffered')
 
         click(download_pdf_pattern)
         save_file_dialog_exists = exists(DownloadDialog.SAVE_FILE_RADIOBUTTON, DEFAULT_FIREFOX_TIMEOUT*3)
@@ -98,14 +98,14 @@ class Test(BaseTest):
         ok_button_exists = exists(DownloadDialog.OK_BUTTON, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, ok_button_exists, 'Button OK exists')
 
-        "The pdf document is successfully downloaded."
         click(DownloadDialog.OK_BUTTON)
 
         download_finished = exists(NavBar.DOWNLOADS_BUTTON)
-        assert_true(self, download_finished, "The pdf document is successfully downloaded.")
+        assert_true(self, download_finished, 'The pdf document is successfully downloaded.')
 
         restart_firefox(self, self.browser.path, self.profile_path, self.base_local_web_url)
         open_downloads()
         file_not_in_downloads = not exists(pdf_downloaded)
-        assert_true(self, file_not_in_downloads, "Downloaded file not in downloads history")
-        click_window_control("close")
+        assert_true(self, file_not_in_downloads,
+                    'The previously downloaded pdf file is not displayed in Firefox download history.')
+        click_window_control('close')
