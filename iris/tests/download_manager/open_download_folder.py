cf9ae09794f8899f7b313eb9bbdd29207b718b8d
Ionut Budeanu <ibudeanu@mozilla.com>
2019-01-14 14:26:51 +0200

    Block tests thar are using downloads_cleanup()

--- iris/tests/download_manager/open_download_folder.py
+++ iris/tests/download_manager/open_download_folder.py
@@ -14,7 +14,7 @@ class Test(BaseTest):
         self.test_case_id = '99480'
         self.test_suite_id = '1827'
         self.locales = ['en-US']
-        self.blocked_by = {'id': '1513494', 'platform': [Platform.LINUX]}
+        self.blocked_by = {'id': '1513494,issue_1811', 'platform': [Platform.LINUX, Platform.WINDOWS]}
 
     def setup(self):
         """Test case setup
