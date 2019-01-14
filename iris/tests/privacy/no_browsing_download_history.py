3cdcf6d826ee49ba4ac27fe655504e505a8b5d20
dbakaev <dbakaev@mozilla.com>
2019-01-14 13:40:38 +0200

    code style corrections

--- iris/tests/privacy/no_browsing_download_history.py
+++ iris/tests/privacy/no_browsing_download_history.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 from iris.test_case import *
 
 
@@ -27,54 +32,54 @@ class Test(BaseTest):
         download_pdf_pattern = Pattern("download_pdf_button.png")
         pdf_downloaded = Pattern("downloaded_pdf.png")
 
-        restore_firefox_focus()
         new_tab()
         navigate("about:preferences#privacy")
         preferences_opened = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED)
         assert_true(self, preferences_opened, "Preferences tab opened")
 
         paste("remember")
-        remember_history_menu_found = exists(remember_history_pattern, 1)
+        remember_history_menu_found = exists(remember_history_pattern)
         assert_true(self, remember_history_menu_found, "History menu found")
         assert_true(self, remember_history_menu_found, "History menu found")
 
-        click(remember_history_pattern, 0.5)
+        click(remember_history_pattern)
         history_dropdown_opened = exists(custom_history_settings_pattern)
         assert_true(self, history_dropdown_opened, "History drop-down menu opened")
 
-        click(custom_history_settings_pattern, 0.5)
+        click(custom_history_settings_pattern)
         click(remember_browsing_download_pattern)
         close_tab()
 
         new_tab()
         navigate(LocalWeb.FIREFOX_TEST_SITE)
-        firefox_test_site_opened = exists(LocalWeb.FIREFOX_LOGO, 10)
+        firefox_test_site_opened = exists(LocalWeb.FIREFOX_LOGO, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_test_site_opened, "Firefox test site opened")
 
         close_tab()
         new_tab()
         navigate(LocalWeb.FOCUS_TEST_SITE)
-        focus_test_site_opened = exists(LocalWeb.FOCUS_LOGO, 10)
+        focus_test_site_opened = exists(LocalWeb.FOCUS_LOGO, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, focus_test_site_opened, "Focus test site opened")
 
         close_tab()
         new_tab()
         navigate(LocalWeb.POCKET_TEST_SITE)
-        pocket_site_opened = exists(LocalWeb.POCKET_LOGO, 10)
+        pocket_site_opened = exists(LocalWeb.POCKET_LOGO, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, pocket_site_opened, "Pocket site opened")
         close_tab()
-        click(NavBar.LIBRARY_MENU, 1)
+        click(NavBar.LIBRARY_MENU)
         library_dropdown_opened = exists(LibraryMenu.HISTORY_BUTTON)
         assert_true(self, library_dropdown_opened, "Library dropdown opened")
 
         click(LibraryMenu.HISTORY_BUTTON)
 
-        firefox_test_site_not_in_history = not exists(LocalWeb.FIREFOX_BOOKMARK_SMALL, 1)
-        focus_test_site_not_in_history = not exists(LocalWeb.FOCUS_BOOKMARK_SMALL, 1)
-        pocket_site_not_in_history = not exists(LocalWeb.POCKET_BOOKMARK_SMALL, 1)
+        firefox_test_site_not_in_history = not exists(LocalWeb.FIREFOX_BOOKMARK_SMALL)
+        focus_test_site_not_in_history = not exists(LocalWeb.FOCUS_BOOKMARK_SMALL)
+        pocket_site_not_in_history = not exists(LocalWeb.POCKET_BOOKMARK_SMALL)
 
-        assert_true(self, firefox_test_site_not_in_history and
-                    focus_test_site_not_in_history and pocket_site_not_in_history, "Visited sites not in history")
+        assert_true(self, firefox_test_site_not_in_history, "First site not in history")
+        assert_true(self, focus_test_site_not_in_history, "Second site not in history")
+        assert_true(self, pocket_site_not_in_history, "Visited sites not in history")
 
         restore_firefox_focus()
         new_tab()
@@ -83,17 +88,17 @@ class Test(BaseTest):
         assert_true(self, pdf_bar_located, "PDF buffered")
 
         click(download_pdf_pattern, 1)
-        save_file_dialog_exists = exists(DownloadDialog.SAVE_FILE_RADIOBUTTON, 30)
+        save_file_dialog_exists = exists(DownloadDialog.SAVE_FILE_RADIOBUTTON, DEFAULT_FIREFOX_TIMEOUT*3)
         assert_true(self, save_file_dialog_exists, 'Save file dialog opened')
 
         click(DownloadDialog.SAVE_FILE_RADIOBUTTON)
-        ok_button_exists = exists(DownloadDialog.OK_BUTTON, 5)
+        ok_button_exists = exists(DownloadDialog.OK_BUTTON, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, ok_button_exists, 'Button OK exists')
 
         click(DownloadDialog.OK_BUTTON)
 
         restart_firefox(self, self.browser.path, self.profile_path, self.base_local_web_url)
         open_downloads()
-        file_not_in_downloads = not exists(pdf_downloaded, 1)
+        file_not_in_downloads = not exists(pdf_downloaded)
         assert_true(self, file_not_in_downloads, "Downloaded file not in downloads history")
         click_window_control("close")
