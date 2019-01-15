83d709d93cce095654d364e840e4a45427653579
Pavel Ciapa <pavel@exaltcg.com>
2019-01-15 13:05:04 +0200

    Code review finished for issue_1777

--- iris/tests/content_scroll/print_preview_enabled.py
+++ iris/tests/content_scroll/print_preview_enabled.py
@@ -19,7 +19,8 @@ class Test(BaseTest):
     def run(self):
         print_preview_content_loaded_pattern = Pattern('print_preview_content_loaded.png')
         scroll_content_pattern = Pattern('soap_wiki_print_mode.png')
-        # Scroll bar arrows pattern fo Windows
+
+        # Scroll bar arrows pattern for Windows
         if Settings.is_windows():
             scroll_bar_button_up_pattern = Pattern('scroll_bar_button_up.png').similar(0.6)
             scroll_bar_button_down_pattern = Pattern('scroll_bar_button_down.png').similar(0.6)
@@ -32,8 +33,8 @@ class Test(BaseTest):
         navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
         web_page_loaded_exists = exists(LocalWeb.SOAP_WIKI_SOAP_LABEL, 20)
         assert_true(self, web_page_loaded_exists, 'The website is properly loaded.')
+        click_hamburger_menu_option('Print...')
 
-        click_hamburger_menu_option("Print...")
         print_preview_content_loaded_exists = exists(print_preview_content_loaded_pattern, 20)
         assert_true(self, print_preview_content_loaded_exists, 'Print-preview mode is successfully enabled.')
 
@@ -60,7 +61,7 @@ class Test(BaseTest):
                 click(scroll_bar_location_down, DEFAULT_FX_DELAY)
             if Settings.is_windows():
                 [click(scroll_bar_button_down_pattern, DEFAULT_FX_DELAY, in_region=scroll_bar_region) for _ in range(10)]
-            wait_vanish(scroll_content_pattern, 10)
+            wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
             raise FindError('Content before scrolling is still on the page')
 
@@ -69,59 +70,53 @@ class Test(BaseTest):
         if Settings.is_windows():
             [click(scroll_bar_button_up_pattern, DEFAULT_FX_DELAY) for _ in range(10)]
 
-        after_scroll_content_exists = exists(scroll_content_pattern, 10)
+        after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, before_scroll_content_exists and after_scroll_content_exists,
                     'Scroll up and down using scroll bar is successful.')
 
         # Scroll up and down using arrow keys
-        before_scroll_content_exists = exists(scroll_content_pattern, 10)
+        before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         try:
             repeat_key_down(10)
-            wait_vanish(scroll_content_pattern, 10)
+            wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
             raise FindError('Content before scrolling is still on the page')
         repeat_key_up(10)
-        after_scroll_content_exists = exists(scroll_content_pattern, 10)
+        after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, before_scroll_content_exists and after_scroll_content_exists,
                     'Scroll up and down using arrow keys is successful.')
 
         # Scroll up and down using page up/down keys
-        before_scroll_content_exists = exists(scroll_content_pattern, 10)
+        before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         try:
             [type(Key.PAGE_DOWN) for _ in range(4)]
-            wait_vanish(scroll_content_pattern, 10)
+            wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
             raise FindError('Content before scrolling is still on the page')
         [type(Key.PAGE_UP) for _ in range(4)]
-        after_scroll_content_exists = exists(scroll_content_pattern, 10)
+        after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, before_scroll_content_exists and after_scroll_content_exists,
                     'Scroll up and down using page up/down keys is successful.')
 
         # Scroll up and down using ctrl + up/down keys
-        before_scroll_content_exists = exists(scroll_content_pattern, 10)
+        before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         try:
-            if Settings.is_mac():
-                type(Key.DOWN, modifier=KeyModifier.CMD)
-            else:
-                type(Key.DOWN, modifier=KeyModifier.CTRL)
-            wait_vanish(scroll_content_pattern, 10)
+            type(Key.DOWN, modifier=KeyModifier.CTRL)
+            wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
             raise FindError('Content before scrolling is still on the page')
 
-        if Settings.is_mac():
-            type(Key.UP, modifier=KeyModifier.CMD)
-        else:
-            type(Key.UP, modifier=KeyModifier.CTRL)
+        type(Key.UP, modifier=KeyModifier.CTRL)
 
-        after_scroll_content_exists = exists(scroll_content_pattern, 10)
+        after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, before_scroll_content_exists and after_scroll_content_exists,
                     'Scroll up and down using ctrl + up/down keys is successful.')
 
         # Scroll up and down using space bar
-        before_scroll_content_exists = exists(scroll_content_pattern, 10)
+        before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         try:
             type(Key.SPACE)
-            after_scroll_content_disappeared = wait_vanish(scroll_content_pattern, 10)
+            after_scroll_content_disappeared = wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
             assert_true(self, before_scroll_content_exists and after_scroll_content_disappeared,
                         'Scroll up and down using space bar is successful.')
         except FindError:
