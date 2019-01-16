9df0f3030b4c185bcc438709ab51eea152f35ddc
Pavel Ciapa <pavel@exaltcg.com>
2019-01-16 12:26:26 +0200

    added OSX support for issue_1768

--- iris/tests/privacy/no_cookies_from_visited.py
+++ iris/tests/privacy/no_cookies_from_visited.py
@@ -7,6 +7,7 @@ from iris.test_case import *
 
 
 class Test(BaseTest):
+
     def __init__(self):
         BaseTest.__init__(self)
         self.test_case_id = '105525'
@@ -27,34 +28,44 @@ class Test(BaseTest):
 
         new_tab()
         navigate('about:preferences#privacy')
+
         preferences_opened = exists(custom_content_blocking_pattern)
-        assert_true(self, preferences_opened, 'The page is successfully displayed.')
-
+        assert_true(self, preferences_opened, 'The "about:preferences#privacy" page is successfully displayed.')
         click(custom_content_blocking_pattern)
+
         options_displayed = exists(cookies_unticked_pattern)
-        assert_true(self, options_displayed, 'The options are properly displayed.')
-
+        assert_true(self, options_displayed,
+                    'The cookies options are properly displayed at "Cookies and Site Data" section')
         click(cookies_unticked_pattern)
-        checkbox_set_successfully = exists(cookies_ticked_pattern)
-        strictness_menu_appeared = exists(cookies_blocking_strictness_menu_pattern)
-        assert_true(self, checkbox_set_successfully, 'The checkbox is successfully set.')
-        assert_true(self, strictness_menu_appeared, 'Strictness menu appeared.')
 
+        checkbox_set_successfully = exists(cookies_ticked_pattern)
+        assert_true(self, checkbox_set_successfully, 'The "Block cookies and site data" checkbox is successfully set.')
+
+        #  ### Steps not present in test description, without which test fails
+
+        strictness_menu_appeared = exists(cookies_blocking_strictness_menu_pattern)
+        assert_true(self, strictness_menu_appeared, 'Strictness menu appeared.')
         click(cookies_blocking_strictness_menu_pattern)
+
         dropdown_opened = exists(block_all_cookies_pattern)
         assert_true(self, dropdown_opened, 'Strictness dropdown menu opened')
         click(block_all_cookies_pattern)
+
+        # ### end of steps
 
         navigate('https://www.youtube.com/')
         site_loaded = exists(youtube_logo_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, site_loaded, 'The website is successfully displayed.')
 
         navigate('about:preferences#privacy')
+        preferences_opened = exists(custom_content_blocking_pattern)
+        assert_true(self, preferences_opened, 'The "about:preferences#privacy" page is successfully displayed.')
+
         paste('manage data')
         cookies_data_button_located = exists(manage_cookies_data_pattern)
         assert_true(self, cookies_data_button_located, '\"Manage cookies data\" button displayed.')
-
         click(manage_cookies_data_pattern)
+
         cookies_window_opened = exists(cookies_window_title_pattern)
         assert_true(self, cookies_window_opened, 'Cookies window displayed.')
 
