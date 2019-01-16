97131ce77314617f397f3f709e2bb60bd8229bdf
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-01-16 18:36:21 +0200

    Added changes to script for issue_1777

--- iris/tests/content_scroll/print_preview_enabled.py
+++ iris/tests/content_scroll/print_preview_enabled.py
@@ -36,13 +36,13 @@ class Test(BaseTest):
 
         # Scroll up and down using mouse wheel
         before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, before_scroll_content_exists, 'Content before scrolling is on the page')
+        assert_true(self, before_scroll_content_exists, 'Content before scrolling using mouse wheel is on the page')
 
         scroll(-scroll_height)
         try:
             wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
-            raise FindError('After scrolling content is still on the page')
+            raise FindError('Content is still on the page after scrolling')
         scroll(scroll_height)
 
         after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
@@ -50,13 +50,13 @@ class Test(BaseTest):
 
         # Scroll up and down using arrow keys
         before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, before_scroll_content_exists, 'Content before scrolling is on the page')
+        assert_true(self, before_scroll_content_exists, 'Content before scrolling using arrow keys is on the page')
 
         repeat_key_down(10)
         try:
             wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
-            raise FindError('After scrolling content is still on the page')
+            raise FindError('Content is still on the page after scrolling')
         repeat_key_up(10)
 
         after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
@@ -64,13 +64,13 @@ class Test(BaseTest):
 
         # Scroll up and down using page up/down keys
         before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, before_scroll_content_exists, 'Content before scrolling is on the page')
+        assert_true(self, before_scroll_content_exists, 'Content before scrolling using page up/down is on the page')
 
         [type(Key.PAGE_DOWN) for _ in range(4)]
         try:
             wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
-            raise FindError('After scrolling content is still on the page')
+            raise FindError('Content is still on the page after scrolling')
         [type(Key.PAGE_UP) for _ in range(4)]
 
         after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
@@ -78,13 +78,13 @@ class Test(BaseTest):
 
         # Scroll up and down using ctrl + up/down keys
         before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, before_scroll_content_exists, 'Content before scrolling is on the page')
+        assert_true(self, before_scroll_content_exists, 'Content before scrolling using ctrl + up/down is on the page')
 
         type(Key.DOWN, modifier=KeyModifier.CTRL)
         try:
             wait_vanish(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
         except FindError:
-            raise FindError('After scrolling content is still on the page')
+            raise FindError('Content is still on the page after scrolling')
         type(Key.UP, modifier=KeyModifier.CTRL)
 
         after_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
@@ -92,7 +92,7 @@ class Test(BaseTest):
 
         # Scroll up and down using space bar
         before_scroll_content_exists = exists(scroll_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, before_scroll_content_exists, 'Content before scrolling is on the page')
+        assert_true(self, before_scroll_content_exists, 'Content before scrolling using space bar is on the page')
 
         try:
             type(Key.SPACE)
