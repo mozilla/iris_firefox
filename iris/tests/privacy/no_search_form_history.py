05043f98862437944d204bb538b340bbeb449aad
yeyartsev <eyartsev@mozilla.com>
2019-01-18 14:06:57 +0200

    Changed way for search success validation

--- iris/tests/privacy/no_search_form_history.py
+++ iris/tests/privacy/no_search_form_history.py
@@ -21,7 +21,7 @@ class Test(BaseTest):
         remember_search_history_pattern = Pattern('remember_search_history.png')
         search_history_unticked_pattern = Pattern('remember_search_history_unticked.png')
         add_search_bar_pattern = Pattern('add_search_bar.png')
-        search_tab_pattern = Pattern('random_search.png').similar(0.9)
+        search_tab_pattern = Pattern('google_tab.png')
         search_bar_not_empty_pattern = Pattern('search_bar_not_empty.png')
         search_form_suggestion_pattern = Pattern('search_form_suggestion.png')
         name_form_pattern = Pattern('name_form.png')
@@ -29,7 +29,6 @@ class Test(BaseTest):
         autocomplete_pattern = Pattern('word_autocomplete.png')
         form_address = self.get_asset_path('form.html')
 
-        new_tab()
         navigate("about:preferences#privacy")
         preferences_opened = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED)
         assert_true(self, preferences_opened, 'The page is successfully displayed.')
@@ -37,25 +36,25 @@ class Test(BaseTest):
         paste('remember')
         remember_history_menu_found = exists(remember_history_pattern)
         assert_true(self, remember_history_menu_found, 'History menu found.')
-
         click(remember_history_pattern)
+
         history_dropdown_opened = exists(custom_history_settings_pattern)
         assert_true(self, history_dropdown_opened, 'The option is successfully selected and remembered.')
-
         click(custom_history_settings_pattern)
         click(remember_search_history_pattern)
+
         remember_search_unticked = exists(search_history_unticked_pattern)
         assert_true(self, remember_search_unticked, 'The checkbox is successfully unticked.')
 
         navigate("about:preferences#search")
         search_preferences_opened = exists(add_search_bar_pattern)
         assert_true(self, search_preferences_opened, 'The about:preferences#search page is successfully displayed.')
-
         click(add_search_bar_pattern)
+
         search_bar_appeared = exists(LocationBar.SEARCH_BAR)
         assert_true(self, search_bar_appeared, 'The search bar is successfully added in the toolbar.')
-
         click(LocationBar.SEARCH_BAR)
+
         paste('random')
         type(Key.ENTER)
         search_done = exists(search_tab_pattern, DEFAULT_FIREFOX_TIMEOUT*3)
@@ -75,8 +74,8 @@ class Test(BaseTest):
         password_form_displayed = exists(password_form_pattern)
         assert_true(self, name_form_displayed, "Form displayed.")
         assert_true(self, password_form_displayed, 'The website is successfully displayed.')
-
         click(name_form_pattern)
+
         paste('random')
         click(password_form_pattern)
         paste('Asdf1@3$')
