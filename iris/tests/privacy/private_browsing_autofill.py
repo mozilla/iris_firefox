0c6b15e5284c2a9f8d8426ced500e6a0f6bd806b
Sergey Pronoza <sergey@exaltcg.com>
2019-01-18 12:56:38 +0200

    Added changes to the browser config

--- iris/tests/privacy/private_browsing_autofill.py
+++ iris/tests/privacy/private_browsing_autofill.py
@@ -14,6 +14,10 @@ class Test(BaseTest):
         self.test_case_id = '101666'
         self.test_suite_id = '1956'
         self.locales = ['en-US']
+        self.set_profile_pref({'extensions.formautofill.available': 'on'})
+        self.set_profile_pref({'browser.search.region': 'US'})
+        self.set_profile_pref({'extensions.formautofill.addresses.enabled': 'true'})
+        self.set_profile_pref({'extensions.formautofill.loglevel': 'Debug'})
 
     def run(self):
         name_field_pattern = Pattern('name_field.png').similar(.6)
