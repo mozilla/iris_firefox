f6eb30bbcfc2835aa6fa98e7beda0a00ac97e460
Paul Prokhorov <paul@exaltcg.com>
2019-01-18 11:56:47 +0200

    Changes following code review of the_swish_life.py test

--- iris/tests/plugin_compatibility/the_swish_life.py
+++ iris/tests/plugin_compatibility/the_swish_life.py
@@ -18,24 +18,22 @@ class Test(BaseTest):
 
         navigate('http://theswishlife.com/')
 
-        window_opened = exists(the_swish_life_tab_pattern, 30)
+        window_opened = exists(the_swish_life_tab_pattern, DEFAULT_FIREFOX_TIMEOUT * 3)
         assert_true(self, window_opened, 'The Swish Life home page opened')
 
         page_end()
 
         fashion_tag_exists = exists(fashion_tag_pattern)
-
         if not fashion_tag_exists:
             assert_false(self, fashion_tag_exists, 'The "Fashion" tag exists')
 
         click(fashion_tag_pattern)
 
-        fashion_page_opened = exists(fashion_page_pattern, 30)
+        fashion_page_opened = exists(fashion_page_pattern, DEFAULT_FIREFOX_TIMEOUT * 3)
         assert_true(self, fashion_page_opened, 'The "Fashion" page is opened')
 
         click(the_home_button_pattern)
 
-        home_page_label = exists(fashion_page_pattern, 30)
-        assert_true(self, home_page_label, 'Return to the Swish Life home page. The website and the '
-                                            'browser are stable')
+        home_page_label = exists(fashion_page_pattern, DEFAULT_FIREFOX_TIMEOUT * 3)
+        assert_true(self, home_page_label, 'Return to the Swish Life home page. The website and the browser are stable')
 
