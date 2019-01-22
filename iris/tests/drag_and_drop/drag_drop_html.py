50fd3949e6ef476e7b3c5ceaaed28bf61c4ca797
Pavel Ciapa <pavel@exaltcg.com>
2019-01-22 16:57:38 +0200

    Corrections for osx

--- iris/tests/drag_and_drop/drag_drop_html.py
+++ iris/tests/drag_and_drop/drag_drop_html.py
@@ -22,18 +22,25 @@ class Test(BaseTest):
         drop_here_pattern = Pattern('drop_here.png')
         drop_verified_pattern = Pattern('drop_matching_verified.png')
         drop_not_matching_pattern = Pattern('drop_not_matching.png')
-        correct_result_pattern = Pattern('correct_result.png').similar(0.98)
+        correct_result_pattern = Pattern('correct_result.png')#.similar(0.98)
         soap_url_selected_pattern = Pattern('soap_url_selected.png')
         iris_tab_pattern = Pattern('iris_tab.png')
+        if Settings.is_mac():
+            browser_console_title_pattern = Pattern('browser_console_title.png')
 
         change_preference('devtools.chrome.enabled', True)
-        minimize_window()
+        if not Settings.is_mac():
+            click_window_control('restore')
+        # else:
+        #     minimize_window()
 
-        iris_tab_location = find(iris_tab_pattern)
-        start_position = Location(SCREEN_WIDTH/25, SCREEN_HEIGHT/25)
-        drag_drop(iris_tab_location, start_position)
+            iris_tab_location = find(iris_tab_pattern)
+            start_position = Location(SCREEN_WIDTH/25, SCREEN_HEIGHT/25)
+            drag_drop(iris_tab_location, start_position)
 
         open_browser_console()
+        if Settings.is_mac():
+            click(browser_console_title_pattern)
         paste('window.resizeTo({0}, {1})'.format(SCREEN_WIDTH*0.45, SCREEN_HEIGHT*0.9))
         type(Key.ENTER)
         close_tab()
@@ -50,7 +57,7 @@ class Test(BaseTest):
 
         new_window()
         opened_tab_location = find(Tabs.NEW_TAB_HIGHLIGHTED)
-        new_window_drop_location = Location(SCREEN_WIDTH/2, SCREEN_HEIGHT/20)
+        new_window_drop_location = Location(SCREEN_WIDTH*0.55, SCREEN_HEIGHT/20)
         drag_drop(opened_tab_location, new_window_drop_location)
 
         drop_position_visible = exists(drop_here_pattern)
@@ -62,18 +69,31 @@ class Test(BaseTest):
 
         # Selecting paragraph by triple click on location (pattern click doesn't select)
         paragraph = find(LocalWeb.SOAP_WIKI_SOAP_LABEL)
-        [click(paragraph, DEFAULT_TYPE_DELAY) for _ in range(3)]
+        selection_end = find(LocalWeb.SOAP_WIKI_SOAP_LABEL).offset(0, SCREEN_HEIGHT/10)
+        drag_drop(paragraph, selection_end)
         paragraph_x, paragraph_y = LocalWeb.SOAP_WIKI_SOAP_LABEL.get_size()
-        paragraph.offset(paragraph_x/2, paragraph_y/2)
+        paragraph.offset(paragraph_x / 2, paragraph_y / 2)
+
+
+        # time.sleep(600)
+
+
+        # for _ in range(3):
+        #     click(paragraph, 0.05)
+
         drop_position_offset_x, drop_position_offset_y = drop_here_pattern.get_size()
         drop_html_position = find(drop_here_pattern)
-        drop_html_position.offset(drop_position_offset_x, drop_position_offset_y)
+        drop_html_position.offset(drop_position_offset_x*2, drop_position_offset_y*2)
         drag_drop(paragraph, drop_html_position)
 
         drop_verified = exists(drop_verified_pattern)
         assert_true(self, drop_verified, '"Matching" appears under the "Drop Stuff Here" area.')
+        matched_size_x, matched_size_y = drop_verified_pattern.get_size()
+        result_region_location = find(drop_verified_pattern).offset(matched_size_x, matched_size_y)
+        result_region = Region(result_region_location.x, result_region_location.y, SCREEN_WIDTH/2,
+                               (SCREEN_HEIGHT-result_region_location.y))
 
-        result_correct = exists(correct_result_pattern)
+        result_correct = exists(correct_result_pattern, in_region=result_region)
         assert_true(self, result_correct, 'Actual and expected drop results are equal.')
         select_location_bar()
 
@@ -85,6 +105,6 @@ class Test(BaseTest):
         drop_not_matched = exists(drop_not_matching_pattern)
         assert_true(self, drop_not_matched, '"Not matched" appeared')
 
-        wrong_result = not exists(correct_result_pattern, DEFAULT_UI_DELAY)
+        wrong_result = not exists(correct_result_pattern, in_region=result_region)
         assert_true(self, wrong_result, 'The expected result is different to result.')
         close_window()
