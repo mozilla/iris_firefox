2a8b3004cbf8e94d3862e81a66a1a1ea520d8dc1
Dmitry Bakaev <dbakaev@mozilla.com>
2019-01-23 18:02:13 +0200

    Moved profile configuration to setup

--- iris/tests/privacy/saved_cc_available_in_private_session.py
+++ iris/tests/privacy/saved_cc_available_in_private_session.py
@@ -14,6 +14,9 @@ class Test(BaseTest):
         self.test_case_id = '101667'
         self.test_suite_id = '1956'
         self.locales = ['en-US']
+
+    def setup(self):
+        BaseTest.setup(self)
         self.set_profile_pref({'extensions.formautofill.available': 'on',
                                'extensions.formautofill.creditCards.available': True})
 
