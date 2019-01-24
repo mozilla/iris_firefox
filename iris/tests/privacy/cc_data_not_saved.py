43b3884a0f97a728cab2a33f95ef229523a8309a
Dmitry Bakaev <dbakaev@mozilla.com>
2019-01-24 17:38:00 +0200

    Changed similarity to the most of patterns due to OSX failures Code cleanup

--- iris/tests/privacy/cc_data_not_saved.py
+++ iris/tests/privacy/cc_data_not_saved.py
@@ -20,19 +20,20 @@ class Test(BaseTest):
     def run(self):
         private_browsing_image_pattern = PrivateWindow.private_window_pattern
         visa_logo_pattern = Pattern('visa_logo.png')
-        card_number_field_pattern = Pattern('card_number_field.png')
-        expiration_month_field_pattern = Pattern('expiration_month_field.png')
-        expiration_year_field_pattern = Pattern('expiration_year_field.png')
-        csc_field_pattern = Pattern('csc_field.png')
-        submit_button_pattern = Pattern('submit_button.png')
-        entered_csc_pattern = Pattern('entered_csc.png')
-        find_in_preferences_field_pattern = Pattern('find_in_preferences_field.png')
-        saved_credit_cards_button_pattern = Pattern('saved_credit_cards_button.png')
-        name_field_pattern = Pattern('name_field.png')
+        card_number_field_pattern = Pattern('card_number_field.png').similar(.6)
+        expiration_month_field_pattern = Pattern('expiration_month_field.png').similar(.6)
+        expiration_year_field_pattern = Pattern('expiration_year_field.png').similar(.6)
+        csc_field_pattern = Pattern('csc_field.png').similar(.6)
+        submit_button_pattern = Pattern('submit_button.png').similar(.6)
+        entered_csc_pattern = Pattern('entered_csc.png').similar(.6)
+        find_in_preferences_field_pattern = Pattern('find_in_preferences_field.png').similar(.6)
+        saved_credit_cards_button_pattern = Pattern('saved_credit_cards_button.png').similar(.6)
+        name_field_pattern = Pattern('name_field.png').similar(.6)
 
         new_private_window()
         navigate('https://luke-chang.github.io/autofill-demo/basic_cc.html')
-        page_opened_in_private_browsing_mode = exists(private_browsing_image_pattern) and exists(submit_button_pattern)
+        page_opened_in_private_browsing_mode = exists(private_browsing_image_pattern, DEFAULT_FIREFOX_TIMEOUT) \
+                                               and exists(submit_button_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
         assert_true(self, page_opened_in_private_browsing_mode, 'Test page is opened in a new private window.')
 
         input_data = {
