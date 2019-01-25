34cf4a4636b0f3b8c54ef3e3ca9519fc3858f59e
Paul Prokhorov <paul@exaltcg.com>
2019-01-25 18:26:55 +0200

    Sorted functions in general.py and fixed docstrings for repeat_key_down_until_image_found and repeat_key_up_until_image_found functions

--- iris/api/helpers/general.py
+++ iris/api/helpers/general.py
@@ -1011,6 +1011,32 @@ def repeat_key_down(num):
         type(Key.DOWN)
 
 
+def repeat_key_down_until_image_found(image_pattern, num_of_key_down_presses=10, delay_between_presses=3):
+    """
+    Press the Key Down button until specified image pattern is found.
+
+    :param image_pattern: Image Pattern to search.
+    :param num_of_key_down_presses: Number of presses of the Key Down button.
+    :param delay_between_presses: Number of seconds to wait between the Key Down presses
+    :return: Boolean. True if image pattern found during Key Down button pressing, False otherwise
+    """
+
+    if not isinstance(image_pattern, Pattern):
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    pattern_found = False
+
+    for _ in range(num_of_key_down_presses):
+        pattern_found = exists(image_pattern)
+        if pattern_found:
+            break
+
+        type(Key.DOWN)
+        time.sleep(delay_between_presses)
+
+    return pattern_found
+
+
 def repeat_key_up(num):
     """Repeat UP keystroke a given number of times.
 
@@ -1019,6 +1045,32 @@ def repeat_key_up(num):
     """
     for i in range(num):
         type(Key.UP)
+
+
+def repeat_key_up_until_image_found(image_pattern, num_of_key_up_presses=10, delay_between_presses=3):
+    """
+    Press the Key Up button until specified image pattern is found.
+
+    :param image_pattern: Image Pattern to search.
+    :param num_of_key_up_presses: Number of presses of the Key Up button.
+    :param delay_between_presses: Number of seconds to wait between the Key Down presses
+    :return: Boolean. True if image pattern found during the Key Up button pressing, False otherwise
+    """
+
+    if not isinstance(image_pattern, Pattern):
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    pattern_found = False
+
+    for _ in range(num_of_key_up_presses):
+        pattern_found = exists(image_pattern)
+        if pattern_found:
+            break
+
+        type(Key.UP)
+        time.sleep(delay_between_presses)
+
+    return pattern_found
 
 
 def reset_mouse():
@@ -1089,6 +1141,55 @@ def restore_window_from_taskbar(option=None):
         if Settings.get_os() == Platform.LINUX:
             hover(Location(0, 50))
     time.sleep(Settings.UI_DELAY)
+
+
+def scroll_until_pattern_found(image_pattern, scroll_function, scroll_params, num_of_scroll_iterations=10, timeout=5):
+    """
+    Scrolls until specified image pattern is found.
+
+    :param image_pattern: Image Pattern to search.
+    :param scroll_function: Scrolling function or any callable object (e.g. type, scroll, etc.)
+    :param scroll_params: Tuple of params to pass in the scroll_function
+            (e.g. (Key.UP, ) or (Key.UP, KeyModifier.CTRL) for the type function).
+            NOTE: Tuple should contains from 1 to 2 items.
+    :param num_of_scroll_iterations: Number of scrolling iterations.
+    :param timeout: Number of seconds passed to the 'timeout' param of the 'exist' function.
+    :return: Boolean. True if image pattern found during scrolling, False otherwise
+    """
+
+    scroll_arg = None
+    scroll_modifier = None
+
+    if not isinstance(image_pattern, Pattern):
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    if not callable(scroll_function):
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    if not isinstance(scroll_params, tuple):
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    if len(scroll_params) == 2:
+        scroll_arg, scroll_modifier = scroll_params
+    elif len(scroll_params) == 1:
+        scroll_arg, = scroll_params
+    else:
+        raise ValueError(INVALID_GENERIC_INPUT)
+
+    pattern_found = False
+
+    for _ in range(num_of_scroll_iterations):
+        pattern_found = exists(image_pattern, timeout)
+
+        if pattern_found:
+            break
+
+        if scroll_modifier is None:
+            scroll_function(scroll_arg)
+        else:
+            scroll_function(scroll_arg, scroll_modifier)
+
+    return pattern_found
 
 
 def select_location_bar_option(option_number):
@@ -1212,107 +1313,4 @@ class ZoomType(object):
 
     IN = 300 if Settings.is_windows() else 1
     OUT = -300 if Settings.is_windows() else -1
-
-
-def repeat_key_down_until_image_found(image_pattern, num_of_key_down_presses=10, delay_between_presses=3):
-    """
-    Press the Key Down button until specified image pattern is found.
-    Raise exception if image pattern wasn't found after specified number of presses of the Key Down button.
-
-    :param image_pattern: Image Pattern to search.
-    :param num_of_key_down_presses: Number of presses of the Key Down button.
-    :param delay_between_presses: Number of seconds to wait between the Key Down presses
-    :return: Boolean. True if image pattern found during Key Down button pressing, False otherwise
-    """
-
-    if not isinstance(image_pattern, Pattern):
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    pattern_found = False
-
-    for _ in range(num_of_key_down_presses):
-        pattern_found = exists(image_pattern)
-        if pattern_found:
-            break
-
-        type(Key.DOWN)
-        time.sleep(delay_between_presses)
-
-    return pattern_found
-
-
-def repeat_key_up_until_image_found(image_pattern, num_of_key_up_presses=10, delay_between_presses=3):
-    """
-    Press the Key Up button until specified image pattern is found.
-    Raise exception if image pattern wasn't found after specified number of presses of the Key Up button.
-
-    :param image_pattern: Image Pattern to search.
-    :param num_of_key_up_presses: Number of presses of the Key Up button.
-    :param delay_between_presses: Number of seconds to wait between the Key Down presses
-    :return: Boolean. True if image pattern found during the Key Up button pressing, False otherwise
-    """
-
-    if not isinstance(image_pattern, Pattern):
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    pattern_found = False
-
-    for _ in range(num_of_key_up_presses):
-        pattern_found = exists(image_pattern)
-        if pattern_found:
-            break
-
-        type(Key.UP)
-        time.sleep(delay_between_presses)
-
-    return pattern_found
-
-
-def scroll_until_pattern_found(image_pattern, scroll_function, scroll_params, num_of_scroll_iterations=10, timeout=5):
-    """
-    Scrolls until specified image pattern is found.
-
-    :param image_pattern: Image Pattern to search.
-    :param scroll_function: Scrolling function or any callable object (e.g. type, scroll, etc.)
-    :param scroll_params: Tuple of params to pass in the scroll_function
-            (e.g. (Key.UP, ) or (Key.UP, KeyModifier.CTRL) for the type function).
-            NOTE: Tuple should contains from 1 to 2 items.
-    :param num_of_scroll_iterations: Number of scrolling iterations.
-    :param timeout: Number of seconds passed to the 'timeout' param of the 'exist' function.
-    :return: Boolean. True if image pattern found during scrolling, False otherwise
-    """
-
-    scroll_arg = None
-    scroll_modifier = None
-
-    if not isinstance(image_pattern, Pattern):
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    if not callable(scroll_function):
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    if not isinstance(scroll_params, tuple):
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    if len(scroll_params) == 2:
-        scroll_arg, scroll_modifier = scroll_params
-    elif len(scroll_params) == 1:
-        scroll_arg, = scroll_params
-    else:
-        raise ValueError(INVALID_GENERIC_INPUT)
-
-    pattern_found = False
-
-    for _ in range(num_of_scroll_iterations):
-        pattern_found = exists(image_pattern, timeout)
-
-        if pattern_found:
-            break
-
-        if scroll_modifier is None:
-            scroll_function(scroll_arg)
-        else:
-            scroll_function(scroll_arg, scroll_modifier)
-
-    return pattern_found
 
