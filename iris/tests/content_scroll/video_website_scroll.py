c8412d92be6fbf6d32eabe6ee462ae737acc50a7
Sergey Pronoza <sergey@exaltcg.com>
2019-01-25 10:53:41 +0200

    Added Win 10 support

--- iris/tests/content_scroll/video_website_scroll.py
+++ iris/tests/content_scroll/video_website_scroll.py
@@ -16,10 +16,10 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        show_more_button_pattern = Pattern('show_more_button.png')
         speaker_icon_pattern = Pattern('speaker_icon.png')
         youtube_info_button_pattern = Pattern('youtube_info_button.png')
-        youtube_like_button_pattern = Pattern('subscribe_button.png')
+        youtube_subscribe_button_pattern = Pattern('subscribe_button.png')
+        youtube_autoplay_switch_pattern = Pattern('youtube_autoplay_switch.png')
 
         mouse_wheel_steps = 20
         if Settings.is_windows():
@@ -28,7 +28,7 @@ class Test(BaseTest):
         navigate('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
         speaker_icon_displayed = exists(speaker_icon_pattern, DEFAULT_HEAVY_SITE_LOAD_TIMEOUT)
-        youtube_page_is_downloaded = exists(speaker_icon_pattern, DEFAULT_HEAVY_SITE_LOAD_TIMEOUT)
+        youtube_page_is_downloaded = exists(youtube_info_button_pattern, DEFAULT_HEAVY_SITE_LOAD_TIMEOUT)
         assert_true(self, youtube_page_is_downloaded and speaker_icon_displayed, 'Youtube is properly loaded')
 
         focusing_inside_the_page = find(NavBar.HOME_BUTTON).offset(300, 300)
@@ -37,31 +37,24 @@ class Test(BaseTest):
         # Scroll by mouse wheel
         for times_scroll_down in range(20):
             scroll(-mouse_wheel_steps)
-            youtube_like_button_displayed = exists(youtube_like_button_pattern)
-            if youtube_like_button_displayed:
-                assert_true(self, youtube_like_button_displayed, 'Successfully scrolled to like button by mouse scroll')
+            youtube_subscribe_button_displayed = exists(youtube_subscribe_button_pattern)
+            if youtube_subscribe_button_displayed:
+                assert_true(self, youtube_subscribe_button_displayed, 'Successfully scrolled to comment section'
+                                                                      ' by mouse scroll')
                 break
 
         for times_scroll_down in range(20):
             scroll(-mouse_wheel_steps)
-            youtube_like_button_displayed = exists(youtube_like_button_pattern)
-            if not youtube_like_button_displayed:
-                assert_false(self, youtube_like_button_displayed, 'Successfully scrolled to comment section'
-                                                                  ' by mouse scroll')
-                break
-
-        for times_scroll_down in range(20):
-            scroll(-mouse_wheel_steps)
-            show_more_button_displayed = exists(show_more_button_pattern)
-            if show_more_button_displayed:
-                assert_true(self, show_more_button_displayed, 'Successfully scrolled to the end of the suggestions'
-                                                              ' by mouse scroll')
+            youtube_subscribe_button_displayed = exists(youtube_subscribe_button_pattern)
+            if not youtube_subscribe_button_displayed:
+                assert_false(self, youtube_subscribe_button_displayed, 'Successfully scrolled to comment section'
+                                                                       ' by mouse scroll')
                 break
 
         for times_scroll_up in range(20):
             scroll(mouse_wheel_steps)
             hover(focusing_inside_the_page)
-            top_of_the_page_destinated = exists(youtube_info_button_pattern)
+            top_of_the_page_destinated = exists(youtube_autoplay_switch_pattern)
             if top_of_the_page_destinated:
                 assert_true(self, top_of_the_page_destinated, 'Successfully scrolled to the top of the page by mouse '
                                                               'scroll')
