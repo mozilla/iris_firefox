d0f7db6bdc67e1aca43988540c32dad46c8148b7
icrisan <icrisan@mozilla.com>
2019-02-05 21:10:35 +0200

    Refactore test case to detect locale.

--- iris/tests/search/default_search_code_google.py
+++ iris/tests/search/default_search_code_google.py
@@ -14,10 +14,6 @@ class Test(BaseTest):
         self.test_case_id = '218333'
         self.test_suite_id = '83'
 
-    # def setup(self):
-    #     BaseTest.setup(self)
-    #     self.set_profile_pref({'browser.search.region': 'US'})
-
     def run(self):
         # Detect the build.
         if get_firefox_channel(self.browser.path) == 'beta':
@@ -37,69 +33,84 @@ class Test(BaseTest):
             search_google_for_pattern = Pattern('search_google_for_esr_build.png')
             non_us_client_search_code_pattern = Pattern('non_us_client_search_code_esr_build.png')
 
-        firefoxes = {'en-US': ['US','in', 'id', 'ca'], 'de': ['de'], 'fr': ['fr'], 'pl': ['pl'], 'it': ['it'],
-                     'pt-BR': ['BR'], 'ja': ['ja'], 'es-ES': ['ES'], 'en-GB': ['GB']}
+        regions_by_locales = {'en-US': ['US', 'in', 'id', 'ca'], 'de': ['de'], 'fr': ['fr'], 'pl': ['pl'], 'it': ['it'],
+                              'pt-BR': ['BR'], 'ja': ['ja'], 'es-ES': ['ES'], 'en-GB': ['GB']}
 
-        for key in firefoxes.keys():
-            for value in firefoxes.get(key):
-                navigate('about:preferences#search')
-                expected = exists(default_search_engine_google_pattern, 10)
-                assert_true(self, expected, 'Google is the default search engine.')
+        for value in regions_by_locales.get(self.browser.locale):
+            change_preference('browser.search.region', value)
+            restart_firefox(self,
+                            self.browser.path,
+                            self.profile_path,
+                            LocalWeb.FIREFOX_TEST_SITE,
+                            image=LocalWeb.FIREFOX_LOGO)
 
-                expected = exists(add_search_bar_in_toolbar_pattern, 10)
-                assert_true(self, expected, 'Option is visible in the page.')
+            navigate('about:preferences#search')
+            expected = exists(default_search_engine_google_pattern, 10)
+            assert_true(self, expected, 'Google is the default search engine.')
 
-                click(add_search_bar_in_toolbar_pattern)
+            expected = exists(add_search_bar_in_toolbar_pattern, 10)
+            assert_true(self, expected, 'Option is visible in the page.')
 
-                expected = exists(search_bar_pattern, 10)
-                assert_true(self, expected, 'The search bar is properly enabled in toolbar.')
+            click(add_search_bar_in_toolbar_pattern)
 
-                # Perform a search using the awesome bar and then clear the content from it.
-                select_location_bar()
-                paste('test')
-                type(Key.ENTER)
+            expected = exists(search_bar_pattern, 10)
+            assert_true(self, expected, 'The search bar is properly enabled in toolbar.')
 
+            # Perform a search using the awesome bar and then clear the content from it.
+            select_location_bar()
+            paste('test')
+            type(Key.ENTER)
+
+            if value != 'US':
+                expected = exists(non_us_client_search_code_pattern, 10)
+                assert_true(self, expected, 'Client search code is correct for searches from awesome bar.')
+            else:
                 expected = exists(client_search_code_pattern, 10)
                 assert_true(self, expected, 'Client search code is correct for searches from awesome bar.')
 
-                select_location_bar()
-                type(Key.DELETE)
+            select_location_bar()
+            type(Key.DELETE)
 
-                # Perform a search using the search bar.
-                click(search_bar_pattern)
-                paste('test')
-                type(Key.ENTER)
+            # Perform a search using the search bar.
+            click(search_bar_pattern)
+            paste('test')
+            type(Key.ENTER)
 
+            if value != 'US':
+                expected = exists(non_us_client_search_code_pattern, 10)
+                assert_true(self, expected, 'Client search code is correct for searches from search bar.')
+            else:
                 expected = exists(client_search_code_pattern, 10)
                 assert_true(self, expected, 'Client search code is correct for searches from search bar.')
 
-                select_location_bar()
-                type(Key.DELETE)
+            select_location_bar()
+            type(Key.DELETE)
 
-                close_content_blocking_pop_up()
+            close_content_blocking_pop_up()
 
-                double_click(test_pattern)
-                right_click(test_pattern)
+            double_click(test_pattern)
+            right_click(test_pattern)
 
-                expected = exists(search_google_for_pattern, 10)
-                assert_true(self, expected, 'Option is visible.')
+            expected = exists(search_google_for_pattern, 10)
+            assert_true(self, expected, 'Option is visible.')
 
-                click(search_google_for_pattern)
-                time.sleep(DEFAULT_UI_DELAY)
+            click(search_google_for_pattern)
+            time.sleep(DEFAULT_UI_DELAY)
 
+            if value != 'US':
+                expected = exists(non_us_client_search_code_pattern, 10)
+                assert_true(self, expected, 'Client search code is correct.')
+            else:
                 expected = exists(client_search_code_pattern, 10)
                 assert_true(self, expected, 'Client search code is correct.')
 
-                new_tab()
-                paste('test')
-                type(Key.ENTER)
+            new_tab()
+            paste('test')
+            type(Key.ENTER)
 
+            if value != 'US':
+                expected = exists(non_us_client_search_code_pattern, 10)
+                assert_true(self, expected, 'Client search code is correct for searches from about:newtab page.')
+            else:
                 expected = exists(client_search_code_pattern, 10)
                 assert_true(self, expected, 'Client search code is correct for searches from about:newtab page.')
-
-                change_preference('browser.search.region', value)
-                restart_firefox(self,
-                                self.browser.path,
-                                self.profile_path,
-                                LocalWeb.FIREFOX_TEST_SITE,
-                                image=LocalWeb.FIREFOX_LOGO)
