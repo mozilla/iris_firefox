15828f6464c8652001a72c6fcdc4cc1a730fd420
mariusandreifilip <afilip@mozilla.com>
2019-02-05 16:42:17 +0200

    moved_iris_pluggin_into_target

--- src/__main__.py
+++ src/__main__.py
@@ -15,7 +15,6 @@ from src.core.util import cleanup
 from src.core.util.logger_manager import initialize_logger
 from src.core.util.path_manager import PathManager
 from src.core.util.system import check_7zip, fix_terminal_encoding, init_tesseract_path, reset_terminal_encoding
-from src.iris_pytest_plugin import Plugin
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +26,7 @@ def main():
         target_plugin = get_target(args.application)
         pytest_args = get_test_params(args.application)
         initialize_platform(args)
-        pytest.main(pytest_args, plugins=[target_plugin, Plugin()])
+        pytest.main(pytest_args, plugins=[target_plugin])
     else:
         logger.error('Failed platform verification.')
         exit(1)
--- src/base/target.py
+++ src/base/target.py
@@ -2,8 +2,95 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import pytest
+
+from src.core.api.arg_parser import parse_args
+
+
 class BaseTarget(object):
 
     def __init__(self):
         self.target_name = 'Default target'
         self.cc_settings = {}
+
+    def pytest_sessionstart(self, session):
+        """ called after the ``Session`` object has been created and before performing collection
+           and entering the run test loop.
+
+           :param _pytest.main.Session session: the pytest session object
+        """
+        print('\n\n** Test session {} started **\n'.format(session.name))
+        print('\nIris settings: \n')
+        settings_list = []
+        args = parse_args()
+        for arg in vars(args):
+            settings_list.append('{}: {}'.format(arg, getattr(args, arg)))
+        print(', '.join(settings_list))
+        print('\n')
+
+    def pytest_sessionfinish(self, session):
+        """ called after whole test run finished, right before returning the exit status to the system.
+
+           :param _pytest.main.Session session: the pytest session object
+           :param int exitstatus: the status which pytest will return to the system
+        """
+        print("\n\n** Test session {} complete **\n".format(session.name))
+
+    def pytest_runtestloop(self, session):
+        pass
+        # print('RUN TEST LOOP')
+
+    def pytest_runtest_logstart(self, nodeid, location):
+        pass
+
+    def pytest_runtest_call(self, item):
+        pass
+        # print('Executing: [{}]'.format(item.parent.parent.name))
+
+    def pytest_runtest_logfinish(self, nodeid, location):
+        pass
+
+    @pytest.fixture
+    def option(self,pytestconfig):
+        """
+        fixture for ovewriting values in pytest.ini file
+
+        :return: Option Object
+        """
+
+        new_value = {}
+
+        class Options:
+
+            def get(self, name):
+                return new_value.get(name, pytestconfig.getini(name))
+
+        return Options()
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_runtest_makereport(self, item, call,):
+        """ return a :py:class:`_pytest.runner.TestReport` object
+            for the given :py:class:`pytest.Item <_pytest.main.Item>` and
+            :py:class:`_pytest.runner.CallInfo`.
+
+            Stops at first non-None result
+            """
+
+        if call.when == "call" and call.excinfo is not None:
+            outcome = "Failed"
+            assert_info = call.excinfo
+            # print ('Assertion that fails is :',assert_info)
+
+            object=(item.instance,assert_info)
+
+            self.failed_tests.append(object)
+
+        elif call.when == "call" and call.excinfo is  None:
+            outcome='Passed'
+            pass
+
+def reason_for_failure(report):
+    if report.outcome == 'passed':
+        return ''
+    else:
+        return report.longreprtext
