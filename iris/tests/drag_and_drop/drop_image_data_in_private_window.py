2327c577531cab83a3d7bcdcd37e1eb0d3b3886f
Dmitry Bakaev <dbakaev@mozilla.com>
2019-02-05 16:49:38 +0200

    Changed blocker id

--- iris/tests/drag_and_drop/drop_image_data_in_private_window.py
+++ iris/tests/drag_and_drop/drop_image_data_in_private_window.py
@@ -14,7 +14,7 @@ class Test(BaseTest):
         self.test_case_id = '165087'
         self.test_suite_id = '102'
         self.locales = ['en-US']
-        self.blocked_by = '1837'
+        self.blocked_by = '1328964'
         self.exclude = Platform.ALL
 
     def setup(self):
