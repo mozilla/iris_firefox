8468bec5b6fd595237e08d5440d7b9ae5bf880a3
rrobotin <rrobotin@mozilla.com>
2019-02-08 15:45:17 +0200

    Added wait time on pattern search

--- iris/tests/history/library_open_all_in_tabs.py
+++ iris/tests/history/library_open_all_in_tabs.py
@@ -78,10 +78,12 @@ class Test(BaseTest):
         if close_auxiliary_window:
             library_toolbar_icon = Pattern('library_icon.png')
             firefox_toolbar_icon = Pattern('firefox_icon.png')
-            exists(firefox_toolbar_icon)
+            exists(firefox_toolbar_icon, 10)
             click(firefox_toolbar_icon)
-            exists(library_toolbar_icon)
+            exists(library_toolbar_icon, 10)
             click(library_toolbar_icon)
+
             click_window_control('close')
             time.sleep(DEFAULT_UI_DELAY)
+
             click(firefox_privacy_logo_pattern)
