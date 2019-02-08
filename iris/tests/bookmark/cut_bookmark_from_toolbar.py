be04e31c34d3484b0d11c56a784c2360897509ef
yeyartsev <eyartsev@mozilla.com>
2019-02-08 20:02:08 +0200

    Rebuilt according new description, running against win10

--- iris/tests/bookmark/cut_bookmark_from_toolbar.py
+++ iris/tests/bookmark/cut_bookmark_from_toolbar.py
@@ -10,45 +10,58 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = 'Cut a bookmark from the Bookmarks toolbar submenu.'
-        self.test_case_id = '171638'
+        self.meta = 'Cut items from "Bookmarks Toolbar"'
+        self.test_case_id = '164371'
         self.test_suite_id = '2525'
         self.locale = ['en-US']
         self.exclude = [Platform.MAC, Platform.LINUX]
 
+    def setup(self):
+        BaseTest.setup(self)
+        self.profile = Profile.TEN_BOOKMARKS
+
     def run(self):
-        bookmarks_top_menu_pattern = Pattern('bookmarks_top_menu.png')
+        bookmark_cut_pattern = Pattern('bookmark_cut.png').similar(0.98)
+        open_bookmark_toolbar_pattern = Pattern('open_bookmark_toolbar.png')
+        most_visited_pattern = Pattern('drag_area.png')
+        new_folder_pattern = Pattern('new_folder.png')
+        bookmarks_folder_pattern = Pattern('bookmarks_folder.png')
         cut_item_pattern = Pattern('cut_bookmark.png')
         paste_item_pattern = Pattern('paste_bookmark.png')
         getting_started_bookmark_pattern = Pattern('getting_started_bookmark.png')
-        firefox_bookmarks_folder_pattern = Pattern('firefox_bookmarks_folder.png')
-        open_all_pattern = Pattern('open_all.png')
 
-        type(Key.ALT)
-        firefox_menu_opened = exists(bookmarks_top_menu_pattern)
-        assert_true(self, firefox_menu_opened, 'Firefox menu opened')
-        click(bookmarks_top_menu_pattern)
+        navbar_offset, _ = NavBar.HOME_BUTTON.get_size()
+        navbar_offset *= 1.5
+        right_click(NavBar.HOME_BUTTON.target_offset(navbar_offset, 0))
+        click(open_bookmark_toolbar_pattern)
 
-        bookmarks_dropdown_opened = exists(Library.BOOKMARKS_TOOLBAR)
-        assert_true(self, bookmarks_dropdown_opened, 'Bookmarks menu opened')
-        click(Library.BOOKMARKS_TOOLBAR)
+        bookmark_in_toolbar = exists(getting_started_bookmark_pattern)
+        assert_true(self, bookmark_in_toolbar, 'Bookmark is displayed in bookmark toolbar')
 
+        right_click(most_visited_pattern)
+        click(new_folder_pattern)
+        paste('folder')
+        type(Key.ENTER)
         right_click(getting_started_bookmark_pattern)
-
         click(cut_item_pattern)
 
-        click(firefox_bookmarks_folder_pattern)
-        right_click(open_all_pattern)
+        bookmark_is_cut = exists(bookmark_cut_pattern)
+        assert_true(self, bookmark_is_cut, 'Bookmark displayed in toolbar as cut')
 
+        click(bookmarks_folder_pattern)
+        folder_dropdown_offset_x, folder_dropdown_offset_y = bookmarks_folder_pattern.get_size()
+        folder_dropdown_offset_x //= 2
+        folder_dropdown_offset_y *= 1.5
+        right_click(bookmarks_folder_pattern.target_offset(folder_dropdown_offset_x, folder_dropdown_offset_y))
         click(paste_item_pattern)
 
-        bookmark_in_new_place = exists(getting_started_bookmark_pattern)
-        assert_true(self, bookmark_in_new_place, 'Bookmark is pasted in new place')
+        restore_firefox_focus()
 
-        click(Library.BOOKMARKS_TOOLBAR)
+        bookmark_removed_from_toolbar = not exists(getting_started_bookmark_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, bookmark_removed_from_toolbar, 'The bookmark is moved from toolbar')
 
-        bookmark_deleted_from_default = not exists(getting_started_bookmark_pattern)
-        assert_true(self, bookmark_deleted_from_default, 'bookmark_deleted_from_default')
+        click(bookmarks_folder_pattern)
 
-        click(NavBar.HAMBURGER_MENU)  # Needed for correct firefox exit
-        click(NavBar.HOME_BUTTON)
+        bookmark_in_folder = exists(getting_started_bookmark_pattern)
+        assert_true(self, bookmark_in_folder, '')
+        restore_firefox_focus()
