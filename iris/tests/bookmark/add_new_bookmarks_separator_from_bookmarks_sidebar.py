f10eb3be96bbc1e19e0e64c46a3c07fc3bea52a1
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-02-08 12:53:52 +0200

    Initial commit with script and win images for issue_1615

---
+++ iris/tests/bookmark/add_new_bookmarks_separator_from_bookmarks_sidebar.py
@@ -0,0 +1,56 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = 'Add a new bookmarks separator from Bookmarks Sidebar'
+        self.test_case_id = '171650'
+        self.test_suite_id = '2525'
+        self.locales = ['en-US']
+
+    def run(self):
+        separator_line_pattern = Pattern('separator.png')
+        bookmark_site_pattern = Pattern('bookmark_site.png')
+        mozilla_bookmark_pattern = Pattern('mozilla_bookmark.png')
+        new_separator_pattern = Library.Organize.NEW_SEPARATOR
+        bookmark_menu_pattern = Library.BOOKMARKS_MENU
+
+        bookmarks_sidebar('open')
+
+        bookmark_menu_exists = exists(bookmark_menu_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, bookmark_menu_exists, 'Bookmarks Sidebar is correctly displayed')
+        click(bookmark_menu_pattern)
+
+        mozilla_bookmark_exists = exists(mozilla_bookmark_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, mozilla_bookmark_exists, 'Mozilla bookmarks button exists')
+        click(mozilla_bookmark_pattern)
+
+        bookmark_site_exists = exists(bookmark_site_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, bookmark_site_exists, 'Website bookmark exists')
+        right_click(bookmark_site_pattern)
+
+        new_separator_exists = exists(new_separator_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, new_separator_exists, 'Separator button exists')
+        click(new_separator_pattern)
+
+        separator_line_exists = exists(separator_line_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, separator_line_exists, 'A new separator is displayed above the selected bookmark.')
+
+
+
+
+
+
+
+
+
+
+
+
