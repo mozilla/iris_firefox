ce57d3162cf0347c66d88664eedb05d7512c9a1a
Yegor Yartsev <eyartsev@mozilla.com>
2019-02-14 15:32:25 +0200

    Added script braching for non-windows systems

--- iris/tests/bookmark/edit_bookmark_from_mozilla_section.py
+++ iris/tests/bookmark/edit_bookmark_from_mozilla_section.py
@@ -16,29 +16,48 @@ class Test(BaseTest):
         self.locale = ['en-US']
 
     def run(self):
-        bookmark_properties_pattern = Pattern('bookmark_properties.png')
-        bookmarks_top_menu_pattern = Pattern('bookmarks_top_menu.png')
         customize_firefox_bookmark_pattern = Pattern('customize_firefox_bookmark.png')
         edited_bookmark_pattern = Pattern('edited_bookmark.png')
         firefox_bookmarks_folder_pattern = Pattern('firefox_bookmarks_folder.png')
         name_bookmark_field_pattern = Pattern('name_bookmark_field.png')
 
-        type(Key.ALT)
-        top_menu_displayed = exists(bookmarks_top_menu_pattern)
-        assert_true(self, top_menu_displayed, 'Firefox menu is displayed')
-        click(bookmarks_top_menu_pattern)
+        if Settings.is_windows():
+            bookmark_properties_pattern = Pattern('bookmark_properties.png')
+            bookmarks_top_menu_pattern = Pattern('bookmarks_top_menu.png')
+        else:
+            bookmarks_menu_pattern = Pattern('bookmarks_menu.png')
 
-        bookmarks_dropdown_opened = exists(firefox_bookmarks_folder_pattern)
-        assert_true(self, bookmarks_dropdown_opened, 'Bookmarks dropdown menu is opened')
+        if Settings.is_windows():
+
+            type(Key.ALT)
+            top_menu_displayed = exists(bookmarks_top_menu_pattern)
+            assert_true(self, top_menu_displayed, 'Firefox menu is displayed')
+            click(bookmarks_top_menu_pattern)
+
+        else:
+
+            open_library()
+            library_opened = exists(bookmarks_menu_pattern)
+            assert_true(self, library_opened, 'Library is opened')
+
+            _, bookmarks_menu_height = bookmarks_menu_pattern.get_size()
+            bookmarks_menu_height //= 2
+            click(bookmarks_menu_pattern.target_offset(0, bookmarks_menu_height))
+
+        bookmarks_folder_located = exists(firefox_bookmarks_folder_pattern)
+        assert_true(self, bookmarks_folder_located, 'Mozilla Firefox bookmarks folder is found')
         click(firefox_bookmarks_folder_pattern)
 
         default_bookmarks_displayed = exists(customize_firefox_bookmark_pattern)
         assert_true(self, default_bookmarks_displayed, 'Default bookmarks list is displayed')
-        right_click(customize_firefox_bookmark_pattern)
 
-        bookmark_dropdown_menu_opened = exists(bookmark_properties_pattern)
-        assert_true(self, bookmark_dropdown_menu_opened, 'Bookmark dropdown menu is opened')
-        click(bookmark_properties_pattern)
+        if Settings.is_windows():
+            right_click(customize_firefox_bookmark_pattern)
+            bookmark_dropdown_menu_opened = exists(bookmark_properties_pattern)
+            assert_true(self, bookmark_dropdown_menu_opened, 'Bookmark dropdown menu is opened')
+            click(bookmark_properties_pattern)
+        else:
+            click(customize_firefox_bookmark_pattern)
 
         bookmark_properties_displayed = exists(name_bookmark_field_pattern)
         assert_true(self, bookmark_properties_displayed, 'Bookmark properties are displayed')
@@ -47,15 +66,19 @@ class Test(BaseTest):
         paste('qwert')
         type(Key.ENTER)
 
-        type(Key.ALT)
-        click(bookmarks_top_menu_pattern)
-        click(firefox_bookmarks_folder_pattern)
+        if Settings.is_windows():
+            type(Key.ALT)
+            click(bookmarks_top_menu_pattern)
+            click(firefox_bookmarks_folder_pattern)
 
-        edited_tab_located = exists(edited_bookmark_pattern)
-        assert_true(self, edited_tab_located, 'edited_tab_located')
+        edited_bookmark_located = exists(edited_bookmark_pattern)
+        assert_true(self, edited_bookmark_located, 'Edited bookmark is located among default')
 
-        no_original_tab_located = not exists(customize_firefox_bookmark_pattern, DEFAULT_UI_DELAY)
-        assert_true(self, no_original_tab_located, 'no_original_tab_located')
+        no_original_bookmark_located = not exists(customize_firefox_bookmark_pattern, DEFAULT_UI_DELAY)
+        assert_true(self, no_original_bookmark_located, 'Default "Custom Firefox" bookmark isn\'t found')
 
-        click(NavBar.HAMBURGER_MENU)    # Is's required for correct process termination
-        restore_firefox_focus()
+        if Settings.is_windows():
+            click(NavBar.HAMBURGER_MENU)    # Is's required for correct process termination
+            restore_firefox_focus()
+        else:
+            close_tab()
