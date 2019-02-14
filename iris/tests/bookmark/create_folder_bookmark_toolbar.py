ef06db03a61b9dbd196ea297c06dc36536f0bbd1
pavelciapa <you@example.com>
2019-02-14 15:40:24 +0200

    extra tab for linux

--- iris/tests/bookmark/create_folder_bookmark_toolbar.py
+++ iris/tests/bookmark/create_folder_bookmark_toolbar.py
@@ -45,6 +45,8 @@ class Test(BaseTest):
         click(add_new_bookmark_pattern)
 
         # Fill in field and add bookmark
+        if Settings.is_linux():
+            type(Key.TAB, interval=DEFAULT_FX_DELAY)
         type(Key.TAB, interval=DEFAULT_FX_DELAY)
         paste('https://www.mozilla.org/en-US/firefox/central/')
         type(Key.TAB, interval=DEFAULT_FX_DELAY)
