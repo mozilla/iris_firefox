0921556d742d164ad0887819a58e721ddaf84800
yeyartsev <eyartsev@mozilla.com>
2019-02-18 18:29:33 +0200

    Running properly against win10

---
+++ iris/tests/bookmark/open_bookmark_from_top_menu.py
@@ -0,0 +1,41 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = 'Open a bookmark from \'Other Bookmarks\' section from Bookmarks menu'
+        self.test_suite_id = '2525'
+        self.test_case_id = '163207'
+        self.locale = ['en-US']
+
+    def setup(self):
+        BaseTest.setup(self)
+        self.profile = Profile.TEN_BOOKMARKS
+
+    def run(self):
+        bookmarks_top_menu_pattern = Pattern('bookmarks_top_menu.png')
+        other_bookmarks_pattern = Pattern('other_bookmarks.png')
+
+        if Settings.is_windows():
+            type(Key.ALT)
+
+        top_menu_displayed = exists(bookmarks_top_menu_pattern)
+        assert_true(self, top_menu_displayed, 'Top menu is displayed')
+
+        click(bookmarks_top_menu_pattern)
+        dropdown_displayed = exists(other_bookmarks_pattern)
+        assert_true(self, dropdown_displayed, 'Bookmark dropdown menu is displayed')
+
+        click(other_bookmarks_pattern)
+        bookmark_found = exists(LocalWeb.FIREFOX_BOOKMARK)
+        assert_true(self, bookmark_found, 'Needed bookmark is located in other bookmarks')
+
+        click(LocalWeb.FIREFOX_BOOKMARK)
+        webpage_loaded = exists(LocalWeb.FIREFOX_LOGO)
+        assert_true(self, webpage_loaded, 'Needed webpage is loaded')
