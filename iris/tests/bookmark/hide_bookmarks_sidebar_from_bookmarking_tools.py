614c53d974753e46ff61a13daf7408a7a41d2dfa
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-02-20 20:22:25 +0200

    Added changes to script and linux images for issue_2144

--- iris/tests/bookmark/hide_bookmarks_sidebar_from_bookmarking_tools.py
+++ iris/tests/bookmark/hide_bookmarks_sidebar_from_bookmarking_tools.py
@@ -86,6 +86,8 @@ class Test(BaseTest):
 
         click(hide_bookmarks_sidebar_pattern)
 
+        time.sleep(1)
+
         bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_pattern, DEFAULT_UI_DELAY_LONG)
         assert_false(self, bookmarks_sidebar_menu_exists, 'The bookmarks sidebar is dismissed')
 
