80c6992b7cda0ca537a78de0913f387e6d092bb3
Dmitry Bakaev <dbakaev@mozilla.com>
2019-02-22 17:20:57 +0200

    Changed test steps and bookmark to be pasted, removed unnecessary patterns and added new ones, removed close_tab() and close_window() function calls.

--- iris/tests/bookmark/paste_bookmark_in_different_section.py
+++ iris/tests/bookmark/paste_bookmark_in_different_section.py
@@ -10,22 +10,16 @@ class Test(BaseTest):
     def __init__(self):
         BaseTest.__init__(self)
         self.meta = 'Paste a bookmark in \'Other Bookmarks\' section from Bookmarks menu'
-        self.test_case_id = '171641'
+        self.test_case_id = '163217'
         self.test_suite_id = '2525'
         self.locales = ['en-US']
 
-    def setup(self):
-        BaseTest.setup(self)
-        self.profile = Profile.TEN_BOOKMARKS
-        return
-
     def run(self):
-        other_bookmarks_button_pattern = SidebarBookmarks.OTHER_BOOKMARKS
-        bookmarks_menu_button_pattern = Library.BOOKMARKS_MENU
-        copy_option_pattern = Pattern('copy_option.png')
+        bookmarks_toolbar_folder_pattern = Pattern('bookmarks_toolbar_folder.png')
+        getting_started_bookmark_pattern = Pattern('toolbar_bookmark_icon.png')
+        other_bookmarks_folder_pattern = Pattern('other_bookmarks_folder.png')
         paste_option_pattern = Pattern('paste_option.png')
-        wiki_favicon_bookmark_pattern = Pattern('wiki_bookmark_favicon.png')
-        bookmarks_folder_pasted_pattern = Pattern('bookmarks_folder_pasted.png')
+        copy_option_pattern = Pattern('copy_option.png')
 
         if Settings.is_mac():
             type(text='b', modifier=KeyModifier.CMD + KeyModifier.SHIFT)
@@ -34,33 +28,30 @@ class Test(BaseTest):
         elif Settings.is_linux():
             type(text='o', modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
-        other_bookmarks_button_available = exists(other_bookmarks_button_pattern, DEFAULT_SYSTEM_DELAY)
-        assert_true(self, other_bookmarks_button_available,
+        bookmarks_toolbar_folder_available = exists(bookmarks_toolbar_folder_pattern, DEFAULT_SYSTEM_DELAY)
+        assert_true(self, bookmarks_toolbar_folder_available,
+                    '\'Bookmarks toolbar\' button is available in \'Bookmarks Library\' menu.')
+
+        other_bookmarks_folder_available = exists(other_bookmarks_folder_pattern, DEFAULT_SYSTEM_DELAY)
+        assert_true(self, other_bookmarks_folder_available,
                     '\'Other bookmarks\' button is available in \'Bookmarks Library\' menu.')
 
-        click(other_bookmarks_button_pattern)
-        bookmarks_displayed = exists(wiki_favicon_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
+        click(bookmarks_toolbar_folder_pattern)
+        bookmarks_displayed = exists(getting_started_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
         assert_true(self, bookmarks_displayed,
-                    'The bookmarks saved in \'Other Bookmarks\' section are displayed.')
+                    'The bookmarks saved in \'Bookmarks toolbar\' section are displayed.')
 
-        bookmarks_menu_available = exists(bookmarks_menu_button_pattern, DEFAULT_UI_DELAY)
-        assert_true(self, bookmarks_menu_available,
-                    '\'Bookmarks menu\' button is available in \'Bookmarks Library\' menu.')
-
-        right_click(bookmarks_menu_button_pattern)
+        right_click(getting_started_bookmark_pattern)
         copy_option_available = exists(copy_option_pattern, DEFAULT_UI_DELAY)
         assert_true(self, copy_option_available,
-                    '\'Copy\' option is available after right-click on the \'Bookmarks menu\' icon.')
+                    '\'Copy\' option is available after right-click on the \'Bookmarks toolbar\' icon.')
 
         click(copy_option_pattern)
-        right_click(other_bookmarks_button_pattern)
+        right_click(other_bookmarks_folder_pattern)
         paste_option_available = exists(paste_option_pattern, DEFAULT_UI_DELAY)
         assert_true(self, paste_option_available,
                     '\'Paste\' option is available after right-click on the \'Other Bookmarks\' folder icon.')
 
         click(paste_option_pattern)
-        folder_pasted = exists(bookmarks_folder_pasted_pattern)
+        folder_pasted = exists(getting_started_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
         assert_true(self, folder_pasted, 'The file/folder is correctly pasted in the \'Other Bookmarks\' section.')
-
-        close_tab()
-        close_window()
