faf260ac1bc61955b1e6f1cca8343965534979c2
Sergey Pronoza <sergey@exaltcg.com>
2019-02-25 15:24:53 +0200

    Initial commit of issue 2131 for Win

---
+++ iris/tests/bookmark/add_a_new_bookmark_in_mozilla_firefox_section.py
@@ -0,0 +1,90 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = "Add a new bookmark in 'Mozilla Firefox' section "
+        self.test_case_id = "163373"
+        self.test_suite_id = "2525"
+        self.locale = ["en-US"]
+        self.exclude = [Platform.MAC]
+
+    def run(self):
+        firefox_menu_bookmarks_pattern = Pattern('firefox_menu_bookmarks.png')
+        mozilla_firefox_predefined_bookmarks_pattern = Pattern('mozilla_firefox_predefined_bookmarks.png')
+        mozilla_firefox_bookmarks_folder_pattern = Pattern('mozilla_firefox_bookmarks_folder.png')
+        mozilla_about_us_bookmark_pattern = Pattern('mozilla_about_us_bookmark.png')
+        context_menu_new_bookmark_pattern = Pattern('context_menu_new_bookmark.png')
+        new_bookmark_window_pattern = Pattern('new_bookmark_window.png')
+        soap_bookmark_pattern = Pattern('soap_wiki_tab.png')
+
+        location_to_hover = Location(0, 100)
+
+        hover(location_to_hover)
+        key_down(Key.ALT)
+        time.sleep(DEFAULT_FX_DELAY)
+        key_up(Key.ALT)
+
+        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern)
+        assert_true(self, firefox_menu_bookmarks_exists, 'Firefox menu > Bookmarks exists')
+
+        click(firefox_menu_bookmarks_pattern)
+
+        mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern)
+        assert_true(self, mozilla_firefox_bookmarks_folder_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
+                                                                   'bookmarks folder exists')
+        click(mozilla_firefox_bookmarks_folder_pattern)
+
+        mozilla_firefox_predefined_bookmarks_exists = exists(mozilla_firefox_predefined_bookmarks_pattern)
+        assert_true(self, mozilla_firefox_predefined_bookmarks_exists, 'Predefined Mozilla Firefox related bookmarks '
+                                                                       'displayed')
+
+        right_click(mozilla_about_us_bookmark_pattern)
+
+        context_menu_new_bookmark_pattern_displayed = exists(context_menu_new_bookmark_pattern)
+        assert_true(self, context_menu_new_bookmark_pattern_displayed, 'Context menu New Bookmark option is displayed')
+
+        click(context_menu_new_bookmark_pattern_displayed)
+
+        new_bookmark_window_opened = exists(new_bookmark_window_pattern)
+        assert_true(self, new_bookmark_window_opened, 'New Bookmark window is displayed')
+
+        paste('SOAP')
+        type(Key.TAB)
+        paste(LocalWeb.SOAP_WIKI_TEST_SITE)
+        type(Key.TAB)
+        paste('SOAP')
+        if Settings.is_mac():
+            type(Key.TAB)
+        else:
+            type(Key.TAB)
+            type(Key.TAB)
+        paste('SOAP')
+        type(Key.ENTER)
+
+        hover(location_to_hover)
+        key_down(Key.ALT)
+        time.sleep(DEFAULT_FX_DELAY)
+        key_up(Key.ALT)
+
+        firefox_menu_bookmarks__second_exists = exists(firefox_menu_bookmarks_pattern)
+        assert_true(self, firefox_menu_bookmarks__second_exists, 'Firefox menu > Bookmarks exists')
+
+        click(firefox_menu_bookmarks_pattern)
+
+        mozilla_firefox_bookmarks_folder_second_exists = exists(mozilla_firefox_bookmarks_folder_pattern)
+        assert_true(self, mozilla_firefox_bookmarks_folder_second_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
+                                                                          'bookmarks folder exists')
+        click(mozilla_firefox_bookmarks_folder_pattern)
+
+        bookmark_exists = exists(soap_bookmark_pattern)
+        assert_true(self, bookmark_exists, 'A new bookmark is added in Mozilla Firefox section.')
+
+        close_window()
