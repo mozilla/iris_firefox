0439594ddd833a023004c2481cf68de338e3be2a
Sergey Pronoza <sergey@exaltcg.com>
2019-02-26 11:59:10 +0200

    Initial commit of issue_2137 for Win

---
+++ iris/tests/bookmark/delete_a_bookmark_from_mozilla_firefox_section.py
@@ -0,0 +1,57 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from iris.test_case import *
+
+
+class Test(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.meta = "Delete a bookmark from 'Mozilla Firefox' section "
+        self.test_case_id = "163379"
+        self.test_suite_id = "2525"
+        self.locale = ["en-US"]
+        self.exclude = [Platform.MAC, Platform.LINUX]
+
+    def run(self):
+        firefox_menu_bookmarks_pattern = Pattern('firefox_menu_bookmarks.png')
+        mozilla_firefox_predefined_bookmarks_pattern = Pattern('mozilla_firefox_predefined_bookmarks.png')
+        mozilla_firefox_bookmarks_folder_pattern = Pattern('mozilla_firefox_bookmarks_folder.png')
+        mozilla_about_us_bookmark_pattern = Pattern('mozilla_about_us_bookmark.png')
+        delete_option_pattern = Pattern('delete_bookmark.png')
+
+        location_to_hover = Location(0, 100)
+
+        hover(location_to_hover)
+        key_down(Key.ALT)
+        time.sleep(DEFAULT_FX_DELAY)
+        key_up(Key.ALT)
+
+        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, firefox_menu_bookmarks_exists, 'Firefox menu > Bookmarks exists')
+
+        click(firefox_menu_bookmarks_pattern)
+
+        mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern,
+                                                         DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, mozilla_firefox_bookmarks_folder_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
+                                                                   'bookmarks folder exists')
+        click(mozilla_firefox_bookmarks_folder_pattern)
+
+        mozilla_firefox_predefined_bookmarks_exists = exists(mozilla_firefox_predefined_bookmarks_pattern,
+                                                             DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, mozilla_firefox_predefined_bookmarks_exists, 'Predefined Mozilla Firefox related bookmarks '
+                                                                       'displayed')
+
+        right_click(mozilla_about_us_bookmark_pattern)
+
+        cut_option_exists = exists(delete_option_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, cut_option_exists, 'The Delete option exists')
+
+        click(delete_option_pattern)
+
+        bookmark_deleted = exists(mozilla_about_us_bookmark_pattern)
+        assert_false(self, bookmark_deleted, 'Bookmark is correctly deleted from selected section')
