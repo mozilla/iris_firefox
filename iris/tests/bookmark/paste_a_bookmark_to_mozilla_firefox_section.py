c565ac63472f843d9a20f6ecf04347621ffcf82d
Sergey Pronoza <sergey@exaltcg.com>
2019-02-26 11:51:51 +0200

    Initial commit of issue 2136 for Win

--- iris/tests/bookmark/paste_a_bookmark_to_mozilla_firefox_section.py
+++ iris/tests/bookmark/paste_a_bookmark_to_mozilla_firefox_section.py
@@ -10,8 +10,8 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = "Copy a bookmark from 'Mozilla Firefox' section "
-        self.test_case_id = "163377"
+        self.meta = "Paste a bookmark in 'Mozilla Firefox' section "
+        self.test_case_id = "163378"
         self.test_suite_id = "2525"
         self.locale = ["en-US"]
         self.exclude = [Platform.MAC, Platform.LINUX]
@@ -23,7 +23,7 @@ class Test(BaseTest):
         mozilla_about_us_bookmark_pattern = Pattern('mozilla_about_us_bookmark.png')
         copy_option_pattern = Pattern('copy_option.png')
         paste_option_pattern = Pattern('paste_option.png')
-        other_bookmarks_empty_label_pattern = Pattern('other_bookmarks_empty_label.png')
+        getting_started_in_toolbar_pattern = Pattern('getting_started_in_toolbar.png')
 
         location_to_hover = Location(0, 100)
 
@@ -32,14 +32,30 @@ class Test(BaseTest):
         time.sleep(DEFAULT_FX_DELAY)
         key_up(Key.ALT)
 
-        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern)
+        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_menu_bookmarks_exists, 'Firefox menu > Bookmarks exists')
 
         click(firefox_menu_bookmarks_pattern)
 
+        bookmarks_toolbar_folder_exists = exists(Library.BOOKMARKS_TOOLBAR, DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, bookmarks_toolbar_folder_exists, 'Bookmarks toolbar folder exists')
+
+        click(Library.BOOKMARKS_TOOLBAR)
+
+        getting_started_bookmark_exists = exists(getting_started_in_toolbar_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, getting_started_bookmark_exists, 'Getting started bookmark exists')
+
+        right_click(getting_started_in_toolbar_pattern)
+
+        copy_option_exists = exists(copy_option_pattern)
+        assert_true(self, copy_option_exists, 'The Copy option exists')
+
+        click(copy_option_pattern)
+
         mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern)
         assert_true(self, mozilla_firefox_bookmarks_folder_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
                                                                    'bookmarks folder exists')
+
         click(mozilla_firefox_bookmarks_folder_pattern)
 
         mozilla_firefox_predefined_bookmarks_exists = exists(mozilla_firefox_predefined_bookmarks_pattern)
@@ -48,28 +64,10 @@ class Test(BaseTest):
 
         right_click(mozilla_about_us_bookmark_pattern)
 
-        cut_option_exists = exists(copy_option_pattern)
-        assert_true(self, cut_option_exists, 'The Copy option exists')
-
-        click(copy_option_pattern)
-
-        click(Library.OTHER_BOOKMARKS)
-
-        other_bookmarks_empty_label_exists = exists(other_bookmarks_empty_label_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, other_bookmarks_empty_label_exists, 'Other Bookmarks empty label exists')
-
-        right_click(other_bookmarks_empty_label_pattern)
-
         paste_option_exists = exists(paste_option_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, paste_option_exists, 'Paste option exists')
 
         click(paste_option_pattern)
 
-        bookmark_pasted = exists(mozilla_about_us_bookmark_pattern)
+        bookmark_pasted = exists(getting_started_in_toolbar_pattern)
         assert_true(self, bookmark_pasted, 'Bookmark is correctly pasted in selected section')
-
-        click(mozilla_firefox_bookmarks_folder_pattern)
-
-        bookmark_not_deleted = exists(mozilla_about_us_bookmark_pattern)
-        assert_true(self, bookmark_not_deleted, 'Bookmark pasted in selected section without being deleted from the '
-                                                'previous one.')
