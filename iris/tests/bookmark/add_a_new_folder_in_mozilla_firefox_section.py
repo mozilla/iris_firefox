4b5fc4e06cae6cca2f5a94cc0d99720c6f7c5dbf
Sergey Pronoza <sergey@exaltcg.com>
2019-02-26 15:11:18 +0200

    open_firefox_menu() function and timeouts added

--- iris/tests/bookmark/add_a_new_folder_in_mozilla_firefox_section.py
+++ iris/tests/bookmark/add_a_new_folder_in_mozilla_firefox_section.py
@@ -23,56 +23,51 @@ class Test(BaseTest):
         mozilla_about_us_bookmark_pattern = Pattern('mozilla_about_us_bookmark.png')
         iris_new_folder_pattern = Pattern('iris_new_folder.png')
 
-        location_to_hover = Location(0, 100)
+        open_firefox_menu()
 
-        hover(location_to_hover)
-        key_down(Key.ALT)
-        time.sleep(DEFAULT_FX_DELAY)
-        key_up(Key.ALT)
-
-        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern)
+        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_menu_bookmarks_exists, 'Firefox menu > Bookmarks exists')
 
         click(firefox_menu_bookmarks_pattern)
 
-        mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern)
+        mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern,
+                                                         DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, mozilla_firefox_bookmarks_folder_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
                                                                    'bookmarks folder exists')
         click(mozilla_firefox_bookmarks_folder_pattern)
 
-        mozilla_firefox_predefined_bookmarks_exists = exists(mozilla_firefox_predefined_bookmarks_pattern)
+        mozilla_firefox_predefined_bookmarks_exists = exists(mozilla_firefox_predefined_bookmarks_pattern,
+                                                             DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, mozilla_firefox_predefined_bookmarks_exists, 'Predefined Mozilla Firefox related bookmarks '
                                                                        'displayed')
 
         right_click(mozilla_about_us_bookmark_pattern)
 
-        new_folder_option_exists = exists(Library.Organize.NEW_FOLDER)
+        new_folder_option_exists = exists(Library.Organize.NEW_FOLDER, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, new_folder_option_exists, 'New Folder option exists')
 
         click(Library.Organize.NEW_FOLDER)
 
-        new_folder_window_opened = exists(Bookmarks.StarDialog.NEW_FOLDER_CREATED)
+        new_folder_window_opened = exists(Bookmarks.StarDialog.NEW_FOLDER_CREATED, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, new_folder_window_opened, 'New Folder window is displayed')
 
         paste('Iris New Folder')
         type(Key.ENTER)
 
-        hover(location_to_hover)
-        key_down(Key.ALT)
-        time.sleep(DEFAULT_FX_DELAY)
-        key_up(Key.ALT)
+        open_firefox_menu()
 
-        firefox_menu_bookmarks_second_exists = exists(firefox_menu_bookmarks_pattern)
+        firefox_menu_bookmarks_second_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_menu_bookmarks_second_exists, 'Firefox menu > Bookmarks exists')
 
         click(firefox_menu_bookmarks_pattern)
 
-        mozilla_firefox_bookmarks_folder_second_exists = exists(mozilla_firefox_bookmarks_folder_pattern)
+        mozilla_firefox_bookmarks_folder_second_exists = exists(mozilla_firefox_bookmarks_folder_pattern,
+                                                                DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, mozilla_firefox_bookmarks_folder_second_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
                                                                           'bookmarks folder exists')
         click(mozilla_firefox_bookmarks_folder_pattern)
 
-        folder_exists = exists(iris_new_folder_pattern)
+        folder_exists = exists(iris_new_folder_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, folder_exists, 'A new folder is added in Mozilla Firefox section.')
 
-        close_window()
+        restore_firefox_focus()
