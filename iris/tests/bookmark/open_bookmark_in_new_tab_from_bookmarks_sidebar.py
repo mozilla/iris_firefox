e52557b58718bf78e9a0c64cffde9305b8e5f112
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-01 14:15:48 +0200

    Added changes to script and images for all platform for issue_2172

--- iris/tests/bookmark/open_bookmark_in_new_tab_from_bookmarks_sidebar.py
+++ iris/tests/bookmark/open_bookmark_in_new_tab_from_bookmarks_sidebar.py
@@ -15,44 +15,45 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def setup(self):
-        """Test case setup
-        Override the setup method to use a pre-canned bookmarks profile.
-        """
         BaseTest.setup(self)
         self.profile = Profile.TEN_BOOKMARKS
 
     def run(self):
-        wikipedia_sidebar_logo_pattern = Pattern('wiki_logo.png')
-        if not Settings.is_linux():
-            open_in_new_tab_option_pattern = Pattern('open_in_new_tab.png')
+        firefox_sidebar_logo_pattern = Pattern('firefox_bookmark.png')
         new_tab_is_opened_pattern = Pattern('new_tab_opened.png').similar(0.90)
-        bookmarks_sidebar_menu_header_pattern = SidebarBookmarks.BOOKMARKS_HEADER
         if Settings.is_mac():
             other_bookmarks_pattern = Pattern('other_bookmarks.png')
         else:
             other_bookmarks_pattern = Library.OTHER_BOOKMARKS
+        if not Settings.is_linux():
+            open_in_new_tab_option_pattern = Pattern('open_in_new_tab.png')
 
         bookmarks_sidebar('open')
 
-        bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_sidebar_menu_exists = exists(SidebarBookmarks.BOOKMARKS_HEADER, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmarks_sidebar_menu_exists, 'Bookmarks Sidebar is correctly displayed.')
 
         other_bookmarks_exists = exists(other_bookmarks_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, other_bookmarks_exists, 'Other bookmarks exists')
+
         click(other_bookmarks_pattern)
 
-        wikipedia_sidebar_logo_exists = exists(wikipedia_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
-        assert_true(self, wikipedia_sidebar_logo_exists, 'Wikipedia bookmark exists')
-        right_click(wikipedia_sidebar_logo_pattern)
+        firefox_sidebar_logo_exists = exists(firefox_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
+        assert_true(self, firefox_sidebar_logo_exists, 'Firefox bookmark exists')
+
+        right_click(firefox_sidebar_logo_pattern)
 
         if not Settings.is_linux():
             open_option_exists = exists(open_in_new_tab_option_pattern, DEFAULT_FIREFOX_TIMEOUT)
             assert_true(self, open_option_exists, 'Open in new tab option exists')
             click(open_in_new_tab_option_pattern)
         else:
-            wikipedia_sidebar_logo_exists = exists(wikipedia_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
-            assert_false(self, wikipedia_sidebar_logo_exists, 'Open in new tab option exists')
+            firefox_sidebar_logo_exists = exists(firefox_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
+            assert_false(self, firefox_sidebar_logo_exists, 'Open in new tab option exists')
             type('w')
+
+        firefox_full_logo_exists = exists(LocalWeb.FIREFOX_IMAGE, DEFAULT_SITE_LOAD_TIMEOUT)
+        assert_true(self, firefox_full_logo_exists, 'Firefox content exists')
 
         new_tab_is_opened_not_exists = exists(new_tab_is_opened_pattern, DEFAULT_FIREFOX_TIMEOUT)
         assert_true(self, new_tab_is_opened_not_exists, 'The web page is opened at the new tab')
