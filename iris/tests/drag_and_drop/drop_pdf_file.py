e9160e7fa5f50acbbbecab6a8588a6a41e465f78
pavelciapa <you@example.com>
2019-03-04 12:24:38 +0200

    Added images for linux

--- iris/tests/drag_and_drop/drop_pdf_file.py
+++ iris/tests/drag_and_drop/drop_pdf_file.py
@@ -45,6 +45,7 @@ class Test(BaseTest):
         folderpath = self.get_asset_path('')
 
         navigate('https://mystor.github.io/dragndrop/')
+
         drop_html_data_button_displayed = exists(drop_pdf_file_button_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
         assert_true(self, drop_html_data_button_displayed, 'The demo website loaded successfully')
 
