96b8089300ab56e2645ad3543b68bb647945300b
dbakaev@mozilla.com <Dmitry Bakaev>
2019-03-04 19:27:48 +0200

    Separated clicks with empty lines, replaced DEFAULT_SYSTEM_DELAY and added assert for find().

--- iris/tests/bookmark/open_bookmark_from_toolbar_in_new_tab.py
+++ iris/tests/bookmark/open_bookmark_from_toolbar_in_new_tab.py
@@ -28,32 +28,40 @@ class Test(BaseTest):
         pocket_bookmark_icon_pattern = Pattern('pocket_bookmark_icon.png')
         iris_tab_pattern = Pattern('iris_tab.png')
 
+        iris_tab_exists = exists(iris_tab_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, iris_tab_exists, '\'Iris\' tab is displayed on the screen')
+
         area_to_click = find(iris_tab_pattern)
         area_to_click.x += 300
         area_to_click.y += 5
+
         right_click(area_to_click)
 
-        bookmarks_toolbar_menu_option_available = exists(bookmarks_toolbar_menu_option_pattern, DEFAULT_SYSTEM_DELAY)
+        bookmarks_toolbar_menu_option_available = exists(bookmarks_toolbar_menu_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmarks_toolbar_menu_option_available,
                     '\'Bookmarks Toolbar\' option is available in context menu')
 
         click(bookmarks_toolbar_menu_option_pattern)
+
         bookmarks_folder_available_in_toolbar = exists(most_visited_toolbar_bookmarks_folder_pattern,
                                                        DEFAULT_SYSTEM_DELAY)
         assert_true(self, bookmarks_folder_available_in_toolbar, 'The \'Bookmarks Toolbar\' is enabled.')
 
         click(most_visited_toolbar_bookmarks_folder_pattern)
+
         pocket_bookmark_available = exists(pocket_bookmark_icon_pattern)
         assert_true(self, pocket_bookmark_available,
                     '\'Pocket\' bookmark is available in the \'Most visited\' folder in toolbar')
 
         right_click(pocket_bookmark_icon_pattern)
+
         open_in_new_tab_option_available = exists(open_in_new_tab_option_pattern)
         assert_true(self, open_in_new_tab_option_available,
                     '\'Open in new tab\' option in available in context '
                     'menu after right-click at the bookmark in toolbar.')
 
         click(open_in_new_tab_option_pattern)
+
         select_tab(2)
         website_opened_in_new_tab = exists(pocket_image_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
         assert_true(self, website_opened_in_new_tab, 'The selected website is opened in a new tab.')
