61b43a2d12da1026abff8d086645dca94cc98309
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-05 12:32:22 +0200

    Added changes to script for issie_2174

--- iris/tests/bookmark/open_bookmark_in_new_private_window_from_bookmarks_sidebar.py
+++ iris/tests/bookmark/open_bookmark_in_new_private_window_from_bookmarks_sidebar.py
@@ -15,19 +15,15 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def setup(self):
-        """Test case setup
-        Override the setup method to use a pre-canned bookmarks profile.
-        """
         BaseTest.setup(self)
         self.profile = Profile.TEN_BOOKMARKS
 
     def run(self):
-        wikipedia_sidebar_logo_pattern = Pattern('wiki_logo.png')
-        new_private_window_pattern = PrivateWindow.private_window_pattern
-        if not Settings.is_linux():
-            open_in_new_window_option_pattern = Pattern('open_in_new_private_window.png')
-        wikipedia_main_page_content_pattern = Pattern('wikipedia_main_page.png')
         bookmarks_sidebar_menu_header_pattern = SidebarBookmarks.BOOKMARKS_HEADER
+        new_private_window_pattern = PrivateWindow.private_window_pattern
+        firefox_sidebar_logo_pattern = Pattern('firefox_bookmark.png')
+        open_in_new_window_option_pattern = Pattern('open_in_new_private_window.png')
+
         if Settings.is_mac():
             other_bookmarks_pattern = Pattern('other_bookmarks.png')
         else:
@@ -35,31 +31,28 @@ class Test(BaseTest):
 
         bookmarks_sidebar('open')
 
-        bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmarks_sidebar_menu_exists, 'Bookmarks Sidebar is correctly displayed.')
 
-        other_bookmarks_exists = exists(other_bookmarks_pattern, DEFAULT_UI_DELAY_LONG)
+        other_bookmarks_exists = exists(other_bookmarks_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, other_bookmarks_exists, 'Other bookmarks exists')
+
         click(other_bookmarks_pattern)
 
-        wikipedia_sidebar_logo_exists = exists(wikipedia_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
-        assert_true(self, wikipedia_sidebar_logo_exists, 'Wikipedia bookmark exists')
-        right_click(wikipedia_sidebar_logo_pattern)
+        firefox_sidebar_logo_exists = exists(firefox_sidebar_logo_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, firefox_sidebar_logo_exists, 'Firefox bookmark exists')
 
-        if not Settings.is_linux():
-            open_option_exists = exists(open_in_new_window_option_pattern, DEFAULT_FIREFOX_TIMEOUT)
-            assert_true(self, open_option_exists, 'Open in New Private Window option exists')
-            click(open_in_new_window_option_pattern)
-        else:
-            time.sleep(1)
-            wikipedia_sidebar_logo_exists = exists(wikipedia_sidebar_logo_pattern, DEFAULT_FIREFOX_TIMEOUT)
-            assert_false(self, wikipedia_sidebar_logo_exists, 'Open in a New Private Window option exists')
-            type('p')
+        right_click(firefox_sidebar_logo_pattern)
 
-        wikipedia_main_page_content_exists = exists(wikipedia_main_page_content_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, wikipedia_main_page_content_exists, 'The page is correctly opened')
+        open_option_exists = exists(open_in_new_window_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, open_option_exists, 'Open in New Private Window option exists')
 
-        new_private_window_exists = exists(new_private_window_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        click(open_in_new_window_option_pattern)
+
+        firefox_full_logo_exists = exists(LocalWeb.FIREFOX_IMAGE, DEFAULT_SITE_LOAD_TIMEOUT)
+        assert_true(self, firefox_full_logo_exists, 'Firefox content exists')
+
+        new_private_window_exists = exists(new_private_window_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, new_private_window_exists, 'The page is correctly opened in a new private window.')
 
         close_window()
