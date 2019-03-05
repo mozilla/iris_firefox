8fff1a5ca99a3e42bdc85c31405c625a633c3d01
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-05 14:47:14 +0200

    Added changes to script for issue_2175

--- iris/tests/bookmark/add_new_bookmark_from_bookmarks_sidebar.py
+++ iris/tests/bookmark/add_new_bookmark_from_bookmarks_sidebar.py
@@ -9,15 +9,14 @@ from iris.test_case import *
 class Test(BaseTest):
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = 'Add a New Bookmark from Bookmarks Sidebar '
+        self.meta = 'Add a New Bookmark from Bookmarks Sidebar'
         self.test_case_id = '168930'
         self.test_suite_id = '2525'
         self.locales = ['en-US']
 
     def run(self):
-        new_bookmark_created_pattern = Pattern('new_bookmark_created.png')
-        bookmarks_sidebar_menu_header_pattern = SidebarBookmarks.BOOKMARKS_HEADER
         new_bookmark_option_pattern = Library.Organize.NEW_BOOKMARK
+        new_bookmark_created_pattern = Pattern('new_bookmark_created.png')
         if not Settings.is_mac():
             bookmarks_menu_pattern = Library.BOOKMARKS_MENU
         else:
@@ -29,18 +28,20 @@ class Test(BaseTest):
 
         bookmarks_sidebar('open')
 
-        bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_sidebar_menu_exists = exists(SidebarBookmarks.BOOKMARKS_HEADER, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmarks_sidebar_menu_exists, 'Bookmarks Sidebar is correctly displayed.')
 
-        bookmarks_menu_exists = exists(bookmarks_menu_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_menu_exists = exists(bookmarks_menu_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmarks_menu_exists, 'Bookmarks menu section exists')
+
         right_click(bookmarks_menu_pattern)
 
-        new_bookmark_option_exists = exists(new_bookmark_option_pattern, DEFAULT_UI_DELAY_LONG)
+        new_bookmark_option_exists = exists(new_bookmark_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, new_bookmark_option_exists, 'New Bookmark option exists')
+
         click(new_bookmark_option_pattern)
 
-        new_bookmark_panel_exists = exists(new_bookmark_panel_pattern, DEFAULT_UI_DELAY_LONG)
+        new_bookmark_panel_exists = exists(new_bookmark_panel_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, new_bookmark_panel_exists, 'A New Bookmark... window is opened.')
 
         type(Key.TAB)
@@ -53,13 +54,14 @@ class Test(BaseTest):
             [type(Key.TAB) for _ in range(2)]
         else:
             type(Key.TAB)
+
         paste('search')
 
         type(Key.ENTER)
 
         click(bookmarks_menu_pattern)
 
-        new_bookmark_created_exists = exists(new_bookmark_created_pattern, DEFAULT_UI_DELAY_LONG)
+        new_bookmark_created_exists = exists(new_bookmark_created_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, new_bookmark_created_exists, 'The new bookmark is added in the selected section.')
 
 
