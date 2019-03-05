f3d554a5d99404694ef46b9eec65e5e2fa17d2cf
Dmitry Bakaev <dbakaev@mozilla.com>
2019-03-05 19:21:15 +0200

    Added images for osx support, open_library() function call, replaced DEFAULT_UI_DELAY_LONG timeouts, separated clicks with empty lines.

--- iris/tests/bookmark/open_all_in_tabs_from_recent_tags.py
+++ iris/tests/bookmark/open_all_in_tabs_from_recent_tags.py
@@ -34,38 +34,39 @@ class Test(BaseTest):
         tags_field_pattern = Pattern('tags_field.png')
         test_tag_pattern = Pattern('test_tag.png')
 
-        if Settings.is_mac():
-            type(text='b', modifier=KeyModifier.CMD + KeyModifier.SHIFT)
-        elif Settings.is_windows():
-            type(text='b', modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
-        elif Settings.is_linux():
-            type(text='o', modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
+        open_library()
 
-        library_menu_opened = exists(other_bookmarks_folder_pattern, DEFAULT_SYSTEM_DELAY)
+        library_menu_opened = exists(other_bookmarks_folder_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, library_menu_opened, '\'Library\' menu opened')
 
         click(other_bookmarks_folder_pattern)
+
         click(name_column_header_pattern)
-        firefox_bookmark_available = exists(firefox_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
+
+        firefox_bookmark_available = exists(firefox_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_bookmark_available,
                     '\'Firefox\' bookmark available in \'Other bookmarks\' folder in \'Library\' menu')
 
-        focus_bookmark_available = exists(focus_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
+        focus_bookmark_available = exists(focus_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, focus_bookmark_available,
                     '\'Focus\' bookmarks available in \'Other bookmarks\' folder in \'Library\' menu')
 
         click(firefox_bookmark_pattern)
+
         click(tags_field_pattern)
+
         paste('test')
         type(Key.ENTER)
-        bookmark_tagged = exists(firefox_bookmark_with_tag_pattern, DEFAULT_SYSTEM_DELAY)
+        bookmark_tagged = exists(firefox_bookmark_with_tag_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_tagged, '\'Firefox\' bookmark was tagged')
 
         click(focus_bookmark_pattern)
+
         click(tags_field_pattern)
+
         paste('test')
         type(Key.ENTER)
-        bookmark_tagged = exists(focus_bookmark_with_tag_pattern, DEFAULT_SYSTEM_DELAY)
+        bookmark_tagged = exists(focus_bookmark_with_tag_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_tagged, '\'Focus\' bookmark was tagged')
 
         close_tab()
@@ -75,20 +76,23 @@ class Test(BaseTest):
                     '\'Bookmarks\' option is available in Firefox top menu')
 
         click(top_menu_bookmarks_option_pattern)
-        recent_tags_option_available = exists(recent_tags_option_pattern, DEFAULT_UI_DELAY_LONG)
+
+        recent_tags_option_available = exists(recent_tags_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, recent_tags_option_available,
                     '\'Recent tags\' option available after clicking at \'Bookmarks option\' in Firefox top menu')
 
         mouse_move(recent_tags_option_pattern)
-        test_tag_displayed = exists(test_tag_pattern, DEFAULT_UI_DELAY_LONG)
+        test_tag_displayed = exists(test_tag_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, test_tag_displayed, 'The recent bookmark tags are displayed.')
 
         right_click(test_tag_pattern)
-        open_all_in_tabs_option_available = exists(open_all_in_tabs_option_pattern, DEFAULT_UI_DELAY_LONG)
+
+        open_all_in_tabs_option_available = exists(open_all_in_tabs_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, open_all_in_tabs_option_available,
                     '\'Open all in tabs\' option available after right click at the tag name')
 
         click(open_all_in_tabs_option_pattern)
+
         firefox_page_opened = exists(image_in_firefox_page_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
         next_tab()
         focus_page_opened = exists(image_in_focus_page_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
