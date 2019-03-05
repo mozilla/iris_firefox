a9e50e5ba2218a965a421083242050e66b467607
pavelciapa <pciapa@mozilla.com>
2019-03-05 13:04:10 +0200

    Fixed typo in assert message

--- iris/tests/drag_and_drop/drop_jpeg_image_in_private_window.py
+++ iris/tests/drag_and_drop/drop_jpeg_image_in_private_window.py
@@ -96,18 +96,18 @@ class Test(BaseTest):
         click(library_import_backup_pattern)
 
         restore_context_available = exists(library_import_restore_submenu_pattern)
-        assert_true(self, restore_context_available, '\'Restore\' option from \'Import and Backup\'context menu '
+        assert_true(self, restore_context_available, '\'Restore\' option from \'Import and Backup\'context menu is '
                                                      'available')
 
         click(library_import_restore_submenu_pattern)
 
         choose_file_available = exists(library_import_choose_file_submenu_pattern)
-        assert_true(self, choose_file_available, 'Choose file option from context menu available')
+        assert_true(self, choose_file_available, 'Choose file option from context menu is available')
 
         click(library_import_choose_file_submenu_pattern)
 
         select_bookmark_popup_available = exists(select_bookmark_popup_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, select_bookmark_popup_available, '\'Select a bookmark backup\' window available')
+        assert_true(self, select_bookmark_popup_available, '\'Select a bookmark backup\' window is available')
 
         select_bookmark_popup_before = find(select_bookmark_popup_pattern)
 
@@ -122,12 +122,12 @@ class Test(BaseTest):
 
         if Settings.is_linux():
             json_option_available = exists(file_type_json_pattern)
-            assert_true(self, json_option_available, '\'File type JSON\' option in file picker window available')
+            assert_true(self, json_option_available, '\'File type JSON\' option in file picker window is available')
 
             click(file_type_json_pattern)
 
             all_files_option_available = exists(file_type_all_files_pattern)
-            assert_true(self, all_files_option_available , '\'All Files\' option in file picker window available')
+            assert_true(self, all_files_option_available , '\'All Files\' option in file picker window is available')
 
             click(file_type_all_files_pattern)
 
@@ -143,19 +143,19 @@ class Test(BaseTest):
         assert_true(self, test_file_jpg_located, 'JPG test file is available')
 
         drop_here_available = exists(drop_here_pattern)
-        assert_true(self, drop_here_available, '"Drop here" pattern available')
+        assert_true(self, drop_here_available, '"Drop here" pattern is available')
 
         drag_drop(jpg_bak_file_pattern, drop_here_pattern)
 
         matching_message_displayed = exists(matching_message_pattern, in_region=matching_region)
         assert_true(self, matching_message_displayed, 'Matching appears under the "Drop Stuff Here" area and expected '
-                                                      'result is identical to result. ')
+                                                      'result is identical to result.')
 
         test_file_png_located = exists(png_bak_file_pattern)
         assert_true(self, test_file_png_located, 'PNG test file is available')
 
         drop_here_available = exists(drop_here_pattern)
-        assert_true(self, drop_here_available, '"Drop here" pattern available')
+        assert_true(self, drop_here_available, '"Drop here" pattern is available')
 
         drag_drop(png_bak_file_pattern, drop_here_pattern)
 
