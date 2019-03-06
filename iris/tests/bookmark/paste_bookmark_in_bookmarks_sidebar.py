96dd326350c84cf012f6948156dacc0440a77e4c
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-06 12:56:44 +0200

    Added changes to script and renamed script file for issue_2177

--- iris/tests/bookmark/paste_bookmark_in_bookmarks_sidebar.py
+++ iris/tests/bookmark/paste_bookmark_in_bookmarks_sidebar.py
@@ -19,46 +19,58 @@ class Test(BaseTest):
         bookmark_from_library_pattern = Pattern('bookmark_from_library.png')
         bookmark_from_sidebar_pattern = Pattern('bookmark_from_sidebar.png')
         paste_option_pattern = Pattern('paste_option.png')
-        library_button_pattern = NavBar.LIBRARY_MENU
-        library_menu_bookmarks_pattern = LibraryMenu.BOOKMARKS_OPTION
-        bookmarks_sidebar_menu_header_pattern = SidebarBookmarks.BOOKMARKS_HEADER
+
         if not Settings.is_mac():
             bookmarks_menu_pattern = Library.BOOKMARKS_MENU
         else:
             bookmarks_menu_pattern = Pattern('bookmark_menu_section.png')
 
-        library_button_exists = exists(library_button_pattern, DEFAULT_UI_DELAY_LONG)
+        library_button_exists = exists(NavBar.LIBRARY_MENU, DEFAULT_UI_DELAY_LONG)
         assert_true(self, library_button_exists, 'Library button exists')
-        click(library_button_pattern)
 
-        library_menu_bookmarks_exists = exists(library_menu_bookmarks_pattern, DEFAULT_UI_DELAY_LONG)
+        click(NavBar.LIBRARY_MENU)
+
+        library_menu_bookmarks_exists = exists(LibraryMenu.BOOKMARKS_OPTION, DEFAULT_UI_DELAY_LONG)
         assert_true(self, library_menu_bookmarks_exists, 'Library bookmarks section exists')
-        click(library_menu_bookmarks_pattern)
+
+        click(LibraryMenu.BOOKMARKS_OPTION)
 
         bookmark_from_library_exists = exists(bookmark_from_library_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmark_from_library_exists, 'Bookmark from library exists')
+
         right_click(bookmark_from_library_pattern)
 
         copy_option_exists = exists(copy_option_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, copy_option_exists, 'Copy option exists')
+
         click(copy_option_pattern)
 
-        time.sleep(1)
-        bookmarks_is_copied = exists(copy_option_pattern, DEFAULT_UI_DELAY_LONG)
-        assert_false(self, bookmarks_is_copied, 'The bookmark is correctly copied.')
+        try:
+            bookmarks_is_copied = wait_vanish(copy_option_pattern, DEFAULT_UI_DELAY_LONG)
+            assert_true(self, bookmarks_is_copied, 'The bookmark is correctly copied.')
+        except FindError:
+            raise FindError('Menu is still displayed')
 
         bookmarks_sidebar('open')
 
-        bookmarks_sidebar_menu_header_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_sidebar_menu_header_exists = exists(SidebarBookmarks.BOOKMARKS_HEADER, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmarks_sidebar_menu_header_exists, 'Bookmarks Sidebar is correctly displayed.')
 
         bookmarks_menu_exists = exists(bookmarks_menu_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmarks_menu_exists, 'Bookmark Menu section exists')
+
         right_click(bookmarks_menu_pattern)
 
         paste_option_exists = exists(paste_option_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, paste_option_exists, 'Paste option exists')
+
         click(paste_option_pattern)
+
+        try:
+            paste_option_not_exists = wait_vanish(paste_option_pattern, DEFAULT_UI_DELAY_LONG)
+            assert_true(self, paste_option_not_exists, 'Paste option disappear')
+        except FindError:
+            raise FindError('Paste option still exists')
 
         click(bookmarks_menu_pattern)
 
