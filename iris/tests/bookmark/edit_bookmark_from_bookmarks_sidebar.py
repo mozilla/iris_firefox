d2d27b4df5691a4aa4d87c3b3dac8be17500ccae
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-06 13:59:50 +0200

    Added blank spaces, removed and renamed some variables for issue_2178

--- iris/tests/bookmark/edit_bookmark_from_bookmarks_sidebar.py
+++ iris/tests/bookmark/edit_bookmark_from_bookmarks_sidebar.py
@@ -17,15 +17,15 @@ class Test(BaseTest):
     def run(self):
         properties_option_pattern = Pattern('properties_option.png')
         new_modified_bookmark_pattern = Pattern('bookmark_modified.png')
-        name_before_pattern = Pattern('name_field.png')
-        location_before_pattern = Pattern('location_field.png')
-        tags_before_pattern = Pattern('tags_field.png')
-        keyword_before_pattern = Pattern('keyword_field.png')
-        name_after_pattern = Pattern('name_saved.png')
-        location_after_pattern = Pattern('location_saved.png')
-        tags_after_pattern = Pattern('tags_saved.png')
-        keyword_after_pattern = Pattern('keyword_saved.png')
-        bookmarks_sidebar_menu_header_pattern = SidebarBookmarks.BOOKMARKS_HEADER
+        name_before_editing_pattern = Pattern('name_field.png')
+        location_before_editing_pattern = Pattern('location_field.png')
+        tags_before_editing_pattern = Pattern('tags_field.png')
+        keyword_before_editing_pattern = Pattern('keyword_field.png')
+        name_after_editing_pattern = Pattern('name_saved.png')
+        location_after_editing_pattern = Pattern('location_saved.png')
+        tags_after_editing_pattern = Pattern('tags_saved.png')
+        keyword_after_editing_pattern = Pattern('keyword_saved.png')
+
         if Settings.is_mac():
             other_bookmarks_pattern = Pattern('bookmarks_toolbar.png')
             bookmark_getting_started_pattern = Pattern('bookmark_from_sidebar.png')
@@ -36,38 +36,41 @@ class Test(BaseTest):
 
         bookmarks_sidebar('open')
 
-        bookmarks_sidebar_menu_exists = exists(bookmarks_sidebar_menu_header_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_sidebar_menu_exists = exists(SidebarBookmarks.BOOKMARKS_HEADER, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmarks_sidebar_menu_exists, 'Bookmarks Sidebar is correctly displayed')
 
         other_bookmarks_exists = exists(other_bookmarks_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, other_bookmarks_exists, 'Other bookmarks section exists')
+
         click(other_bookmarks_pattern)
 
         bookmark_getting_started_exists = exists(bookmark_getting_started_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmark_getting_started_exists, 'Getting started bookmark exists')
+
         right_click(bookmark_getting_started_pattern)
 
         properties_option_exists = exists(properties_option_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, properties_option_exists, 'Properties option exists')
+
         click(properties_option_pattern)
 
         if not Settings.is_mac():
             properties_window_exists = exists(properties_window_pattern, DEFAULT_UI_DELAY_LONG)
             assert_true(self, properties_window_exists, 'Properties for "Getting Started" window is opened')
         else:
-            properties_window_exists = exists(name_before_pattern, DEFAULT_UI_DELAY_LONG)
+            properties_window_exists = exists(name_before_editing_pattern, DEFAULT_UI_DELAY_LONG)
             assert_true(self, properties_window_exists, 'Properties for "Getting Started" window is opened')
 
-        name_before_exists = exists(name_before_pattern, DEFAULT_UI_DELAY_LONG)
+        name_before_exists = exists(name_before_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, name_before_exists, 'Name field exists')
 
-        location_before_exists = exists(location_before_pattern, DEFAULT_UI_DELAY_LONG)
+        location_before_exists = exists(location_before_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, location_before_exists, 'Location field exists')
 
-        tags_before_exists = exists(tags_before_pattern, DEFAULT_UI_DELAY_LONG)
+        tags_before_exists = exists(tags_before_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, tags_before_exists, 'Tags field exists')
 
-        keyword_before_exists = exists(keyword_before_pattern, DEFAULT_UI_DELAY_LONG)
+        keyword_before_exists = exists(keyword_before_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, keyword_before_exists, 'Keyword field exists')
 
         paste('New Name')
@@ -77,6 +80,7 @@ class Test(BaseTest):
         type(Key.TAB)
 
         paste('Tag')
+
         if Settings.is_mac():
             type(Key.TAB)
         else:
@@ -88,23 +92,25 @@ class Test(BaseTest):
 
         new_modified_bookmark_exists = exists(new_modified_bookmark_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, new_modified_bookmark_exists, 'New modified bookmark exists')
+
         right_click(new_modified_bookmark_pattern)
 
         properties_option_exists = exists(properties_option_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, properties_option_exists, 'Properties option exists')
+
         click(properties_option_pattern)
 
-        name_after_exists = exists(name_after_pattern, DEFAULT_UI_DELAY_LONG)
+        name_after_exists = exists(name_after_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, name_after_exists, 'Name field changes are correctly saved')
 
         type(Key.TAB)
 
-        location_after_exists = exists(location_after_pattern, DEFAULT_UI_DELAY_LONG)
+        location_after_exists = exists(location_after_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, location_after_exists, 'Location field changes are correctly saved')
 
         type(Key.TAB)
 
-        tags_after_exists = exists(tags_after_pattern, DEFAULT_UI_DELAY_LONG)
+        tags_after_exists = exists(tags_after_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, tags_after_exists, 'Tags field changes are correctly saved')
 
         if Settings.is_mac():
@@ -112,11 +118,7 @@ class Test(BaseTest):
         else:
             [type(Key.TAB) for _ in range(2)]
 
-        keyword_after_exists = exists(keyword_after_pattern, DEFAULT_UI_DELAY_LONG)
+        keyword_after_exists = exists(keyword_after_editing_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, keyword_after_exists, 'Keyword field changes are correctly saved')
 
         type(Key.ENTER)
-
-
-
-
