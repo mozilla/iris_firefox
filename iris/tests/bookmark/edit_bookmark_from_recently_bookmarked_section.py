f4f8f925d9aa2835df066494a6647236266fa7f2
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-07 10:59:11 +0200

    Test script is fully added for issue_2155

--- iris/tests/bookmark/edit_bookmark_from_recently_bookmarked_section.py
+++ iris/tests/bookmark/edit_bookmark_from_recently_bookmarked_section.py
@@ -21,8 +21,23 @@ class Test(BaseTest):
 
     def run(self):
         properties_option_pattern = Pattern('properties_option.png')
-        properties_for_firefox_pattern = Pattern('properties_for_firefox.png')
-        firefox_sidebar_logo_pattern = Pattern('firefox_bookmark.png')
+        new_modified_bookmark_pattern = Pattern('bookmark_modified.png')
+        name_before_editing_pattern = Pattern('name_field.png')
+        location_before_editing_pattern = Pattern('location_field.png')
+        tags_before_editing_pattern = Pattern('tags_field.png')
+        keyword_before_editing_pattern = Pattern('keyword_field.png')
+        name_after_editing_pattern = Pattern('name_saved.png')
+        location_after_editing_pattern = Pattern('location_saved.png')
+        tags_after_editing_pattern = Pattern('tags_saved.png')
+        keyword_after_editing_pattern = Pattern('keyword_saved.png')
+
+        if Settings.is_mac():
+            other_bookmarks_pattern = Pattern('bookmarks_toolbar.png')
+            bookmark_getting_started_pattern = Pattern('bookmark_from_sidebar.png')
+        else:
+            properties_window_pattern = Pattern('properties_window.png')
+            bookmark_getting_started_pattern = Pattern('toolbar_bookmark_icon.png')
+
 
         library_button_exists = exists(NavBar.LIBRARY_MENU, DEFAULT_UI_DELAY_LONG)
         assert_true(self, library_button_exists, 'View history, saved bookmarks and more section exists')
@@ -34,15 +49,81 @@ class Test(BaseTest):
 
         click(LibraryMenu.BOOKMARKS_OPTION)
 
-        firefox_sidebar_logo_exists = exists(firefox_sidebar_logo_pattern, DEFAULT_UI_DELAY_LONG)
-        assert_true(self, firefox_sidebar_logo_exists, 'Firefox bookmarks exists')
+        bookmark_getting_started_exists = exists(bookmark_getting_started_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, bookmark_getting_started_exists, 'Getting started bookmark exists')
 
-        right_click(firefox_sidebar_logo_pattern)
+        right_click(bookmark_getting_started_pattern)
 
-        properties_option_exists = exists(properties_option_pattern, DEFAULT_UI_DELAY_LONG)
+        properties_option_exists = exists(properties_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, properties_option_exists, 'Properties option exists')
 
         click(properties_option_pattern)
 
-        properties_for_firefox_exists = exists(properties_for_firefox_pattern)
-        assert_true(self, properties_for_firefox_exists, '')
+        if not Settings.is_mac():
+            properties_window_exists = exists(properties_window_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+            assert_true(self, properties_window_exists, 'Properties for "Getting Started" window is opened')
+        else:
+            properties_window_exists = exists(name_before_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+            assert_true(self, properties_window_exists, 'Properties for "Getting Started" window is opened')
+
+        name_before_exists = exists(name_before_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, name_before_exists, 'Name field exists')
+
+        location_before_exists = exists(location_before_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, location_before_exists, 'Location field exists')
+
+        tags_before_exists = exists(tags_before_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, tags_before_exists, 'Tags field exists')
+
+        keyword_before_exists = exists(keyword_before_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, keyword_before_exists, 'Keyword field exists')
+
+        paste('New Name')
+        type(Key.TAB)
+
+        paste('wikipedia.org')
+        type(Key.TAB)
+
+        paste('Tag')
+
+        if Settings.is_mac():
+            type(Key.TAB)
+        else:
+            [type(Key.TAB) for _ in range(2)]
+
+        paste('test')
+
+        type(Key.ENTER)
+
+        new_modified_bookmark_exists = exists(new_modified_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, new_modified_bookmark_exists, 'New modified bookmark exists')
+
+        right_click(new_modified_bookmark_pattern)
+
+        properties_option_exists = exists(properties_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, properties_option_exists, 'Properties option exists')
+
+        click(properties_option_pattern)
+
+        name_after_exists = exists(name_after_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, name_after_exists, 'Name field changes are correctly saved')
+
+        type(Key.TAB)
+
+        location_after_exists = exists(location_after_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, location_after_exists, 'Location field changes are correctly saved')
+
+        type(Key.TAB)
+
+        tags_after_exists = exists(tags_after_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, tags_after_exists, 'Tags field changes are correctly saved')
+
+        if Settings.is_mac():
+            type(Key.TAB)
+        else:
+            [type(Key.TAB) for _ in range(2)]
+
+        keyword_after_exists = exists(keyword_after_editing_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, keyword_after_exists, 'Keyword field changes are correctly saved')
+
+        type(Key.ENTER)
