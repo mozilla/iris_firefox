3b4041c4fef0ff77e9c8548dc075d973ab26889d
pavelciapa <pciapa@mozilla.com>
2019-03-11 10:35:41 +0200

    Created constant for scroll to pattern

--- iris/tests/drag_and_drop/drop_jpeg_image.py
+++ iris/tests/drag_and_drop/drop_jpeg_image.py
@@ -45,6 +45,7 @@ class Test(BaseTest):
             file_type_json_pattern = Pattern('file_type_json.png')
 
         DRAG_AND_DROP_DURATION = 3
+        SCROLL_DELAY = 0.5
         folderpath = self.get_asset_path('')
 
         navigate('https://mystor.github.io/dragndrop/')
@@ -59,7 +60,7 @@ class Test(BaseTest):
                                                     'has been selected.')
 
         matching_block_available = scroll_until_pattern_found(not_matching_message_pattern, scroll, (-25,), 20,
-                                                              DEFAULT_UI_DELAY)
+                                                              SCROLL_DELAY)
         assert_true(self, matching_block_available, 'The drop result verification area is displayed on the page')
 
         not_matching_message_location = find(not_matching_message_pattern)
@@ -114,7 +115,7 @@ class Test(BaseTest):
             type('1', KeyModifier.CMD)  # change view of finder
         else:
             paste(folderpath)
-            type(Key.ENTER, interval=DEFAULT_UI_DELAY)
+            type(Key.ENTER, interval=SCROLL_DELAY)
 
         if Settings.is_linux():
             json_option_available = exists(file_type_json_pattern)
@@ -129,7 +130,7 @@ class Test(BaseTest):
 
         else:
             type('*')  # Show all files in Windows Explorer
-            type(Key.ENTER, interval=DEFAULT_UI_DELAY)
+            type(Key.ENTER, interval=SCROLL_DELAY)
 
         select_bookmark_popup_after = Location(SCREEN_WIDTH / 2, library_popup_tab_before.y)
         #  drag-n-drop right to prevent fails on osx
