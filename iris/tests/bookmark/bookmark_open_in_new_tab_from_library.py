b9fb4797cea5b6e2e30fb5c05520e32b2ba7535f
Sergey Pronoza <sergey@exaltcg.com>
2019-03-11 15:08:22 +0200

    Fixes after review

--- iris/tests/bookmark/bookmark_open_in_new_tab_from_library.py
+++ iris/tests/bookmark/bookmark_open_in_new_tab_from_library.py
@@ -10,14 +10,15 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = 'Open a bookmark using \'Open\' option from Library'
-        self.test_case_id = '169257'
+        self.meta = 'Open a bookmark in a New Tab from Library '
+        self.test_case_id = '169258'
         self.test_suite_id = '2525'
         self.locales = ['en-US']
 
     def run(self):
         soap_wiki_tab_pattern = Pattern('soap_wiki_tab.png')
         wiki_bookmark_logo_pattern = Pattern('wiki_bookmark_logo.png')
+        open_in_new_tab_pattern = Pattern('open_in_new_tab.png')
 
         home_width, home_height = NavBar.HOME_BUTTON.get_size()
         tabs_region = Region(0, 0, SCREEN_WIDTH, home_height * 4)
@@ -25,12 +26,12 @@ class Test(BaseTest):
         navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
 
         soap_wiki_opened = exists(soap_wiki_tab_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
-        assert_true(self, soap_wiki_opened, 'Soap wiki page opened')
+        assert_true(self, soap_wiki_opened, 'The test page is opened')
 
         bookmark_page()
 
         stardialog_displayed = exists(Bookmarks.StarDialog.DONE, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, stardialog_displayed, 'Bookmark added')
+        assert_true(self, stardialog_displayed, 'Previously added bookmark exists in Library')
 
         click(Bookmarks.StarDialog.DONE)
 
@@ -50,18 +51,18 @@ class Test(BaseTest):
 
         right_click(soap_wiki_tab_pattern)
 
-        type(Key.DOWN)
-        type(Key.DOWN)
-        type(Key.ENTER)
+        open_in_new_tab_option_exists = exists(open_in_new_tab_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, open_in_new_tab_option_exists, 'Open in New Tab option exists')
+
+        click(open_in_new_tab_pattern)
 
         soap_wiki_opened_from_bookmarks = exists(soap_wiki_tab_pattern, DEFAULT_SITE_LOAD_TIMEOUT, tabs_region)
-        assert_true(self, soap_wiki_opened_from_bookmarks, 'Soap wiki page opened with Open in a New Tab from Library')
+        assert_true(self, soap_wiki_opened_from_bookmarks, 'The test page is downloaded')
 
         new_tab_exists = exists(Tabs.NEW_TAB_NOT_HIGHLIGHTED)
-        assert_true(self, new_tab_exists, 'New tab displayed')
+        assert_true(self, new_tab_exists, 'The selected bookmark page is opened in a new tab.')
 
         open_library()
 
         click(Library.TITLE)
         close_window_control('auxiliary')
-
