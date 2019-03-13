521d443ac20712b32846157fdfa2caf7c5eacb13
pavelciapa <pciapa@mozilla.com>
2019-03-13 18:25:36 +0200

    Rename UI class patterns in content_blocking_tour

--- iris/api/core/firefox_ui/content_blocking_tour.py
+++ iris/api/core/firefox_ui/content_blocking_tour.py
@@ -8,6 +8,6 @@ from iris.api.core.pattern import Pattern
 class ContentBlockingTour(object):
     DIFFERENCES_TO_EXPECT_LABEL = Pattern('differences_to_expect_label.png')
     NEXT_BUTTON_SECOND_TOUR_STEP = Pattern('next_button_second_tour_step.png')
-    TURN_OFF_BLOCKING_LABEL = Pattern('UITourTooltipTitle_turn_off_blocking.png')
+    TURN_OFF_BLOCKING_LABEL = Pattern('uitourtooltiptitle_turn_off_blocking.png')
     GOT_IT_BUTTON = Pattern('got_it_button.png')
     RESTART_TOUR_BUTTON = Pattern('restart_tour_button.png')
