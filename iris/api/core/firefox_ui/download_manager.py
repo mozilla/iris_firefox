f0673b8cfd067715aa5b79e1acb5b720d8fb1642
pavelciapa <pciapa@mozilla.com>
2019-03-13 18:07:46 +0200

    Rename UI class patterns in download_manager

--- iris/api/core/firefox_ui/download_manager.py
+++ iris/api/core/firefox_ui/download_manager.py
@@ -6,16 +6,16 @@ from iris.api.core.pattern import Pattern
 
 
 class DownloadManager(object):
-    SHOW_ALL_DOWNLOADS = Pattern('downloadsHistory_show_all_Downloads.png')
+    SHOW_ALL_DOWNLOADS = Pattern('downloadshistory_show_all_downloads.png')
     DOWNLOADS_FOLDER = Pattern('downloads_folder.png')
 
     class DownloadsPanel(object):
         DOWNLOADS_BUTTON = Pattern('downloads_button_open.png').similar(0.95)
-        DOWNLOAD_RETRY = Pattern('downloadRetry.png')
-        DOWNLOAD_RETRY_HIGHLIGHTED = Pattern('downloadRetry_highlighted.png').similar(0.95)
-        DOWNLOAD_CANCEL = Pattern('downloadCancel.png')
-        DOWNLOAD_CANCEL_HIGHLIGHTED = Pattern('downloadCancel_highlighted.png').similar(0.95)
-        NO_DOWNLOADS_FOR_THIS_SESSION = Pattern('emptyDownloads.png')
+        DOWNLOAD_RETRY = Pattern('downloadretry.png')
+        DOWNLOAD_RETRY_HIGHLIGHTED = Pattern('downloadretry_highlighted.png').similar(0.95)
+        DOWNLOAD_CANCEL = Pattern('downloadcancel.png')
+        DOWNLOAD_CANCEL_HIGHLIGHTED = Pattern('downloadcancel_highlighted.png').similar(0.95)
+        NO_DOWNLOADS_FOR_THIS_SESSION = Pattern('emptydownloads.png')
         OPEN_DOWNLOAD_FOLDER = Pattern('download_button_open_containing_folder.png')
         OPEN_CONTAINING_FOLDER = Pattern('open_containing_folder.png')
         TIME_LEFT = Pattern('time_left.png')
@@ -65,7 +65,7 @@ class DownloadManager(object):
         PANEL_HEADER_DOWNLOADS = Pattern('panel_header_downloads.png')
         SHOW_ALL_DOWNLOADS = Pattern('library_menu_downloads_more.png')
         FILE_MOVED_OR_MISSING = Pattern('file_moved_or_missing.png')
-        EXTRA_SMALL_FILE_5MB_ZIP = Pattern('5MB_zip.png')
+        EXTRA_SMALL_FILE_5MB_ZIP = Pattern('5mb_zip.png')
 
     class AboutDownloads(object):
         NO_DOWNLOADS = Pattern('there_are_no_downloads.png')
