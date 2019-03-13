4be19cb2d885ef8b1f537c4bfe22a363f2fe46d9
pavelciapa <pciapa@mozilla.com>
2019-03-13 18:55:52 +0200

    Rename UI class patterns in library

--- iris/api/core/firefox_ui/library.py
+++ iris/api/core/firefox_ui/library.py
@@ -12,61 +12,61 @@ class Library(object):
     FORWARD_BUTTON_DISABLED = Pattern('forward_button_disabled.png')
     BACK_BUTTON_ENABLED = Pattern('back_button_enabled.png')
     FORWARD_BUTTON_ENABLED = Pattern('forward_button_enabled.png')
-    ORGANIZE_BUTTON = Pattern('organizeButton.png')
+    ORGANIZE_BUTTON = Pattern('organizebutton.png')
 
     class Organize(object):
         NEW_BOOKMARK = Pattern('newbookmark.png')
         NEW_FOLDER = Pattern('newfolder.png')
         NEW_SEPARATOR = Pattern('newseparator.png')
         if Settings.get_os() != Platform.MAC:
-            CLOSE = Pattern('orgClose.png')
+            CLOSE = Pattern('orgclose.png')
 
-    VIEWS_BUTTON = Pattern('viewMenu.png')
+    VIEWS_BUTTON = Pattern('viewmenu.png')
 
     class Views(object):
-        SHOW_COLUMNS = Pattern('viewColumns.png')
+        SHOW_COLUMNS = Pattern('viewcolumns.png')
 
         class ShowColumns(object):
-            NAME = Pattern('menucol_placesContentTitle.png')
-            TAGS = Pattern('menucol_placesContentTags.png')
-            LOCATION = Pattern('menucol_placesContentUrl.png')
-            MOST_RECENT_VISIT = Pattern('menucol_placesContentDate.png')
-            VISIT_COUNT = Pattern('menucol_placesContentVisitCount.png')
-            ADDED = Pattern('menucol_placesContentDateAdded.png')
-            LAST_MODIFIED = Pattern('menucol_placesContentLastModified.png')
+            NAME = Pattern('menucol_placescontenttitle.png')
+            TAGS = Pattern('menucol_placescontenttags.png')
+            LOCATION = Pattern('menucol_placescontenturl.png')
+            MOST_RECENT_VISIT = Pattern('menucol_placescontentdate.png')
+            VISIT_COUNT = Pattern('menucol_placescontentvisitcount.png')
+            ADDED = Pattern('menucol_placescontentdateadded.png')
+            LAST_MODIFIED = Pattern('menucol_placescontentlastmodified.png')
 
-        SORT = Pattern('viewSort.png')
+        SORT = Pattern('viewsort.png')
 
         class Sort(object):
-            UNSORTED = Pattern('viewUnsorted.png')
-            SORT_BY_NAME = Pattern('sort_by_menucol_placesContentTitle.png')
-            SORT_BY_TAGS = Pattern('sort_by_menucol_placesContentTags.png')
-            SORT_BY_LOCATION = Pattern('sort_by_menucol_placesContentUrl.png')
-            SORT_BY_MOST_RECENT_VISIT = Pattern('sort_by_menucol_placesContentDate.png')
-            SORT_BY_VISIT_COUNT = Pattern('sort_by_menucol_placesContentVisitCount.png')
-            SORT_BY_ADDED = Pattern('sort_by_menucol_placesContentDateAdded.png')
-            SORT_BY_LAST_MODIFIED = Pattern('sort_by_menucol_placesContentLastModified.png')
-            AZ_SORT_ORDER = Pattern('viewSortAscending.png')
-            ZA_SORT_ORDER = Pattern('viewSortDescending.png')
+            UNSORTED = Pattern('viewunsorted.png')
+            SORT_BY_NAME = Pattern('sort_by_menucol_placescontenttitle.png')
+            SORT_BY_TAGS = Pattern('sort_by_menucol_placescontenttags.png')
+            SORT_BY_LOCATION = Pattern('sort_by_menucol_placescontenturl.png')
+            SORT_BY_MOST_RECENT_VISIT = Pattern('sort_by_menucol_placescontentdate.png')
+            SORT_BY_VISIT_COUNT = Pattern('sort_by_menucol_placescontentvisitcount.png')
+            SORT_BY_ADDED = Pattern('sort_by_menucol_placescontentdateadded.png')
+            SORT_BY_LAST_MODIFIED = Pattern('sort_by_menucol_placescontentlastmodified.png')
+            AZ_SORT_ORDER = Pattern('viewsortascending.png')
+            ZA_SORT_ORDER = Pattern('viewsortdescending.png')
 
-    IMPORT_AND_BACKUP_BUTTON = Pattern('maintenanceButton.png')
+    IMPORT_AND_BACKUP_BUTTON = Pattern('maintenancebutton.png')
 
     class ImportAndBackup(object):
-        BACKUP = Pattern('backupBookmarks.png')
-        RESTORE = Pattern('fileRestoreMenu.png')
+        BACKUP = Pattern('backupbookmarks.png')
+        RESTORE = Pattern('filerestoremenu.png')
 
         class Restore(object):
-            CHOOSE_FILE = Pattern('restoreFromFile.png')
+            CHOOSE_FILE = Pattern('restorefromfile.png')
 
-        IMPORT_BOOKMARKS_FROM_HTML = Pattern('fileImport.png')
-        EXPORT_BOOKMARKS_FROM_HTML = Pattern('fileExport.png')
-        IMPORT_DATA_FROM_ANOTHER_BROWSER = Pattern('browserImport.png')
+        IMPORT_BOOKMARKS_FROM_HTML = Pattern('fileimport.png')
+        EXPORT_BOOKMARKS_FROM_HTML = Pattern('fileexport.png')
+        IMPORT_DATA_FROM_ANOTHER_BROWSER = Pattern('browserimport.png')
 
     class DownloadLibrary(object):
         DOWNLOAD_CANCEL_HIGHLIGHTED = Pattern('download_button_cancel_icon.png')
         DOWNLOADS = Pattern('downloads.png')
-        SEARCH_DOWNLOADS = Pattern('searchFilter_downloads.png')
-        CLEAR_DOWNLOADS = Pattern('clearDownloadsButton.png')
+        SEARCH_DOWNLOADS = Pattern('searchfilter_downloads.png')
+        CLEAR_DOWNLOADS = Pattern('cleardownloadsbutton.png')
 
     HISTORY = Pattern('history.png')
     HISTORY_NAME = Pattern('history_name.png')
@@ -74,7 +74,7 @@ class Library(object):
     TODAY_NAME = Pattern('today_name.png')
     HISTORY_OLDER_THAN_6_MONTHS = Pattern('history_older_than_6_months.png')
     OLDER_THAN_6_MONTHS_NAME = Pattern('older_than_6_months_name.png')
-    SEARCH_HISTORY = Pattern('searchFilter_history.png')
+    SEARCH_HISTORY = Pattern('searchfilter_history.png')
     TAGS = Pattern('tags.png')
     TAGS_NAME = Pattern('tags_name.png')
     ALL_BOOKMARKS = Pattern('all_bookmarks.png')
@@ -85,7 +85,7 @@ class Library(object):
     BOOKMARKS_MENU_NAME = Pattern('bookmarks_menu_name.png')
     OTHER_BOOKMARKS = Pattern('other_bookmarks.png')
     OTHER_BOOKMARKS_NAME = Pattern('other_bookmarks_name.png')
-    SEARCH_BOOKMARKS = Pattern('searchFilter_bookmarks.png')
-    NAME_COLUMN = Pattern('placesContentTitle.png')
-    TAGS_COLUMN = Pattern('placesContentTags.png')
-    LOCATION_COLUMN = Pattern('placesContentUrl.png')
+    SEARCH_BOOKMARKS = Pattern('searchfilter_bookmarks.png')
+    NAME_COLUMN = Pattern('placescontenttitle.png')
+    TAGS_COLUMN = Pattern('placescontenttags.png')
+    LOCATION_COLUMN = Pattern('placescontenturl.png')
