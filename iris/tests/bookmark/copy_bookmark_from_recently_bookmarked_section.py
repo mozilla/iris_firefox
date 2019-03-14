819efe13a9996e609b92291ba92f997578100edc
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-14 15:10:27 +0200

    Added changes to timeouts due to new Settings for issue_2154

--- iris/tests/bookmark/copy_bookmark_from_recently_bookmarked_section.py
+++ iris/tests/bookmark/copy_bookmark_from_recently_bookmarked_section.py
@@ -29,54 +29,54 @@ class Test(BaseTest):
         copy_option_pattern = Pattern('copy_option.png')
         paste_option_pattern = Pattern('paste_option.png')
 
-        library_button_exists = exists(library_button_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        library_button_exists = exists(library_button_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, library_button_exists, 'View history, saved bookmarks and more section exists')
 
         click(library_button_pattern)
 
-        bookmarks_menu_option_exists = exists(bookmarks_menu_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        bookmarks_menu_option_exists = exists(bookmarks_menu_option_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, bookmarks_menu_option_exists, 'The Bookmarks menu is correctly displayed')
 
         click(bookmarks_menu_option_pattern)
 
-        recently_wikipedia_bookmark_exists = exists(recently_wikipedia_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        recently_wikipedia_bookmark_exists = exists(recently_wikipedia_bookmark_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, recently_wikipedia_bookmark_exists, 'Wikipedia bookmarks exists')
 
         right_click(recently_wikipedia_bookmark_pattern)
 
-        copy_option_exists = exists(copy_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        copy_option_exists = exists(copy_option_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, copy_option_exists, 'Copy option exists')
 
         click(copy_option_pattern)
 
         try:
-            menu_disappeared = wait_vanish(copy_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+            menu_disappeared = wait_vanish(copy_option_pattern, Settings.tiny_firefox_timeout)
             assert_true(self, menu_disappeared, 'The selected website is correctly copied')
         except FindError:
             raise FindError('The selected website isn\'t correctly copied')
 
         bookmarks_sidebar('open')
 
-        sidebar_bookmarks_header_exists = exists(sidebar_bookmarks_header_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        sidebar_bookmarks_header_exists = exists(sidebar_bookmarks_header_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, sidebar_bookmarks_header_exists, 'Bookmarks sidebar exists')
 
-        sidebar_bookmarks_toolbar_exists = exists(sidebar_bookmarks_toolbar_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        sidebar_bookmarks_toolbar_exists = exists(sidebar_bookmarks_toolbar_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, sidebar_bookmarks_toolbar_exists, 'Bookmarks toolbar section exists')
 
         right_click(sidebar_bookmarks_toolbar_pattern)
 
-        paste_option_exists = exists(paste_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        paste_option_exists = exists(paste_option_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, paste_option_exists, 'Paste option exists')
 
         click(paste_option_pattern)
 
         try:
-            paste_option_disappeared = wait_vanish(paste_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+            paste_option_disappeared = wait_vanish(paste_option_pattern, Settings.tiny_firefox_timeout)
             assert_true(self, paste_option_disappeared, 'Paste option is gone')
         except FindError:
             raise FindError('Paste still exists')
 
         click(sidebar_bookmarks_toolbar_pattern)
 
-        wiki_sidebar_bookmark_exists = exists(wiki_sidebar_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
+        wiki_sidebar_bookmark_exists = exists(wiki_sidebar_bookmark_pattern, Settings.tiny_firefox_timeout)
         assert_true(self, wiki_sidebar_bookmark_exists, 'The bookmark is correctly pasted in the selected section')
