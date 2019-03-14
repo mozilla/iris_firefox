8f99af01bfafdfa4ad3d0b33a3066b179ccf522f
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-14 16:53:46 +0200

    Added changes to timeouts for issue_2149

--- iris/tests/bookmark/open_bookmark_from_recently_bookmarked_section_using_open_option.py
+++ iris/tests/bookmark/open_bookmark_from_recently_bookmarked_section_using_open_option.py
@@ -27,28 +27,28 @@ class Test(BaseTest):
         wikipedia_content_pattern = Pattern('wikipedia_content.png')
         open_option_pattern = Pattern('open_option.png')
 
-        library_button_exists = exists(library_button_pattern, DEFAULT_UI_DELAY_LONG)
+        library_button_exists = exists(library_button_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_true(self, library_button_exists, 'View history, saved bookmarks and more section exists')
 
         click(library_button_pattern)
 
-        bookmarks_menu_option_exists = exists(bookmarks_menu_option_pattern, DEFAULT_UI_DELAY_LONG)
+        bookmarks_menu_option_exists = exists(bookmarks_menu_option_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_true(self, bookmarks_menu_option_exists, 'The Bookmarks menu is correctly displayed')
 
         click(bookmarks_menu_option_pattern)
 
-        recently_wikipedia_bookmark_exists = exists(recently_wikipedia_bookmark_pattern, DEFAULT_UI_DELAY_LONG)
+        recently_wikipedia_bookmark_exists = exists(recently_wikipedia_bookmark_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_true(self, recently_wikipedia_bookmark_exists, 'Wikipedia bookmark exists in recently bookmarked section')
 
         right_click(recently_wikipedia_bookmark_pattern)
 
-        open_option_exists = exists(open_option_pattern, DEFAULT_UI_DELAY_LONG)
+        open_option_exists = exists(open_option_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_true(self, open_option_exists, 'Open option exists')
 
         click(open_option_pattern)
 
-        wikipedia_content_exists = exists(wikipedia_content_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
+        wikipedia_content_exists = exists(wikipedia_content_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, wikipedia_content_exists, 'Wikipedia content exists')
 
-        new_tab_is_not_opened_exists = exists(new_tab_is_opened_pattern, DEFAULT_UI_DELAY_LONG)
+        new_tab_is_not_opened_exists = exists(new_tab_is_opened_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_false(self, new_tab_is_not_opened_exists, 'The selected website is opened in the current tab.')
