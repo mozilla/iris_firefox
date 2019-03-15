610d8409c8bc9c1670bce73a3956a32ad4c3abc2
Paul Prokhorov <paul@exaltcg.com>
2019-03-15 18:40:09 +0200

    Changed approach to call wait time constants in open_bookmark_new_private_window_from_ibrary.py

--- iris/tests/bookmark/open_bookmark_new_private_window_from_ibrary.py
+++ iris/tests/bookmark/open_bookmark_new_private_window_from_ibrary.py
@@ -16,6 +16,10 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
+        open_new_private_window_pattern = Pattern('open_in_private_window.png')
+        bookmark_menu_pattern = Library.BOOKMARKS_MENU
+        private_window_pattern = PrivateWindow.private_window_pattern
+
         if not Settings.is_mac():
             mozilla_bookmark_pattern = Pattern('mozilla_bookmark.png')
             bookmark_site_pattern = Pattern('bookmark_site.png')
@@ -23,31 +27,30 @@ class Test(BaseTest):
             mozilla_bookmark_pattern = Pattern('mozilla_firefox_bookmark.png')
             bookmark_site_pattern = Pattern('website_bookmark_from_library.png')
 
-        open_new_private_window_pattern = Pattern('open_in_private_window.png')
-        bookmark_menu_pattern = Library.BOOKMARKS_MENU
-        private_window_pattern = PrivateWindow.private_window_pattern
-
         open_library()
 
-        library_is_displayed = exists(bookmark_menu_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        library_is_displayed = exists(bookmark_menu_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, library_is_displayed, 'Library is correctly open')
 
         click(bookmark_menu_pattern)
 
-        mozilla_bookmark_exists = exists(mozilla_bookmark_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        mozilla_bookmark_exists = exists(mozilla_bookmark_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, mozilla_bookmark_exists, 'Mozilla bookmark exists')
 
         double_click(mozilla_bookmark_pattern)
 
-        bookmark_site_exists = exists(bookmark_site_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        bookmark_site_exists = exists(bookmark_site_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, bookmark_site_exists, 'Website bookmark exists')
 
         right_click(bookmark_site_pattern)
 
-        open_new_private_window_exists = exists(open_new_private_window_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        open_new_private_window_exists = exists(open_new_private_window_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, open_new_private_window_exists, 'Open new private window button exists')
 
         click(open_new_private_window_pattern)
 
-        private_window_exists = exists(private_window_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        private_window_exists = exists(private_window_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, private_window_exists, 'The selected bookmark page is opened in a new private window.')
+
+        close_window()
+        close_window_control('auxiliary')
