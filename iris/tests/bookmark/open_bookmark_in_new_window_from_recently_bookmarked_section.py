bb19186e4f1219c4d5381ff290165aa9736918c2
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-18 12:33:13 +0200

    Added small changes to script for issue_2151

--- iris/tests/bookmark/open_bookmark_in_new_window_from_recently_bookmarked_section.py
+++ iris/tests/bookmark/open_bookmark_in_new_window_from_recently_bookmarked_section.py
@@ -45,6 +45,8 @@ class Test(BaseTest):
         firefox_full_logo_exists = exists(LocalWeb.FIREFOX_IMAGE, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, firefox_full_logo_exists, 'Bookmark is correctly opened in the New Window.')
 
-        bookmark_opened_in_current_tab = exists(LocalWeb.IRIS_LOGO_INACTIVE_TAB, Settings.SHORT_FIREFOX_TIMEOUT)
+        bookmark_opened_in_current_tab = exists(LocalWeb.IRIS_LOGO_ACTIVE_TAB, Settings.TINY_FIREFOX_TIMEOUT)
         assert_false(self, bookmark_opened_in_current_tab,
                      'The page that was previously displayed in the current tab is no longer displayed')
+
+        close_window()
