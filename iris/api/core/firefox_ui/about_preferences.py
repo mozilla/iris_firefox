c185f7f9eefa2e72ad5cf8a53d8a46596832f677
pavelciapa <pciapa@mozilla.com>
2019-03-18 16:42:38 +0200

    Changed UI classes and file names in module about_preferences

--- iris/api/core/firefox_ui/about_preferences.py
+++ iris/api/core/firefox_ui/about_preferences.py
@@ -7,24 +7,24 @@ from iris.api.core.pattern import Pattern
 
 
 class AboutPreferences(object):
-    PRIVACY_AND_SECURITY_BUTTON_SELECTED = Pattern('category-privacy_button_selected.png')
-    PRIVACY_AND_SECURITY_BUTTON_NOT_SELECTED = Pattern('category-privacy_button_not_selected.png')
+    PRIVACY_AND_SECURITY_BUTTON_SELECTED = Pattern('category_privacy_button_selected.png')
+    PRIVACY_AND_SECURITY_BUTTON_NOT_SELECTED = Pattern('category_privacy_button_not_selected.png')
 
     class Privacy(object):
         CONTENT_TRACKING_TRACKERS_CHECKBOX_SELECTED = \
-            Pattern('contentblockingtrackingprotectioncheckbox_checkbox_selected.png')
+            Pattern('content_blocking_tracking_protection_checkbox_selected.png')
         CONTENT_TRACKING_TRACKERS_CHECKBOX_NOT_SELECTED = \
-            Pattern('contentblockingtrackingprotectioncheckbox_checkbox_not_selected.png')
+            Pattern('content_blocking_tracking_protection_checkbox_not_selected.png')
         CONTENT_TRACKING_TRACKERS_ALWAYS_RADIO_SELECTED = \
-            Pattern('content-blocking-tracking-protection-option-always_radio_selected.png')
+            Pattern('content_blocking_tracking_protection_option_always_radio_selected.png')
         CONTENT_TRACKING_TRACKERS_ALWAYS_RADIO_NOT_SELECTED \
-            = Pattern('content-blocking-tracking-protection-option-always_radio_not_selected.png')
+            = Pattern('content_blocking_tracking_protection_option_always_radio_not_selected.png')
         CONTENT_TRACKING_TRACKERS_ONLY_PRIVATE_WINDOWS_RADIO_SELECTED = \
-            Pattern('content-blocking-tracking-protection-option-private_radio_selected.png')
+            Pattern('content_blocking_tracking_protection_option_private_radio_selected.png')
         CONTENT_TRACKING_TRACKERS_ONLY_PRIVATE_WINDOWS_RADIO_NOT_SELECTED = \
-            Pattern('content-blocking-tracking-protection-option-private_radio_not_selected.png')
+            Pattern('content_blocking_tracking_protection_option_private_radio_not_selected.png')
 
-        TRACKING_PROTECTION_EXCEPTIONS_BUTTON = Pattern('trackingprotectionexceptions_button.png')
+        TRACKING_PROTECTION_EXCEPTIONS_BUTTON = Pattern('tracking_protection_exceptions_button.png')
 
         class Exceptions(object):
             EXCEPTIONS_CONTENT_BLOCKING_LABEL = Pattern('exceptions_content_blocking_label.png')
