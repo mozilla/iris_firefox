6bf87c31a038e5a0e692e619d8a307b1b217ef00
ashughes1 <anthony.s.hughes@gmail.com>
2019-03-19 15:35:03 -0700

    Update run_update_suite.sh to support running multiple tests after update.

--- tools/run_update_suite.sh
+++ tools/run_update_suite.sh
@@ -4,6 +4,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# TEST STRINGS
+SEARCH_CODE_TESTS='default_search_code_baidu,default_search_code_google,default_search_code_yandex'
+MANUAL_UPDATE_TESTS='manual_update,'$SEARCH_CODE_TESTS
+BACKGROUND_UPDATE_TESTS='background_update,'$SEARCH_CODE_TESTS
+
 for i in "$@"
 do
 case $i in
@@ -19,6 +24,10 @@ case $i in
     CHANNEL="${i#*=}"
     shift # past argument=value
     ;;
+    -t=*|--tests=*)
+    TESTS="${i#*=}"
+    shift # past argument=value
+    ;;
     *)
           # unknown option
     ;;
@@ -30,14 +39,20 @@ if [[ -n "$LOCALES" ]] && [[ -n "$VERSIONS" ]]; then
     do
         locale=$(echo -e ${LOCALES} | sed "s/,/ /g" | awk "{print $"${i}"}")
         version=$(echo -e ${VERSIONS} | sed "s/,/ /g" | awk "{print $"${i}"}")
+        tests=$(echo -e ${TESTS} | sed "s/,/ /g" | awk "{print $"${i}"}")
 
         if [[ -n "$locale" ]] && [[ -n "$version" ]]; then
+            if [[ -n "$tests" ]]; then
+                MANUAL_UPDATE_TESTS=${MANUAL_UPDATE_TESTS}","${TESTS}
+                BACKGROUND_UPDATE_TESTS=${BACKGROUND_UPDATE_TESTS}","${TESTS}
+            fi
             echo -e "\n\n${GREEN}Starting MANUAL update test: ${NC}\n"
 
             echo -e "LOCALE = $locale"
             echo -e "FIREFOX VERSION = $version"
             echo -e "CHANNEL = $CHANNEL\n\n"
-            pipenv run iris -l ${locale} -f ${version} -u ${CHANNEL} -t manual_update -o
+
+            pipenv run iris -l ${locale} -f ${version} -u ${CHANNEL} -t ${MANUAL_UPDATE_TESTS} -o
 
             echo -e "\n\n${GREEN}Starting BACKGROUND update test: ${NC}\n"
 
@@ -45,7 +60,7 @@ if [[ -n "$LOCALES" ]] && [[ -n "$VERSIONS" ]]; then
             echo -e "FIREFOX VERSION = $version"
             echo -e "CHANNEL = $CHANNEL\n\n"
 
-            pipenv run iris -l ${locale} -f ${version} -u ${CHANNEL} -t background_update -o
+            pipenv run iris -l ${locale} -f ${version} -u ${CHANNEL} -t ${BACKGROUND_UPDATE_TESTS} -o
         fi
     done
 else
