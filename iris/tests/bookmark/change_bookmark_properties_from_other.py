7933c1411e4f859d55b8db4fe625d52948f9fcf8
yeyartsev <eyartsev@mozilla.com>
2019-03-19 13:28:30 +0200

    Stopped using findall function and text search in bookmark properties fields

--- iris/tests/bookmark/change_bookmark_properties_from_other.py
+++ iris/tests/bookmark/change_bookmark_properties_from_other.py
@@ -23,13 +23,16 @@ class Test(BaseTest):
         bookmark_location_field_pattern = Pattern('location_field_label.png')
         bookmarks_top_menu_pattern = Pattern('bookmarks_top_menu.png')
         other_bookmarks_pattern = Pattern('other_bookmarks.png')
-        firefox_bookmark_top_menu_pattern = Pattern('firefox_bookmark_top_menu.png')
+        firefox_bookmark_top_menu_pattern = Pattern('firefox_bookmark_top_menu.png').similar(0.9)
         bookmark_properties_item_pattern = Pattern('bookmark_properties_button.png')
         name_bookmark_field_pattern = Pattern('name_bookmark_field.png')
         tags_field_pattern = Pattern('tags_field_label.png')
         keyword_field_pattern = Pattern('keyword_field_label.png')
         pocket_bookmark_name_pattern = Pattern('pocket_bookmark_name.png')
-        tag = 'Tag'
+        pocket_url_saved_pattern = Pattern('pocket_url.png')
+        tag_bookmark_saved_pattern = Pattern('tag_bookmark.png')
+        test_keyword_saved_pattern = Pattern('test_keyword.png')
+        tag = 'Tag_bookmark'
         keyword = 'test'
 
         open_firefox_menu()
@@ -43,10 +46,6 @@ class Test(BaseTest):
         assert_true(self, bookmarks_menu_opened, 'Bookmarks menu is opened')
 
         click(other_bookmarks_pattern)
-
-        pocket_bookmarks_list = find_all(pocket_bookmark_name_pattern)
-        pocket_bookmark_unique = len(pocket_bookmarks_list) == 1
-        assert_true(self, pocket_bookmark_unique, 'Only one pocket bookmark is displayed, %d' % len(pocket_bookmarks_list))
 
         firefox_bookmark_displayed = exists(firefox_bookmark_top_menu_pattern)
         assert_true(self, firefox_bookmark_displayed, 'Firefox bookmark is properly displayed')
@@ -109,15 +108,15 @@ class Test(BaseTest):
 
         click(other_bookmarks_pattern)
 
-        pocket_bookmarks_list = find_all(pocket_bookmark_name_pattern)
-        two_pocket_bookmarks_displayed = len(pocket_bookmarks_list) == 2
-        assert_true(self, two_pocket_bookmarks_displayed, 'Two "Pocket" bookmarks are saved')
-
         firefox_bookmark_not_exists = not exists(firefox_bookmark_top_menu_pattern)
         assert_true(self, firefox_bookmark_not_exists, 'Firefox bookmark doesn\'t exist')
 
         pocket_bookmark_at_firefox_bookmark_place = exists(pocket_bookmark_name_pattern, in_region=bookmark_region)
         assert_true(self, pocket_bookmark_at_firefox_bookmark_place, 'Pocket bookmark replaced firefox bookmark')
+
+        hover(Location(SCREEN_WIDTH, other_bookmarks_location_y))
+
+        hover(Location(SCREEN_WIDTH, bookmark_location_y))
 
         right_click(pocket_bookmark_name_pattern, in_region=bookmark_region)
 
@@ -127,15 +126,15 @@ class Test(BaseTest):
         assert_true(self, location_field_reachable, 'Location field is reachable')
         location_field_location = find(bookmark_location_field_pattern)
         location_region = Region(location_field_location.x, location_field_location.y,
-                                 SCREEN_WIDTH // 10, SCREEN_HEIGHT // 10)
-        tags_edited = exists(LocalWeb.POCKET_TEST_SITE, Settings.SITE_LOAD_TIMEOUT, location_region)
+                                 SCREEN_WIDTH // 5, SCREEN_HEIGHT // 10)
+        tags_edited = exists(pocket_url_saved_pattern, in_region=location_region)
         assert_true(self, tags_edited, 'Location is changed')
 
         tags_field_reachable = exists(tags_field_pattern)
         assert_true(self, tags_field_reachable, 'Tags field is reachable')
         tags_field_location = find(tags_field_pattern)
         tags_region = Region(tags_field_location.x, tags_field_location.y, SCREEN_WIDTH // 10, SCREEN_HEIGHT // 10)
-        tags_edited = exists(tag, Settings.SITE_LOAD_TIMEOUT, tags_region)
+        tags_edited = exists(tag_bookmark_saved_pattern, in_region=tags_region)
         assert_true(self, tags_edited, 'Tags are edited')
 
         keywords_field_reachable = exists(keyword_field_pattern)
@@ -143,7 +142,7 @@ class Test(BaseTest):
         keywords_field_location = find(keyword_field_pattern)
         keywords_region = Region(keywords_field_location.x, keywords_field_location.y,
                                  SCREEN_WIDTH // 10, SCREEN_HEIGHT // 10)
-        keywords_edited = exists(keyword, Settings.SITE_LOAD_TIMEOUT, keywords_region)
+        keywords_edited = exists(test_keyword_saved_pattern, in_region=keywords_region)
         assert_true(self, keywords_edited, 'Keywords are edited')
 
         type(Key.ESC)
