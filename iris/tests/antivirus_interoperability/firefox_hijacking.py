c3885663d12af42102b6186ab1ddccbd11484517
Serhii Pronoza <spronoza@mozilla.com>
2019-03-20 16:11:20 +0200

    Patterns added for Win

--- iris/tests/antivirus_interoperability/firefox_hijacking.py
+++ iris/tests/antivirus_interoperability/firefox_hijacking.py
@@ -20,7 +20,6 @@ class Test(BaseTest):
         about_preferences_home_pattern = Pattern('about_preferences_home.png')
         default_search_engine_pattern = Pattern('default_search_engine.png')
         firefox_home_default_pattern = Pattern('firefox_home_default.png')
-        #search_suggestions_default_pattern = Pattern('search_suggestions_default.png')
         search_result_default_pattern = Pattern('search_result_default.png')
         top_sites_pattern = Pattern('top_sites.png')
         bing_search_suggestion_pattern = Pattern('bing_search_suggestion.png')
@@ -37,23 +36,23 @@ class Test(BaseTest):
 
         navigate('about:preferences#search')
 
-        about_preferences_search_opened = exists(about_preferences_search_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        about_preferences_search_opened = exists(about_preferences_search_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, about_preferences_search_opened, 'About preferences search page is successfully opened')
 
-        search_engine_is_default = exists(default_search_engine_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        search_engine_is_default = exists(default_search_engine_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, search_engine_is_default, 'The default search engine is not changed')
 
         navigate('about:preferences#home')
 
-        about_preferences_home_opened = exists(about_preferences_home_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        about_preferences_home_opened = exists(about_preferences_home_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, about_preferences_home_opened, 'About preferences home page is successfully opened')
 
-        firefox_home_is_default = exists(firefox_home_default_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        firefox_home_is_default = exists(firefox_home_default_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, firefox_home_is_default, 'The default home page is not changed')
 
         click(NavBar.HOME_BUTTON)
 
-        new_tab_pattern_exists = exists(Tabs.NEW_TAB_HIGHLIGHTED, DEFAULT_FIREFOX_TIMEOUT)
+        new_tab_pattern_exists = exists(Tabs.NEW_TAB_HIGHLIGHTED, Settings.FIREFOX_TIMEOUT)
         top_sites_pattern_exists = exists(top_sites_pattern)
         assert_true(self, new_tab_pattern_exists and top_sites_pattern_exists, 'The set homepage is opened')
 
@@ -75,13 +74,13 @@ class Test(BaseTest):
                                        google_search_suggestion_width, google_search_suggestion_height)
 
             search_suggestions_displays = exists(search_engine, in_region=suggestion_region)
-            assert_true(self, search_suggestions_displays, 'Search engine #' + str(num) + ' exists')
+            assert_true(self, search_suggestions_displays, 'The search suggestions #' + str(num) + ' exists')
 
             coordinate_x += google_search_suggestion_width
 
-        search_suggestions_displays = exists(wiki_search_suggestion_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, search_suggestions_displays, 'The search suggestions dropdown is displayed, '
-                                                       'no extra search engines are added.')
+        search_suggestions_displays = exists(wiki_search_suggestion_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, search_suggestions_displays, 'All search suggestions is displayed and no'
+                                                       ' extra search engines are added.')
 
         type(Key.ENTER)
 
