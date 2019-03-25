94c205b7331dd2a5c0a8183d7d9aaae56056fa81
alinrus <arus@mozilla.com>
2019-03-25 16:30:30 +0200

    Fix the test case

--- iris/tests/toolbars_window_controls/browser_control_console.py
+++ iris/tests/toolbars_window_controls/browser_control_console.py
@@ -20,7 +20,6 @@ class Test(BaseTest):
         url = "about:blank"
         browser_console = Pattern('browser_console.png')
         clear_web_console = Pattern('clear.png')
-        toggle_filter_bar = Pattern('toggle.png')
 
         navigate(url)
         pop_up_region = click_hamburger_menu_option('Web Developer')
@@ -34,13 +33,11 @@ class Test(BaseTest):
         # Check if the Browser Console options are available.
         expected_2 = exists(clear_web_console, 10)
         assert_true(self, expected_2, 'Clear the web console option is present.')
-        expected_3 = exists(toggle_filter_bar, 10)
-        assert_true(self, expected_3, 'Toggle filter bar option is present.')
 
         click_window_control('close')
         try:
-            expected_4 = wait_vanish(browser_console, 10)
-            assert_true(self, expected_4, 'Browser Console successfully closed.')
+            expected_3 = wait_vanish(browser_console, 10)
+            assert_true(self, expected_3, 'Browser Console successfully closed.')
         except FindError:
             logger.error('Browser Console not closed')
 
@@ -48,8 +45,8 @@ class Test(BaseTest):
         open_browser_console()
         click_window_control("minimize")
         try:
-            expected_5 = wait_vanish(browser_console, 10)
-            assert_true(self, expected_5, 'Browser Console successfully minimized.')
+            expected_4 = wait_vanish(browser_console, 10)
+            assert_true(self, expected_4, 'Browser Console successfully minimized.')
         except FindError:
             logger.error('Browser Console not minimized.')
 
@@ -57,9 +54,9 @@ class Test(BaseTest):
         click_window_control("maximize")
         top_page = Region(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 4)
         try:
-            mouse_move(toggle_filter_bar)
-            expected_6 = top_page.exists(browser_console, 10)
-            assert_true(self, expected_6, 'Browser Console successfully maximized.')
+            mouse_move(clear_web_console)
+            expected_5 = top_page.exists(browser_console, 10)
+            assert_true(self, expected_5, 'Browser Console successfully maximized.')
         except FindError:
             logger.error('Browser Console not maximized.')
 
