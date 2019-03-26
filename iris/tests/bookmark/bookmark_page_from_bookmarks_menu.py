64094db2223bda82f6b4b1b04995faa2d03c6e98
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-03-26 14:11:49 +0200

    Changed timeouts for issue_2139

--- iris/tests/bookmark/bookmark_page_from_bookmarks_menu.py
+++ iris/tests/bookmark/bookmark_page_from_bookmarks_menu.py
@@ -23,28 +23,28 @@ class Test(BaseTest):
         firefox_logo_exists = exists(LocalWeb.FIREFOX_LOGO, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, firefox_logo_exists, 'Website is properly loaded')
 
-        library_button_exists = exists(NavBar.LIBRARY_MENU, Settings.TINY_FIREFOX_TIMEOUT)
+        library_button_exists = exists(NavBar.LIBRARY_MENU)
         assert_true(self, library_button_exists, 'View history, saved bookmarks and more section exists')
 
         click(NavBar.LIBRARY_MENU)
 
-        bookmarks_menu_option_exists = exists(LibraryMenu.BOOKMARKS_OPTION, Settings.TINY_FIREFOX_TIMEOUT)
+        bookmarks_menu_option_exists = exists(LibraryMenu.BOOKMARKS_OPTION)
         assert_true(self, bookmarks_menu_option_exists, 'Bookmarks menu option exists')
 
         click(LibraryMenu.BOOKMARKS_OPTION)
 
-        star_button_unstarred_exists = exists(LocationBar.STAR_BUTTON_UNSTARRED, Settings.TINY_FIREFOX_TIMEOUT)
+        star_button_unstarred_exists = exists(LocationBar.STAR_BUTTON_UNSTARRED)
         assert_true(self, star_button_unstarred_exists, 'Unstarred star-shaped button exists')
 
-        bookmark_menu_is_displayed = exists(bookmark_this_page_pattern, Settings.TINY_FIREFOX_TIMEOUT)
+        bookmark_menu_is_displayed = exists(bookmark_this_page_pattern)
         assert_true(self, bookmark_menu_is_displayed, 'The Bookmarks menu is correctly displayed')
 
         click(bookmark_this_page_pattern)
 
-        star_button_starred_exists = exists(LocationBar.STAR_BUTTON_STARRED, Settings.TINY_FIREFOX_TIMEOUT)
+        star_button_starred_exists = exists(LocationBar.STAR_BUTTON_STARRED)
         assert_true(self, star_button_starred_exists, 'Star-shaped button changed its color to blue')
 
-        bookmarks_menu_option_exists = exists(LibraryMenu.BOOKMARKS_OPTION, Settings.TINY_FIREFOX_TIMEOUT)
+        bookmarks_menu_option_exists = exists(LibraryMenu.BOOKMARKS_OPTION)
         assert_false(self, bookmarks_menu_option_exists, 'Bookmarks menu is dismissed')
 
         star_panel_exists = exists(Bookmarks.StarDialog.NEW_BOOKMARK, Settings.FIREFOX_TIMEOUT)
