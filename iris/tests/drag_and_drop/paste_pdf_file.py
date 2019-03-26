99f39c2d6774a430d36e19b7e494d4cfe3a29b72
pavelciapa <pciapa@mozilla.com>
2019-03-26 15:16:41 +0200

    Added closing file picker for windows

--- iris/tests/drag_and_drop/paste_pdf_file.py
+++ iris/tests/drag_and_drop/paste_pdf_file.py
@@ -29,11 +29,6 @@ class Test(BaseTest):
         PASTE_DELAY = 0.5
         folderpath = self.get_asset_path('')
 
-        new_private_window()
-
-        private_window_opened = exists(PrivateWindow.private_window_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, private_window_opened, 'A new private window is successfully loaded.')
-
         navigate('https://mystor.github.io/dragndrop/')
 
         paste_pdf_file_button_displayed = exists(paste_pdf_button_pattern, Settings.SITE_LOAD_TIMEOUT)
@@ -42,9 +37,8 @@ class Test(BaseTest):
         click(paste_pdf_button_pattern)
 
         paste_pdf_option_selected = exists(paste_pdf_file_selected_button_pattern)
-        assert_true(self, paste_pdf_option_selected,
-                    'The paste-pdf-file changed color to red which indicates that it '
-                    'has been selected.')
+        assert_true(self, paste_pdf_option_selected, 'The paste-pdf-file changed color to red which indicates that it '
+                                                     'has been selected.')
 
         matching_block_available = scroll_until_pattern_found(not_matching_message_pattern, scroll, (-25,), 20, 1)
         assert_true(self, matching_block_available, 'The drop result verification area is displayed on the page')
@@ -66,6 +60,15 @@ class Test(BaseTest):
             paste(folderpath)
             type(Key.ENTER)
 
+        elif Settings.is_linux():
+            open_directory(folderpath)
+
+            finder_window_loaded = exists(MainWindow.CLOSE_BUTTON)
+            assert_true(self, finder_window_loaded, 'Explorer window successfully loaded')
+
+            type(' ', KeyModifier.ALT, PASTE_DELAY)
+            type('m', interval=PASTE_DELAY)
+
         elif Settings.is_mac():
             open_directory(folderpath)
             finder_window_loaded = exists(MainWindow.MAIN_WINDOW_CONTROLS)
@@ -77,37 +80,20 @@ class Test(BaseTest):
             type(Key.ENTER)
             type('2', KeyModifier.CMD)  # change view of finder
 
-            finder_window_location = find(MainWindow.MAIN_WINDOW_CONTROLS)
-            finder_window_before = Location(finder_window_location.x + 10, finder_window_location.y)
-            finder_window_after = Location(SCREEN_WIDTH / 2, finder_window_location.y)
-
-            drag_drop(finder_window_before, finder_window_after)
-
-        elif Settings.is_linux():
-            open_directory(folderpath)
-
-            finder_window_loaded = exists(MainWindow.CLOSE_BUTTON)
-            assert_true(self, finder_window_loaded, 'Explorer window successfully loaded')
-
-            finder_window_location = find(MainWindow.CLOSE_BUTTON)
-            finder_window_after = Location(SCREEN_WIDTH / 2, finder_window_location.y)
-
-            type(' ', KeyModifier.ALT, PASTE_DELAY)
-            type('m', interval=PASTE_DELAY)
-
-            mouse_move(finder_window_after)
-            click(finder_window_after)
-
         test_file_pdf_located = exists(pdf_file_pattern)
         assert_true(self, test_file_pdf_located, 'PDF test file is available')
 
         click(pdf_file_pattern)
 
         edit_copy()
+
+        # closing File picker (Explorer) window
         if Settings.is_windows():
             type(Key.ESC, interval=1)
         elif Settings.is_linux():
-            change_window_view()
+            type('q', KeyModifier.CTRL)
+        elif Settings.is_mac():
+            type('w', KeyModifier.CMD+KeyModifier.ALT)
 
         drop_here_available = exists(drop_here_pattern)
         assert_true(self, drop_here_available, '"Drop here" pattern available')
@@ -123,13 +109,30 @@ class Test(BaseTest):
         if Settings.is_windows():
             open_file_picker()
             finder_window_loaded = exists(AuxiliaryWindow.CLOSE_BUTTON)
-            assert_true(self, finder_window_loaded, 'Explorer window successfully loaded')
+            assert_true(self, finder_window_loaded, 'Finder/Explorer window successfully loaded')
 
             paste(folderpath)
             type(Key.ENTER)
 
-        else:
-            change_window_view()
+        elif Settings.is_linux():
+            open_directory(folderpath)
+
+            finder_window_loaded = exists(MainWindow.CLOSE_BUTTON)
+            assert_true(self, finder_window_loaded, 'Explorer window successfully loaded')
+
+            type(' ', KeyModifier.ALT, PASTE_DELAY)
+            type('m', interval=PASTE_DELAY)
+
+        elif Settings.is_mac():
+            open_directory(folderpath)
+            finder_window_loaded = exists(MainWindow.MAIN_WINDOW_CONTROLS)
+            assert_true(self, finder_window_loaded, 'Finder window successfully loaded')
+
+            # open folder in Finder
+            type('g', modifier=KeyModifier.CMD + KeyModifier.SHIFT)
+            paste(folderpath)
+            type(Key.ENTER)
+            type('2', KeyModifier.CMD)  # change view of Finder
 
         test_file_jpg_located = exists(jpg_file_pattern)
         assert_true(self, test_file_jpg_located, 'JPG test file is available')
@@ -137,12 +140,14 @@ class Test(BaseTest):
         click(jpg_file_pattern)
 
         edit_copy()
+
+        # closing File picker (Explorer) window
         if Settings.is_windows():
             type(Key.ESC, interval=1)
         elif Settings.is_linux():
             type('q', KeyModifier.CTRL)
         elif Settings.is_mac():
-            type('w', KeyModifier.CMD)
+            type('w', KeyModifier.CMD+KeyModifier.ALT)
 
         drop_here_available = exists(drop_here_pattern)
         assert_true(self, drop_here_available, '"Drop here" pattern available')
@@ -156,5 +161,3 @@ class Test(BaseTest):
         #                                                   'expected result is different from result.')
 
         type(Key.ESC)
-
-        close_tab()
