e9f7684894ed0a86a28af285b8d93dcd5d90d23f
pavelciapa <pciapa@mozilla.com>
2019-03-26 18:57:48 +0200

    Added condition for osx for Finder window

--- iris/tests/drag_and_drop/paste_txt_file_in_private_window.py
+++ iris/tests/drag_and_drop/paste_txt_file_in_private_window.py
@@ -60,11 +60,18 @@ class Test(BaseTest):
         open_directory(folderpath)
 
         if Settings.is_mac():
-            time.sleep(Settings.FIREFOX_TIMEOUT)
-            type('2', KeyModifier.CMD, PASTE_DELAY)  # change view of finder
-
-        test_file_txt_located = exists(txt_file_pattern)
-        assert_true(self, test_file_txt_located, 'TXT test file is available')
+            for i in range(3):
+                time.sleep(Settings.TINY_FIREFOX_TIMEOUT)
+                type('2', KeyModifier.CMD, PASTE_DELAY)  # change view of finder
+                test_file_jpg_located = exists(jpg_file_pattern)
+                if test_file_jpg_located:
+                    assert_true(self, test_file_jpg_located, 'JPG test file is available')
+                    break
+                elif i == 2 and not test_file_jpg_located:
+                    assert_true(self, test_file_jpg_located, 'JPG test file is available')
+        else:
+            test_file_jpg_located = exists(jpg_file_pattern)
+            assert_true(self, test_file_jpg_located, 'JPG test file is available')
 
         click(txt_file_pattern)
 
@@ -92,11 +99,18 @@ class Test(BaseTest):
         open_directory(folderpath)
 
         if Settings.is_mac():
-            time.sleep(Settings.FIREFOX_TIMEOUT)
-            type('2', KeyModifier.CMD, PASTE_DELAY)  # change view of finder
-
-        test_file_jpg_located = exists(jpg_file_pattern)
-        assert_true(self, test_file_jpg_located, 'JPG test file is available')
+            for i in range(3):
+                time.sleep(Settings.TINY_FIREFOX_TIMEOUT)
+                type('2', KeyModifier.CMD, PASTE_DELAY)  # change view of finder
+                test_file_jpg_located = exists(jpg_file_pattern)
+                if test_file_jpg_located:
+                    assert_true(self, test_file_jpg_located, 'JPG test file is available')
+                    break
+                elif i == 2 and not test_file_jpg_located:
+                    assert_true(self, test_file_jpg_located, 'JPG test file is available')
+        else:
+            test_file_jpg_located = exists(jpg_file_pattern)
+            assert_true(self, test_file_jpg_located, 'JPG test file is available')
 
         click(jpg_file_pattern)
 
