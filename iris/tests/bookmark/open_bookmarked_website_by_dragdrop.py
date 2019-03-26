c831c6256962b7cddc5266a5b89385eaa426542e
Dmitry Bakaev <dbakaev@mozilla.com>
2019-03-26 18:18:50 +0200

    Excluded open_bookmarked_website_by_dragndrop.py test coverage for osx.

--- iris/tests/bookmark/open_bookmarked_website_by_dragdrop.py
+++ iris/tests/bookmark/open_bookmarked_website_by_dragdrop.py
@@ -14,33 +14,48 @@ class Test(BaseTest):
         self.test_case_id = '164168'
         self.test_suite_id = '2525'
         self.locales = ['en-US']
+        self.exclude = [Platform.MAC]
 
     def setup(self):
         BaseTest.setup(self)
         self.profile = Profile.TEN_BOOKMARKS
 
     def run(self):
+
+        bookmarks_top_menu_toolbar_menu_item_pattern = Pattern('firefox_menu_bookmarks_toolbar.png')
         getting_started_toolbar_bookmark_pattern = Pattern('mozilla_bookmark_icon.png')
         most_visited_bookmarks_folder_pattern = Pattern('most_visited_bookmarks.png')
+        bookmarks_top_menu_option = Pattern('firefox_menu_bookmarks.png')
         pocket_bookmark_pattern = Pattern('pocket_most_visited.png')
 
-        open_bookmarks_toolbar()
+        open_firefox_menu()
 
-        most_visited_bookmarks_folder_available = exists(most_visited_bookmarks_folder_pattern,
-                                                         Settings.SHORT_FIREFOX_TIMEOUT)
+        bookmarks_option_available_in_top_menu = exists(bookmarks_top_menu_option, Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, bookmarks_option_available_in_top_menu,
+                    '\'Bookmarks\' menu item available in firefox top menu')
+
+        click(bookmarks_top_menu_option)
+
+        bookmarks_top_menu_toolbar_option_available = exists(bookmarks_top_menu_toolbar_menu_item_pattern)
+        assert_true(self, bookmarks_top_menu_toolbar_option_available,
+                    '\'Bookmarks toolbar\' option available in \'Bookmarks\' top menu section')
+
+        mouse_move(bookmarks_top_menu_toolbar_menu_item_pattern)
+
+        most_visited_bookmarks_folder_available = exists(most_visited_bookmarks_folder_pattern)
         assert_true(self, most_visited_bookmarks_folder_available,
                     '\'Most visited\' folder is available on the bookmarks toolbar')
 
-        getting_started_bookmark_available = exists(getting_started_toolbar_bookmark_pattern,
-                                                    Settings.SHORT_FIREFOX_TIMEOUT)
+        getting_started_bookmark_available = exists(getting_started_toolbar_bookmark_pattern)
         assert_true(self, getting_started_bookmark_available,
                     '\'Getting started\' bookmark is available on the bookmarks toolbar')
 
-        click(most_visited_bookmarks_folder_pattern)
+        mouse_move(most_visited_bookmarks_folder_pattern)
 
         pocket_bookmark_available = exists(pocket_bookmark_pattern, Settings.TINY_FIREFOX_TIMEOUT)
         assert_true(self, pocket_bookmark_available,
-                    '\'Pocket\' bookmark is available in \'Most visited\' folder on the toolbar')
+                    '\'Pocket\' bookmark is available in \'Most visited\' '
+                    'folder on the \'Bookmarks toolbar\' top menu section')
 
         drag_drop(pocket_bookmark_pattern, Location(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
