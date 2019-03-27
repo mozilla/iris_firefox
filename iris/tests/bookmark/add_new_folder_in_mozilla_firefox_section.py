6c42a6b7a1c2194dbbb86ce8cba086f7f4745b90
Serhii Pronoza <spronoza@mozilla.com>
2019-03-27 13:45:45 +0200

    Proper image addded for iris_new_folder_pattern for linux

--- iris/tests/bookmark/add_new_folder_in_mozilla_firefox_section.py
+++ iris/tests/bookmark/add_new_folder_in_mozilla_firefox_section.py
@@ -10,10 +10,10 @@ class Test(BaseTest):
 
     def __init__(self):
         BaseTest.__init__(self)
-        self.meta = "Add a new Folder in 'Mozilla Firefox' section "
-        self.test_case_id = "163374"
-        self.test_suite_id = "2525"
-        self.locale = ["en-US"]
+        self.meta = 'Add a new Folder in \'Mozilla Firefox\' section'
+        self.test_case_id = '163374'
+        self.test_suite_id = '2525'
+        self.locale = ['en-US']
         self.exclude = [Platform.MAC]
 
     def run(self):
