be296cfd41472ecbb2ace928ac2ab962387f1fe1
Serhii Pronoza <spronoza@mozilla.com>
2019-03-28 17:34:29 +0200

    Fixes after review

--- iris/tests/bookmark/add_new_bookmark_in_mozilla_firefox_section.py
+++ iris/tests/bookmark/add_new_bookmark_in_mozilla_firefox_section.py
@@ -29,13 +29,13 @@ class Test(BaseTest):
 
         open_firefox_menu()
 
-        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        firefox_menu_bookmarks_exists = exists(firefox_menu_bookmarks_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_menu_bookmarks_exists, 'Firefox menu > Bookmarks exists')
 
         click(firefox_menu_bookmarks_pattern)
 
         mozilla_firefox_bookmarks_folder_exists = exists(mozilla_firefox_bookmarks_folder_pattern,
-                                                         DEFAULT_FIREFOX_TIMEOUT)
+                                                         Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, mozilla_firefox_bookmarks_folder_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
                                                                    'bookmarks folder exists')
         click(mozilla_firefox_bookmarks_folder_pattern)
@@ -57,38 +57,41 @@ class Test(BaseTest):
 
         right_click(mozilla_about_us_bookmark_pattern)
 
-        context_menu_new_bookmark_pattern_displayed = exists(Library.Organize.NEW_BOOKMARK, DEFAULT_FIREFOX_TIMEOUT)
+        context_menu_new_bookmark_pattern_displayed = exists(Library.Organize.NEW_BOOKMARK, Settings.FIREFOX_TIMEOUT)
         assert_true(self, context_menu_new_bookmark_pattern_displayed, 'Context menu New Bookmark option is displayed')
 
         click(Library.Organize.NEW_BOOKMARK)
 
-        new_bookmark_window_opened = exists(new_bookmark_window_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        new_bookmark_window_opened = exists(new_bookmark_window_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, new_bookmark_window_opened, 'New Bookmark window is displayed')
 
         paste('SOAP')
         type(Key.TAB)
+
         paste(LocalWeb.SOAP_WIKI_TEST_SITE)
         type(Key.TAB)
+
         paste('SOAP')
         type(Key.TAB)
         type(Key.TAB)
+
         paste('SOAP')
         type(Key.ENTER)
 
         open_firefox_menu()
 
-        firefox_menu_bookmarks__second_exists = exists(firefox_menu_bookmarks_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        firefox_menu_bookmarks__second_exists = exists(firefox_menu_bookmarks_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, firefox_menu_bookmarks__second_exists, 'Firefox menu > Bookmarks exists')
 
         click(firefox_menu_bookmarks_pattern)
 
         mozilla_firefox_bookmarks_folder_second_exists = exists(mozilla_firefox_bookmarks_folder_pattern,
-                                                                DEFAULT_FIREFOX_TIMEOUT)
+                                                                Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, mozilla_firefox_bookmarks_folder_second_exists, 'Firefox menu > Bookmarks > Mozilla Firefox '
                                                                           'bookmarks folder exists')
         click(mozilla_firefox_bookmarks_folder_pattern)
 
-        bookmark_exists = exists(new_soap_bookmark_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        bookmark_exists = exists(new_soap_bookmark_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_exists, 'A new bookmark is added in Mozilla Firefox section.')
 
         restore_firefox_focus()
