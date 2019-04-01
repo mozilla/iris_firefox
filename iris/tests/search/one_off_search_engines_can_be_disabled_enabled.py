374f9cfc28c0210237786a42e5fbcfa029e9c762
Pavel Ciapa <pciapa@mozilla.com>
2019-04-01 16:15:05 +0300

    Added region to second check

--- iris/tests/search/one_off_search_engines_can_be_disabled_enabled.py
+++ iris/tests/search/one_off_search_engines_can_be_disabled_enabled.py
@@ -34,31 +34,31 @@ class Test(BaseTest):
         select_search_bar()
         type(Key.DOWN)
 
-        expected = exists(change_search_settings_pattern, 10)
+        expected = exists(change_search_settings_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The \'Change Search Settings\' button found in the page.')
 
         click(change_search_settings_pattern)
 
-        expected = exists(about_preferences_search_page_pattern, 10)
+        expected = exists(about_preferences_search_page_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The \'about:preferences#search\' page opened.')
 
         # Deselect several search engines from the One-click search engines list.
         paste('one-click')
 
-        expected = exists(search_engine_pattern, 10)
+        expected = exists(search_engine_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The \'One-Click Search Engines\' section found.')
 
-        expected = exists(check_engine_pattern, 10)
+        expected = exists(check_engine_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The check engine pattern found in the page.')
 
         region_int = Screen.LEFT_THIRD
         region = region_int.middle_third_horizontal()
 
-        while region.exists(check_engine_pattern, 2):
+        while region.exists(check_engine_pattern, Settings.TINY_FIREFOX_TIMEOUT):
             click(check_engine_pattern)
 
         try:
-            expected = wait_vanish(check_engine_pattern, 5)
+            expected = wait_vanish(check_engine_pattern, Settings.SHORT_FIREFOX_TIMEOUT, region)
             assert_true(self, expected, 'Each search engine is unchecked.')
         except FindError:
             raise FindError('There are search engines still checked.')
@@ -66,24 +66,28 @@ class Test(BaseTest):
         # Perform a search from a new tab and a private window.
         # Check that the unchecked search engines are no longer displayed in the search bar, one-offs list.
         new_tab()
+
         select_search_bar()
         type(Key.DOWN)
 
         for i in range(pattern_list.__len__()):
             try:
-                expected = wait_vanish(pattern_list[i], 10)
+                expected = wait_vanish(pattern_list[i], Settings.FIREFOX_TIMEOUT)
                 assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list not found.')
+
             except FindError:
                 raise FindError('Element found at position ' + i.__str__() + ' in the list found.')
 
         new_private_window()
+
         select_search_bar()
         type(Key.DOWN)
 
         for i in range(pattern_list.__len__()):
             try:
-                expected = wait_vanish(pattern_list[i], 10)
+                expected = wait_vanish(pattern_list[i], Settings.FIREFOX_TIMEOUT)
                 assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list not found.')
+
             except FindError:
                 raise FindError('Element found at position ' + i.__str__() + ' in the list found.')
 
@@ -95,30 +99,35 @@ class Test(BaseTest):
 
         click(change_search_settings_pattern)
 
-        expected = exists(about_preferences_search_page_pattern, 10)
+        expected = exists(about_preferences_search_page_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The \'about:preferences#search\' page opened.')
 
         type(Key.TAB)
 
         click(amazon_search_bar_pattern.target_offset(-10, 5))
+
         click(bing_search_bar_pattern.target_offset(-10, 5))
+
         click(duckduckgo_search_bar_pattern.target_offset(-10, 5))
 
         # Repeat the above steps to see that the search engines are restored.
         region = Screen.UPPER_RIGHT_CORNER
 
         new_tab()
+
         select_search_bar()
         paste('test')
 
         for i in range(pattern_list.__len__()):
             try:
-                expected = region.exists(pattern_list[i], 10)
+                expected = region.exists(pattern_list[i], Settings.FIREFOX_TIMEOUT)
                 assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list found.')
+
             except FindError:
                 raise FindError('Element found at position ' + i.__str__() + ' in the list not found.')
 
         new_private_window()
+
         select_search_bar()
         type(Key.DOWN)
 
@@ -126,6 +135,7 @@ class Test(BaseTest):
             try:
                 expected = region.exists(pattern_list[i], 10)
                 assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list found.')
+
             except FindError:
                 raise FindError('Element found at position ' + i.__str__() + ' in the list not found.')
 
@@ -133,8 +143,10 @@ class Test(BaseTest):
 
         # Perform a search using a restored one-off search engine.
         type(Key.DOWN)
-        region.click(bing_search_bar_pattern)
-        time.sleep(DEFAULT_UI_DELAY_LONG)
 
-        expected = exists(test_search_bing_pattern, 10)
+        region.click(bing_search_bar_pattern)
+
+        time.sleep(Settings.TINY_FIREFOX_TIMEOUT)
+
+        expected = exists(test_search_bing_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The search is performed with the Bing engine.')
