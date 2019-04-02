f1a3ec98bbe51df869e545b6deaa2cdcc6a79afc
pavelciapa <pciapa@mozilla.com>
2019-04-02 12:58:50 +0300

    Replaced double quotes with single quotes in assert messages.

--- iris/tests/drag_and_drop/paste_jpeg_file.py
+++ iris/tests/drag_and_drop/paste_jpeg_file.py
@@ -36,8 +36,8 @@ class Test(BaseTest):
         click(paste_jpeg_button_pattern)
 
         paste_jpeg_option_selected = exists(paste_jpeg_file_selected_button_pattern)
-        assert_true(self, paste_jpeg_option_selected, 'The paste-jpeg-file changed color to red which indicates that it '
-                                                     'has been selected.')
+        assert_true(self, paste_jpeg_option_selected, 'The paste-jpeg-file changed color to red which indicates that '
+                                                      'it has been selected.')
 
         matching_block_available = scroll_until_pattern_found(not_matching_message_pattern, scroll, (-25,), 20, 1)
         assert_true(self, matching_block_available, 'The drop result verification area is displayed on the page')
@@ -54,27 +54,27 @@ class Test(BaseTest):
         select_file_in_folder(folderpath, jpg_file_pattern, edit_copy)
 
         drop_here_available = exists(drop_here_pattern)
-        assert_true(self, drop_here_available, '"Drop here" pattern available')
+        assert_true(self, drop_here_available, '\'Drop here\' pattern available')
 
         click(drop_here_pattern, drag_and_drop_duration)
 
         edit_paste()
 
         matching_message_displayed = exists(matching_message_pattern, in_region=matching_region)
-        assert_true(self, matching_message_displayed, 'Matching appears under the "Drop Stuff Here" area and expected'
+        assert_true(self, matching_message_displayed, 'Matching appears under the \'Drop Stuff Here\' area and expected'
                                                       'result is identical to result. ')
 
         select_file_in_folder(folderpath, pdf_file_pattern, edit_copy)
 
         drop_here_available = exists(drop_here_pattern)
-        assert_true(self, drop_here_available, '"Drop here" pattern available')
+        assert_true(self, drop_here_available, '\'Drop here\' pattern available')
 
         click(drop_here_pattern, drag_and_drop_duration)
 
         edit_paste()
 
         not_matching_message_displayed = exists(not_matching_message_pattern, in_region=not_matching_region)
-        assert_true(self, not_matching_message_displayed, 'Not Matching appears under the "Drop Stuff Here" area and '
+        assert_true(self, not_matching_message_displayed, 'Not Matching appears under the \'Drop Stuff Here\' area and '
                                                           'expected result is different from result.')
 
         type(Key.ESC)
