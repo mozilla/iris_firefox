62b5d755f33ae3f9796888ef206270bd50d3318b
Dmitry Bakaev <dbakaev@mozilla.com>
2019-04-03 16:56:51 +0300

    Replaced timeouts, separated clicks and tab opening/switching activities with empty lines.

--- iris/tests/drag_and_drop/paste_html.py
+++ iris/tests/drag_and_drop/paste_html.py
@@ -26,46 +26,57 @@ class Test(BaseTest):
         drop_stuff_here_area_pattern = Pattern('drop_here.png')
 
         navigate('https://mystor.github.io/dragndrop/')
-        page_opened = exists(paste_html_data_radiobutton_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
+        page_opened = exists(paste_html_data_radiobutton_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, page_opened, 'Firefox started and page loaded successfully.')
 
         click(paste_html_data_radiobutton_pattern)
+
         paste_html_data_selected = exists(paste_html_data_radiobutton_selected_pattern)
         assert_true(self, paste_html_data_selected,
                     'The \'paste-html-data\' changed color to red which indicates that it has been selected.')
 
         new_tab()
+
         select_tab(2)
+
         navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
-        page_opened = exists(phrase_from_wiki_page_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
+        page_opened = exists(phrase_from_wiki_page_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, page_opened, 'Web page successfully loads.')
 
         double_click(phrase_from_wiki_page_pattern)
+
         edit_copy()
+
         text_copied = Env.get_clipboard() == 'SOAP'
         assert_true(self, text_copied, 'The text is copied to the clipboard.')
 
         select_tab(1)
+
         drop_stuff_here_area_pattern = exists(drop_stuff_here_area_pattern)
         assert_true(self, drop_stuff_here_area_pattern, '\'Drop stuff here\' area is displayed on the page')
 
         edit_paste()
+
         matching_message_exists = scroll_until_pattern_found(matching_message_pattern, type, (Key.PAGE_DOWN,))
         assert_true(self, matching_message_exists,
                     '\'Matching\' appears under the \'Drop Stuff Here\' '
                     'area, the expected result is identical to the result.')
 
         select_tab(2)
+
         image_exists = exists(image_from_wiki_article_pattern)
         assert_true(self, image_exists, 'Image is displayed on the page')
 
         right_click(image_from_wiki_article_pattern)
+
         copy_image_option_available = exists(copy_image_context_menu_pattern)
         assert_true(self, copy_image_option_available,
                     '\'Copy Image\' option is available in the context menu after right clicking at the image')
 
         click(copy_image_context_menu_pattern)
+
         select_tab(1)
+
         edit_paste()
 
         not_matching_message_displayed = scroll_until_pattern_found(not_matching_message_pattern, type,
