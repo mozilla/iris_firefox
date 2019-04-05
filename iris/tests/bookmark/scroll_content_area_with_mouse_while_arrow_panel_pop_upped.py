da19dbf8525fe26eaae993d0cdd4554eafb6e0b2
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-04-05 18:30:39 +0300

    Added changes to script for issue_2211

--- iris/tests/bookmark/scroll_content_area_with_mouse_while_arrow_panel_pop_upped.py
+++ iris/tests/bookmark/scroll_content_area_with_mouse_while_arrow_panel_pop_upped.py
@@ -23,7 +23,7 @@ class Test(BaseTest):
         assert_true(self, soap_wiki_label_exists, 'Soap page is opened')
 
         library_icon_exists = exists(NavBar.LIBRARY_MENU, Settings.SHORT_FIREFOX_TIMEOUT)
-        assert_true(self, library_icon_exists, 'Site information panel icon exists')
+        assert_true(self, library_icon_exists, 'The Library menu icon exists')
 
         click(NavBar.LIBRARY_MENU)
 
@@ -51,3 +51,24 @@ class Test(BaseTest):
         assert_true(self, hamburger_menu_icon_exists, 'Hamburger menu icon exists')
 
         click(NavBar.HAMBURGER_MENU)
+
+        hamburger_menu_panel_exists = exists(HamburgerMenu.HAMBURGER_MENU_ZOOM_INDICATOR,
+                                             Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, hamburger_menu_panel_exists, 'Hamburger menu panel is displayed')
+
+        mouse_move(LocalWeb.SOAP_WIKI_SOAP_LABEL)
+
+        scroll_until_pattern_found(soap_scroll_content, scroll, (-SCREEN_HEIGHT, None), 100,
+                                   Settings.TINY_FIREFOX_TIMEOUT/2)
+
+        try:
+            soap_wiki_label_not_exists = wait_vanish(LocalWeb.SOAP_WIKI_SOAP_LABEL.similar(.9),
+                                                     Settings.SHORT_FIREFOX_TIMEOUT)
+            assert_true(self, soap_wiki_label_not_exists,
+                        'Scroll using mouse wheel is successful with hamburger menu pop upped panel')
+        except FindError:
+            raise FindError('Content is still on the page')
+
+        click(soap_scroll_content)
+
+        page_home()
