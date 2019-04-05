77d820db5827c665387d9fbfedff0494e0690888
Pavel Ciapa <pciapa@mozilla.com>
2019-04-05 13:37:00 +0300

    Added scroll untill pattern

--- iris/tests/awesomebar/search_engine_can_be_added.py
+++ iris/tests/awesomebar/search_engine_can_be_added.py
@@ -18,7 +18,6 @@ class Test(BaseTest):
 
     def run(self):
         moz_pattern = Pattern('moz.png')
-        url = LocalWeb.FIREFOX_TEST_SITE
         search_engine_pattern = Pattern('search_engine.png')
         search_settings_pattern = Pattern('search_settings.png')
         amazon_one_off_button_pattern = Pattern('amazon_one_off_button.png')
@@ -36,44 +35,44 @@ class Test(BaseTest):
         add_button_pattern = Pattern('add_button.png')
         startpage_https_search_engine_pattern = Pattern('startpage_https_search_engine.png')
         startpage_one_off_button_pattern = Pattern('startpage_one_off_button.png')
-        find_add_ons = Pattern('find_add_ons.png')
+        find_add_ons_pattern = Pattern('find_add_ons.png')
 
-        region = Region(0, 0, SCREEN_WIDTH, 2 * SCREEN_HEIGHT / 3)
+        left_two_thirds_of_screen_region = Region(0, 0, SCREEN_WIDTH, 2 * SCREEN_HEIGHT / 3)
 
-        navigate(url)
+        navigate(LocalWeb.FIREFOX_TEST_SITE)
 
-        expected = exists(LocalWeb.FIREFOX_LOGO, 10)
-        assert_true(self, expected, 'Page successfully loaded, firefox logo found.')
+        site_loaded = exists(LocalWeb.FIREFOX_LOGO, 10)
+        assert_true(self, site_loaded, 'Page successfully loaded, firefox logo found.')
 
         select_location_bar()
         paste('moz')
 
-        pattern_list = [moz_pattern, search_settings_pattern, amazon_one_off_button_pattern,
+        one_of_pattern_list = [moz_pattern, search_settings_pattern, amazon_one_off_button_pattern,
                         bing_one_off_button_pattern, duck_duck_go_one_off_button_pattern, google_one_off_button_pattern,
                         twitter_one_off_button_pattern, wikipedia_one_off_button_pattern]
 
         # Deleted assert for ebay because we no longer have the ebay search engine in some locations.
 
         # Check that the default one-off list is displayed in the awesomebar.
-        for i in range(pattern_list.__len__()):
+        for one_search_engine in range(one_of_pattern_list.__len__()):
             try:
                 if Settings.get_os() == Platform.MAC:
-                    expected = region.exists(pattern_list[i].similar(0.7), 10)
-                    assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list found.')
+                    one_of_pattern_exists= left_two_thirds_of_screen_region.exists(one_of_pattern_list[one_search_engine].similar(0.7), 10)
+                    assert_true(self, one_of_pattern_exists, 'Element found at position ' + one_search_engine.__str__() + ' in the list found.')
                 else:
-                    expected = region.exists(pattern_list[i].similar(0.9), 10)
-                    assert_true(self, expected, 'Element found at position ' + i.__str__() + ' in the list found.')
+                    one_of_pattern_exists = left_two_thirds_of_screen_region.exists(one_of_pattern_list[one_search_engine].similar(0.9), 10)
+                    assert_true(self, one_of_pattern_exists, 'Element found at position ' + one_search_engine.__str__() + ' in the list found.')
             except FindError:
-                raise FindError('Element found at position ' + i.__str__() + ' in the list not found.')
+                raise FindError('Element found at position ' + one_search_engine.__str__() + ' in the list not found.')
 
         click(search_settings_pattern)
         time.sleep(DEFAULT_UI_DELAY)
 
-        expected = exists(about_preferences_search_page_pattern, 10)
-        assert_true(self, expected, 'The \'about:preferences#search\' page successfully loaded.')
+        search_page = exists(about_preferences_search_page_pattern, 10)
+        assert_true(self, search_page, 'The \'about:preferences#search\' page successfully loaded.')
 
-        expected = exists(default_search_engine_dropdown_pattern, 10)
-        assert_true(self, expected, 'Default search engine dropdown found.')
+        search_engine_dropdown = exists(default_search_engine_dropdown_pattern, 10)
+        assert_true(self, search_engine_dropdown, 'Default search engine dropdown found.')
 
         click(default_search_engine_dropdown_pattern)
 
@@ -86,14 +85,17 @@ class Test(BaseTest):
         previous_tab()
 
         select_location_bar()
+
         type(Key.DELETE)
+
         time.sleep(DEFAULT_UI_DELAY_LONG)
         paste('moz')
+
         time.sleep(DEFAULT_UI_DELAY_LONG)
         type(Key.SPACE)
 
-        expected = exists(moz_search_amazon_search_engine_pattern, 10)
-        assert_true(self, expected, 'Default search engine successfully changed.')
+        amazon_search_engine = exists(moz_search_amazon_search_engine_pattern, 10)
+        assert_true(self, amazon_search_engine, 'Default search engine successfully changed.')
 
         # Remove the 'Google' search engine.
         next_tab()
@@ -105,10 +107,15 @@ class Test(BaseTest):
             type(Key.SPACE)
         else:
             type(Key.TAB)
-            click(search_engine_pattern.target_offset(20, 150))
+            google_one_off_button = scroll_until_pattern_found(google_one_off_button_pattern, scroll, (-25,), 20, 1)
+            assert_true(self, google_one_off_button, 'google_one_off_button_pattern')
+            google_one_off_button_location = find(google_one_off_button_pattern)
+            google_button_width, google_button_height = google_one_off_button_pattern.get_size()
+            click(Location(google_one_off_button_location.x - int(google_button_width/2),
+                           google_one_off_button_location.y + int(google_button_width/2)))
 
-        expected = exists(search_engine_pattern, 10)
-        assert_true(self, expected, 'One-Click Search Engines section found.')
+        one_click_search_engine_pattern = exists(search_engine_pattern, 10)
+        assert_true(self, one_click_search_engine_pattern, 'One-Click Search Engines section found.')
 
         # Check that unchecked search engine is successfully removed from the one-off searches bar.
         previous_tab()
@@ -128,8 +135,8 @@ class Test(BaseTest):
             except FindError:
                 raise FindError('Unchecked search engine not removed from the one-off searches bar.')
         else:
-            expected = exists(google_one_off_button_pattern.similar(0.9), 10)
-            assert_false(self, expected, 'Unchecked search engine successfully removed from the one-off searches bar.')
+            google_one_off_search_engine = exists(google_one_off_button_pattern.similar(0.9), 10)
+            assert_false(self, google_one_off_search_engine, 'Unchecked search engine successfully removed from the one-off searches bar.')
 
         # Add a new search engine.
         next_tab()
@@ -147,34 +154,32 @@ class Test(BaseTest):
 
         click(find_more_search_engines_pattern)
 
-        try:
-            wait(find_add_ons, 10)
-            logger.debug('Find add-ons field is present on the page.')
-            click(find_add_ons)
-        except FindError:
-            raise FindError('Find add-ons field is NOT present on the page, aborting.')
+        find_add_ons = exists(find_add_ons_pattern, Settings.SITE_LOAD_TIMEOUT)
+        assert_true(self, find_add_ons, 'Find add-ons field is present on the page.')
+
+        click(find_add_ons_pattern)
 
         paste('startpage')
 
-        expected = exists(add_startpage_https_privacy_search_engine_pattern, 10)
-        assert_true(self, expected, '\'Startpage HTTPS Privacy Search Engine\' engine successfully found.')
+        startpage_search_engine = exists(add_startpage_https_privacy_search_engine_pattern, 10)
+        assert_true(self, startpage_search_engine, '\'Startpage HTTPS Privacy Search Engine\' engine successfully found.')
 
         click(add_startpage_https_privacy_search_engine_pattern)
 
-        expected = exists(add_to_firefox_pattern, 10)
-        assert_true(self, expected, '\'Add to Firefox\' button found.')
+        add_to_firefox_button = exists(add_to_firefox_pattern, 10)
+        assert_true(self, add_to_firefox_button, '\'Add to Firefox\' button found.')
 
         click(add_to_firefox_pattern)
 
-        expected = exists(add_button_pattern, 10)
-        assert_true(self, expected, '\'Add\' button found.')
+        add_button = exists(add_button_pattern, 10)
+        assert_true(self, add_button, '\'Add\' button found.')
 
-        click(add_button_pattern)
+        click(add_button_pattern, 1)
 
         previous_tab()
 
-        expected = exists(startpage_https_search_engine_pattern, 10)
-        assert_true(self, expected, 'The search engine added found in the \'One-Click Search Engines\' section.')
+        startpage_search_engine = exists(startpage_https_search_engine_pattern, 10)
+        assert_true(self, startpage_search_engine, 'The search engine added found in the \'One-Click Search Engines\' section.')
 
         # Perform a new search in the url bar and make sure that everything looks ok after all the above changes.
         previous_tab()
@@ -184,15 +189,15 @@ class Test(BaseTest):
         paste('moz')
         type(Key.SPACE)
 
-        expected = exists(moz_search_amazon_search_engine_pattern, 10)
-        assert_true(self, expected, 'Default search engine is still changed.')
+        moz_search_amazon_search_engine = exists(moz_search_amazon_search_engine_pattern, 10)
+        assert_true(self, moz_search_amazon_search_engine, 'Default search engine is still changed.')
 
-        expected = exists(startpage_one_off_button_pattern, 10)
-        assert_true(self, expected, 'Newly added search engine successfully found in the one-off searches bar.')
+        startpage_one_off_button = exists(startpage_one_off_button_pattern, 10)
+        assert_true(self, startpage_one_off_button, 'Newly added search engine successfully found in the one-off searches bar.')
 
         if Settings.get_os() == Platform.MAC:
-            expected = exists(google_one_off_button_pattern.similar(0.9), 10)
-            assert_false(self, expected, 'Unchecked search engine is still removed from the one-off searches bar.')
+            google_one_off_button = exists(google_one_off_button_pattern.similar(0.9), 10)
+            assert_false(self, google_one_off_button, 'Unchecked search engine is still removed from the one-off searches bar.')
         else:
-            expected = exists(google_one_off_button_pattern, 10)
-            assert_false(self, expected, 'Unchecked search engine is still removed from the one-off searches bar.')
+            google_one_off_button = exists(google_one_off_button_pattern, 10)
+            assert_false(self, google_one_off_button, 'Unchecked search engine is still removed from the one-off searches bar.')
