42890ec26085f752100c259c05001054b9129986
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-04-08 11:32:56 +0300

    Finished script for issue_2211

--- iris/tests/bookmark/scroll_content_area_with_mouse_while_arrow_panel_pop_upped.py
+++ iris/tests/bookmark/scroll_content_area_with_mouse_while_arrow_panel_pop_upped.py
@@ -15,7 +15,8 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        soap_scroll_content = Pattern('soap_scroll_content.png')
+        soap_scroll_content_pattern = Pattern('soap_scroll_content.png')
+        site_information_panel_pattern = Pattern('site_information_panel.png')
 
         navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
 
@@ -32,7 +33,7 @@ class Test(BaseTest):
 
         mouse_move(LocalWeb.SOAP_WIKI_SOAP_LABEL)
 
-        scroll_until_pattern_found(soap_scroll_content, scroll, (-SCREEN_HEIGHT, None), 100,
+        scroll_until_pattern_found(soap_scroll_content_pattern, scroll, (-SCREEN_HEIGHT, None), 100,
                                    Settings.TINY_FIREFOX_TIMEOUT/2)
 
         try:
@@ -43,7 +44,7 @@ class Test(BaseTest):
         except FindError:
             raise FindError('Content is still on the page')
 
-        click(soap_scroll_content)
+        click(soap_scroll_content_pattern)
 
         page_home()
 
@@ -58,7 +59,7 @@ class Test(BaseTest):
 
         mouse_move(LocalWeb.SOAP_WIKI_SOAP_LABEL)
 
-        scroll_until_pattern_found(soap_scroll_content, scroll, (-SCREEN_HEIGHT, None), 100,
+        scroll_until_pattern_found(soap_scroll_content_pattern, scroll, (-SCREEN_HEIGHT, None), 100,
                                    Settings.TINY_FIREFOX_TIMEOUT/2)
 
         try:
@@ -69,6 +70,57 @@ class Test(BaseTest):
         except FindError:
             raise FindError('Content is still on the page')
 
-        click(soap_scroll_content)
+        click(soap_scroll_content_pattern)
+
+        page_home()
+
+        site_information_icon_exists = exists(site_information_panel_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, site_information_icon_exists, 'Site information icon exists')
+
+        click(site_information_panel_pattern)
+
+        site_information_panel_exists = exists(SiteInformationPanel.SITE_INFORMATION_PANEL_LABEL,
+                                               Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, site_information_panel_exists, 'Site information panel is displayed')
+
+        mouse_move(LocalWeb.SOAP_WIKI_SOAP_LABEL)
+
+        scroll_until_pattern_found(soap_scroll_content_pattern, scroll, (-SCREEN_HEIGHT, None), 100,
+                                   Settings.TINY_FIREFOX_TIMEOUT / 2)
+
+        try:
+            soap_wiki_label_not_exists = wait_vanish(LocalWeb.SOAP_WIKI_SOAP_LABEL.similar(.9),
+                                                     Settings.SHORT_FIREFOX_TIMEOUT)
+            assert_true(self, soap_wiki_label_not_exists,
+                        'Scroll using mouse wheel is successful with hamburger menu pop upped panel')
+        except FindError:
+            raise FindError('Content is still on the page')
+
+        click(soap_scroll_content_pattern)
+
+        page_home()
+
+        star_button_exists = exists(LocationBar.STAR_BUTTON_UNSTARRED, Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, star_button_exists, 'Star button icon exists')
+
+        click(LocationBar.STAR_BUTTON_UNSTARRED)
+
+        star_panel_exists = exists(Bookmarks.StarDialog.NEW_BOOKMARK, Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, star_panel_exists, 'Star panel is displayed')
+
+        mouse_move(LocalWeb.SOAP_WIKI_SOAP_LABEL)
+
+        scroll_until_pattern_found(soap_scroll_content_pattern, scroll, (-SCREEN_HEIGHT, None), 100,
+                                   Settings.TINY_FIREFOX_TIMEOUT / 2)
+
+        try:
+            soap_wiki_label_not_exists = wait_vanish(LocalWeb.SOAP_WIKI_SOAP_LABEL.similar(.9),
+                                                     Settings.SHORT_FIREFOX_TIMEOUT)
+            assert_true(self, soap_wiki_label_not_exists,
+                        'Scroll using mouse wheel is successful with hamburger menu pop upped panel')
+        except FindError:
+            raise FindError('Content is still on the page')
+
+        click(soap_scroll_content_pattern)
 
         page_home()
