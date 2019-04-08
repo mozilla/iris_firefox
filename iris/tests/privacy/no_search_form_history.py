6cc4339f840874b619bb724eab178ec805a6f167
yeyartsev <eyartsev@mozilla.com>
2019-04-08 16:53:01 +0300

    Clicks are wrapped with asserts

--- iris/tests/privacy/no_search_form_history.py
+++ iris/tests/privacy/no_search_form_history.py
@@ -29,35 +29,40 @@ class Test(BaseTest):
         autocomplete_pattern = Pattern('word_autocomplete.png')
         form_address = self.get_asset_path('form.html')
 
-        navigate("about:preferences#privacy")
+        navigate('about:preferences#privacy')
         preferences_opened = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED)
         assert_true(self, preferences_opened, 'The page is successfully displayed.')
 
         paste('remember')
         remember_history_menu_found = exists(remember_history_pattern)
         assert_true(self, remember_history_menu_found, 'History menu found.')
+
         click(remember_history_pattern)
 
         history_dropdown_opened = exists(custom_history_settings_pattern)
         assert_true(self, history_dropdown_opened, 'The option is successfully selected and remembered.')
+
         click(custom_history_settings_pattern)
+
         click(remember_search_history_pattern)
 
         remember_search_unticked = exists(search_history_unticked_pattern)
         assert_true(self, remember_search_unticked, 'The checkbox is successfully unticked.')
 
-        navigate("about:preferences#search")
+        navigate('about:preferences#search')
         search_preferences_opened = exists(add_search_bar_pattern)
         assert_true(self, search_preferences_opened, 'The about:preferences#search page is successfully displayed.')
+
         click(add_search_bar_pattern)
 
         search_bar_appeared = exists(LocationBar.SEARCH_BAR)
         assert_true(self, search_bar_appeared, 'The search bar is successfully added in the toolbar.')
+
         click(LocationBar.SEARCH_BAR)
 
         paste('random')
         type(Key.ENTER)
-        search_done = exists(search_tab_pattern, DEFAULT_FIREFOX_TIMEOUT*3)
+        search_done = exists(search_tab_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, search_done, 'The Search is successfully performed.')
 
         new_tab()
@@ -71,18 +76,23 @@ class Test(BaseTest):
 
         navigate(form_address)
         name_form_displayed = exists(name_form_pattern)
-        password_form_displayed = exists(password_form_pattern)
-        assert_true(self, name_form_displayed, "Form displayed.")
-        assert_true(self, password_form_displayed, 'The website is successfully displayed.')
+        assert_true(self, name_form_displayed, 'Form displayed.')
+
         click(name_form_pattern)
 
         paste('random')
+        password_form_displayed = exists(password_form_pattern)
+        assert_true(self, password_form_displayed, 'The website is successfully displayed.')
+
         click(password_form_pattern)
+
         paste('Asdf1@3$')
         type(Key.ENTER)
 
         navigate(form_address)
+
         click(name_form_pattern)
+
         type('r')
         no_autocomplete = not exists(autocomplete_pattern)
         assert_true(self, no_autocomplete, 'The form history is not saved.')
