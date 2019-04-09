4c50463690b2491e921c71e437323ae65a971450
Pavel Ciapa <pciapa@mozilla.com>
2019-04-09 10:00:27 +0300

    Call new_tab() earlier

--- iris/tests/search/search_suggestions_can_be_disabled.py
+++ iris/tests/search/search_suggestions_can_be_disabled.py
@@ -20,11 +20,9 @@ class Test(BaseTest):
         provide_search_suggestions_pattern = Pattern('provide_search_suggestions.png')
         show_suggestions_pattern = Pattern('show_suggestions.png')
         google_logo_content_search_field_pattern = Pattern('google_logo_content_search_field.png')
+        search_suggestions_not_displayed_search_bar_pattern = Pattern('search_suggestions_are_disabled_search_bar.png')
         search_suggestions_not_displayed_content_search_pattern = \
             Pattern('search_suggestions_are_disabled_content_search.png')
-        search_suggestions_are_disabled_search_bar_pattern = Pattern('search_suggestions_are_disabled_search_bar.png')
-        search_suggestions_displayed_content_search_pattern = Pattern('search_suggestions_displayed_content_search.png')
-        search_suggestions_displayed_search_bar_pattern = Pattern('search_suggestions_displayed_search_bar.png')
 
         # Enable the search bar.
         change_preference('browser.search.widget.inNavBar', True)
@@ -47,16 +45,16 @@ class Test(BaseTest):
         assert_true(self, provide_search_suggestions, 'The \'Provide search suggestions\' option is disabled.')
 
         # Type in some random text in the Search Bar and content search field.
+        new_tab()
+
         select_search_bar()
 
         paste('test')
 
-        suggestions_search_not_displayed = exists(search_suggestions_are_disabled_search_bar_pattern,
+        suggestions_search_not_displayed = exists(search_suggestions_not_displayed_search_bar_pattern,
                                                   Settings.FIREFOX_TIMEOUT)
         assert_true(self, suggestions_search_not_displayed, 'Search suggestions are not displayed for any of these'
                                                             ' locations.')
-
-        new_tab()
 
         google_logo_search_field = exists(google_logo_content_search_field_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, google_logo_search_field, 'Google logo from content search field found.')
@@ -66,32 +64,36 @@ class Test(BaseTest):
         paste('test')
 
         suggestions_content_not_displayed = exists(search_suggestions_not_displayed_content_search_pattern,
-                                           Settings.FIREFOX_TIMEOUT)
+                                                   Settings.FIREFOX_TIMEOUT)
         assert_true(self, suggestions_content_not_displayed, 'Search suggestions are not shown for the input '
                                                              'in question.')
 
         # Go to 'about:preferences#search' and check the 'Provide search suggestions' option.
         navigate('about:preferences#search')
 
+        provide_search_suggestions = exists(provide_search_suggestions_pattern, Settings.SITE_LOAD_TIMEOUT)
+        assert_true(self, provide_search_suggestions, 'The \'Provide search suggestions\' option is disabled.')
+
         click(provide_search_suggestions_pattern)
 
         # Type in some random text in the Search Bar and content search field.
+        new_tab()
+
         select_search_bar()
 
         paste('test')
 
-        suggestions_search_displayed = exists(search_suggestions_displayed_search_bar_pattern,
-                                              Settings.FIREFOX_TIMEOUT)
+        suggestions_search_displayed = exists(search_suggestions_not_displayed_search_bar_pattern,
+                                              Settings.SITE_LOAD_TIMEOUT)
         assert_false(self, suggestions_search_displayed, 'Search suggestions are shown for the input in question.')
 
-        new_tab()
-
-        google_logo_search_field = exists(google_logo_content_search_field_pattern, Settings.SITE_LOAD_TIMEOUT)
+        google_logo_search_field = exists(google_logo_content_search_field_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, google_logo_search_field, 'Google logo from content search field found.')
 
         click(google_logo_content_search_field_pattern)
 
         paste('test')
 
-        suggestions_content_displayed = exists(search_suggestions_displayed_content_search_pattern, Settings.FIREFOX_TIMEOUT)
+        suggestions_content_displayed = exists(search_suggestions_not_displayed_content_search_pattern,
+                                               Settings.FIREFOX_TIMEOUT)
         assert_false(self, suggestions_content_displayed, 'Search suggestions are shown for the input in question.')
