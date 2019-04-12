edd59b601ac2d27fd00054e42939ddb1f65e1521
Serhii Pronoza <spronoza@mozilla.com>
2019-04-12 13:06:25 +0300

    Win 10 support added for issue 2209

--- iris/tests/bookmark/reordering_bookmarks_via_dragdrop.py
+++ iris/tests/bookmark/reordering_bookmarks_via_dragdrop.py
@@ -22,6 +22,11 @@ class Test(BaseTest):
 
     def run(self):
         library_mozilla_firefox_folder_pattern = Pattern('library_mozilla_firefox_folder.png')
+        mozilla_about_us_bookmark_pattern = Pattern('library_about_us_bookmark.png')
+        customize_firefox_bookmark_pattern = Pattern('library_customize_firefox_bookmark.png')
+        get_involved_bookmark_pattern = Pattern('library_get_involved_bookmark.png')
+        help_and_tutorials_bookmark_pattern = Pattern('library_help_and_tutorials_bookmark.png')
+        help_and_tutorial_first_line_pattern = Pattern('help_and_tutorial_first_line.png')
 
         open_library()
 
@@ -31,10 +36,57 @@ class Test(BaseTest):
         maximize_window()
 
         bookmark_menu_folder_exists = exists(Library.BOOKMARKS_MENU)
-        assert_true(self, bookmark_menu_folder_exists, 'bookmark_menu_folder_exists')
+        assert_true(self, bookmark_menu_folder_exists, 'Bookmark menu folder exists')
 
-        click(Library.BOOKMARKS_MENU)
+        double_click(Library.BOOKMARKS_MENU)
 
         mozilla_firefox_folder_exists = exists(library_mozilla_firefox_folder_pattern)
-        assert_true(self, mozilla_firefox_folder_exists, 'mozilla_firefox_folder_exists')
+        assert_true(self, mozilla_firefox_folder_exists, 'Mozilla Firefox folder exists')
 
+        click(library_mozilla_firefox_folder_pattern)
+
+        mozilla_customize_firefox_bookmark_exists = exists(customize_firefox_bookmark_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, mozilla_customize_firefox_bookmark_exists, 'Customize Firefox bookmark is displayed')
+
+        mozilla_get_involved_bookmark_exists = exists(get_involved_bookmark_pattern)
+        assert_true(self, mozilla_get_involved_bookmark_exists, 'Get Involved bookmark is displayed')
+
+        mozilla_help_and_tutorials_bookmark_exists = exists(help_and_tutorials_bookmark_pattern)
+        assert_true(self, mozilla_help_and_tutorials_bookmark_exists, 'Help and Tutorials bookmark is displayed')
+
+        mozilla_about_us_bookmark_exists = exists(mozilla_about_us_bookmark_pattern)
+        assert_true(self, mozilla_about_us_bookmark_exists, 'About Us bookmark is displayed')
+
+        help_and_tutorials_location = find(help_and_tutorials_bookmark_pattern)
+        help_and_tutorials_width, help_and_tutorials_height = help_and_tutorials_bookmark_pattern.get_size()
+        first_bookmark_region = Region(help_and_tutorials_location.x, help_and_tutorials_location.y,
+                                       help_and_tutorials_width, help_and_tutorials_height)
+
+        second_bookmark_region = Region(help_and_tutorials_location.x,
+                                        help_and_tutorials_location.y + help_and_tutorials_height,
+                                        help_and_tutorials_width, help_and_tutorials_height)
+
+        location_to_drop = find(get_involved_bookmark_pattern).below(10)
+
+        click(mozilla_about_us_bookmark_pattern)
+
+        type(Key.DELETE)
+
+        help_and_tutorial_bookmark_position = exists(help_and_tutorial_first_line_pattern)
+        assert_true(self, help_and_tutorial_bookmark_position, 'Help and tutorial bookmark is placed on the first line')
+
+        drag_drop(help_and_tutorials_location, location_to_drop)
+
+        mozilla_customize_firefox_bookmark_exists = exists(customize_firefox_bookmark_pattern, Settings.FIREFOX_TIMEOUT,
+                                                           first_bookmark_region)
+        assert_true(self, mozilla_customize_firefox_bookmark_exists, 'Customize Firefox bookmark is placed on the first'
+                                                                     'line after replacing Help and tutorial by drag '
+                                                                     'and drop to the third line')
+
+        mozilla_get_involved_bookmark_exists = exists(get_involved_bookmark_pattern, in_region=second_bookmark_region)
+        assert_true(self, mozilla_get_involved_bookmark_exists, 'Get Involved bookmark is displayed is placed on the '
+                                                                'second line after replacing Help and tutorial by drag '
+                                                                'and drop to the third line. '
+                                                                'Help and tutorial placed on the third line')
+
+        close_window_control('auxiliary')
