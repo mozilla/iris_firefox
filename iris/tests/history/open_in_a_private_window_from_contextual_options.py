aaf043ba0fa41f8551aead0b0aeaed208eb4b558
spronoza <spronoza@mozilla.com>
2019-04-18 12:59:32 +0300

    open_library_menu() replaced on the separate actions because of unstable work of it

--- iris/tests/history/open_in_a_private_window_from_contextual_options.py
+++ iris/tests/history/open_in_a_private_window_from_contextual_options.py
@@ -24,27 +24,39 @@ class Test(BaseTest):
     def run(self):
         show_all_history_pattern = History.HistoryMenu.SHOW_ALL_HISTORY
         last_searched_item = Pattern('iris_bookmark_focus.png')
-        iris_logo = Pattern('iris_logo.png')
-        private_window = Pattern('private_window.png')
 
         # Check that the Library window is displayed properly.
-        open_library_menu('History')
-        expected = exists(show_all_history_pattern, 10)
+        library_button_exists = exists(NavBar.LIBRARY_MENU)
+        assert_true(self, library_button_exists, 'Library button exists')
+
+        click(NavBar.LIBRARY_MENU)
+
+        history_button_exists = exists(LibraryMenu.HISTORY_BUTTON, Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_true(self, history_button_exists, 'History button exists')
+
+        click(LibraryMenu.HISTORY_BUTTON)
+
+        expected = exists(show_all_history_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, '\"Show All History\" option exists.')
+
         click(show_all_history_pattern)
 
         # Open the last searched item in a new private window.
-        expected = exists(last_searched_item, 10)
+        expected = exists(last_searched_item, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'Last searched item was found.')
+
         right_click(last_searched_item)
+
         type(text='p')
 
         # Assert the newly opened window.
-        expected = exists(private_window, 10)
+        expected = exists(PrivateWindow.private_window_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The private window was successfully opened.')
-        expected = exists(iris_logo, 10)
+
+        expected = exists(LocalWeb.IRIS_LOGO, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'The page was successfully opened.')
 
         # Close the private window and then the auxiliary window.
         close_window()
+
         click_window_control('close')
