d5e151a5162008576889e50ec22b148c8133a7a5
spronoza <spronoza@mozilla.com>
2019-04-18 18:40:49 +0300

    Pattern used instead of string in open_library_menu

--- iris/tests/history/history_sidebar_delete_site.py
+++ iris/tests/history/history_sidebar_delete_site.py
@@ -31,33 +31,40 @@ class Test(BaseTest):
 
         # Open a page to create some history.
         new_tab()
+
         navigate(LocalWeb.MOZILLA_TEST_SITE)
-        expected_1 = exists(LocalWeb.MOZILLA_LOGO, 10)
+        expected_1 = exists(LocalWeb.MOZILLA_LOGO, Settings.SITE_LOAD_TIMEOUT)
+
         assert_true(self, expected_1, 'Mozilla page loaded successfully.')
         close_tab()
 
         # Open the History sidebar.
         history_sidebar()
-        expected_2 = exists(search_history_box_pattern, 10)
+
+        expected_2 = exists(search_history_box_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected_2, 'Sidebar was opened successfully.')
-        expected_3 = exists(history_today_sidebar_pattern, 10)
+
+        expected_3 = exists(history_today_sidebar_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected_3, 'Expand history button displayed properly.')
+
         click(history_today_sidebar_pattern)
 
         # Delete a page from the History sidebar.
-        expected_4 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL, 10)
+        expected_4 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected_4, 'Mozilla page is displayed in the History list successfully.')
 
         right_click(LocalWeb.MOZILLA_BOOKMARK_SMALL)
+
         type(text='d')
 
         try:
-            expected_5 = wait_vanish(LocalWeb.MOZILLA_BOOKMARK_SMALL, 10)
+            expected_5 = wait_vanish(LocalWeb.MOZILLA_BOOKMARK_SMALL, Settings.FIREFOX_TIMEOUT)
             assert_true(self, expected_5, 'Mozilla page was deleted successfully from the history.')
         except FindError:
             raise FindError('Mozilla page is still displayed in the history.')
 
         # Check that Mozilla page is not displayed in the Recent History list.
-        open_library_menu('History')
+        open_library_menu(LibraryMenu.HISTORY_BUTTON)
+
         expected_6 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL.similar(0.9), 5)
         assert_false(self, expected_6, 'Mozilla page is not displayed in the Recent History list.')
