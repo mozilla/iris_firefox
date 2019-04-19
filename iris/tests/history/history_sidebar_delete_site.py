b5b683b4b195b19089cc333fbe4b51b7b078e05c
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-04-19 18:50:33 +0300

    Issue 2905 (Fix history_sidebar_delete_site test) (#2906)
    * Pattern used instead of string in open_library_menu
    
    * Pattern used instead of string in open_library_menu
    
    * exists variables names changed

--- iris/tests/history/history_sidebar_delete_site.py
+++ iris/tests/history/history_sidebar_delete_site.py
@@ -31,33 +31,41 @@ class Test(BaseTest):
 
         # Open a page to create some history.
         new_tab()
+
         navigate(LocalWeb.MOZILLA_TEST_SITE)
-        expected_1 = exists(LocalWeb.MOZILLA_LOGO, 10)
-        assert_true(self, expected_1, 'Mozilla page loaded successfully.')
+
+        test_site_loaded = exists(LocalWeb.MOZILLA_LOGO, Settings.SITE_LOAD_TIMEOUT)
+        assert_true(self, test_site_loaded, 'Mozilla page loaded successfully.')
+
         close_tab()
 
         # Open the History sidebar.
         history_sidebar()
-        expected_2 = exists(search_history_box_pattern, 10)
-        assert_true(self, expected_2, 'Sidebar was opened successfully.')
-        expected_3 = exists(history_today_sidebar_pattern, 10)
-        assert_true(self, expected_3, 'Expand history button displayed properly.')
+
+        sidebar_opened = exists(search_history_box_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, sidebar_opened, 'Sidebar was opened successfully.')
+
+        expand_history_button_displayed = exists(history_today_sidebar_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, expand_history_button_displayed, 'Expand history button displayed properly.')
+
         click(history_today_sidebar_pattern)
 
         # Delete a page from the History sidebar.
-        expected_4 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL, 10)
-        assert_true(self, expected_4, 'Mozilla page is displayed in the History list successfully.')
+        test_page_added_to_history = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, test_page_added_to_history, 'Mozilla page is displayed in the History list successfully.')
 
         right_click(LocalWeb.MOZILLA_BOOKMARK_SMALL)
+
         type(text='d')
 
         try:
-            expected_5 = wait_vanish(LocalWeb.MOZILLA_BOOKMARK_SMALL, 10)
-            assert_true(self, expected_5, 'Mozilla page was deleted successfully from the history.')
+            test_page_deleted = wait_vanish(LocalWeb.MOZILLA_BOOKMARK_SMALL, Settings.FIREFOX_TIMEOUT)
+            assert_true(self, test_page_deleted, 'Mozilla page was deleted successfully from the history.')
         except FindError:
             raise FindError('Mozilla page is still displayed in the history.')
 
         # Check that Mozilla page is not displayed in the Recent History list.
-        open_library_menu('History')
-        expected_6 = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL.similar(0.9), 5)
-        assert_false(self, expected_6, 'Mozilla page is not displayed in the Recent History list.')
+        open_library_menu(LibraryMenu.HISTORY_BUTTON)
+
+        test_page_not_displayed = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL.similar(0.9), Settings.SHORT_FIREFOX_TIMEOUT)
+        assert_false(self, test_page_not_displayed, 'Mozilla page is not displayed in the Recent History list.')
