e7a4708b532be5ef5224cae710cec9c72a521458
dbakaev <45363191+dbakaev@users.noreply.github.com>
2019-04-22 11:38:17 +0300

    Fixed coverage for toolbar_properties test case (#2922)

--- iris/tests/bookmark/toolbar_properties.py
+++ iris/tests/bookmark/toolbar_properties.py
@@ -21,62 +21,60 @@ class Test(BaseTest):
         return
 
     def run(self):
-        bookmarks_toolbar_menu_option_pattern = Pattern('bookmarks_toolbar_menu_option.png')
-        iris_tab_pattern = Pattern('iris_tab.png')
         getting_started_toolbar_bookmark_pattern = Pattern('toolbar_bookmark_icon.png')
-        bookmark_properties_option = Pattern('properties_option.png')
-        properties_popup_save_button_pattern = Pattern('save_bookmark_name.png')
-        properties_tags_field_pattern = Pattern('tags_field.png')
-        properties_keyword_field_pattern = Pattern('keyword_field.png')
         renamed_toolbar_bookmark_pattern = Pattern('renamed_toolbar_bookmark.png')
-        name_modified_pattern = Pattern('name_modified.png')
+        properties_popup_save_button_pattern = Pattern('save_bookmark_name.png')
+        properties_keyword_field_pattern = Pattern('keyword_field.png')
+        bookmark_properties_option = Pattern('properties_option.png')
         keyword_modified_pattern = Pattern('keyword_modified.png')
+        properties_tags_field_pattern = Pattern('tags_field.png')
+        name_modified_pattern = Pattern('name_modified.png')
         tags_modified_pattern = Pattern('tags_modified.png')
 
-        area_to_click = find(iris_tab_pattern)
-        area_to_click.x += 300
-        area_to_click.y += 5
-        right_click(area_to_click)
+        open_bookmarks_toolbar()
 
-        bookmarks_toolbar_menu_option_available = exists(bookmarks_toolbar_menu_option_pattern, DEFAULT_UI_DELAY)
-        assert_true(self, bookmarks_toolbar_menu_option_available,
-                    '\'Bookmarks Toolbar\' option is available in context menu')
-
-        click(bookmarks_toolbar_menu_option_pattern)
-        bookmarks_folder_available_in_toolbar = exists(getting_started_toolbar_bookmark_pattern, DEFAULT_UI_DELAY)
+        bookmarks_folder_available_in_toolbar = exists(getting_started_toolbar_bookmark_pattern)
         assert_true(self, bookmarks_folder_available_in_toolbar, 'The \'Bookmarks Toolbar\' is enabled.')
 
         right_click(getting_started_toolbar_bookmark_pattern)
-        properties_option_available = exists(bookmark_properties_option, DEFAULT_SYSTEM_DELAY)
+
+        properties_option_available = exists(bookmark_properties_option, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, properties_option_available,
                     '\'Properties\' option in available in context menu after right-click at the bookmark in toolbar.')
 
         click(bookmark_properties_option)
-        properties_opened = exists(properties_popup_save_button_pattern, DEFAULT_SYSTEM_DELAY)
+
+        properties_opened = exists(properties_popup_save_button_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, properties_opened, 'Properties for \'Getting Started\' window is opened.')
 
         paste('New Name')
-        name_modified = exists(name_modified_pattern, DEFAULT_UI_DELAY)
+
+        name_modified = exists(name_modified_pattern)
         assert_true(self, name_modified, 'Another value successfully entered into the \'Name\' field')
 
-        keyword_field_available = exists(properties_keyword_field_pattern, DEFAULT_UI_DELAY)
+        keyword_field_available = exists(properties_keyword_field_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, keyword_field_available, '\'Keyword\' field is available in properties window.')
 
         click(properties_keyword_field_pattern)
+
         paste('test')
-        keyword_edited = exists(keyword_modified_pattern, DEFAULT_UI_DELAY)
+
+        keyword_edited = exists(keyword_modified_pattern)
         assert_true(self, keyword_edited, 'Another value successfully entered into the \'Keyword\' field')
 
-        tags_field_available = exists(properties_tags_field_pattern, DEFAULT_UI_DELAY)
+        tags_field_available = exists(properties_tags_field_pattern)
         assert_true(self, tags_field_available, '\'Tags\' field is available in properties window.')
 
         click(properties_tags_field_pattern)
+
         paste('Tag')
-        tags_edited = exists(tags_modified_pattern, DEFAULT_UI_DELAY)
+
+        tags_edited = exists(tags_modified_pattern)
         assert_true(self, tags_edited, 'Another value successfully entered into the \'Tags\' field')
 
         click(properties_popup_save_button_pattern)
-        bookmark_renamed = exists(renamed_toolbar_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
+
+        bookmark_renamed = exists(renamed_toolbar_bookmark_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_renamed, 'The bookmark information is correctly updated.')
 
         restart_firefox(self,
@@ -84,12 +82,14 @@ class Test(BaseTest):
                         self.profile_path,
                         self.base_local_web_url)
 
-        bookmark_renamed = exists(renamed_toolbar_bookmark_pattern, DEFAULT_SYSTEM_DELAY)
+        bookmark_renamed = exists(renamed_toolbar_bookmark_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_renamed, 'The bookmark name persists modified after restarting Firefox.')
 
         right_click(renamed_toolbar_bookmark_pattern)
+
         click(bookmark_properties_option)
-        properties_opened = exists(properties_popup_save_button_pattern, DEFAULT_SYSTEM_DELAY)
+
+        properties_opened = exists(properties_popup_save_button_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, properties_opened, 'Properties for \'Getting Started\' window is opened.')
 
         assert_true(self, keyword_edited, '\'Keyword\' value persists modified after restarting Firefox.')
