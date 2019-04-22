57d2d1e3c51bd970e034f0c68456629c416c199b
icrisan <icrisan@mozilla.com>
2019-04-22 16:34:16 +0300

    Update test case for MAC OS.

--- iris/tests/awesomebar/cursor_is_not_reset_to_start_of_address_bar.py
+++ iris/tests/awesomebar/cursor_is_not_reset_to_start_of_address_bar.py
@@ -13,27 +13,40 @@ class Test(BaseTest):
         self.meta = 'Cursor gets reset to start of address bar, when I alt-tab away from Firefox and back.'
         self.test_case_id = '224506'
         self.test_suite_id = '1902'
-        # This feature does not work properly on MAC
-        self.exclude = Platform.MAC
         self.locales = ['en-US']
 
     def run(self):
         select_location_bar()
         paste('a.b')
 
-        open_library()
-        expected = exists(Library.TITLE, 10)
-        assert_true(self, expected, 'The library was opened successfully.')
+        # MAC acts different from the other supported OSes. Using the change_window_view() helper while having the
+        # browser and an auxiliary window from the same browser opened does not move focus from the browser to the
+        # auxiliary window and vice-versa. MAC moves focus to a different window(e.g: terminal).
+        if Settings.get_os() == Platform.MAC:
+            # Move focus away from the browser.
+            change_window_view()
+            # Move focus back to the browser.
+            change_window_view()
+        else:
+            # Move focus away from the browser by opening an auxiliary window of the browser.
+            open_library()
+            expected = exists(Library.TITLE, 10)
+            assert_true(self, expected, 'The library was opened successfully.')
 
-        change_window_view()
+            # Move focus back to the browser.
+            change_window_view()
 
-        try:
-            expected = wait_vanish(Library.TITLE, 5)
-            assert_true(self, expected, 'The browser is in focus again.')
-        except FindError:
-            raise FindError('The library is still in focus.')
+            try:
+                expected = wait_vanish(Library.TITLE, 5)
+                assert_true(self, expected, 'The browser is in focus again.')
+            except FindError:
+                raise FindError('The library is still in focus.')
 
         paste('test')
         select_location_bar()
         url_text = copy_to_clipboard()
         assert_equal(self, url_text, 'a.btest', 'The cursor did not change its position while the window was changed.')
+
+        if Settings.get_os() == Platform.WINDOWS or Settings.get_os() == Platform.LINUX:
+            change_window_view()
+            click_window_control('close')
