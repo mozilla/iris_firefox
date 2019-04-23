c9453021f9fcc09b65b3b9172420fa9095566265
rrobotin <rrobotin@mozilla.com>
2019-04-23 15:31:27 +0300

    Fixed issue_2864

--- bootstrap/win_bootstrap.sh
+++ bootstrap/win_bootstrap.sh
@@ -4,6 +4,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+CWD=$(powershell -Command "(Get-Location).Path")
 
 echo -e "\n${RED}##### Starting Windows bootstrap #####${NC}\n"
 
@@ -31,7 +32,7 @@ fi
 
 
 echo -e "\n${GREEN}  --->  Installing Git #####${NC}\n"
-if command -v git &>/dev/null; then
+if [[ $(scoop list | grep git) =~ git ]]; then
     echo -e "${GREEN}  --->  Skipping Git install. Already installed. ${NC}\n"
     powershell -Command "scoop update git"
     powershell -Command "scoop update openssh"
@@ -40,10 +41,29 @@ else
     powershell -Command "scoop install openssh"
 fi
 
+echo -e "\n${GREEN}  --->  Installing Wget #####${NC}\n"
+if [[ $(scoop list | grep wget) =~ wget ]]; then
+    echo -e "${GREEN}  --->  Skipping Wget install. Already installed. ${NC}\n"
+     powershell -Command "scoop update wget"
+else
+    powershell -Command "scoop install wget"
+fi
 
 echo -e "\n${GREEN}  --->  Installing 'which' & 'sudo' #####${NC}\n"
-powershell -Command "scoop install which"
-powershell -Command "scoop install sudo"
+echo -e "\n${GREEN}  --->  Installing which #####${NC}\n"
+if [[ $(scoop list | grep which) =~ which ]]; then
+    echo -e "${GREEN}  --->  Skipping which install. Already installed. ${NC}\n"
+    powershell -Command "scoop update which"
+else
+    powershell -Command "scoop install which"
+fi
+echo -e "\n${GREEN}  --->  Installing sudo #####${NC}\n"
+if [[ $(scoop list | grep sudo) =~ sudo ]]; then
+    echo -e "${GREEN}  --->  Skipping sudo install. Already installed. ${NC}\n"
+    powershell -Command "scoop update sudo"
+else
+    powershell -Command "scoop install sudo"
+fi
 
 
 echo -e "\n${GREEN}--->  Installing/updating 7zip #####${NC}\n"
@@ -55,52 +75,52 @@ else
 fi
 
 
-echo -e "\n${GREEN} --->  Installing Tesseract 4 ${NC} \n"
+echo -e "\n${GREEN} --->  Installing Tesseract 3 ${NC} \n"
 if command -v tesseract &>/dev/null; then
     echo -e "${GREEN}  --->  Tesseract already installed. ${NC}\n"
-    echo -e "${GREEN}    --->  Checking Tesseract version. ${NC}\n"
-    if [[ $(tesseract -v | grep "tesseract 3.05") ]]; then
-        echo -e "${RED}  --->  You have Tesseract 3, removing and installing Tesseract 4.${NC}\n"
-        powershell -Command "scoop uninstall tesseract3"
-        powershell -Command "scoop uninstall tesseract"
-        powershell -Command "scoop install tesseract"
-        powershell -Command "scoop install tesseract-languages"
-        powershell -Command "scoop reset tesseract-languages"
-    else
-        echo -e "${GREEN}    --->  Tesseract is the correct version. ${NC}\n"
-        powershell -Command "scoop update tesseract"
-        powershell -Command "scoop update tesseract-languages"
-    fi
+    # No need to update to latest Tesseract, due to https://github.com/mozilla/iris/issues/2864
 else
-    powershell -Command "scoop install tesseract"
-    powershell -Command "scoop install tesseract-languages"
-    powershell -Command "scoop reset tesseract-languages"
+    powershell -Command "scoop install tesseract3"
 fi
 
-
-echo -e "\n${GREEN}  --->  Installing Python 2.7 #####${NC}\n"
-if command -v python2 &>/dev/null; then
-    echo -e "${GREEN}  --->  Skipping Python 2.7 install. Already installed. ${NC}\n"
-    powershell -Command "scoop update python27"
+echo -e "\n${GREEN}  --->  Installing Python 2.7.15 #####${NC}\n"
+if [[ $(scoop list |grep 'python2715') =~ 2715 ]]; then
+    echo -e "${GREEN}  --->  Skipping Python 2.7.15 install. Already installed. ${NC}\n"
 else
-    powershell -Command "scoop install python27" | grep 'bucket already exists.' &> /dev/null
+    echo -e "\n${GREEN}  --->  Install Python 2.7.15 #####${NC}\n"
+    powershell -Command "scoop install "${CWD}"\bootstrap\python2715.json"
     if [ $? != 0 ]; then
-       echo -e "\n${RED} --->  Python 2.7 now installed. You need to restart the terminal 2nd time and run the bootstrap.sh again to complete the install.${NC}\n"
+       echo -e "\n${RED} --->  Python 2.7.15 now installed. You need to restart the terminal 2nd time and run the bootstrap.sh again to complete the install.${NC}\n"
        sleep 20
        exit
     fi
 fi
 
+echo -e "\n${GREEN}  --->  installing/upgrading pip ${NC}\n"
+if command -v pip &>/dev/null; then
+    powershell -Command "python -m pip install --upgrade pip"
+else
+    powershell -Command "curl https://bootstrap.pypa.io/get-pip.py -o bootstrap\get-pip.py"
+    if [ $? != 0 ]; then
+        echo -e "\n${RED} --->  Pip package management installed. You need to restart the terminal and run the bootstrap.sh again.${NC}\n"
+        sleep 20
+        exit
+    fi
+    echo -e "\n${GREEN}  --->  installing pip ${NC}\n"
+    powershell -Command "python "${CWD}"\bootstrap\get-pip.py"
+    powershell -Command "python -m pip install --upgrade pip"
+fi
 
 echo -e "\n${GREEN}  --->  installing/upgrading pipenv ${NC}\n"
 if command -v pipenv &>/dev/null; then
     powershell -Command "python -m pip install --upgrade pip"
     powershell -Command "python -m pip install --upgrade pipenv"
 else
-    powershell -Command "pip install pipenv"
+    powershell -Command "python -m pip install pipenv"
 fi
 
 # Create download files
+echo -e "\n${GREEN}  --->  Create download files ${NC}\n"
 mkdir iris/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
 cd iris/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
 
