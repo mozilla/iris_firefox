624901cb60908adb4beb45120115d7ed68f91fbd
pavelciapa <pciapa@mozilla.com>
2019-04-23 15:12:01 +0300

    Rename variable for region

--- iris/tests/awesomebar/one_off_searches_in_private_window.py
+++ iris/tests/awesomebar/one_off_searches_in_private_window.py
@@ -20,12 +20,10 @@ class Test(BaseTest):
         search_settings_pattern = Pattern('search_settings.png')
         twitter_one_off_button_highlight_pattern = Pattern('twitter_one_off_button_highlight.png')
         new_tab_twitter_search_results_pattern = Pattern('new_tab_twitter_search_results.png')
-        new_tab_twitter_search_results_pattern2 = Pattern('new_tab_twitter_search_results_2.png')
+        new_tab_twitter_search_results_2_pattern = Pattern('new_tab_twitter_search_results_2.png')
         google_on_off_button_private_window_pattern = Pattern('google_on_off_button_private_window.png')
         magnifying_glass_pattern = Pattern('magnifying_glass.png')
         test_pattern = Pattern('test.png')
-
-        url = LocalWeb.FIREFOX_TEST_SITE
 
         new_private_window()
 
@@ -33,9 +31,9 @@ class Test(BaseTest):
         navigate('https://www.google.com/preferences?hl=en-US&fg=1#languages')
         click(Utils.SAVE_BUTTON_GOOGLE)
 
-        navigate(url)
+        navigate(LocalWeb.FIREFOX_TEST_SITE)
 
-        region = Region(0, 0, SCREEN_WIDTH, 2 * SCREEN_HEIGHT / 3)
+        top_two_thirds_of_screen = Region(0, 0, SCREEN_WIDTH, 2 * SCREEN_HEIGHT / 3)
 
         firefox_site_loaded = exists(LocalWeb.FIREFOX_LOGO, Settings.FIREFOX_TIMEOUT)
         assert_true(self, firefox_site_loaded, 'Page successfully loaded, firefox logo found.')
@@ -43,23 +41,23 @@ class Test(BaseTest):
         select_location_bar()
         paste('moz')
 
-        mozilla_search_one_offs_available = region.exists(moz_pattern, Settings.FIREFOX_TIMEOUT)
+        mozilla_search_one_offs_available = top_two_thirds_of_screen.exists(moz_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, mozilla_search_one_offs_available, 'Searched string found at the bottom of the drop-down list.')
 
-        search_settings_button = region.exists(search_settings_pattern, Settings.FIREFOX_TIMEOUT)
+        search_settings_button = top_two_thirds_of_screen.exists(search_settings_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, search_settings_button, 'The \'Search settings\' button is displayed in the awesome bar.')
 
         repeat_key_up(3)
-        key_to_one_off_search(twitter_one_off_button_highlight_pattern, )
+        key_to_one_off_search(twitter_one_off_button_highlight_pattern)
 
-        twitter_one_off_highlight = region.exists(twitter_one_off_button_highlight_pattern, Settings.FIREFOX_TIMEOUT)
+        twitter_one_off_highlight = top_two_thirds_of_screen.exists(twitter_one_off_button_highlight_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, twitter_one_off_highlight, 'The \'Twitter\' one-off button is highlighted.')
 
         type(Key.ENTER)
         time.sleep(Settings.TINY_FIREFOX_TIMEOUT)
 
         new_tab_twitter_search_results = exists(new_tab_twitter_search_results_pattern, Settings.FIREFOX_TIMEOUT) \
-            or exists(new_tab_twitter_search_results_pattern2, 5)
+            or exists(new_tab_twitter_search_results_2_pattern, 5)
         assert_true(self, new_tab_twitter_search_results, 'Twitter search results are opened in the same tab.')
 
         new_tab()
@@ -68,7 +66,7 @@ class Test(BaseTest):
         select_location_bar()
         paste('test')
 
-        google_on_off_button_private_window = region.exists(google_on_off_button_private_window_pattern, Settings.FIREFOX_TIMEOUT)
+        google_on_off_button_private_window = top_two_thirds_of_screen.exists(google_on_off_button_private_window_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, google_on_off_button_private_window, 'The\'Google\' one-off button found.')
 
         if Settings.get_os() == Platform.MAC:
@@ -85,10 +83,10 @@ class Test(BaseTest):
 
         next_tab()
 
-        magnifying_glass = region.exists(magnifying_glass_pattern, Settings.FIREFOX_TIMEOUT)
+        magnifying_glass = top_two_thirds_of_screen.exists(magnifying_glass_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, magnifying_glass, 'Page successfully loaded using the \'Google\' engine.')
 
-        test_item = region.exists(test_pattern, Settings.FIREFOX_TIMEOUT)
+        test_item = top_two_thirds_of_screen.exists(test_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, test_item,
                     'Searched item is successfully found in the page opened by the \'Google\' search engine.')
 
