170bfc57c14e2ce2f5836981221a40594cf01fe4
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-04-23 15:42:21 +0300

    Issue 2814 (Fix copy_history_menu_into_bookmark_toolbar test) (#2941)
    * access_bookmarking_tools() replaced with open_bookmarks_toolbar()
    Variables for exists renamed. Timeouts replaced. String replaced with pattern in open_library_menu()
    
    * Timeout for the last assert replaced

--- iris/tests/history/copy_history_menu_into_bookmark_toolbar.py
+++ iris/tests/history/copy_history_menu_into_bookmark_toolbar.py
@@ -16,17 +16,12 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def setup(self):
-        """Test case setup
-
-        This overrides the setup method in the BaseTest class, so that it can use a brand new profile.
-        """
         BaseTest.setup(self)
         self.profile = Profile.TEN_BOOKMARKS
         return
 
     def run(self):
         show_all_history_pattern = History.HistoryMenu.SHOW_ALL_HISTORY
-        view_bookmarks_toolbar = LibraryMenu.BookmarksOption.BookmarkingTools.VIEW_BOOKMARKS_TOOLBAR
         bookmarks_toolbar_most_visited_pattern = SidebarBookmarks.BookmarksToolbar.MOST_VISITED
         library_pattern = Library.TITLE
         history_pattern = Library.HISTORY
@@ -35,37 +30,45 @@ class Test(BaseTest):
         history_bookmarks_toolbar_pattern = Pattern('history_bookmarks_toolbar.png')
 
         # Open the Bookmarks toolbar.
-        access_bookmarking_tools(view_bookmarks_toolbar)
-        expected = exists(bookmarks_toolbar_most_visited_pattern, 10)
-        assert_true(self, expected, 'Bookmarks Toolbar has been activated.')
+        open_bookmarks_toolbar()
 
         # Check that the Library window is displayed properly.
-        open_library_menu('History')
-        expected = exists(show_all_history_pattern, 10)
-        assert_true(self, expected, '\"Show All History\" option exists.')
+        open_library_menu(LibraryMenu.HISTORY_BUTTON)
+
+        show_all_history_exists = exists(show_all_history_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, show_all_history_exists, '\"Show All History\" option exists.')
+
         click(show_all_history_pattern)
-        expected = exists(library_pattern, 10)
-        assert_true(self, expected, '\"Library\" window was displayed properly.')
+
+        library_displayed = exists(library_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, library_displayed, '\"Library\" window was displayed properly.')
 
         # Copy the history.
-        expected = exists(history_pattern, 10)
-        assert_true(self, expected, 'History item was found.')
+        history_item_exists = exists(history_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, history_item_exists, 'History item was found.')
+
         right_click(history_pattern)
-        expected = exists(copy_pattern, 10)
-        assert_true(self, expected, 'Copy option was found.')
+
+        copy_option_exists = exists(copy_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, copy_option_exists, 'Copy option was found.')
+
         click(copy_pattern)
 
         click_window_control('close')
-        time.sleep(DEFAULT_UI_DELAY)
 
         # Paste the history.
+        bookmarks_toolbar_most_visited_exists = exists(bookmarks_toolbar_most_visited_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, bookmarks_toolbar_most_visited_exists, 'Bookmarks Toolbar > Most Visited exists')
+
         right_click(bookmarks_toolbar_most_visited_pattern)
-        expected = exists(paste_pattern, 10)
-        assert_true(self, expected, 'Paste option was found.')
+
+        paste_option_exists = exists(paste_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, paste_option_exists, 'Paste option was found.')
+
         click(paste_pattern)
 
         # Check that the history was copied.
-        time.sleep(DEFAULT_UI_DELAY)
         region = Region(0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        expected = region.exists(history_bookmarks_toolbar_pattern, 10)
-        assert_true(self, expected, 'History was successfully copied.')
+
+        history_copied = region.exists(history_bookmarks_toolbar_pattern, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, history_copied, 'History was successfully copied.')
