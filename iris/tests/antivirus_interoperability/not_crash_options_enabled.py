bad1cffcc60fb082ac1ac1fd436063ae81e10e12
Nikita Evdokimov <46671489+nevdokimov@users.noreply.github.com>
2019-04-24 13:33:59 +0300

    Issue 2588 (#2951)
    * Fix logic and live resource for issue_2588
    
    * Changed timeouts and added win7 images for issue_2588
    
    * Added osx images for issue_2588
    
    * Added some changes to script and linux images for issue_2588

--- iris/tests/antivirus_interoperability/not_crash_options_enabled.py
+++ iris/tests/antivirus_interoperability/not_crash_options_enabled.py
@@ -20,13 +20,17 @@ class Test(BaseTest):
         self.set_profile_pref({'media.autoplay.default': 0})
 
     def run(self):
-        soundcloud_logo_pattern = Pattern('soundcloud_logo.png')
+        bandcamp_logo_pattern = Pattern('bandcamp_logo.png')
+        play_button_pattern = Pattern('play_button.png')
         sound_on_pattern = Pattern('sound_on.png').similar(0.9)
         youtube_autoplay_switch_pattern = Pattern('youtube_autoplay_switch.png')
         google_images_page_mark_pattern = Pattern('google_images_page_mark.png')
         show_more_results_button_pattern = Pattern('show_more_results_button.png')
 
-        mouse_wheel_steps = 100
+        if Settings.is_linux():
+            mouse_wheel_steps = 10
+        if Settings.is_mac():
+            mouse_wheel_steps = 50
         if Settings.is_windows():
             mouse_wheel_steps = SCREEN_HEIGHT
 
@@ -38,15 +42,21 @@ class Test(BaseTest):
         sound_video_played = exists(sound_on_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, sound_video_played, 'The video is properly loaded and displayed.')
 
-        navigate('https://soundcloud.com/')
+        navigate('https://bandcamp.com/')
 
-        soundcloud_logo_exists = exists(soundcloud_logo_pattern, Settings.HEAVY_SITE_LOAD_TIMEOUT)
-        assert_true(self, soundcloud_logo_exists, 'Soundcloud is properly loaded')
+        bandcamp_logo_exists = exists(bandcamp_logo_pattern, Settings.HEAVY_SITE_LOAD_TIMEOUT)
+        assert_true(self, bandcamp_logo_exists, 'Bandcamp is properly loaded')
+
+        center_location = Location(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        mouse_move(center_location)
+
+        scroll_until_pattern_found(play_button_pattern, scroll, (-mouse_wheel_steps,), 100,
+                                   Settings.TINY_FIREFOX_TIMEOUT/2)
 
         sound_on_not_exists = exists(sound_on_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_false(self, sound_on_not_exists, 'The sound icon doesn\'t exists.')
 
-        type(Key.SPACE)
+        click(play_button_pattern)
 
         sound_on_exists = exists(sound_on_pattern, Settings.SHORT_FIREFOX_TIMEOUT)
         assert_true(self, sound_on_exists, 'The sound is properly loaded and played.')
@@ -63,5 +73,5 @@ class Test(BaseTest):
 
         show_more_results_button_exists = scroll_until_pattern_found(show_more_results_button_pattern,
                                                                      scroll, (-mouse_wheel_steps,), 100,
-                                                                     Settings.TINY_FIREFOX_TIMEOUT)
+                                                                     Settings.TINY_FIREFOX_TIMEOUT/2)
         assert_true(self, show_more_results_button_exists, 'The images are properly displayed.')
