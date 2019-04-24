96f9186a9727291531f2ae7fffa93be0957343f8
pavelciapa <pciapa@mozilla.com>
2019-04-24 13:11:21 +0300

    Added blocked_by

--- iris/tests/bookmark/edit_bookmark_from_bookmarks_menu.py
+++ iris/tests/bookmark/edit_bookmark_from_bookmarks_menu.py
@@ -14,6 +14,7 @@ class Test(BaseTest):
         self.test_case_id = '163195'
         self.test_suite_id = '2525'
         self.locales = ['en-US']
+        self.blocked_by = {'id': '1527258', 'platform': Platform.WINDOWS}
 
     def setup(self):
         BaseTest.setup(self)
