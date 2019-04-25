7f257fb2e89d14a714aafbf88d8876e71aa97f22
icrisan <icrisan@mozilla.com>
2019-04-25 18:10:52 +0300

    Click find_in_options pattern to avoid intermittent failures.

--- iris/tests/download_manager/__init__.py
+++ iris/tests/download_manager/__init__.py
@@ -1,2 +1,2 @@
-# TO DO: Remove Linux workaround on 'change_download_folder.py' and 'remove_the_downloads_folder' when '1513494'
-# is fixed.
+# TO DO: Remove Linux workaround on 'change_download_folder.py' and 'remove_the_downloads_folder.py' when issue
+# '1513494' is fixed.
