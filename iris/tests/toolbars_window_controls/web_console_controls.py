f7c18091a368f0963b3d71ddb77e47c7e7f869a8
yeyartsev <eyartsev@mozilla.com>
2019-04-30 13:16:08 +0300

    web_console_controls is rewriten for win10

--- iris/tests/toolbars_window_controls/web_console_controls.py
+++ iris/tests/toolbars_window_controls/web_console_controls.py
@@ -17,48 +17,27 @@ class Test(BaseTest):
 
     def run(self):
 
-        url = 'about:home'
         close_dev_tools_button_pattern = Pattern('close_dev_tools.png')
         dev_tools_window_pattern = Pattern('dev_tools_window.png')
-        close_message_pattern = Pattern('close_message.png').similar(0.5)
+        close_message_pattern = Pattern('close_message.png')
         responsive_design_button_pattern = Pattern('responsive_design.png')
         customize_dev_tools_pattern = Pattern('customize_developer_tools.png')
-        customize_dev_tools_message_pattern = Pattern('customize_dev_tools_message.png').similar(0.5)
-        responsive_design_message_pattern = Pattern('responsive_design_message.png').similar(0.5)
+        customize_dev_tools_message_pattern = Pattern('customize_dev_tools_message.png')
+        responsive_design_message_pattern = Pattern('responsive_design_message.png')
         responsive_design_active_pattern = Pattern('responsive_design_active.png')
         dock_to_left_pattern = Pattern('dock_to_left.png')
         dock_to_right_pattern = Pattern('dock_to_right.png')
         separate_window_pattern = Pattern('separate_window_option.png')
-        dock_to_bottom_pattern = Pattern('dock_to_bottom_option.png').similar(0.5)
+        dock_to_bottom_pattern = Pattern('dock_to_bottom_option.png')
 
-        navigate(url)
         open_web_console()
-
-        buttons = [responsive_design_button_pattern, customize_dev_tools_pattern, close_dev_tools_button_pattern]
-        button_messages = [responsive_design_message_pattern,
-                           customize_dev_tools_message_pattern,
-                           close_message_pattern]
 
         right_upper_corner = Region(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         left_upper_corner = Region(0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         button_region = Region(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-        # for button in buttons:
-        #     expected_buttons = button_region.exists(button, 5)
-        #     assert_true(self, expected_buttons, 'option %s has been found.' % button)
-        #
-        # # Check if the labels are displayed when the cursor hovers over the options.
-        #
-        # for m_index, button in enumerate(buttons):
-        #     button_region.hover(button)
-        #     expected_messages = button_region.exists(button_messages[m_index], 10)
-        #     assert_true(self, expected_messages, 'Message %s found.' % button_messages[m_index])
-
-        # Checking the button functionality.
-
-        # button_region.click(responsive_design_button_pattern)
         responsive_design_button_displayed = exists(responsive_design_button_pattern)
-        assert_true(self,responsive_design_button_displayed, '"Responsive design" button displayed')
+        assert_true(self, responsive_design_button_displayed, '"Responsive design" button displayed')
 
         hover(responsive_design_button_pattern)
 
@@ -70,73 +49,66 @@ class Test(BaseTest):
 
         hover(customize_dev_tools_pattern)
 
-        customize_dev_tools_message_displayed = exists(customize_dev_tools_message_pattern,
-                                                       Settings.SHORT_FIREFOX_TIMEOUT)
+        customize_dev_tools_message_displayed = exists(customize_dev_tools_message_pattern)
         assert_true(self, customize_dev_tools_message_displayed, '"Customize dev tools" message is displayed')
 
-        close_dev_tools_button_displayed = exists(close_dev_tools_button_pattern)
+        close_dev_tools_button_displayed = exists(close_dev_tools_button_pattern, in_region=button_region)
         assert_true(self, close_dev_tools_button_displayed, '"Close dev tools" button displayed')
 
-        hover(close_dev_tools_button_pattern)
+        hover(close_dev_tools_button_pattern, in_region=button_region)
 
         close_message_displayed = exists(close_message_pattern)
         assert_true(self, close_message_displayed, '"Close dev tools" message is displayed')
 
-        responsive_design_assert = exists(responsive_design_active_pattern, 10)
+        click(responsive_design_button_pattern)
+
+        responsive_design_assert = exists(responsive_design_active_pattern)
         assert_true(self, responsive_design_assert, 'Responsive Design Mode is enabled.')
 
-        button_region.click(customize_dev_tools_pattern)
+        click(customize_dev_tools_pattern, in_region=button_region)
 
-        try:
-            wait(dock_to_left_pattern, 10)
-            logger.debug('Dock to left option found.')
-            click(dock_to_left_pattern)
-        except FindError:
-            raise FindError('Dock to left option not found, aborting.')
+        dock_to_left_button_exists = exists(dock_to_left_pattern)
+        assert_true(self, dock_to_left_button_exists, '"Dock to Left" button is displayed')
 
-        dock_to_left_assert = left_upper_corner.exists(customize_dev_tools_pattern, 10)
-        assert_true(self, dock_to_left_assert, 'Dock to left option works as expected.')
+        click(dock_to_left_pattern)
 
-        left_upper_corner.click(customize_dev_tools_pattern)
+        dock_to_left_assert = exists(customize_dev_tools_pattern, in_region=left_upper_corner)
+        assert_true(self, dock_to_left_assert, '"Dock to Left" option works as expected.')
 
-        try:
-            wait(dock_to_right_pattern, 10)
-            logger.debug('Dock to right option found.')
-            click(dock_to_right_pattern)
-        except FindError:
-            raise FindError('Dock to right option not found, aborting.')
+        click(customize_dev_tools_pattern, in_region=left_upper_corner)
 
-        dock_to_right_assert = right_upper_corner.exists(customize_dev_tools_pattern, 10)
-        assert_true(self, dock_to_right_assert, 'Dock to right option works as expected.')
+        dock_to_right_button_displayed = exists(dock_to_right_pattern)
+        assert_true(self, dock_to_right_button_displayed, '"Dock to Right" button is displayed')
 
-        right_upper_corner.click(customize_dev_tools_pattern)
+        click(dock_to_right_pattern)
 
-        try:
-            wait(separate_window_pattern, 10)
-            logger.debug('Separate Window option found.')
-            click(separate_window_pattern)
-        except FindError:
-            raise FindError('Separate Window option not found, aborting.')
+        docked_to_right = exists(customize_dev_tools_pattern, in_region=right_upper_corner)
+        assert_true(self, docked_to_right, 'Panel wad docked to right')
 
-        separate_window_assert = exists(dev_tools_window_pattern, 10)
+        click(customize_dev_tools_pattern, in_region=right_upper_corner)
+
+        separate_window_button_displayed = exists(separate_window_pattern)
+        assert_true(self, separate_window_button_displayed, '"Separate window" button is displayed')
+
+        click(separate_window_pattern)
+
+        separate_window_assert = exists(dev_tools_window_pattern)
         assert_true(self, separate_window_assert, 'Separate Window option works as expected.')
 
         click(customize_dev_tools_pattern)
 
-        try:
-            wait(dock_to_bottom_pattern, 10)
-            logger.debug('Dock To Bottom option found.')
-            click(dock_to_bottom_pattern)
-        except FindError:
-            raise FindError('Dock To Bottom option not found, aborting.')
+        dock_to_bottom_button_displayed = exists(dock_to_bottom_pattern)
+        assert_true(self, dock_to_bottom_button_displayed, '"Dock to Bottom" button is displayed')
 
-        dock_to_bottom_assert = exists(close_dev_tools_button_pattern, 10)
+        click(dock_to_bottom_pattern)
+
+        dock_to_bottom_assert = exists(close_dev_tools_button_pattern)
         assert_true(self, dock_to_bottom_assert, 'Dock To Bottom option works as expected.')
 
-        button_region.click(close_dev_tools_button_pattern)
+        click(close_dev_tools_button_pattern, in_region=button_region)
 
         try:
-            close_button_assert = button_region.wait_vanish(customize_dev_tools_pattern, 10)
+            close_button_assert = button_region.wait_vanish(customize_dev_tools_pattern)
             assert_true(self, close_button_assert, 'Close button works.')
         except FindError:
             raise FindError('The Web Console can not be closed, aborting test.')
