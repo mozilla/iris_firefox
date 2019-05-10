379133100b60955e0b7c1d481603b39dfbc01d66
pavelciapa <pciapa@mozilla.com>
2019-05-10 17:01:07 +0300

    Added restart via quit firefox

--- iris/tests/session_restore/pinned_tabs_not_restored_after_restart.py
+++ iris/tests/session_restore/pinned_tabs_not_restored_after_restart.py
@@ -21,17 +21,8 @@ class Test(BaseTest):
         firefox_tab_pattern = Pattern('firefox_tab.png')
         firefox_pinned_tab_pattern = Pattern('firefox_pinned_tab.png')
         pin_tab_item_pattern = Pattern('pin_tab_item.png')
-        hamburger_menu_button_pattern = NavBar.HAMBURGER_MENU.similar(0.95)
-        iris_tab_logo_pattern = Pattern('iris_tab.png')
-        if not Settings.is_mac():
-            hamburger_menu_quit_item_pattern = Pattern('hamburger_menu_exit.png')
 
         click_duration = 2
-
-        iris_tab_logo = exists(iris_tab_logo_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
-        assert_true(self, iris_tab_logo, 'Iris tab available')
-        iris_tab_logo_location = find(iris_tab_logo_pattern)
-        proper_hamburger_menu_region = Region(0, iris_tab_logo_location.y, width=SCREEN_WIDTH, height=SCREEN_HEIGHT/4)
 
         navigate(LocalWeb.FOCUS_TEST_SITE)
 
@@ -71,28 +62,23 @@ class Test(BaseTest):
         firefox_test_tab_pinned = exists(firefox_pinned_tab_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, firefox_test_tab_pinned, 'Firefox tab successfully pinned.')
 
-        # Quit via Hamburger menu
-        hamburger_menu_button_exists = exists(hamburger_menu_button_pattern, DEFAULT_FIREFOX_TIMEOUT,
-                                              in_region=proper_hamburger_menu_region)
-        assert_true(self, hamburger_menu_button_exists, 'Hamburger menu appears on screen.')
-
-        if not Settings.is_mac():
-            click(hamburger_menu_button_pattern, DEFAULT_UI_DELAY, in_region=proper_hamburger_menu_region)
-            hamburger_menu_quit_displayed = exists(hamburger_menu_quit_item_pattern, DEFAULT_FIREFOX_TIMEOUT)
-            assert_true(self, hamburger_menu_quit_displayed, 'Close Firefox from the "Hamburger" menu.')
-            click(hamburger_menu_quit_item_pattern, DEFAULT_UI_DELAY)
+        if Settings.is_mac():
+            quit_firefox()
+        elif Settings.is_linux():
+            click_hamburger_menu_option('Quit')
         else:
-            type('q', KeyModifier.CMD)
+            click_hamburger_menu_option('Exit')
 
-        #  firefox_runner = None to prevent automatic restore of previous session
         status = self.firefox_runner.process_handler.wait(Settings.FIREFOX_TIMEOUT)
         if status is None:
             self.firefox_runner.stop()
             self.firefox_runner = None
 
-        # keyboard shortcut of method restart_firefox() can break test
         self.firefox_runner = launch_firefox(self.browser.path, self.profile_path, self.base_local_web_url)
         self.firefox_runner.start()
+
+        firefox_restarted = exists(LocalWeb.IRIS_LOGO, Settings.SITE_LOAD_TIMEOUT)
+        assert_true(self, firefox_restarted, 'Firefox restarted successfully')
 
         close_tab()
 
