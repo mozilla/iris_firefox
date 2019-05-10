412d36658bb44aeb56375543e88b2834fd13593a
pavelciapa <pciapa@mozilla.com>
2019-05-10 18:38:39 +0300

    Added condition for about:blank page

--- iris/tests/session_restore/about_blank_about_newtab_not_restored.py
+++ iris/tests/session_restore/about_blank_about_newtab_not_restored.py
@@ -17,6 +17,8 @@ class Test(BaseTest):
 
     def run(self):
         blank_white_squre_400_200_pattern = Pattern('blank_white_squre_400_200.png')
+        middle_vertical_region = Screen.MIDDLE_THIRD_VERTICAL
+        center_region = middle_vertical_region.middle_third_horizontal()
 
         navigate('about:newtab')
         top_sites_available = exists(Utils.TOP_SITES, Settings.FIREFOX_TIMEOUT)
@@ -26,8 +28,8 @@ class Test(BaseTest):
         navigate('about:blank')
         time.sleep(Settings.TINY_FIREFOX_TIMEOUT)  # Pattern of new tab will be found before loading about:blank
 
-        top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT)
-        assert_false(self, top_sites_available, 'about:blank website loaded successfully')
+        about_blank_tab = exists(blank_white_squre_400_200_pattern, Settings.TINY_FIREFOX_TIMEOUT, center_region)
+        assert_true(self, about_blank_tab, 'about:blank website loaded successfully')
 
         restart_firefox(self, self.browser.path, self.profile_path, self.base_local_web_url,
                         image=LocalWeb.IRIS_LOGO_ACTIVE_TAB)
@@ -36,19 +38,10 @@ class Test(BaseTest):
 
         close_tab()
 
-        middle_vertical_region = Screen.MIDDLE_THIRD_VERTICAL
-        center_region = middle_vertical_region.middle_third_horizontal()
-
         top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT, Screen.LEFT_HALF)
-        assert_true(self, top_sites_available, 'about:newtab website loaded successfully')
-
-        new_tab_not_highlighted = exists(Tabs.NEW_TAB_NOT_HIGHLIGHTED)
-        assert_true(self, new_tab_not_highlighted, 'Another not highlighted new tab is available')
+        assert_true(self, top_sites_available, 'about:newtab website restored successfully')
 
         next_tab()
 
-        new_tab_highlighted = exists(Tabs.NEW_TAB_HIGHLIGHTED)
-        assert_true(self, new_tab_highlighted, 'Highlighted new tab is available')
-
         about_blank_tab = exists(blank_white_squre_400_200_pattern, Settings.TINY_FIREFOX_TIMEOUT, center_region)
-        assert_false(self, about_blank_tab, 'about:blank website loaded successfully')
+        assert_true(self, about_blank_tab, 'about:blank website restored successfully')
