8dfd84c9d29c56ab189a924ae96eaec8575a58b8
pavelciapa <pciapa@mozilla.com>
2019-05-10 18:30:54 +0300

    Added images and logic for blank page

--- iris/tests/session_restore/about_blank_about_newtab_not_restored.py
+++ iris/tests/session_restore/about_blank_about_newtab_not_restored.py
@@ -16,9 +16,9 @@ class Test(BaseTest):
         self.locales = ['en-US']
 
     def run(self):
-        new_tab()
-        navigate('about:newtab')
+        blank_white_squre_400_200_pattern = Pattern('blank_white_squre_400_200.png')
 
+        navigate('about:newtab')
         top_sites_available = exists(Utils.TOP_SITES, Settings.FIREFOX_TIMEOUT)
         assert_true(self, top_sites_available, 'about:newtab website loaded successfully')
 
@@ -29,28 +29,26 @@ class Test(BaseTest):
         top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT)
         assert_false(self, top_sites_available, 'about:blank website loaded successfully')
 
-        restart_firefox(self, self.browser.path, self.profile_path, self.base_local_web_url)
+        restart_firefox(self, self.browser.path, self.profile_path, self.base_local_web_url,
+                        image=LocalWeb.IRIS_LOGO_ACTIVE_TAB)
 
-        new_tab_not_highlighted = exists(Tabs.NEW_TAB_NOT_HIGHLIGHTED)
-        assert_true(self, new_tab_not_highlighted, 'Not highlighted new tab is available')
+        click_hamburger_menu_option('Restore Previous Session')
 
-        click(Tabs.NEW_TAB_NOT_HIGHLIGHTED, 1)
+        close_tab()
 
-        top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT)
+        middle_vertical_region = Screen.MIDDLE_THIRD_VERTICAL
+        center_region = middle_vertical_region.middle_third_horizontal()
 
-        if top_sites_available:
-            assert_true(self, top_sites_available, 'about:newtab website loaded successfully')
-        else:
-            assert_false(self, top_sites_available, 'about:blank website loaded successfully')
+        top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT, Screen.LEFT_HALF)
+        assert_true(self, top_sites_available, 'about:newtab website loaded successfully')
 
         new_tab_not_highlighted = exists(Tabs.NEW_TAB_NOT_HIGHLIGHTED)
         assert_true(self, new_tab_not_highlighted, 'Another not highlighted new tab is available')
 
-        click(Tabs.NEW_TAB_NOT_HIGHLIGHTED, 1)
+        next_tab()
 
-        top_sites_available = exists(Utils.TOP_SITES, Settings.TINY_FIREFOX_TIMEOUT)
+        new_tab_highlighted = exists(Tabs.NEW_TAB_HIGHLIGHTED)
+        assert_true(self, new_tab_highlighted, 'Highlighted new tab is available')
 
-        if top_sites_available:
-            assert_true(self, top_sites_available, 'about:newtab website loaded successfully')
-        else:
-            assert_false(self, top_sites_available, 'about:blank website loaded successfully')
+        about_blank_tab = exists(blank_white_squre_400_200_pattern, Settings.TINY_FIREFOX_TIMEOUT, center_region)
+        assert_false(self, about_blank_tab, 'about:blank website loaded successfully')
