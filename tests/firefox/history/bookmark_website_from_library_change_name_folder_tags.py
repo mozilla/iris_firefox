40676893a0249cee6a335015aa39abef921451c9
Ionut Budeanu <ibudeanu@mozilla.com>
2019-05-13 12:11:40 +0300

    Added platfrom check for pattern creation

--- tests/firefox/history/bookmark_website_from_library_change_name_folder_tags.py
+++ tests/firefox/history/bookmark_website_from_library_change_name_folder_tags.py
@@ -21,7 +21,8 @@ class Test(FirefoxTest):
         library_bookmarks_custom_iris_pattern = Pattern('library_bookmarks_custom_iris.png')
         save_bookmark_button_pattern = Pattern('save_bookmark_name.png')
         library_other_bookmarks_pattern = Library.OTHER_BOOKMARKS
-        new_bookmark_folder_bookmarks_menu_pattern = Pattern('new_bookmark_folder_bookmarks_menu.png')
+        if OSHelper.is_mac():
+            new_bookmark_folder_bookmarks_menu_pattern = Pattern('new_bookmark_folder_bookmarks_menu.png')
 
         # Open History and check if it is populated with the Iris page.
         open_library_menu('History')
