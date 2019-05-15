dc565e25e3c43b564b3545b539913c81a86802d8
pavelciapa <pciapa@mozilla.com>
2019-05-15 15:42:20 +0300

    Changed timeout variables to constants

--- iris/tests/session_restore/restored_for_dirty_profile.py
+++ iris/tests/session_restore/restored_for_dirty_profile.py
@@ -24,7 +24,6 @@ class Test(BaseTest):
     def run(self):
         firefox_tab_pattern = Pattern('firefox_tab.png')
         pin_tab_pattern = Pattern('pin_tab_item.png')
-        firefox_pinned_tab_pattern = Pattern('firefox_pinned_tab.png')
         default_zoom_level_toolbar_customize_page_pattern = NavBar.DEFAULT_ZOOM_LEVEL_TOOLBAR_CUSTOMIZE_PAGE
         zoom_controls_customize_page_pattern = NavBar.ZOOM_CONTROLS_CUSTOMIZE_PAGE
         toolbar_pattern = NavBar.TOOLBAR
@@ -33,13 +32,14 @@ class Test(BaseTest):
         restore_previous_session_pattern = Pattern('hamburger_restore_previous_session.png')
         firefox_pinned_tab_pattern = Pattern('firefox_pinned_tab.png')
 
-
         hamburger_menu_quit_item_pattern = None
         if not Settings.is_mac():
             hamburger_menu_quit_item_pattern = Pattern('hamburger_menu_exit.png')
 
         # Define some Location / Region variables
-        top_screen_region = Region(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT // 5)
+        click_duration = 1
+        top_screen_region = Region(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT//5)
+        bottom_half_region = Screen.BOTTOM_HALF
 
         iris_tab_logo = exists(iris_tab_logo_pattern, DEFAULT_SITE_LOAD_TIMEOUT)
         assert_true(self, iris_tab_logo, 'Iris tab available')
@@ -53,30 +53,30 @@ class Test(BaseTest):
 
         hamburger_menu_button_location = find(hamburger_menu_button_pattern, proper_hamburger_menu_region)
 
-        click(hamburger_menu_button_location, 1)
+        click(hamburger_menu_button_location, click_duration)
 
         restore_previous_session_exists = exists(restore_previous_session_pattern, DEFAULT_FIREFOX_TIMEOUT)
-        assert_true(self, restore_previous_session_exists, '"Restore previous session" item located')
+        assert_true(self, restore_previous_session_exists, '\'Restore previous session\' item located')
 
         restore_previous_session_location = find(restore_previous_session_pattern)
 
         restore_firefox_focus()
 
-        #  Step 2: change few buttons position
+        #  Step 2: Customize Firefox: change few buttons position
 
         expected = exists(NavBar.DEFAULT_ZOOM_LEVEL_TOOLBAR_CUSTOMIZE_PAGE, Settings.TINY_FIREFOX_TIMEOUT, top_screen_region)
-        assert_false(self, expected, 'Zoom indicator not displayed by default in the url bar.')
+        assert_false(self, expected, 'Zoom indicator not displayed by default in the URL bar.')
 
         click_hamburger_menu_option('Customize...')
 
-        expected = exists(zoom_controls_customize_page_pattern, 10)
+        expected = exists(zoom_controls_customize_page_pattern, Settings.FIREFOX_TIMEOUT)
         assert_true(self, expected, 'Zoom controls found in the \'Customize\' page.')
 
         drag_drop(zoom_controls_customize_page_pattern, toolbar_pattern, 0.5)
         time.sleep(Settings.UI_DELAY)
         reset_mouse()
 
-        expected = exists(default_zoom_level_toolbar_customize_page_pattern, 10,
+        expected = exists(default_zoom_level_toolbar_customize_page_pattern, Settings.FIREFOX_TIMEOUT,
                           in_region=Region(0, 0, SCREEN_WIDTH, 300))
         assert_true(self, expected, 'Zoom controls successfully dragged and dropped in toolbar.')
 
@@ -86,47 +86,43 @@ class Test(BaseTest):
 
         navigate(LocalWeb.FIREFOX_TEST_SITE)
 
-        firefox_tab_opened = exists(firefox_tab_pattern)
+        firefox_tab_opened = exists(firefox_tab_pattern, Settings.SITE_LOAD_TIMEOUT)
         assert_true(self, firefox_tab_opened, 'Firefox webpage is opened')
 
-        right_click(firefox_tab_pattern)
+        right_click(firefox_tab_pattern, Settings.TINY_FIREFOX_TIMEOUT)
 
         unpinned_dropdown_opened = exists(pin_tab_pattern)
-        assert_true(self, unpinned_dropdown_opened, 'Dropdown menu for tab opened')
+        assert_true(self, unpinned_dropdown_opened, 'Tab dropdown menu opened')
 
         click(pin_tab_pattern)
 
         second_tab_pinned = exists(firefox_pinned_tab_pattern)
         assert_true(self, second_tab_pinned, 'Firefox tab is pinned')
 
-        # open several different website
+        # open several different websites
         local_url = [LocalWeb.FOCUS_TEST_SITE, LocalWeb.MOZILLA_TEST_SITE]
         local_url_logo_pattern = [LocalWeb.FOCUS_LOGO, LocalWeb.MOZILLA_LOGO]
 
         for _ in range(2):
             new_tab()
             navigate(local_url[_])
-            website_loaded = exists(local_url_logo_pattern[_], 20)
-            assert_true(self, website_loaded,
-                        'Website {0} loaded'
-                        .format(_ + 1))
+            website_loaded = exists(local_url_logo_pattern[_], Settings.SITE_LOAD_TIMEOUT)
+            assert_true(self, website_loaded, 'Website {0} loaded' .format(_ + 1))
 
         # Customize Firefox: set a theme, change a few button position, pin a tab, etc.
         click_hamburger_menu_option('Customize...')
 
-        region_bottom_half = Screen.BOTTOM_HALF
-
-        expected = region_bottom_half.exists(Customize.THEMES_DEFAULT_SET, 10)
+        expected = exists(Customize.THEMES_DEFAULT_SET, Settings.FIREFOX_TIMEOUT, bottom_half_region)
         assert_true(self, expected, 'Themes button is available.')
 
         click(Customize.THEMES_DEFAULT_SET)
 
-        expected = region_bottom_half.exists(Customize.DARK_THEME_OPTION, 10)
+        expected = exists(Customize.DARK_THEME_OPTION, Settings.FIREFOX_TIMEOUT, bottom_half_region)
         assert_true(self, expected, 'Dark theme option is available.')
 
         click(Customize.DARK_THEME_OPTION)
 
-        expected = region_bottom_half.exists(Customize.DARK_THEME_SET, 10)
+        expected = exists(Customize.DARK_THEME_SET, Settings.FIREFOX_TIMEOUT, bottom_half_region)
         assert_true(self, expected, 'Dark theme is set.')
 
         close_customize_page()
@@ -136,34 +132,39 @@ class Test(BaseTest):
             type('q', KeyModifier.CMD)
 
         else:
-            click(hamburger_menu_button_location, DEFAULT_UI_DELAY)
+            click(hamburger_menu_button_location, click_duration)
 
             hamburger_menu_quit_displayed = exists(hamburger_menu_quit_item_pattern, DEFAULT_FIREFOX_TIMEOUT)
-            assert_true(self, hamburger_menu_quit_displayed, 'Close Firefox from the "Hamburger" menu.')
+            assert_true(self, hamburger_menu_quit_displayed, 'Close Firefox from the \'Hamburger\' menu.')
 
-            click(hamburger_menu_quit_item_pattern, DEFAULT_UI_DELAY)
+            click(hamburger_menu_quit_item_pattern, click_duration)
 
         restart_firefox(self,
                         self.browser.path,
                         self.profile_path,
-                        'about:home', image=NavBar.HAMBURGER_MENU_DARK_THEME)
+                        'about:home',
+                        image=NavBar.HAMBURGER_MENU_DARK_THEME)
 
-        click(NavBar.HAMBURGER_MENU_DARK_THEME, 1)
+        hamburger_menu_button_exists = exists(NavBar.HAMBURGER_MENU_DARK_THEME, Settings.FIREFOX_TIMEOUT)
+        assert_true(self, hamburger_menu_button_exists, 'The Hamburger menu is successfully displayed.')
 
-        click(restore_previous_session_location, 1)
+        click(NavBar.HAMBURGER_MENU_DARK_THEME, click_duration)
+
+        click(restore_previous_session_location, click_duration)
 
         select_tab(1)
 
         firefox_page_content_restored = exists(LocalWeb.FIREFOX_LOGO)
         assert_true(self, firefox_page_content_restored, 'Firefox pinned tab restored successfully.')
 
-        local_url_logo_pattern = [LocalWeb.FOCUS_LOGO, LocalWeb.MOZILLA_LOGO]
-
         select_tab(2)
+
         focus_tab_restored = exists(LocalWeb.FOCUS_LOGO)
         assert_true(self, focus_tab_restored, 'Focus tab restored successfully.')
 
         select_tab(3)
+
         mozilla_tab_restored = exists(LocalWeb.MOZILLA_LOGO)
-        assert_true(self, mozilla_tab_restored, 'Mozilla tab restored successfully.')
-        
+        assert_true(self, mozilla_tab_restored, 'Mozilla tab restored successfully.\nThe previous session is '
+                                                'successfully restored (All previously closed tabs are successfully'
+                                                'restored).')
