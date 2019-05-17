5f9753c9ba8ec1d021e575acf424599e8d9aa8aa
Paul Silaghi <psilaghi@mozilla.com>
2019-05-17 18:45:23 +0300

    Fix issue 252 and add latest control center files.

--- src/control_center/assets/service-worker.js
+++ src/control_center/assets/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96f307a25d6fd0a19cc41cd87ead08f2.js"
+  "/precache-manifest.79a7a78eb18dc33e5560794c68d2ce79.js"
 );
 
 workbox.clientsClaim();
