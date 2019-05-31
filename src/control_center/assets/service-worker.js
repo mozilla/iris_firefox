c3ac84a3d3828169182ee537cc814f5eb58aafdb
Paul Silaghi <psilaghi@mozilla.com>
2019-05-31 17:42:03 +0300

    Fix issues #383, #365, #366, #359, #360

--- src/control_center/assets/service-worker.js
+++ src/control_center/assets/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.373c8722601282ac1efeb1267af44065.js"
+  "/precache-manifest.3bb1a4af3453dddd07c92378110f70fd.js"
 );
 
 workbox.clientsClaim();
