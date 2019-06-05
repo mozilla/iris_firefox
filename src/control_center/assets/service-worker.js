c3dfc8ec93742c8e7eebcb3cacfdb54d24284f9c
Paul Silaghi <psilaghi@mozilla.com>
2019-06-05 16:58:34 +0300

    Fix issue 413 - add gallery title.

--- src/control_center/assets/service-worker.js
+++ src/control_center/assets/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45485fc71d2dd38e236543aaaf936bdc.js"
+  "/precache-manifest.4561dfe1d8051c18e0a9bf02e46d5db3.js"
 );
 
 workbox.clientsClaim();
