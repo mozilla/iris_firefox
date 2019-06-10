090ba7354a6d80fee5d2960b65ef7207fb30114c
Serhii Pronoza <spronoza@mozilla.com>
2019-06-10 17:28:11 +0300

    Changed History region and timeouts

--- tests/firefox/antivirus/data_after_restart.py
+++ tests/firefox/antivirus/data_after_restart.py
@@ -24,12 +24,12 @@ class Test(FirefoxTest):
         wiki_logo_unactive_tab_pattern = Pattern('wiki_logo_unactive_tab.png')
 
         navigate(LocalWeb.SOAP_WIKI_TEST_SITE)
-        assert exists(LocalWeb.SOAP_WIKI_SOAP_LABEL, Settings.DEFAULT_SITE_LOAD_TIMEOUT), \
+        assert exists(LocalWeb.SOAP_WIKI_SOAP_LABEL, FirefoxSettings.HEAVY_SITE_LOAD_TIMEOUT), \
             'SOAP Wiki site successfully opened.'
 
         new_tab()
         navigate('https://edition.cnn.com')
-        assert exists(LocalWeb.CNN_LOGO, Settings.DEFAULT_HEAVY_SITE_LOAD_TIMEOUT), 'The CNN site successfully opened'
+        assert exists(LocalWeb.CNN_LOGO, FirefoxSettings.HEAVY_SITE_LOAD_TIMEOUT), 'The CNN site successfully opened'
 
         close_content_blocking_pop_up()
         history_sidebar()
@@ -37,10 +37,7 @@ class Test(FirefoxTest):
 
         history_sidebar_location = find(Sidebar.HistorySidebar.SIDEBAR_HISTORY_TITLE)
         history_width, history_height = Sidebar.HistorySidebar.SIDEBAR_HISTORY_TITLE.get_size()
-        history_sidebar_region = Region(history_sidebar_location.x,
-                                        history_sidebar_location.y,
-                                        history_width,
-                                        Screen.SCREEN_HEIGHT / 2)
+        history_sidebar_region = Region(0, history_sidebar_location.y, history_width*3, Screen.SCREEN_HEIGHT / 2)
 
         assert exists(Sidebar.HistorySidebar.Timeline.TODAY), 'The Today timeline displayed.'
 
@@ -69,33 +66,33 @@ class Test(FirefoxTest):
         click(Bookmarks.StarDialog.PANEL_FOLDER_DEFAULT_OPTION.similar(0.6))
         click(Bookmarks.StarDialog.PANEL_OPTION_BOOKMARK_TOOLBAR.similar(0.6))
         click(Bookmarks.StarDialog.DONE)
-        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, Settings.DEFAULT_SITE_LOAD_TIMEOUT), \
+        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, FirefoxSettings.SITE_LOAD_TIMEOUT), \
             'The CNN bookmark is successfully added.'
-        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, Settings.DEFAULT_SITE_LOAD_TIMEOUT), \
+        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, FirefoxSettings.SITE_LOAD_TIMEOUT), \
             'The Wikipedia bookmark is successfully added.'
 
         new_tab()
         navigate('https://www.youtube.com/')
-        assert exists(youtube_logo_pattern, Settings.DEFAULT_SITE_LOAD_TIMEOUT), 'The Youtube site successfully opened.'
+        assert exists(youtube_logo_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT), 'The Youtube site successfully opened.'
 
         new_tab()
         navigate('https://twitter.com/')
-        assert exists(twitter_logo_pattern, Settings.DEFAULT_SITE_LOAD_TIMEOUT), 'The Twitter site successfully opened.'
+        assert exists(twitter_logo_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT), 'The Twitter site successfully opened.'
 
         open_browser_console()
-        assert exists(browser_console_pattern, Settings.DEFAULT_SITE_LOAD_TIMEOUT), 'Browser console displayed.'
+        assert exists(browser_console_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT), 'Browser console displayed.'
 
         restart_via_console()
-        assert exists(browser_console_pattern, Settings.DEFAULT_SITE_LOAD_TIMEOUT), 'Browser console reopened.'
+        assert exists(browser_console_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT), 'Browser console reopened.'
 
         click(browser_console_pattern)
         close_window_control('auxiliary')
         assert exists(NavBar.HOME_BUTTON, 180), 'Firefox is successfully restarted.'
 
         restore_firefox_focus()
-        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, Settings.DEFAULT_SITE_LOAD_TIMEOUT), \
+        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, FirefoxSettings.SITE_LOAD_TIMEOUT), \
             'The CNN bookmark is successfully restored.'
-        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, Settings.DEFAULT_SITE_LOAD_TIMEOUT), \
+        assert bookmarks_toolbar_region.exists(LocalWeb.CNN_LOGO, FirefoxSettings.SITE_LOAD_TIMEOUT), \
             'The Wikipedia bookmark is successfully restored.'
         assert history_sidebar_region.exists(LocalWeb.CNN_LOGO.similar(0.6)), 'The CNN site is added to history.'
         assert history_sidebar_region.exists(wikipedia_logo_pattern), 'The Wikipedia site is added to history.'
