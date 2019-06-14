d251bfdd3aa9004a765e59a9f6a240b6a1447de9
pavelciapa <pciapa@mozilla.com>
2019-06-14 19:29:41 +0300

    Edited test case

--- tests/firefox/prefs/several_custom_settings_remains_checked.py
+++ tests/firefox/prefs/several_custom_settings_remains_checked.py
@@ -16,7 +16,13 @@ class Test(FirefoxTest):
         locale=['en-US'],
     )
     def run(self, firefox):
-        custom_history_settings_pattern = Pattern('custom_history_settings.png')
+        use_custom_settings_for_history_pattern = Pattern('custom_history_settings.png')
+        remember_history_pattern = Pattern('remember_history.png')
+        restart_browser_pattern = Pattern('restart_browser.png')
+        always_use_private_browsing_mode_unticked_pattern = Pattern('always_use_private_browsing_mode_unticked.png')
+        always_use_private_browsing_mode_ticked_pattern = Pattern('always_use_private_browsing_mode_ticked.png')
+        remember_browsing_search_clear_history_unticked_pattern = \
+            Pattern('remember_browsing_search_clear_history_unticked.png')
 
         if OSHelper.is_windows():
             value = Screen.SCREEN_HEIGHT/2
@@ -37,7 +43,11 @@ class Test(FirefoxTest):
         assert privacy_selected, 'The about:preferences page is successfully loaded. The options' \
                                  ' for "Privacy & Security" section are displayed.'
 
+        center_screen_location = Location(Screen.SCREEN_WIDTH // 2, Screen.SCREEN_HEIGHT // 2)
+        move(center_screen_location)
+
         remember_history = scroll_until_pattern_found(remember_history_pattern, Mouse().scroll, (0, -value), 100)
+
         assert remember_history, '"Remember history" option from the History dropdown list found.'
 
         click(remember_history_pattern)
@@ -52,7 +62,28 @@ class Test(FirefoxTest):
 
         click(always_use_private_browsing_mode_unticked_pattern)
 
+        restart_browser = exists(restart_browser_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert restart_browser, 'Restart browser button available'
 
+        click(restart_browser_pattern)
 
+        firefox_restarted = exists(Tabs.NEW_TAB_HIGHLIGHTED, FirefoxSettings.HEAVY_SITE_LOAD_TIMEOUT)
+        assert firefox_restarted, 'Firefox is successfully restarted.'
 
+        navigate('about:preferences#privacy')
 
+        privacy_selected = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED,
+                                  FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        assert privacy_selected, 'The about:preferences page is successfully loaded.'
+
+        move(center_screen_location)
+
+        remember_browsing_search_clear_history_unticked = scroll_until_pattern_found(
+            remember_browsing_search_clear_history_unticked_pattern, Mouse().scroll, (0, -value), 100)
+
+        assert remember_browsing_search_clear_history_unticked, \
+            '"Use custom settings for history" option from the History dropdown list found.' \
+            '\nNOTE: In the affected builds, the following options are ticked and grayed out: ' \
+            '\n"Remember browsing and download history" ' \
+            '\n"Remember search and form history"' \
+            '\nTo see the behaviour click here. [https://bug1524995.bmoattachments.org/attachment.cgi?id=9041165]'
