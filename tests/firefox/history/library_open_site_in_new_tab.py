7267a2365b1a49e8d30f6a9e2b85ba1cd905b3ed
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-06-18 13:32:52 +0300

    Fix for library_open_site_in_new_tab.py (#3278)
    * Changed expected_5 pattern and timeouts
    
    * Variables name changed

--- tests/firefox/history/library_open_site_in_new_tab.py
+++ tests/firefox/history/library_open_site_in_new_tab.py
@@ -19,51 +19,56 @@ class Test(FirefoxTest):
         iris_bookmark_pattern = Pattern('iris_bookmark.png')
         show_all_history_pattern = History.HistoryMenu.SHOW_ALL_HISTORY
         history_today_pattern = Library.HISTORY_TODAY
-        library_bookmarks_mozilla_pattern = Pattern('library_bookmarks_mozilla.png')
         iris_tab_icon = Pattern('iris_logo_tab.png')
 
         # Open a page to create some today's history.
         new_tab()
+
         navigate(LocalWeb.MOZILLA_TEST_SITE)
 
-        expected_1 = exists(LocalWeb.MOZILLA_LOGO, 10)
-        assert expected_1 is True, 'Mozilla page loaded successfully.'
+        test_site_opened = exists(LocalWeb.MOZILLA_LOGO, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert test_site_opened is True, 'Mozilla page loaded successfully.'
 
         close_tab()
 
         # Select the History option from the View History, saved bookmarks and more Menu.
         open_library_menu('History')
+
         right_upper_corner = Screen().new_region(Screen.SCREEN_WIDTH / 2, 0, Screen.SCREEN_WIDTH / 2,
                                                  Screen.SCREEN_HEIGHT / 2)
 
-        expected_2 = right_upper_corner.exists(iris_bookmark_pattern, 10)
-        assert expected_2 is True, 'Iris page is displayed in the History menu list.'
+        iris_displayed_in_history = right_upper_corner.exists(iris_bookmark_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert iris_displayed_in_history is True, 'Iris page is displayed in the History menu list.'
 
         # Click on the Show All History button.
         click(show_all_history_pattern, 2)
 
-        expected_3 = exists(history_today_pattern, 10)
-        assert expected_3 is True, 'Today history option is available.'
+        today_option_exists = exists(history_today_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert today_option_exists is True, 'Today history option is available.'
+
+        type(Key.DOWN)
 
         # Verify if Mozilla page is present in Today's History.
 
-        expected_4 = exists(library_bookmarks_mozilla_pattern, 10)
-        assert expected_4 is True, 'Mozilla page is displayed successfully in the History list.'
+        mozilla_displayed_in_history = exists(LocalWeb.MOZILLA_BOOKMARK_SMALL, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert mozilla_displayed_in_history is True, 'Mozilla page is displayed successfully in the History list.'
 
         # Open the Mozilla page using the 'Open in a New Tab' button from the context menu.
-        right_click(library_bookmarks_mozilla_pattern)
-        time.sleep(Settings.DEFAULT_UI_DELAY_SHORT)
+        right_click(LocalWeb.MOZILLA_BOOKMARK_SMALL)
+
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
+
         type(text='w')
 
         # Close the library.
         open_library()
-        time.sleep(Settings.DEFAULT_UI_DELAY_SHORT)
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
         click_window_control('close')
-        time.sleep(Settings.DEFAULT_UI_DELAY_SHORT)
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
 
         # Check that the Mozilla page loaded successfully in a new tab.
-        expected_5 = exists(LocalWeb.MOZILLA_LOGO, 10)
-        assert expected_5 is True, 'Mozilla page loaded successfully.'
+        test_site_opened = exists(LocalWeb.MOZILLA_LOGO, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert test_site_opened is True, 'Mozilla page loaded successfully.'
 
-        expected_6 = exists(iris_tab_icon, 10)
-        assert expected_6 is True, 'Iris local page is still open in the first tab.'
+        iris_page_displayed = exists(iris_tab_icon, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert iris_page_displayed is True, 'Iris local page is still open in the first tab.'
