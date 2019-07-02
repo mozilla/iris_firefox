557283b8e1d09ec93e41007480f3ef362b22dff8
Nikita Evdokimov <46671489+nevdokimov@users.noreply.github.com>
2019-07-02 18:50:51 +0300

    Fixed two_selections_at_the_same_time test case (#3338)
    * Fixed two_selections_at_the_same_time test case
    
    * Added method change_preference to issue_3335
    
    * Added changes to script for issue_3335
    
    * duck_duck_go_one_off_button.png replaced
    
    * Added new changes to patterns for issue_3335
    
    * Added small changes

--- tests/firefox/awesomebar/two_selections_at_the_same_time.py
+++ tests/firefox/awesomebar/two_selections_at_the_same_time.py
@@ -20,65 +20,46 @@ class Test(FirefoxTest):
         duck_duck_go_one_off_button_pattern = Pattern('duck_duck_go_one_off_button.png')
         search_with_url_autocomplete_pattern = Pattern('search_with_url_autocomplete.png')
         twitter_one_off_button_highlight_pattern = Pattern('twitter_one_off_button_highlight.png')
-        default_status_pattern = Pattern('default_status.png')
-        modified_status_pattern = Pattern('modified_status.png')
-        true_value_pattern = Pattern('true_value.png')
-        false_value_pattern = Pattern('false_value.png')
         amazon_logo_pattern = Pattern('amazon_logo.png')
-        accept_risk_pattern = Pattern('accept_risk.png')
 
         region = Region(0, 0, Screen().width, 2 * Screen().height / 3)
+        move_to_region = Region(0, 0, Screen().width/2, Screen().height/2)
 
         navigate('www.amazon.com')
 
-        expected = exists(amazon_logo_pattern, 10)
-        assert expected, 'Page successfully loaded, amazon logo found.'
+        amazon_logo_exists = exists(amazon_logo_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert amazon_logo_exists, 'Page successfully loaded, amazon logo found.'
 
-        navigate('about:config')
-
-        # Change focus from the url bar.
-        if OSHelper.is_windows() or OSHelper.is_linux():
-            click(NavBar.HAMBURGER_MENU.target_offset(-170, 15))
-            type(Key.ENTER)
-        else:
-            if exists(accept_risk_pattern):
-                click(accept_risk_pattern)
-        expected = region.exists(default_status_pattern, 10)
-        assert expected, 'The \'about:config\' page successfully loaded and default status is correct.'
-
-        paste('keyword.enabled')
-        type(Key.ENTER)
-
-        expected = region.exists(default_status_pattern, 10)
-        assert expected, 'The \'keyword.enabled\' preference has status \'default\' by default.'
-
-        expected = region.exists(true_value_pattern, 10)
-        assert expected, 'The \'keyword.enabled\' preference has value \'true\' by default.'
-
-        double_click(default_status_pattern)
-
-        expected = region.exists(modified_status_pattern, 10)
-        assert expected, 'The \'keyword.enabled\' preference has status \'modified\' after the preference has changed.'
-
-        expected = region.exists(false_value_pattern, 10)
-        assert expected, 'The \'keyword.enabled\' preference has value \'false\' after the preference has changed.'
+        change_preference('keyword.enabled', False)
 
         select_location_bar()
-        paste('amaz')
 
-        expected = region.exists(search_with_url_autocomplete_pattern, 10)
-        assert expected, 'Search is performed with url autocomplete for pages where you have been before.'
+        type('amaz')
+
+        search_with_url_autocomplete_exists = region.exists(search_with_url_autocomplete_pattern,
+                                                            FirefoxSettings.FIREFOX_TIMEOUT)
+        assert search_with_url_autocomplete_exists, 'Search is performed with url autocomplete' \
+                                                    ' for pages where you have been before.'
+
+        duck_duck_go_one_off_button_exists = exists(duck_duck_go_one_off_button_pattern)
+        assert duck_duck_go_one_off_button_exists, 'DuckDuckGo button exists.'
+
+        move(move_to_region)
 
         hover(duck_duck_go_one_off_button_pattern)
 
-        expected = exists(hover_duck_duck_go_one_off_button_pattern, 10)
-        assert expected, 'Mouse is over the \'DuckDuckGo\' search engine.'
+        hover_duck_duck_go_one_off_button_exists = exists(hover_duck_duck_go_one_off_button_pattern,
+                                                          FirefoxSettings.FIREFOX_TIMEOUT)
+        assert hover_duck_duck_go_one_off_button_exists, 'Mouse is over the \'DuckDuckGo\' search engine.'
 
-        expected = region.exists(search_with_url_autocomplete_pattern, 10)
-        assert expected, 'The autocomplete is still displayed after user hovers an one-off button.'
+        search_with_url_autocomplete_exists = region.exists(search_with_url_autocomplete_pattern,
+                                                            FirefoxSettings.FIREFOX_TIMEOUT)
+        assert search_with_url_autocomplete_exists, 'The autocomplete is still displayed after' \
+                                                    ' user hovers an one-off button.'
 
         repeat_key_up(3)
         key_to_one_off_search(twitter_one_off_button_highlight_pattern)
 
-        expected = region.exists(twitter_one_off_button_highlight_pattern, 10)
-        assert expected, 'The \'Twitter\' one-off button is highlighted.'
+        twitter_one_off_button_highlight_exists = region.exists(twitter_one_off_button_highlight_pattern,
+                                                                FirefoxSettings.FIREFOX_TIMEOUT)
+        assert twitter_one_off_button_highlight_exists, 'The \'Twitter\' one-off button is highlighted.'
