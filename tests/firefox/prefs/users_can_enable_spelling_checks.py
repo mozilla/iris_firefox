2e192fb291ae4802ecb9a89d652cd1fe4798ec62
pavelciapa <pciapa@mozilla.com>
2019-07-04 16:20:36 +0300

    Added undelined red word check

--- tests/firefox/prefs/users_can_enable_spelling_checks.py
+++ tests/firefox/prefs/users_can_enable_spelling_checks.py
@@ -15,8 +15,16 @@ class Test(FirefoxTest):
         locale=['en-US'],
     )
     def run(self, firefox):
-        preferences_search_pattern = AboutPreferences.ABOUT_PREFERENCE_SEARCH_PAGE_PATTERN
+        text_editor_title_pattern = Pattern('text_editor_title.png')
+        check_your_spelling_unchecked_pattern = Pattern('check_your_spelling_unchecked.png')
+        check_your_spelling_checked_pattern = Pattern('check_your_spelling_unchecked.png')
+        word_underlined_red_pattern = Pattern('word_underlined_red.png')
         text_editor_page = self.get_asset_path('editor.html')
+
+        if OSHelper.is_windows():
+            scroll_height = Screen.SCREEN_HEIGHT*2
+        elif OSHelper.is_linux() or OSHelper.is_mac():
+            scroll_height = Screen.SCREEN_HEIGHT//100
 
         navigate('about:preferences#general')
 
@@ -35,9 +43,47 @@ class Test(FirefoxTest):
 
         # The word that was spelled wrong is underlined with a red squiggly line.
 
-        paste('test   ttteeesssttt')
+        paste('test   ttteeesssttt ')
+
+        wrong_spelled_word_underlined_red = exists(word_underlined_red_pattern)
+        assert wrong_spelled_word_underlined_red, 'The word that was spelled wrong is underlined with a red ' \
+                                                  'squiggly line.'
 
         # From "Language and Appearance", underneath the "Language", uncheck the box "Check your spelling as you type".
 
+        new_tab()
+
         navigate('about:preferences#general')
 
+        page_loaded = exists(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_NOT_SELECTED)
+        assert page_loaded, 'about:preferences#general page loaded.'
+
+        screen_center_location = Location(Screen.SCREEN_WIDTH // 2, Screen.SCREEN_HEIGHT // 2)
+
+        hover(screen_center_location)
+
+        check_your_spelling = scroll_until_pattern_found(check_your_spelling_checked_pattern, Mouse().scroll,
+                                                         (None, -scroll_height), 40,
+                                                         FirefoxSettings.TINY_FIREFOX_TIMEOUT/2)
+        assert check_your_spelling, '"Check you spelling..." option found.'
+
+        click(check_your_spelling_checked_pattern)
+
+        check_your_spelling_unchecked = find_in_region_from_pattern(check_your_spelling_unchecked_pattern,
+                                                                    AboutPreferences.UNCHECKED_BOX)
+        assert check_your_spelling_unchecked, '"Check you spelling..." is unchecked.'
+
+        new_tab()
+
+        navigate(text_editor_page)
+
+        text_editor_loaded = exists(text_editor_title_pattern)
+        assert text_editor_loaded, 'Text editor page loaded.'
+
+        click(screen_center_location)
+
+        paste('test   ttteeesssttt ')
+
+        wrong_spelled_word_underlined_red = exists(word_underlined_red_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        assert wrong_spelled_word_underlined_red is False, 'The word that was spelled wrong is underlined with a red ' \
+                                                           'squiggly line.'
