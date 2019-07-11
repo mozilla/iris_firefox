05309d3d73915f7fc16afca24fdf5052c3b16c37
Nikita Evdokimov <nevdokimov@mozilla.com>
2019-07-11 14:02:48 +0300

    Added changes to script for OSX and osx images

--- tests/firefox/prefs/firefox_can_allow_deny_pages_to_choose_own_fonts.py
+++ tests/firefox/prefs/firefox_can_allow_deny_pages_to_choose_own_fonts.py
@@ -15,19 +15,24 @@ class Test(FirefoxTest):
         locale=['en-US'],
     )
     def run(self, firefox):
+        preferences_general_option_pattern = Pattern('preferences_general_option.png')
         font_of_site_pattern = Pattern('font_of_site.png')
         font_of_site_changed_pattern = Pattern('font_of_site_changed.png')
         advanced_button_pattern = Pattern('advanced_button.png')
         fonts_subdialog_label_pattern = Pattern('fonts_subdialog_label.png')
-        pages_have_own_fonts_checked_pattern = Pattern('prefs_checked_box.png')
-        pages_have_own_fonts_unchecked_pattern = Pattern('prefs_unchecked_box.png')
+
+        location = Location(Screen.SCREEN_WIDTH/2, Screen.SCREEN_HEIGHT/2)
 
         if OSHelper.is_windows():
             value = Screen.SCREEN_HEIGHT
-        else:
+        elif OSHelper.is_linux():
             value = Screen.SCREEN_HEIGHT/200
+        else:
+            value = Screen.SCREEN_HEIGHT/10
 
         navigate('http://www.psimadethis.com/')
+
+        move(location)
 
         font_of_site_exists = scroll_until_pattern_found(font_of_site_pattern, Mouse().scroll,
                                                          (0, -value), 20,
@@ -42,6 +47,9 @@ class Test(FirefoxTest):
 
         navigate('about:preferences#general')
 
+        preferences_general_option_exists = exists(preferences_general_option_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert preferences_general_option_exists, 'The about:preferences page is successfully loaded'
+
         type('Fonts and Colors', interval=0.2)
 
         advanced_button_exists = exists(advanced_button_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
@@ -52,19 +60,18 @@ class Test(FirefoxTest):
         fonts_subdialog_label_exists = exists(fonts_subdialog_label_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert fonts_subdialog_label_exists, 'The "Fonts" subdialog is displayed'
 
-        [type(Key.TAB) for _ in range(2)]
+        if OSHelper.is_windows() and OSHelper.is_linux():
+            [type(Key.TAB) for _ in range(2)]
 
-        [type(Key.DOWN) for _ in range(6)]
+            [type(Key.DOWN) for _ in range(6)]
 
-        pages_have_own_fonts_checked_exists = exists(pages_have_own_fonts_checked_pattern,
-                                                     FirefoxSettings.FIREFOX_TIMEOUT)
+        pages_have_own_fonts_checked_exists = exists(AboutPreferences.CHECKED_BOX, FirefoxSettings.FIREFOX_TIMEOUT)
         assert pages_have_own_fonts_checked_exists, ' "Allow pages to choose their own fonts,' \
                                                     ' instead of your selections above" checked exists'
 
-        click(pages_have_own_fonts_checked_pattern)
+        click(AboutPreferences.CHECKED_BOX)
 
-        pages_have_own_fonts_unchecked_exists = exists(pages_have_own_fonts_unchecked_pattern,
-                                                       FirefoxSettings.FIREFOX_TIMEOUT)
+        pages_have_own_fonts_unchecked_exists = exists(AboutPreferences.UNCHECKED_BOX, FirefoxSettings.FIREFOX_TIMEOUT)
         assert pages_have_own_fonts_unchecked_exists, ' "Allow pages to choose their own fonts,' \
                                                       ' instead of your selections above" unchecked exists'
 
@@ -85,15 +92,3 @@ class Test(FirefoxTest):
 
         font_of_site_exists = exists(font_of_site_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert font_of_site_exists is False, 'Previous fonts is not available'
-
-
-
-
-
-
-
-
-
-
-
-
