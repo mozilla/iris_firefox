2a420ceb9936cfce3a59c6cef0b6cf18c31eea70
spronoza <spronoza@mozilla.com>
2019-07-17 18:34:33 +0300

    Initial commit of issue_3054 for OSX

---
+++ tests/firefox/safe_browsing/split_google_key_management_between_gls_and_safebrowsing.py
@@ -0,0 +1,69 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from targets.firefox.fx_testcase import *
+
+
+class Test(FirefoxTest):
+
+    @pytest.mark.details(
+        description='Bug 1531176 - Split the Google key management between gls and safe browsing',
+        test_case_id='294453',
+        test_suite_id='69',
+        locale=['en-US'],
+    )
+    def run(self, firefox):
+        browser_console_title_pattern = Pattern('browser_console_title.png')
+        console_log_arrow_icon_pattern = Pattern('console_log_arrow_icon.png')
+
+        change_preference('devtools.chrome.enabled', 'true')
+
+        open_browser_console()
+
+        browser_console_opened = exists(browser_console_title_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert browser_console_opened, 'Browser Console opened'
+
+        paste('print(AppConstants.MOZ_GOOGLE_SAFEBROWSING_API_KEY)')
+
+        type(Key.ENTER)
+
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+
+        value_returned = exists(console_log_arrow_icon_pattern)
+        assert value_returned, 'value_returned'
+
+        right_click(console_log_arrow_icon_pattern)
+        type('c')
+
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+
+        returned_value = get_clipboard().replace('\n', '').replace('\r', '').replace('\"', '')
+        assert returned_value.endswith('c5Dovo'), 'The returned value ends in: c5Dovo'
+
+        close_window_control('auxiliary')
+
+        open_browser_console()
+
+        browser_console_opened = exists(browser_console_title_pattern, FirefoxSettings.FIREFOX_TIMEOUT,)
+        assert browser_console_opened, 'Browser Console opened'
+
+        paste('print(AppConstants.MOZ_GOOGLE_LOCATION_SERVICE_API_KEY)')
+
+        type(Key.ENTER)
+
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+
+        value_returned = exists(console_log_arrow_icon_pattern)
+        assert value_returned, 'value_returned'
+
+        right_click(console_log_arrow_icon_pattern)
+        type('c')
+
+        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+
+        returned_value = get_clipboard().replace('\n', '').replace('\r', '').replace('\"', '')
+        assert returned_value.endswith('_rptiQ'), 'The returned value ends in: _rptiQ'
+
+        close_window_control('auxiliary')
