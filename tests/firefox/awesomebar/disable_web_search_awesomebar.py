f5c006a26a9a698f289e09b1a2b6c15384edaed3
pavelciapa <pciapa@mozilla.com>
2019-07-31 16:58:02 +0300

    pass region as parameter

--- tests/firefox/awesomebar/disable_web_search_awesomebar.py
+++ tests/firefox/awesomebar/disable_web_search_awesomebar.py
@@ -22,7 +22,7 @@ class Test(FirefoxTest):
         mozilla_support_url_pattern = Pattern('mozilla_support_url.png')
         amazon_logo_pattern = Pattern('amazon_logo.png')
 
-        region = Region(0, 0, Screen.SCREEN_WIDTH, 2 * Screen.SCREEN_HEIGHT / 3)
+        top_two_thirds_region = Region(0, 0, Screen.SCREEN_WIDTH, 2 * Screen.SCREEN_HEIGHT / 3)
 
         navigate('www.amazon.com')
 
@@ -32,12 +32,14 @@ class Test(FirefoxTest):
         select_location_bar()
         type('moz')
 
-        one_off_button_exists = region.exists(google_one_off_button_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        one_off_button_exists = exists(google_one_off_button_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                       region=top_two_thirds_region)
         assert one_off_button_exists, 'The \'Google\' one-off button found.'
 
         click(google_one_off_button_pattern)
 
-        mozilla_support_url_exists = region.exists(mozilla_support_url_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        mozilla_support_url_exists = exists(mozilla_support_url_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                            region=top_two_thirds_region)
         if mozilla_support_url_exists:
             select_location_bar()
             type('moz')
@@ -45,7 +47,8 @@ class Test(FirefoxTest):
             click(google_one_off_button_pattern)
             time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT/2)
 
-        google_search_results_displayed = region.exists(google_search_results_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        google_search_results_displayed = exists(google_search_results_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                                 region=top_two_thirds_region)
         assert google_search_results_displayed, 'Google search results are displayed.'
 
         change_preference('keyword.enabled', 'false')
@@ -53,7 +56,8 @@ class Test(FirefoxTest):
         select_location_bar()
         type('amaz')
 
-        autocomplete_performed = region.exists(search_with_url_autocomplete_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        autocomplete_performed = exists(search_with_url_autocomplete_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                        region=top_two_thirds_region)
         assert autocomplete_performed, 'Search is performed with url autocomplete for pages where you have been before.'
 
         type(Key.ENTER)
@@ -64,10 +68,13 @@ class Test(FirefoxTest):
         select_location_bar()
         type('test')
 
-        google_one_off_button_found = region.exists(google_one_off_button_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        google_one_off_button_found = exists(google_one_off_button_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                             region=top_two_thirds_region)
         assert google_one_off_button_found, 'The \'Google\' one-off button found.'
 
         click(google_one_off_button_pattern)
 
-        google_search_results_displayed = region.exists(google_search_results_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        google_search_results_displayed = exists(google_search_results_pattern, FirefoxSettings.FIREFOX_TIMEOUT,
+                                                 region=top_two_thirds_region)
+
         assert google_search_results_displayed, 'Google search results are displayed.'
