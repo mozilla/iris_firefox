40ab5b736875984c20e216225e3658d65e8909df
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-08-07 18:48:00 +0300

    Fix search_suggestions_can_be_disabled (#3587)
    * search_bar_pattern replaced on LocationBar.SEARCH_BAR.
    
    * SEARCH_BAR changed to SEARCH_BAR_MAGNIFYING_GLASS

--- tests/firefox/search_and_update/search_suggestions_can_be_disabled.py
+++ tests/firefox/search_and_update/search_suggestions_can_be_disabled.py
@@ -15,7 +15,6 @@ class Test(FirefoxTest):
         test_suite_id='83',
     )
     def run(self, firefox):
-        search_bar_pattern = Pattern('search_bar.png')
         provide_search_suggestions_pattern = Pattern('provide_search_suggestions.png')
         show_suggestions_pattern = Pattern('show_suggestions.png')
         google_logo_content_search_field_pattern = Pattern('google_logo_content_search_field.png')
@@ -26,10 +25,10 @@ class Test(FirefoxTest):
         # Enable the search bar.
         change_preference('browser.search.widget.inNavBar', True)
 
-        search_bar_enabled = exists(search_bar_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
+        search_bar_enabled = exists(LocationBar.SEARCH_BAR_MAGNIFYING_GLASS, FirefoxSettings.FIREFOX_TIMEOUT)
         assert search_bar_enabled is True, 'The search bar is successfully enabled.'
 
-        right_click(search_bar_pattern)
+        right_click(LocationBar.SEARCH_BAR_MAGNIFYING_GLASS)
 
         show_suggestions_available = exists(show_suggestions_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert show_suggestions_available is True, 'The \'Show Suggestions\' option is visible.'
