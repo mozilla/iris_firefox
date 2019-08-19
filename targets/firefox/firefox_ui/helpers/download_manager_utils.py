2d5ad91930b16fa5ed946ba96826d8b71186a35c
pavelciapa <pciapa@mozilla.com>
2019-08-19 14:16:16 +0300

    Changed timeout constants

--- targets/firefox/firefox_ui/helpers/download_manager_utils.py
+++ targets/firefox/firefox_ui/helpers/download_manager_utils.py
@@ -171,12 +171,13 @@ def cancel_in_progress_downloads_from_the_library(private_window=False):
     return steps
 
 
-def download_file(file_to_download, accept_download, max_number_of_attempts=20, accept_download_available=True):
+def download_file(file_to_download, accept_download, max_number_of_attempts=20, expect_accept_download_available=True):
     """
     :param file_to_download: Pattern of file to be downloaded.
     :param accept_download: Accept download pattern.
     :param max_number_of_attempts: Max number of attempts to locate file_to_download pattern.
-    :param accept_download_available: False if Windows don't have accept download button
+    :param expect_accept_download_available: True if we expect accept_download button, False - if we don't;
+            (in Windows 7 download UI don't have extra accept button in certain cases)
     :return: None.
     """
     for _ in range(max_number_of_attempts):
@@ -192,7 +193,7 @@ def download_file(file_to_download, accept_download, max_number_of_attempts=20, 
             raise APIHelperError('File to be downloaded not found.')
 
     try:
-        wait(DownloadFiles.SAVE_FILE, 90)
+        wait(DownloadFiles.SAVE_FILE, Settings.DEFAULT_HEAVY_SITE_LOAD_TIMEOUT)
         logger.debug('The \'Save file\' option is present in the page.')
 
         time.sleep(Settings.DEFAULT_SYSTEM_DELAY) # prevent click on inactive button on windows
@@ -202,9 +203,9 @@ def download_file(file_to_download, accept_download, max_number_of_attempts=20, 
     except FindError:
         raise APIHelperError('The \'Save file\' option is not present in the page, aborting.')
 
-    if OSHelper.is_mac() or OSHelper.is_linux() or (OSHelper.is_windows() and accept_download_available):
+    if expect_accept_download_available:
         try:
-            accept_download_button = exists(accept_download, 5)
+            accept_download_button = exists(accept_download, Settings.FIREFOX_TIMEOUT)
             if accept_download_button:
                 logger.debug('The accept download button found in the page.')
                 click(accept_download)
