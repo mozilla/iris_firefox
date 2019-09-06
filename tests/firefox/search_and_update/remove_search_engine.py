79611ea7a79298e0a2b457c1482edc0d15942251
pavelciapa <pciapa@mozilla.com>
2019-09-06 13:08:50 +0300

    Added Key.up for windows os

--- tests/firefox/search_and_update/remove_search_engine.py
+++ tests/firefox/search_and_update/remove_search_engine.py
@@ -53,6 +53,7 @@ class Test(FirefoxTest):
         click(restore_default_search_engine_pattern)
 
         type(Key.UP)
+        type(Key.UP)
 
         expected = exists(wikipedia_search_bar_pattern, 10)
         assert expected is True, 'The \'Wikipedia\' search engine is restored.'
