04780a94835d2d0e7f7bbc3777d5912d31ac929f
dbakaev <45363191+dbakaev@users.noreply.github.com>
2019-09-13 16:31:46 +0300

    fix code for win

--- tests/firefox/private_browsing/track_protection_exceptions_normal_sessions.py
+++ tests/firefox/private_browsing/track_protection_exceptions_normal_sessions.py
@@ -19,7 +19,7 @@ class Test(FirefoxTest):
         remove_website_button_pattern = AboutPreferences.Privacy.Exceptions.REMOVE_WEBSITE_BUTTON
         privacy_page_pattern = AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED
         tracking_protection_shield_pattern = LocationBar.TRACKING_PROTECTION_SHIELD_ACTIVATED
-        tracking_protection_shield_deactivated_pattern = LocationBar.TRACKING_PROTECTION_SHIELD_DEACTIVATED
+        tracking_protection_shield_deactivated_pattern = Pattern('tracking_protection_shield_activated.png')
 
         turn_off_blocking_pattern = Pattern('turn_off_blocking_for_site_button.png')
         manage_exceptions_button_pattern = Pattern('manage_exceptions_button.png')
@@ -37,7 +37,7 @@ class Test(FirefoxTest):
 
         navigate('about:preferences#privacy')
         navigated_to_preferences = exists(privacy_page_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert navigated_to_preferences is True, 'The about:preferences#privacy page is successfully displayed.'
+        assert navigated_to_preferences, 'The about:preferences#privacy page is successfully displayed.'
 
         privacy_strict_checkbox_unchecked_displayed = exists(privacy_strict_checkbox_unchecked_pattern)
         if privacy_strict_checkbox_unchecked_displayed:
@@ -48,75 +48,87 @@ class Test(FirefoxTest):
         click(AboutPreferences.PRIVACY_AND_SECURITY_BUTTON_SELECTED)
 
         privacy_strict_checkbox_checked_displayed = exists(privacy_strict_checkbox_checked_pattern)
-        assert privacy_strict_checkbox_checked_displayed is True, 'The "Strict" content blocking' \
-                                                                  ' option was successfully saved'
+        assert privacy_strict_checkbox_checked_displayed, 'The "Strict" content blocking' \
+                                                          ' option was successfully saved'
 
         navigate('https://itisatrap.org/firefox/its-a-tracker.html')
 
         close_content_blocking_pop_up()
 
+        if OSHelper.is_windows():  # to prevent pop-up covering pattern
+            type(Key.ESC)
+
         firefox_tracker_site_logo_displayed = exists(firefox_tracker_site_content_pattern)
-        assert firefox_tracker_site_logo_displayed is True, 'The website is successfully displayed.'
+        assert firefox_tracker_site_logo_displayed, 'The website is successfully displayed.'
 
         tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern)
-        assert tracking_protection_shield_displayed is True, 'The Tracking protection shield' \
-                                                             ' is displayed near the address bar.'
+        assert tracking_protection_shield_displayed, 'The Tracking protection shield' \
+                                                     ' is displayed near the address bar.'
 
         third_party_tracker_correctly_blocked_displayed = exists(third_party_tracker_correctly_blocked_text)
-        assert third_party_tracker_correctly_blocked_displayed is True, 'Simulated third-party tracker' \
-                                                                        ' was correctly blocked is displayed.'
+        assert third_party_tracker_correctly_blocked_displayed, 'Simulated third-party tracker' \
+                                                                ' was correctly blocked is displayed.'
 
         first_party_tracker_correctly_blocked_displayed = exists(first_party_tracker_correctly_blocked_text)
-        assert first_party_tracker_correctly_blocked_displayed is True, 'Simulated first-party tracker' \
-                                                                        ' was correctly loaded is displayed.'
+        assert first_party_tracker_correctly_blocked_displayed, 'Simulated first-party tracker' \
+                                                                ' was correctly loaded is displayed.'
 
         dnt_signal_correctly_sent_displayed = exists(dnt_signal_correctly_sent_text)
-        assert dnt_signal_correctly_sent_displayed is True, 'The DNT signal was correctly sent is displayed.'
+        assert dnt_signal_correctly_sent_displayed, 'The DNT signal was correctly sent is displayed.'
 
         click(tracking_protection_shield_pattern)
 
         turn_off_blocking_displayed = exists(turn_off_blocking_pattern)
-        assert turn_off_blocking_displayed is True, 'Turn off blocking for this site is displayed.'
+        assert turn_off_blocking_displayed, 'Turn off blocking for this site is displayed.'
 
         click(turn_off_blocking_pattern)
 
+        if OSHelper.is_windows():  # to prevent pop-up covering pattern
+            type(Key.ESC)
+
+        if not exists(firefox_tracker_site_content_pattern,
+                      FirefoxSettings.FIREFOX_TIMEOUT):
+            if OSHelper.is_mac():
+                type('r', KeyModifier.CMD)
+            else:
+                type('r', KeyModifier.CTRL)
         firefox_tracker_site_logo_displayed = exists(firefox_tracker_site_content_pattern,
                                                      FirefoxSettings.FIREFOX_TIMEOUT)
-        assert firefox_tracker_site_logo_displayed is True, 'Site is reloaded after turning off content blocking'
+        assert firefox_tracker_site_logo_displayed, 'Site is reloaded after turning off content blocking'
 
         tracking_protection_shield_deactivated_displayed = exists(tracking_protection_shield_deactivated_pattern)
-        assert tracking_protection_shield_deactivated_displayed is True, 'The tracking protection shield is displayed' \
-                                                                         ' as deactivated (strikethrough).'
+        assert tracking_protection_shield_deactivated_displayed, 'The tracking protection shield is displayed' \
+                                                                 ' as deactivated (strikethrough).'
         type(Key.ESC)
 
         move(tracking_protection_shield_deactivated_pattern)
         tracking_content_detected_pattern_displayed = exists(tracking_content_detected_pattern,
                                                              FirefoxSettings.TINY_FIREFOX_TIMEOUT)
-        assert tracking_content_detected_pattern_displayed is True, 'On hover, the tracking protection' \
-                                                                    ' shield displays a "Tracking content detected"' \
-                                                                    ' tooltip message.'
+        assert tracking_content_detected_pattern_displayed, 'On hover, the tracking protection' \
+                                                            ' shield displays a "Tracking content detected"' \
+                                                            ' tooltip message.'
 
         third_party_tracker_incorrectly_loaded_displayed = exists(third_party_tracker_incorrectly_loaded_text)
-        assert third_party_tracker_incorrectly_loaded_displayed is True, 'Simulated third-party tracker' \
-                                                                         ' was incorrectly loaded is displayed.'
+        assert third_party_tracker_incorrectly_loaded_displayed, 'Simulated third-party tracker' \
+                                                                 ' was incorrectly loaded is displayed.'
 
         first_party_tracker_correctly_blocked_displayed = exists(first_party_tracker_correctly_blocked_text)
-        assert first_party_tracker_correctly_blocked_displayed is True, 'Simulated first-party tracker' \
-                                                                        ' was correctly loaded is displayed.'
+        assert first_party_tracker_correctly_blocked_displayed, 'Simulated first-party tracker' \
+                                                                ' was correctly loaded is displayed.'
 
         dnt_signal_correctly_sent_displayed = exists(dnt_signal_correctly_sent_text)
-        assert dnt_signal_correctly_sent_displayed is True, 'The DNT signal was correctly sent is displayed.'
+        assert dnt_signal_correctly_sent_displayed, 'The DNT signal was correctly sent is displayed.'
 
         navigate('about:preferences#privacy')
 
         click(manage_exceptions_button_pattern)
         exceptions_content_blocking_panel_displayed = exists(exceptions_content_blocking_panel_pattern)
-        assert exceptions_content_blocking_panel_displayed is True, 'The Exceptions - Content Blocking panel' \
-                                                                    ' is successfully displayed.'
+        assert exceptions_content_blocking_panel_displayed, 'The Exceptions - Content Blocking panel' \
+                                                            ' is successfully displayed.'
 
         itstrap_site_exception_displayed = exists(itstrap_site_exception_selected_pattern)
-        assert itstrap_site_exception_displayed is True, 'The previously accessed website is displayed' \
-                                                         ' as exception inside the panel.'
+        assert itstrap_site_exception_displayed, 'The previously accessed website is displayed' \
+                                                 ' as exception inside the panel.'
 
         click(remove_website_button_pattern)
         click(AboutPreferences.Privacy.Exceptions.SAVE_CHANGES_BUTTON)
@@ -126,20 +138,24 @@ class Test(FirefoxTest):
         assert itstrap_site_exception_displayed is False, 'The website is successfully removed from the panel.'
 
         navigate('https://itisatrap.org/firefox/its-a-tracker.html')
+
+        if OSHelper.is_windows():  # to prevent pop-up covering pattern
+            type(Key.ESC)
+
         firefox_tracker_site_logo_displayed = exists(firefox_tracker_site_content_pattern)
-        assert firefox_tracker_site_logo_displayed is True, 'The website is successfully displayed.'
+        assert firefox_tracker_site_logo_displayed, 'The website is successfully displayed.'
 
         tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern)
-        assert tracking_protection_shield_displayed is True, 'The Tracking protection shield' \
-                                                             ' is displayed near the address bar.'
+        assert tracking_protection_shield_displayed, 'The Tracking protection shield' \
+                                                     ' is displayed near the address bar.'
 
         third_party_tracker_correctly_blocked_displayed = exists(third_party_tracker_correctly_blocked_text)
-        assert third_party_tracker_correctly_blocked_displayed is True, 'Simulated third-party tracker' \
-                                                                        ' was correctly blocked is displayed.'
+        assert third_party_tracker_correctly_blocked_displayed, 'Simulated third-party tracker' \
+                                                                ' was correctly blocked is displayed.'
 
         first_party_tracker_correctly_blocked_displayed = exists(first_party_tracker_correctly_blocked_text)
-        assert first_party_tracker_correctly_blocked_displayed is True, 'Simulated first-party tracker' \
-                                                                        ' was correctly loaded is displayed.'
+        assert first_party_tracker_correctly_blocked_displayed, 'Simulated first-party tracker' \
+                                                                ' was correctly loaded is displayed.'
 
         dnt_signal_correctly_sent_displayed = exists(dnt_signal_correctly_sent_text)
-        assert dnt_signal_correctly_sent_displayed is True, 'The DNT signal was correctly sent is displayed.'
+        assert dnt_signal_correctly_sent_displayed, 'The DNT signal was correctly sent is displayed.'
