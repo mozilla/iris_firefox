e3628aa58497c04d084c44d0430b207e7622ca7f
Serhii Pronoza <spronoza@mozilla.com>
2019-09-17 06:09:44 -0700

    add_button.png updated for win

--- tests/firefox/anti_tracking/saved_cc_available_in_private_session.py
+++ tests/firefox/anti_tracking/saved_cc_available_in_private_session.py
@@ -48,29 +48,29 @@ class Test(FirefoxTest):
 
         navigate('about:preferences#privacy')
 
-        search_field_exists = exists(find_in_preferences_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        search_field_exists = exists(find_in_preferences_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert search_field_exists, 'Preferences page is opened'
 
         paste('Autofill')
 
-        saved_addresses_button_exists = exists(saved_addresses_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        saved_addresses_button_exists = exists(saved_addresses_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert saved_addresses_button_exists, '\'Saved addresses\' button is displayed on the Preferences page'
 
         click(saved_addresses_button_pattern)
 
-        add_button_exists = exists(add_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        add_button_exists = exists(add_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert add_button_exists, '\'Add\' button is displayed on the \'Saved addresses\' popup'
 
         click(add_button_pattern)
 
-        address_first_name_field_exists = exists(address_first_name_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        address_first_name_field_exists = exists(address_first_name_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert address_first_name_field_exists, '\'First Name\' field is displayed on the \'Add new address\' popup'
 
         click(address_first_name_field_pattern)
 
         paste('Maria')
 
-        save_button_exists = exists(save_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        save_button_exists = exists(save_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert save_button_exists, '\'Save\' button is displayed on the \'Add new address\' popup'
 
         click(save_button_pattern)
@@ -84,12 +84,12 @@ class Test(FirefoxTest):
         except FindError:
             raise FindError('Entered data did not vanish after clicking the \'Submit button\'')
 
-        saved_credit_cards_button_exists = exists(saved_credit_cards_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        saved_credit_cards_button_exists = exists(saved_credit_cards_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert saved_credit_cards_button_exists, '\'Saved credit cards\' button is displayed on the Preferences page'
 
         click(saved_credit_cards_button_pattern)
 
-        add_button_exists = exists(add_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        add_button_exists = exists(add_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert add_button_exists, '\'Add\' button is displayed on the \'Saved credit cards\' popup'
 
         click(add_button_pattern)
@@ -99,64 +99,64 @@ class Test(FirefoxTest):
 
         click(card_type_dropdown_pattern.similar(0.6))
 
-        card_type_dropdown_item_exists = exists(card_type_dropdown_item_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        card_type_dropdown_item_exists = exists(card_type_dropdown_item_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert card_type_dropdown_item_exists, '\'Visa\' option is available on the \'Card type\' dropdown'
 
         click(card_type_dropdown_item_pattern)
 
-        popup_card_number_field_exists = exists(popup_card_number_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        popup_card_number_field_exists = exists(popup_card_number_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert popup_card_number_field_exists, '\'Card Number\' field is displayed on the \'Edit Credit Card\' popup'
 
         click(popup_card_number_field_pattern)
 
         paste('4443695376356261')
 
-        popup_name_on_card_field_exists = exists(popup_name_on_card_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        popup_name_on_card_field_exists = exists(popup_name_on_card_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert popup_name_on_card_field_exists, '\'Name on card\' field is displayed on the \'Edit Credit Card\' popup'
 
         click(popup_name_on_card_field_pattern)
 
         paste('Maria')
 
-        expiration_month_dropdown_exists = exists(expiration_month_dropdown_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        expiration_month_dropdown_exists = exists(expiration_month_dropdown_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert expiration_month_dropdown_exists, '\'Expiration month\' dropdown is displayed on the ' \
                                                  '\'Edit Credit Card\' popup'
 
         click(expiration_month_dropdown_pattern)
 
         expiration_month_dropdown_item_exists = exists(expiration_month_dropdown_item_pattern,
-                                                       FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+                                                       FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert expiration_month_dropdown_item_exists, '\'05 - May\' option is displayed on the dropdown ' \
                                                       '\'Expiration month\''
 
         click(expiration_month_dropdown_item_pattern)
 
-        expiration_year_dropdown_exists = exists(expiration_year_dropdown_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        expiration_year_dropdown_exists = exists(expiration_year_dropdown_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert expiration_year_dropdown_exists, '\'Expiration year\' dropdown is displayed on the ' \
                                                 '\'Edit Credit Card\' popup'
 
         click(expiration_year_dropdown_pattern)
 
         expiration_year_dropdown_item_exists = exists(expiration_year_dropdown_item_pattern,
-                                                      FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+                                                      FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert expiration_year_dropdown_item_exists, '\'2029\' option is available on the \'Expiration year\' dropdown'
 
         click(expiration_year_dropdown_item_pattern)
 
-        billing_address_dropdown_exists = exists(billing_address_dropdown_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        billing_address_dropdown_exists = exists(billing_address_dropdown_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert billing_address_dropdown_exists, '\'Billing address\' dropdown is displayed on the ' \
                                                 '\'Edit Credit Card\' popup'
 
         click(billing_address_dropdown_pattern)
 
         billing_address_dropdown_item_exists = exists(billing_address_dropdown_item_pattern,
-                                                      FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+                                                      FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert billing_address_dropdown_item_exists, 'Previously saved address displayed on the \'Billing address\' ' \
                                                      'dropdown'
 
         click(billing_address_dropdown_item_pattern)
 
-        save_button_exists = exists(save_button_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        save_button_exists = exists(save_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert save_button_exists, '\'Save\' button is displayed on the \'Edit Credit Card\' popup'
 
         click(save_button_pattern)
@@ -169,12 +169,12 @@ class Test(FirefoxTest):
         page_opened = exists(submit_button_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT)
         assert page_opened, 'Test page is opened.'
 
-        card_number_field_exists = exists(card_number_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        card_number_field_exists = exists(card_number_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert card_number_field_exists, '\'Card number\' field is displayed on the page'
 
         click(card_number_field_pattern)
 
-        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT+1)
+        time.sleep(FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
 
         click(card_number_field_pattern)
 
@@ -192,12 +192,12 @@ class Test(FirefoxTest):
         page_opened_in_private_window = exists(submit_button_pattern,  FirefoxSettings.SITE_LOAD_TIMEOUT)
         assert page_opened_in_private_window, 'Test page is opened in a new Private window'
 
-        card_number_field_exists = exists(card_number_field_pattern, FirefoxSettings.TINY_FIREFOX_TIMEOUT)
+        card_number_field_exists = exists(card_number_field_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
         assert card_number_field_exists, '\'Card Number\' field is displayed on the page'
 
         click(card_number_field_pattern)
 
-        time.sleep(FirefoxSettings.TINY_FIREFOX_TIMEOUT+1)
+        time.sleep(FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
 
         click(card_number_field_pattern)
 
