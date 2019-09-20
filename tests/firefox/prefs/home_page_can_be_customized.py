8d792de861cca590d1487b424f24a8375232dec0
spronoza <spronoza@mozilla.com>
2019-09-20 13:46:00 +0300

    Srting arguments in asserts changed on pattern

--- tests/firefox/prefs/home_page_can_be_customized.py
+++ tests/firefox/prefs/home_page_can_be_customized.py
@@ -17,39 +17,29 @@ class Test(FirefoxTest):
     )
     def run(self, firefox):
         homepage_preferences_pattern = Pattern('homepage_preferences.png')
+        default_setting_home_pattern = Pattern('default_new_tab_setting_home.png')
+        custom_url_option_pattern = Pattern('custom_url_option.png')
+        url_field_pattern = Pattern('url_field.png')
 
         navigate('about:preferences#home')
 
         preferences_page_opened = exists(homepage_preferences_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert preferences_page_opened, 'The about:preferences page is successfully loaded.'
 
-        homepage_preferences_location = find(homepage_preferences_pattern)
-        homepage_section_width, homepage_preferences_height = homepage_preferences_pattern.get_size()
-        homepage_section_region = Region(homepage_preferences_location.x, homepage_preferences_location.y,
-                                         homepage_section_width * 3, homepage_preferences_height)
-
-        home_option_displayed = exists('Firefox Home', FirefoxSettings.FIREFOX_TIMEOUT, homepage_section_region)
+        home_option_displayed = exists(default_setting_home_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert home_option_displayed, 'The options for "Home" section are displayed.'
 
-        click('Firefox Home')
+        click(default_setting_home_pattern)
 
-        time.sleep(Settings.DEFAULT_UI_DELAY_LONG)
-
-        homepage_section_region = Region(homepage_preferences_location.x + homepage_section_width,
-                                         homepage_preferences_location.y, homepage_section_width * 2,
-                                         homepage_preferences_height * 4)
-
-        custom_option_displayed = exists('Custom', FirefoxSettings.FIREFOX_TIMEOUT, homepage_section_region)
+        custom_option_displayed = exists(custom_url_option_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert custom_option_displayed, 'The \'Custom\' option for "Home" section is displayed.'
 
-        click('Custom')
+        click(custom_url_option_pattern)
 
-        time.sleep(Settings.DEFAULT_UI_DELAY_LONG)
-
-        custom_option_displayed = exists('Paste', FirefoxSettings.FIREFOX_TIMEOUT, homepage_section_region)
+        custom_option_displayed = exists(url_field_pattern, FirefoxSettings.FIREFOX_TIMEOUT)
         assert custom_option_displayed, 'The \'Paste a URL...\' field is displayed.'
 
-        click('Paste')
+        click(url_field_pattern)
 
         time.sleep(Settings.DEFAULT_UI_DELAY)
 
