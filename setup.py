756f03d6147eb8fa02294a159637b7305030ae80
Matt Wobensmith <mwobensmith@mozilla.com>
2019-09-23 16:14:09 -0700

    Remove core code, import as package instead

--- setup.py
+++ setup.py
@@ -49,9 +49,5 @@ setup(
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRE,
     extras_require={'dev': DEV_REQUIRES},  # For `pip install -e .[dev]`
-    entry_points={
-        'console_scripts': [
-            'iris = src.__main__:main'
-        ]
-    }
+    entry_points={}
 )
--- targets/firefox/firefox_ui/test_utils.py
+++ targets/firefox/firefox_ui/test_utils.py
@@ -3,10 +3,10 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 import logging
 
-from src.core.api.errors import FindError, APIHelperError
-from src.core.api.finder.finder import wait, exists
-from src.core.api.finder.pattern import Pattern
-from src.core.api.mouse.mouse import click
+from moziris.api.errors import FindError, APIHelperError
+from moziris.api.finder.finder import wait, exists
+from moziris.api.finder.pattern import Pattern
+from moziris.api.mouse.mouse import click
 from targets.firefox.firefox_ui.nav_bar import NavBar
 
 logger = logging.getLogger(__name__)
