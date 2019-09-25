d175f7768085c435eb7614300218eebe59b6bf5b
Matt Wobensmith <mwobensmith@mozilla.com>
2019-09-25 09:01:31 -0700

    Testing easier deployment for package work

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from moziris.api.settings import Settings
+from moziris.scripts.main import main
+
+
+def test():
+    root = os.path.split(__file__)[0]
+    sys.path.append(root)
+    Settings.set_code_root_from_caller()
+    main()
