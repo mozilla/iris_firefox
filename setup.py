0aacf5a6a22518b69581228ef3493b58521a6911
Matt Wobensmith <mwobensmith@mozilla.com>
2019-09-25 19:30:41 -0700

    Adding new command to invoke project to simplify startup

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     extras_require={'dev': DEV_REQUIRES},  # For `pip install -e .[dev]`
     entry_points={
         "console_scripts": [
-            "iris1 = main:test"
+            "moziris = main:test"
         ]
     }
 )
