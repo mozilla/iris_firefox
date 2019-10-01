992c5065cc35cebacf3349e9c8e07bacbbb90148
Matt Wobensmith <mwobensmith@mozilla.com>
2019-10-01 09:53:06 -0700

    Copy download files to nightly target

--- bootstrap/linux_bootstrap.sh
+++ bootstrap/linux_bootstrap.sh
@@ -237,7 +237,9 @@ else
 fi
 
 # Create download files
+echo -e "\n${GREEN}  --->  Create download files ${NC}\n"
 mkdir -p targets/firefox/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
+mkdir -p targets/nightly/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
 cd targets/firefox/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
 
 dd if=/dev/zero of=1GB.zip bs=1024 count=1024000
@@ -248,5 +250,6 @@ dd if=/dev/zero of=50MB.zip bs=1024 count=51200
 dd if=/dev/zero of=20MB.zip bs=1024 count=20500
 dd if=/dev/zero of=10MB.zip bs=1024 count=10200
 dd if=/dev/zero of=5MB.zip bs=1024 count=5100
+cp -R * ../../../../../nightly/local_web/thinkbroadband/thinkbroadband_files/downloaded_archives
 
-cd ../../../../../
+cd ../../../../../../
