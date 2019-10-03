cb053c8d97add3e270ac1f2593ea7124db005c0f
pavelciapa <pciapa@mozilla.com>
2019-10-03 13:34:24 +0300

    Updated script for different website

--- tests/firefox/content_scroll/image_rich_scrolling.py
+++ tests/firefox/content_scroll/image_rich_scrolling.py
@@ -13,39 +13,28 @@ class Test(FirefoxTest):
         test_case_id="C4661",
         test_suite_id="102",
         locale=["en-US"],
-        blocked_by = {'id': 'issue_3761', 'platform': OSPlatform.ALL}
+        # blocked_by = {'id': 'issue_3761', 'platform': OSPlatform.ALL}
     )
     def run(self, firefox):
-        show_more_results_button_pattern = Pattern('show_more_results_button.png')
-        google_images_page_mark_pattern = Pattern('google_images_page_mark.png')
-        google_save_button_pattern = Pattern('google_save_button.png')
+        image_site_loaded_pattern = Pattern('image_site_loaded.png')
+        image_site_next_page_pattern = Pattern('image_site_next_page.png')
 
         mouse_wheel_steps = 200
         if OSHelper.is_windows():
             mouse_wheel_steps = 1600
 
-        navigate('https://www.google.com/preferences?hl=en#languages')
+        navigate('https://pixabay.com/images/search/pages/')
 
-        google_save_button_exists = exists(google_save_button_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT)
-        assert google_save_button_exists is True, 'Google language preferences page is opened'
-
-        click(google_save_button_pattern)
-
-        navigate('http://images.google.com/?gws_rd=ssl')
-
-        google_images_page_opened = exists(google_images_page_mark_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT)
-        assert google_images_page_opened is True, 'Google images site is properly loaded'
-
-        paste('cute kittens:3')
-        type(Key.ENTER)
+        image_site_loaded = exists(image_site_loaded_pattern, FirefoxSettings.SITE_LOAD_TIMEOUT)
+        assert image_site_loaded is True, 'Pixabay images site is properly loaded'
 
         # Scroll by mouse wheel
-        show_more_results_button_destinated = scroll_until_pattern_found(show_more_results_button_pattern,
-                                                                         Mouse().scroll, (0, -mouse_wheel_steps,), 100,
-                                                                         FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
-        assert show_more_results_button_destinated is True, 'Successfully scrolled to footer by mouse scroll'
+        bottom_page_button_destinated = scroll_until_pattern_found(image_site_next_page_pattern,
+                                                                   Mouse().scroll, (0, -mouse_wheel_steps,), 100,
+                                                                   FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
+        assert bottom_page_button_destinated is True, 'Successfully scrolled to footer by mouse scroll'
 
-        google_images_page_mark_destinated = scroll_until_pattern_found(google_images_page_mark_pattern,
-                                                                        Mouse().scroll, (0, mouse_wheel_steps,), 100,
-                                                                        FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
-        assert google_images_page_mark_destinated is True, 'Successfully scrolled from footer to header by mouse scroll'
+        top_page_button_destibated = scroll_until_pattern_found(image_site_loaded_pattern,
+                                                                Mouse().scroll, (0, mouse_wheel_steps,), 100,
+                                                                FirefoxSettings.TINY_FIREFOX_TIMEOUT/3)
+        assert top_page_button_destibated is True, 'Successfully scrolled from footer to header by mouse scroll'
