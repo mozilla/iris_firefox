43388b9441cacad926b4788aaa9646a18ddbca9b
pavelciapa <pciapa@mozilla.com>
2019-10-04 17:50:32 +0300

    Updated local site to exteral

--- tests/firefox/download_manager/change_download_folder.py
+++ tests/firefox/download_manager/change_download_folder.py
@@ -21,7 +21,7 @@ class Test(FirefoxTest):
                      'browser.warnOnQuit': False}
     )
     def run(self, firefox):
-        navigate(LocalWeb.THINKBROADBAND_TEST_SITE)
+        navigate('https://irisfirefoxtestfiles.netlify.com')
 
         # Wait for the page to be loaded.
         try:
@@ -79,7 +79,7 @@ class Test(FirefoxTest):
 
         time.sleep(10)
 
-        navigate(LocalWeb.THINKBROADBAND_TEST_SITE)
+        navigate('https://irisfirefoxtestfiles.netlify.com')
 
         # Wait for the page to be loaded.
         try:
