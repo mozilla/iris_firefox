972ca34af5cfe898ed780c781404561c648661e5
spronoza <spronoza@mozilla.com>
2019-10-22 15:47:03 +0300

    change_preferences() was updated

--- targets/firefox/firefox_ui/helpers/general.py
+++ targets/firefox/firefox_ui/helpers/general.py
@@ -92,7 +92,7 @@ def change_preference(pref_name, value):
         time.sleep(Settings.DEFAULT_UI_DELAY)
 
         try:
-            retrieved_value = copy_to_clipboard()
+            retrieved_value = copy_to_clipboard().split("\t")[1]
         except Exception:
             raise APIHelperError("Failed to retrieve preference value.")
 
@@ -101,13 +101,12 @@ def change_preference(pref_name, value):
             return None
         else:
             type(Key.ENTER)
-            dialog_box_pattern = Pattern("preference_dialog_icon.png")
-            try:
-                wait(dialog_box_pattern, 3)
-                paste(value)
-                type(Key.ENTER)
-            except FindError:
-                pass
+            if not value == "true" or value == "false":
+                try:
+                    paste(value)
+                    type(Key.ENTER)
+                except FindError:
+                    pass
 
         close_tab()
     except Exception:
