62d932928e8f5683286a2ee6b495fac4ffb5fb1d
tracywalker <twalker@mozilla.com>
2019-12-02 15:33:58 -0600

    Add and use pattern for selector button

--- tests/nightly/history/clear_all_history.py
+++ tests/nightly/history/clear_all_history.py
@@ -13,7 +13,7 @@ class Test(FirefoxTest):
     def run(self, firefox):
         searched_history_logo_pattern = Sidebar.HistorySidebar.EXPLORED_HISTORY_ICON.similar(0.9)
         if OSHelper.is_mac():
-            clear_recent_history_last_hour_pattern = History.ClearRecentHistory.TimeRange.CLEAR_CHOICE_LAST_HOUR
+            selector_button_pattern = Pattern("sanitize_dialog_selector_button.png").similar(0.6)
 
         # Open some pages to create some history.
         new_tab()
@@ -36,7 +36,9 @@ class Test(FirefoxTest):
         for step in open_clear_recent_history_window():
             assert step.resolution, step.message
         if OSHelper.is_mac():
-            click(clear_recent_history_last_hour_pattern)
+            expected_3 = exists(selector_button_pattern, FirefoxSettings.SHORT_FIREFOX_TIMEOUT)
+            assert expected_3, "Sanitize dialog duration selector button found."
+            click(selector_button_pattern)
             for i in range(4):
                 type(Key.DOWN)
                 time.sleep(Settings.DEFAULT_UI_DELAY)
