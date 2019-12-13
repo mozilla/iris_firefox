9982233273928c8cc51e3c93d07b8b459caf38b4
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-12-13 11:43:33 +0200

    Test was disabled (#4396)

--- tests/firefox/private_browsing/track_protection_turn_off_for_site.py
+++ tests/firefox/private_browsing/track_protection_turn_off_for_site.py
@@ -12,6 +12,7 @@ class Test(FirefoxTest):
         test_case_id="107431",
         test_suite_id="1826",
         locales=["en-US"],
+        blocked_by={"id": "4387", "platform": OSPlatform.ALL}
     )
     def run(self, firefox):
         tracking_protection_shield_pattern = Pattern("tracking_protection_shield_activated.png")
