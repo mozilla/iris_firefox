742247fceefbd1486496c35a11116b35a3a4b968
Serhii Pronoza <46671906+spronoza@users.noreply.github.com>
2019-12-18 12:19:55 +0200

    blocked_by={"id": "4420", "platform": OSPlatform.ALL} (#4423)

--- tests/firefox/prefs/popup_windows_can_be_enabled.py
+++ tests/firefox/prefs/popup_windows_can_be_enabled.py
@@ -12,6 +12,7 @@ class Test(FirefoxTest):
         test_case_id="159259",
         test_suite_id="2241",
         locale=["en-US"],
+        blocked_by={"id": "4420", "platform": OSPlatform.ALL}
     )
     def run(self, firefox):
         browser_privacy_hover_pattern = Pattern("browser_privacy_hover.png")
