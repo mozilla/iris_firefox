dd512dac8cf2f5c8ba29d2641e6d4cd9addcb630
Ranjit Kumar <54833244+ranjit-exalt@users.noreply.github.com>
2019-12-24 19:52:27 +0530

    Issue 4338: Issue fixes for themes test suite (#4425)
    * check_for_dark_and_light_themes:
    1. Images replaced for all four OS as per new firefox UI
    2. Rearranged pytest details.
    3. Corrected assert error messages.
    4. Replaced hardcoded wait value with FIREFOX_TIMEOUT
    5. Added view port condition handling for mac.
    6. Blocked for linux OS due to https://github.com/mozilla/iris_firefox/issues/4422
    
    * check_the_browser_appearance_with_default_themes:
    1. Images replaced for all four OS as per new firefox UI
    2. Corrected assert error messages.
    3. Replaced hardcoded wait value with FIREFOX_TIMEOUT
    4. click_window_control("close") not working for windows 7, fixed it with ALT+F4 condition.
    5. Removed undesired conditions from script.
    
    * no_glitches_in_tab_layout.py:
    1. Rearranged pytest details.
    2. Corrected assert error messages.
    3. Replaced hardcoded wait value with appropriate variable wait.
    4. Added view port condition handling for mac.
    5. Added DEFAULT_UI_DELAY, for click to work at line 52.
    
    * check_for_dark_and_light_themes (Commit 2):
    1. Replaced open_addons() with open_hamburger_menu('Add-ons') for https://github.com/mozilla/iris_firefox/issues/4422
    2. In general.py, Added 'Add-ons' in option-list and implemented else if logic for the same.
    3. Removed blocked_by={"id": "4422", "platform": OSPlatform.LINUX} from line 13.
    
    * check_for_dark_and_light_themes (Commit 3):
    1. For Linux private window, even clicking Add-ons doesn't work, so fixed that with navigate("about:addons")
    2. Replaced Add-ons image for Linux

--- tests/firefox/themes/check_the_browser_appearance_with_default_themes.py
+++ tests/firefox/themes/check_the_browser_appearance_with_default_themes.py
@@ -16,115 +16,85 @@ class Test(FirefoxTest):
     def run(self, firefox):
         open_addons()
 
-        expected = exists(AboutAddons.THEMES, 10)
+        expected = exists(AboutAddons.THEMES, FirefoxSettings.FIREFOX_TIMEOUT)
         assert expected, "Add-ons page successfully loaded."
 
         click(AboutAddons.THEMES)
 
-        expected = exists(AboutAddons.Themes.DARK_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Dark theme option found in the page."
-
-        expected = exists(AboutAddons.Themes.LIGHT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Light theme option found in the page."
-
         expected = exists(AboutAddons.Themes.DEFAULT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Default theme option found in the page."
+        assert expected, "Default theme option not found in the page."
+
+        expected = exists(AboutAddons.Themes.DARK_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "Dark theme option not found in the page."
+
+        if OSHelper.is_mac():
+            click(Pattern("disabled_theme_header.png"))
+            type(Key.DOWN)
+        expected = exists(AboutAddons.Themes.LIGHT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "Light theme option not found in the page."
 
         # Using the DEFAULT theme check that options from menu bar work correctly.
         click(AboutAddons.Themes.DEFAULT_THEME)
         time.sleep(Settings.DEFAULT_UI_DELAY)
 
-        action_can_be_performed = exists(AboutAddons.Themes.ACTION_BUTTON)
-        assert action_can_be_performed is False, "Theme can be enabled/disabled."
-
-        previous_tab()
-
-        expected = exists(AboutAddons.Themes.IRIS_TAB_LIGHT_OR_DEFAULT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Tab information is correctly displayed for DEFAULT theme."
-
         open_library()
 
         expected = exists(Library.BOOKMARKS_MENU, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Expand bookmarks menu button displayed properly."
+        assert expected, "Expand bookmarks menu button  doesn't displayed properly."
 
-        click_window_control("close")
+        if OSHelper.is_windows():
+            type(text=Key.F4, modifier=KeyModifier.ALT)
+        else:
+            click_window_control("close")
 
         # Enable the LIGHT theme and check that options from menu bar work correctly using the selected theme.
-        next_tab()
-        expected = exists(AboutAddons.THEMES, 10)
-        assert expected, "Add-ons page is in focus."
-
         navigate_back()
 
-        expected = exists(AboutAddons.Themes.LIGHT_THEME, 10)
-        assert expected, "Light theme option found in the page."
+        expected = exists(AboutAddons.Themes.LIGHT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "Light theme option  doesn't found in the page."
 
         click(AboutAddons.Themes.LIGHT_THEME)
 
-        action_can_be_performed = exists(AboutAddons.Themes.ACTION_BUTTON)
-        assert action_can_be_performed, "Theme can be enabled."
-        click(AboutAddons.Themes.ACTION_BUTTON)
-
         expected = exists(AboutAddons.Themes.ENABLE_BUTTON, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "ENABLE button found in the page."
+        assert expected, "ENABLE button  doesn't found in the page."
 
         click(AboutAddons.Themes.ENABLE_BUTTON)
 
-        action_can_be_performed = exists(AboutAddons.Themes.ACTION_BUTTON)
-        assert action_can_be_performed, "Theme can be disabled."
-        click(AboutAddons.Themes.ACTION_BUTTON)
-
         expected = exists(AboutAddons.Themes.DISABLE_BUTTON, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "DISABLE button found in the page."
-
-        previous_tab()
-
-        expected = exists(AboutAddons.Themes.IRIS_TAB_LIGHT_OR_DEFAULT_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "LIGHT theme successfully applied."
+        assert expected, "DISABLE button  doesn't found in the page."
 
         open_library()
 
         expected = exists(Library.BOOKMARKS_MENU, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "Expand bookmarks menu button displayed properly."
+        assert expected, "Expand bookmarks menu button  doesn't displayed properly."
 
-        click_window_control("close")
+        if OSHelper.is_windows():
+            type(text=Key.F4, modifier=KeyModifier.ALT)
+        else:
+            click_window_control("close")
 
         # Enable the DARK theme and check that options from menu bar work correctly using the selected theme.
-        next_tab()
-        expected = exists(AboutAddons.THEMES, 10)
-        assert expected, "Add-ons page is in focus."
-
         navigate_back()
 
-        expected = exists(AboutAddons.Themes.DARK_THEME, 10)
-        assert expected, "Dark theme option found in the page."
+        expected = exists(AboutAddons.Themes.DARK_THEME, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "Dark theme option  doesn't found in the page."
 
         click(AboutAddons.Themes.DARK_THEME)
 
-        action_can_be_performed = exists(AboutAddons.Themes.ACTION_BUTTON)
-        assert action_can_be_performed, "Theme can be enabled."
-        click(AboutAddons.Themes.ACTION_BUTTON)
-
         expected = exists(AboutAddons.Themes.ENABLE_BUTTON, FirefoxSettings.FIREFOX_TIMEOUT)
-        assert expected, "ENABLE button found in the page."
+        assert expected, "ENABLE button  doesn't found in the page."
 
         click(AboutAddons.Themes.ENABLE_BUTTON)
 
-        action_can_be_performed = exists(AboutAddons.Themes.ACTION_BUTTON)
-        assert action_can_be_performed, "Theme can be disabled."
-        click(AboutAddons.Themes.ACTION_BUTTON)
-
-        expected = exists(AboutAddons.Themes.DISABLE_BUTTON, 10)
-        assert expected, "DISABLE button found in the page."
-
-        previous_tab()
-
-        expected = exists(AboutAddons.Themes.IRIS_TAB_DARK_THEME, 10)
-        assert expected, "DARK theme successfully applied."
+        expected = exists(AboutAddons.Themes.DISABLE_BUTTON, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "DISABLE button  doesn't found in the page."
 
         open_library()
 
-        expected = exists(Library.BOOKMARKS_MENU, 10)
-        assert expected, "Expand bookmarks menu button displayed properly."
+        expected = exists(Library.BOOKMARKS_MENU, FirefoxSettings.FIREFOX_TIMEOUT)
+        assert expected, "Expand bookmarks menu button doesn't displayed properly."
 
-        click_window_control("close")
+        if OSHelper.is_windows():
+            type(text=Key.F4, modifier=KeyModifier.ALT)
+        else:
+            click_window_control("close")
